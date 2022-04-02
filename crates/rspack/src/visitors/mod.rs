64ae3815b9c9caf9852f9d016b35c212272e061f
Yunfei He <i.heyunfei@gmail.com>
2022-04-03 04:38:26 +0800

    wip: Add constant folder

--- crates/rspack/src/visitors/mod.rs
+++ crates/rspack/src/visitors/mod.rs
@@ -3,5 +3,7 @@ use std::collections::HashSet;
 use swc_atoms::JsWord;
 
 mod dependency_scanner;
+mod constant_folder;
 
 pub use dependency_scanner::DependencyScanner;
+pub use constant_folder::ConstantFolder;
