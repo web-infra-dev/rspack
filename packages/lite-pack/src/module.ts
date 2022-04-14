28f49cf710c61d7a903f74bae86a6f8be0f37c97
hardfist <yangjianzju@gmail.com>
2022-04-14 18:23:40 +0800

    chore: support dynamic import

--- packages/lite-pack/src/module.ts
+++ packages/lite-pack/src/module.ts
@@ -2,35 +2,36 @@ import fs from 'fs';
 import * as babel from '@babel/core';
 import { Resolver } from './resolver';
 import { Loader } from './loader';
-import { AST, Parser } from './parser';
+import { AST, ImportType, Parser } from './parser';
 import { Compiler } from './compiler';
 import path from 'path';
 import { Chunk } from './chunk';
 import * as esbuild from 'esbuild';
 export type NormalModuleOptions = {
   path: string;
-  resolveDir:string;
+  resolveDir: string;
   importer: string;
   compiler: Compiler;
   isEntry: boolean;
-  entryKey?:string;
-}
+  entryKey?: string;
+};
 export class ModuleNode {
   isEntry: boolean;
-  entryKey?:string;
-  contents!:string;
-  path:string;
-  importer:string;
-  resolveDir:string; // we need resolveDir to handle virtual Module resolve
+  entryKey?: string;
+  contents!: string;
+  path: string;
+  importer: string;
+  resolveDir: string; // we need resolveDir to handle virtual Module resolve
   fullPath!: string;
   ast!: AST;
   #resolver!: Resolver;
   #loader!: Loader;
   #parser!: Parser;
-  #compiler!: Compiler
+  #compiler!: Compiler;
   chunks: Set<Chunk> = new Set();
-  depMap: Map<string,string> = new Map();
-  constructor(options:NormalModuleOptions){
+  depMap: Map<string, string> = new Map();
+  imports: ImportType[] = [];
+  constructor(options: NormalModuleOptions) {
     this.path = options.path;
     this.importer = options.importer;
     this.resolveDir = options.resolveDir;
@@ -41,86 +42,97 @@ export class ModuleNode {
     this.isEntry = options.isEntry;
     this.entryKey = options.entryKey;
   }
-  static create(options:NormalModuleOptions){
+  static create(options: NormalModuleOptions) {
     return new ModuleNode(options);
   }
-  build(){
+  build() {
     this._doBuild();
   }
-  rebuild(){
+  rebuild() {
     this._doBuild();
   }
-  _doBuild(){
+  _doBuild() {
     const fullPath = this.#resolver.resolveRequest(this.path, this.resolveDir);
     const contents = this.#loader.load_and_transform(fullPath);
-    const ast = this.#parser.parse(contents);
+    const { imports, ast } = this.#parser.parse(contents);
+    this.imports = imports;
     this.contents = contents;
     this.ast = ast;
     this.fullPath = fullPath;
-    this.#compiler.moduleGraph.addNode(fullPath,this);
+    this.#compiler.moduleGraph.addNode(fullPath, this);
     const importerModule = this.#compiler.moduleGraph.getNodeById(this.importer);
     importerModule?.depMap.set(this.path, fullPath);
     this.#compiler.moduleGraph.addEdge(this.importer, fullPath);
     this.buildDeps();
   }
-  buildDeps(){
-    this.ast?.program.body.forEach(node => {
-      if(node.type === 'ImportDeclaration'){
-        const moduleId = node.source.value;
-        const newModule = ModuleNode.create({
-          importer: this.fullPath,
-          resolveDir: path.dirname(this.fullPath),
-          path: moduleId,
-          compiler: this.#compiler,
-          isEntry:false
-        })
-        this.#compiler.addModule(newModule)
-      }
-    })
+  buildDeps() {
+    console.log('imports:', this.imports);
+    for (const record of this.imports) {
+      const moduleId = record.id;
+      const newModule = ModuleNode.create({
+        importer: this.fullPath,
+        resolveDir: path.dirname(this.fullPath),
+        path: moduleId,
+        compiler: this.#compiler,
+        isEntry: false,
+      });
+      this.#compiler.addModule(newModule);
+    }
   }
-  generator(){
-    const {types: t} = babel;
+  generator() {
+    const { types: t } = babel;
     const self = this;
     const code = babel.transformSync(this.contents, {
-      plugins:[require('@babel/plugin-transform-modules-commonjs'), function(){
-                return {
+      plugins: [
+        function () {
+          return {
             visitor: {
-              ImportDeclaration(path) {
-                const newIdentifier =
-                    path.scope.generateUidIdentifier('imported');
-
-                for (const specifier of path.get('specifiers')) {
-                  const binding =
-                      specifier.scope.getBinding(specifier.node.local.name);
-                  const importedKey = specifier.isImportDefaultSpecifier() ?
-                      'default' :
-                      specifier.get('imported.name').node;
-
-                  for (const referencePath of binding.referencePaths) {
-                    referencePath.replaceWith(t.memberExpression(
-                        newIdentifier, t.stringLiteral(importedKey), true));
+              CallExpression:({node}) => {
+                if(node.callee.type === 'Import'){
+                  const argument = node.arguments[0];
+                  if(argument.type === 'StringLiteral'){
+                    const id = argument.value;
+                    const replaceId = self.depMap.get(id);
+                    console.log('xxxx:', replaceId);
+                    node.arguments[0] = t.stringLiteral(replaceId!);
                   }
                 }
-                const importPath:string = path.get('source.value').node;
-                const importerPath = self.depMap.get(importPath);
-                path.replaceWith(t.variableDeclaration('const', [
-                  t.variableDeclarator(
-                      newIdentifier,
-                      t.callExpression(
-                          t.identifier('require'),
-                          [
-                            t.stringLiteral(importerPath),
-                          ])),
-                ]));
-              }
-            },
-          } as babel.PluginObj
-      }]
-    })!.code!;
+              },
+              ImportDeclaration(path) {
+                const newIdentifier = path.scope.generateUidIdentifier('imported');
 
-    return `rs.define(${JSON.stringify(this.fullPath)},function test(require,exports,module){${code}});`
+                for (const specifier of path.get('specifiers')) {
+                  const binding = specifier.scope.getBinding(specifier.node.local.name);
+                  const importedKey = specifier.isImportDefaultSpecifier()
+                    ? 'default'
+                    : specifier.get('imported.name').node;
+
+                  for (const referencePath of binding.referencePaths) {
+                    referencePath.replaceWith(t.memberExpression(newIdentifier, t.stringLiteral(importedKey), true));
+                  }
+                }
+                const importPath: string = path.get('source.value').node;
+                const importerPath = self.depMap.get(importPath);
+                path.replaceWith(
+                  t.variableDeclaration('const', [
+                    t.variableDeclarator(
+                      newIdentifier,
+                      t.callExpression(t.identifier('require'), [t.stringLiteral(importerPath)])
+                    ),
+                  ])
+                );
+              },
+            },
+          } as babel.PluginObj;
+        },
+        require('@babel/plugin-transform-modules-commonjs'),
+        require('@babel/plugin-proposal-dynamic-import'),
+      ],
+    })!.code!;
+    console.log('xxx:', code);
+    return `rs.define(${JSON.stringify(this.fullPath)},function test(require,exports,module){${code}});`;
   }
-  addChunk(chunk:Chunk){
+  addChunk(chunk: Chunk) {
     this.chunks.add(chunk);
   }
 }
--- packages/lite-pack/src/parser.ts
+++ packages/lite-pack/src/parser.ts
@@ -1,22 +1,37 @@
 import * as babel from '@babel/core';
+import traverse from '@babel/traverse'
+export type ImportType = {
+  kind: 'require' | 'import' | 'dynamic-import',
+  id: string;
+}
 export class Parser {
   constructor(){
 
   }
   parse(content:string){
-    const ast = babel.parseSync(content, {
-      plugins: [{
-        visitor: {
-          Identifier(path){
-            console.log('path:',path);
-          },
-          ImportDeclaration(node){
-            console.log('node:',node);
+    const ast = babel.parseSync(content)!;
+    const imports: ImportType[] = []
+    traverse(ast, {
+      CallExpression:({node}) => {
+        if(node.callee.type === 'Import'){
+          const argument = node.arguments[0];
+          if(argument.type === 'StringLiteral'){
+            imports.push({
+              kind: 'dynamic-import',
+              id: argument.value
+            })
           }
         }
-      }]
-    });
-    return ast!
+      },
+      ImportDeclaration:({node}) => {
+        const id = node.source.value;
+        imports.push({
+          kind: 'import',
+          id: id
+        })
+      }
+    })
+    return {ast,imports}
   }
 }
 type fn = typeof babel.parseSync
