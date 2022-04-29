ee2d93f108481088c685b567716e88302af6eee6
hardfist <yangjianzju@gmail.com>
2022-04-29 22:09:01 +0800

    chore: support hmr (#47)

--- packages/rspack/src/node/rspack.ts
+++ packages/rspack/src/node/rspack.ts
@@ -1,23 +1,26 @@
-import type { BundleOptions, ExternalObject } from "@rspack/binding"
-import * as binding from "@rspack/binding"
+import type { BundleOptions, ExternalObject } from '@rspack/binding';
+import * as binding from '@rspack/binding';
 
-export type { BundleOptions }
+export type { BundleOptions };
 
 class Rspack {
-    private _instance: ExternalObject<any>
-    
-    constructor(rawOptions: BundleOptions) {
-        const options: BundleOptions = {
-            minify: false,
-            ...rawOptions
-        }
-        this._instance = binding.newRspack(JSON.stringify(options))
-    }
+  private _instance: ExternalObject<any>;
 
-    async build() {
-        return binding.build(this._instance)
-    }
+  constructor(rawOptions: BundleOptions) {
+    const options: BundleOptions = {
+      minify: false,
+      ...rawOptions,
+    };
+    this._instance = binding.newRspack(JSON.stringify(options));
+  }
+
+  async build() {
+    return binding.build(this._instance);
+  }
+  async rebuild(changefile: string) {
+    return binding.rebuild(this._instance, changefile);
+  }
 }
 
-export { Rspack }
+export { Rspack };
 export default Rspack;
