4d706558f2f96acceaefcf5a6318054682eb27b8
Yunfei He <i.heyunfei@gmail.com>
2022-05-06 20:50:39 +0800

    feat: disable chrome_tracing by default

--- crates/rspack/examples/bundle.rs
+++ crates/rspack/examples/bundle.rs
@@ -2,14 +2,15 @@ use rspack::bundler::{BundleOptions, Bundler};
 
 #[tokio::main]
 async fn main() {
+  let guard = rspack::utils::log::enable_tracing_by_env_with_chrome_layer();
   let mut bundler = Bundler::new(
     BundleOptions {
       // entries: vec![
       //   "./fixtures/basic/entry-a.js".to_owned(),
       //   "./fixtures/basic/entry-b.js".to_owned(),
       // ],
-      entries: vec!["../../examples/react/index.js".to_owned()],
-      // entries: vec!["./packages/rspack/node_modules/lodash-es/lodash.js".to_owned()],
+      // entries: vec!["../../examples/react/index.js".to_owned()],
+      entries: vec!["../../packages/rspack/node_modules/lodash-es/lodash.js".to_owned()],
       outdir: "./dist".to_string(),
       code_splitting: false,
       ..Default::default()
@@ -20,4 +21,5 @@ async fn main() {
   // println!("assets: {:#?}", bundler.ctx.assets.lock().unwrap());
   bundler.write_assets_to_disk();
   // guard.lock().unwrap().as_mut().unwrap().flush();
+  guard.map(|g| g.flush());
 }
