c4a9b33d3bd8e59b643622b184b043d917ab1ea6
Yunfei He <i.heyunfei@gmail.com>
2022-05-06 12:53:28 +0800

    wip: Tweak

--- crates/rspack/src/lib.rs
+++ crates/rspack/src/lib.rs
@@ -1,12 +1,4 @@
-#![deny(clippy::all)]
-
-extern crate core;
-
 pub mod chunk_spliter;
-pub mod external_module;
-
 pub mod utils;
-
 pub use swc_ecma_ast as ast;
-
 pub mod bundler;
--- crates/rspack/src/plugin_driver.rs
+++
@@ -1,1 +0,0 @@
-// pub use rspack_core::PluginDriver;
