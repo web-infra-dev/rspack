c53cc58809e53925f0d10be953147d0f1347fda9
hardfist <yangjianzju@gmail.com>
2022-05-06 20:23:11 +0800

    chore: support jsx (#64)

---
+++ crates/rspack/fixtures/basic-ts/tab.jsx
@@ -0,0 +1,1 @@
+export const Tab = () => <div>tab</div>;
--- examples/react/index.js
+++ examples/react/index.js
@@ -1,5 +1,1 @@
-import './base.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-const app = React.createElement('div', null, 'hello world');
-ReactDOM.render(app, document.getElementById('root'));
+import './app';
