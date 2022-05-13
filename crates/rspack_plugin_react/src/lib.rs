156f501b53b26565dfb34b29af5d1a38123dd5e2
underfin <2218301630@qq.com>
2022-05-13 15:50:14 +0800

    feat: react refresh

--- crates/rspack_plugin_react/src/lib.rs
+++ crates/rspack_plugin_react/src/lib.rs
@@ -1,39 +1,90 @@
+mod react_hmr;
+use async_trait::async_trait;
+use react_hmr::{
+  hmr_entry, hmr_entry_path, hmr_runtime_path, load_hmr_runtime_path, FoundReactRefreshVisitor,
+  InjectReactRefreshEntryFloder, ReactHmrFolder,
+};
+use rspack_core::{
+  ast, BundleContext, BundleMode, LoadedSource, Plugin, PluginLoadHookOutput,
+  PluginResolveHookOutput, ResolvedURI, SWC_GLOBALS,
+};
+use rspack_swc::swc_common::{comments::SingleThreadedComments, GLOBALS};
+use rspack_swc::swc_ecma_transforms_react::{react, Options, RefreshOptions, Runtime};
+use rspack_swc::swc_ecma_visit::{FoldWith, VisitWith};
 use std::path::Path;
-
-use rspack_core::{ast, BundleMode, Plugin, SWC_GLOBALS};
-use rspack_swc::{swc_common, swc_ecma_transforms_react as swc_react, swc_ecma_visit};
-use swc_common::{comments::SingleThreadedComments, GLOBALS};
-use swc_ecma_visit::FoldWith;
-use swc_react::RefreshOptions;
 
 pub static PLUGIN_NAME: &'static str = "rspack_plugin_react";
 
 #[derive(Debug)]
 pub struct ReactPlugin {
-  pub runtime: swc_react::Runtime,
+  pub runtime: Runtime,
 }
 
+#[async_trait]
 impl Plugin for ReactPlugin {
   fn name(&self) -> &'static str {
     PLUGIN_NAME
+  }
+
+  async fn resolve(
+    &self,
+    _ctx: &BundleContext,
+    id: &str,
+    _importer: Option<&str>,
+  ) -> PluginResolveHookOutput {
+    if id == hmr_runtime_path || id == hmr_entry_path {
+      Some(ResolvedURI {
+        uri: id.to_string(),
+        external: false,
+      })
+    } else {
+      None
+    }
+  }
+
+  async fn load(&self, _ctx: &BundleContext, id: &str) -> PluginLoadHookOutput {
+    if id == hmr_runtime_path {
+      return Some(LoadedSource::new(load_hmr_runtime_path(
+        &_ctx.options.as_ref().root,
+      )));
+    } else if id == hmr_entry_path {
+      return Some(LoadedSource::new(hmr_entry.to_string()));
+    } else {
+      return None;
+    }
   }
 
   fn transform(
     &self,
     ctx: &rspack_core::BundleContext,
     path: &Path,
-    ast: ast::Module,
+    mut ast: ast::Module,
   ) -> rspack_core::PluginTransformHookOutput {
+    let id = path.to_str().unwrap_or("").to_string();
+    if ctx.options.react.refresh {
+      let is_entry = ctx
+        .options
+        .entries
+        .iter()
+        .find(|e| e.as_str() == id)
+        .is_some();
+
+      if is_entry {
+        ast = ast.fold_with(&mut InjectReactRefreshEntryFloder {});
+      }
+    }
+
+    let is_node_module = id.find("node_modules").is_some();
     let is_maybe_has_jsx = path.extension().map_or(true, |ext| ext != "ts");
     if is_maybe_has_jsx {
       GLOBALS.set(&SWC_GLOBALS, || {
-        let mut react_folder = swc_react::react::<SingleThreadedComments>(
+        let mut react_folder = react::<SingleThreadedComments>(
           ctx.compiler.cm.clone(),
           None,
-          swc_react::Options {
+          Options {
             development: matches!(ctx.options.mode, BundleMode::Dev),
             runtime: Some(self.runtime),
-            refresh: if ctx.options.react.refresh {
+            refresh: if ctx.options.react.refresh && !is_node_module {
               Some(RefreshOptions {
                 ..Default::default()
               })
@@ -44,8 +95,26 @@ impl Plugin for ReactPlugin {
           },
           ctx.top_level_mark.clone(),
         );
-        let mut folds = &mut react_folder;
-        ast.fold_with(&mut folds)
+
+        ast = ast.fold_with(&mut react_folder);
+
+        if !ctx.options.react.refresh {
+          return ast;
+        }
+
+        match is_node_module {
+          true => ast,
+          false => {
+            let mut f = FoundReactRefreshVisitor {
+              is_refresh_boundary: false,
+            };
+            ast.visit_with(&mut f);
+            match f.is_refresh_boundary {
+              true => ast.fold_with(&mut ReactHmrFolder { id }),
+              false => ast,
+            }
+          }
+        }
       })
     } else {
       ast
