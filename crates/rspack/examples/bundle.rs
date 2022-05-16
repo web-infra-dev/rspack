01aeef5a73829fe96a4c4f7f915208a5d5e53342
hardfist <yangjianzju@gmail.com>
2022-05-16 19:33:36 +0800

    chore: fix progress plugin not working (#155)

--- crates/rspack/examples/bundle.rs
+++ crates/rspack/examples/bundle.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use rspack::bundler::{BundleOptions, Bundler};
 use rspack_core::{BundleMode, BundleReactOptions};
 use tracing::instrument;
@@ -6,13 +8,18 @@ use tracing::instrument;
 #[tokio::main]
 async fn main() {
   let guard = rspack::utils::log::enable_tracing_by_env_with_chrome_layer();
+  let dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+  let example = dir
+    .join("../../examples/react/src/index.js")
+    .to_string_lossy()
+    .to_string();
   let mut bundler = Bundler::new(
     BundleOptions {
       // entries: vec![
       //   "./fixtures/basic/entry-a.js".to_owned(),
       //   "./fixtures/basic/entry-b.js".to_owned(),
       // ],
-      entries: vec!["../../examples/react/src/index.js".to_owned()],
+      entries: vec![example],
       // entries: vec!["../../packages/rspack/node_modules/lodash-es/lodash.js".to_owned()],
       outdir: "./dist".to_string(),
       code_splitting: true,
