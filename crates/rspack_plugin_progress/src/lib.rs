4e5cc4fa91217072f2e87e24e8e93ff4346a2a91
yanyuhao.joy <yanyuhao.joy@bytedance.com>
2022-05-18 10:23:44 +0800

    feat: code clean

--- crates/rspack_plugin_progress/src/lib.rs
+++ crates/rspack_plugin_progress/src/lib.rs
@@ -1,45 +1,66 @@
 use async_trait::async_trait;
 use core::fmt::Debug;
 use rspack_core::{BundleContext, Plugin, PluginLoadHookOutput};
-use rspack_swc::swc_common::private::serde::de;
-use std::ops::MulAssign;
-use std::sync::atomic::{AtomicU32, AtomicU64, AtomicU8, Ordering};
-use std::sync::{Arc, Condvar, Mutex, MutexGuard, Weak};
+use std::sync::{Arc, Mutex};
 extern crate console;
 use console::{style, Color, Term};
-use rspack_core::{ast, BundleMode};
-use std::cell::RefCell;
+use once_cell::sync::Lazy;
 use std::io;
-use std::path::Path;
-use std::thread;
-use std::time::Duration;
 use std::{env, fs};
 
 pub static PLUGIN_NAME: &'static str = "rspack_progress";
-fn get_bar(w: u16, current: u16, fe_color: u8, bg_color: u8) -> String {
-  let fe = format!("{}", style(" ").bg(Color::Color256(fe_color)).to_string());
-  let bg = format!("{}", style(" ").bg(Color::Color256(bg_color)).to_string());
-
-  let mut s = String::from("");
-  for _ in 0..current {
-    s += &fe;
-  }
-  for _ in 0..w - current {
-    s += &bg;
-  }
-  s
-}
+// color256 https://www.ditig.com/256-colors-cheat-sheet
 static CYAN: u8 = 51;
+static GREEN: u8 = 2;
 static GREY: u8 = 8;
 
-fn clamp(n: u32, min: u32, max: u32) -> u32 {
-  if n < min {
-    return min;
+static TERM: Lazy<Term> = Lazy::new(|| {
+  let term = Term::buffered_stdout();
+  term
+});
+
+static MAX_BAR_WIDTH: u32 = 25;
+static MAX_TEXT_WIDTH: usize = 30;
+static FRAMES: &'static [&str; 10] = &["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+
+fn get_bar_str(
+  key: &str,
+  total: u32,
+  current: u32,
+  text: &str,
+  fe_color: u8,
+  bg_color: u8,
+) -> String {
+  let mut s = style(format!(" {} ◯ ", key))
+    .fg(Color::Color256(fe_color))
+    .to_string();
+  if total == 0 {
+    s += &style(FRAMES[(current % FRAMES.len() as u32) as usize])
+      .fg(Color::Color256(fe_color))
+      .to_string();
+    s += " ";
+  } else {
+    let left = (((current as f32) / (total as f32) * MAX_BAR_WIDTH as f32).round() as u32)
+      .clamp(0, MAX_BAR_WIDTH);
+    let fe = style(" ").bg(Color::Color256(fe_color)).to_string();
+    let bg = style(" ").bg(Color::Color256(bg_color)).to_string();
+    for _ in 0..left {
+      s += &fe;
+    }
+    for _ in 0..MAX_BAR_WIDTH - left {
+      s += &bg;
+    }
   }
-  if n > max {
-    return max;
-  }
-  n
+
+  let percent = ((current as f32 / total as f32 * 100.0) as u32).clamp(0, 100);
+  s += &style(&format!(" {:3}% ", percent))
+    .fg(Color::Color256(fe_color))
+    .to_string();
+  s += &style(&truncate(&text, MAX_TEXT_WIDTH)).dim().to_string();
+  let term_width = TERM.size().1 as usize;
+  let line_width = console::measure_text_width(&s);
+  s += &" ".repeat(term_width.saturating_sub(line_width));
+  s
 }
 
 fn truncate(s: &str, max_chars: usize) -> String {
@@ -58,69 +79,45 @@ fn truncate(s: &str, max_chars: usize) -> String {
 
 #[derive(Debug, Clone)]
 pub struct ProgressBar {
-  term: Arc<Mutex<Term>>,
   current: Arc<Mutex<u32>>,
   total: Arc<Mutex<u32>>,
+  done: Arc<Mutex<bool>>,
 }
+
 impl ProgressBar {
   pub fn new(total: u32) -> ProgressBar {
     let current = Arc::new(Mutex::new(0));
     let total = Arc::new(Mutex::new(total));
-    let term = Arc::new(Mutex::new(Term::stdout()));
+    let done = Arc::new(Mutex::new(false));
 
     ProgressBar {
       total,
       current,
-      term,
+      done,
     }
   }
 
-  pub fn update(&self, key: &str, filename: &str, delta: u32, clear: bool) -> io::Result<()> {
-    let term = self.term.lock().unwrap();
-    let mut out = stdout().into_raw_mode().unwrap();
-    let mut pos = (0, 2);
-
-    if clear {
-      term.clear_screen()?;
-    } else {
-      pos = out.cursor_pos().unwrap_or((0, 2));
-    }
-    term.move_cursor_to(0, 1)?;
-    term.clear_line()?;
-    let mut x = self.current.lock().unwrap();
-    *x = *x + delta;
-    let t = *self.total.lock().unwrap();
-    let c = *x;
-
-    let mut s = style(key).cyan().to_string();
-    s += " ";
-    let frames: Vec<&str> = vec!["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
-    if t == 0 {
-      s += &style(frames[(c % frames.len() as u32) as usize])
-        .cyan()
-        .to_string();
-      s += " ";
-    } else {
-      let d = clamp(((c as f32) / (t as f32) * 100.0 / 4.0) as u32, 0, 25);
-      s += &get_bar(25, d as u16, CYAN, GREY);
-      let p = clamp((c as f32 / t as f32 * 100.0) as u32, 0, 100);
-      s += &style(&format!(" {:3}% ", p)).cyan().to_string();
-    }
-
-    s += &style(&truncate(filename, 25)).dim().to_string();
-    s += "\n";
-    term.write_str(&s)?;
-    term.move_cursor_to(0, (pos.1 - 1) as usize)?;
+  pub fn update(&self, key: &str, filename: &str, delta: u32) -> io::Result<()> {
+    let mut current = self.current.lock().unwrap();
+    *current = *current + delta;
+    let total = *self.total.lock().unwrap();
+    TERM.clear_line()?;
+    let s = get_bar_str(key, total, *current, filename, GREEN, GREY);
+    TERM.write_str(&s)?;
+    TERM.flush()?;
     Ok(())
   }
 
   pub fn finish_and_clear(&self) -> io::Result<()> {
+    TERM.clear_line()?;
+    let total = *self.current.lock().unwrap();
+    let mut s = style("finished ✔ ").green().to_string();
+    s += &format!("{} files transformed. \n", total);
+    TERM.write_str(&s)?;
+    TERM.flush()?;
     Ok(())
   }
 }
-use std::io::stdout;
-use termion::cursor::DetectCursorPos;
-use termion::raw::IntoRawMode;
 #[derive(Debug)]
 pub struct ProgressPlugin {
   progress: ProgressBar,
@@ -128,7 +125,6 @@ pub struct ProgressPlugin {
 impl ProgressPlugin {
   pub fn new() -> ProgressPlugin {
     let progress = ProgressBar::new(25);
-    progress.update("RsPack", "", 0, true).unwrap();
     ProgressPlugin { progress }
   }
 }
@@ -159,13 +155,22 @@ impl Plugin for ProgressPlugin {
   }
 
   async fn load(&self, _ctx: &BundleContext, id: &str) -> PluginLoadHookOutput {
+    let done = self.progress.done.lock().unwrap();
+    if *done {
+      return None;
+    }
     // if matches!(_ctx.options.mode, BundleMode::Dev) {
     //   return None;
     // }
-    self.progress.update("RsPack", id, 1, false).unwrap();
+    self.progress.update("RsPack", id, 1).unwrap();
     None
   }
   async fn build_end(&self, _ctx: &BundleContext) {
+    let mut done = self.progress.done.lock().unwrap();
+    if *done {
+      return;
+    }
+    *done = true;
     // if matches!(_ctx.options.mode, BundleMode::Dev) {
     //   return;
     // }
