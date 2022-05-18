b6bbc4b6982e87d7986b88c1fbd1cd22af719f3e
HeYunfei <i.heyunfei@gmail.com>
2022-05-18 12:38:29 +0800

    fix: if the loader is None, resolve it by it's file extension (#189)

--- crates/rspack_core/src/utils/plugin_hook/load.rs
+++ crates/rspack_core/src/utils/plugin_hook/load.rs
@@ -1,6 +1,8 @@
+use std::path::Path;
+
 use tracing::instrument;
 
-use crate::{plugin_driver::PluginDriver, Loader};
+use crate::{plugin_driver::PluginDriver, Loader, LoaderOptions};
 
 #[instrument(skip_all)]
 #[inline]
@@ -12,6 +14,18 @@ pub async fn load(id: &str, plugin_driver: &PluginDriver) -> (String, Loader) {
     .unwrap_or_else(|| std::fs::read_to_string(id).expect(&format!("load failed for {:?}", id)));
   let loader = plugin_result
     .and_then(|load_output| load_output.loader)
-    .unwrap_or_else(|| panic!("No loader to deal with file: {:?}", id));
+    .unwrap_or_else(|| {
+      guess_loader_by_id(id, &plugin_driver.ctx.options.loader)
+        .unwrap_or_else(|| panic!("No loader to deal with file: {:?}", id))
+    });
   (content, loader)
+}
+
+fn guess_loader_by_id(id: &str, options: &LoaderOptions) -> Option<Loader> {
+  let loader = *Path::new(id)
+    .extension()
+    .and_then(|ext| ext.to_str())
+    .and_then(|ext| options.get(ext))?;
+
+  Some(loader)
 }
