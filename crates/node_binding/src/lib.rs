af1ae7e3f412b2a23535e0ea09ab14b4300539a6
hardfist <yangjianzju@gmail.com>
2022-06-14 21:35:37 +0800

    chore: remove write assets

--- crates/node_binding/src/lib.rs
+++ crates/node_binding/src/lib.rs
@@ -82,7 +82,6 @@ pub fn build(env: Env, binding_context: External<RspackBindingContext>) -> Resul
         .build(None)
         .await
         .map_err(|e| Error::new(napi::Status::GenericFailure, format!("{:?}", e)))?;
-      bundler.write_assets_to_disk();
       Ok(map)
     },
     |_env, ret| Ok(ret),
