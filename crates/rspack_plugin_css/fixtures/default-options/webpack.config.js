eed642c972c694ac9b8beeb026db3e34fa75fb40
Yunfei He <i.heyunfei@gmail.com>
2022-06-20 14:14:47 +0800

    wip: migrate css cases

--- crates/rspack_plugin_css/fixtures/default-options/webpack.config.js
+++ crates/rspack_plugin_css/fixtures/default-options/webpack.config.js
@@ -1,14 +1,14 @@
-import Self from "../../../src";
+import Self from '../../../src'
 
 module.exports = {
-  entry: "./index.js",
+  entry: './index.js',
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [Self.loader, "css-loader"],
+        use: [Self.loader, 'css-loader'],
       },
     ],
   },
   plugins: [new Self()],
-};
+}
