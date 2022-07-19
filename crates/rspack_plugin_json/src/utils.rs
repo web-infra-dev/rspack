32d7adb5e667f89fc79488d05900903f50f1f3cd
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-07-19 22:47:13 +0800

    feat: 🎸 finish

--- crates/rspack_plugin_json/src/utils.rs
+++ crates/rspack_plugin_json/src/utils.rs
@@ -1,21 +1,42 @@
-pub fn escape_json(json_str: &str) -> String {
-  use std::fmt::Write;
+use std::borrow::Cow;
 
-  let mut escaped = String::with_capacity(json_str.len());
+const ESCAPE_STRING_LENGTH: usize = 3;
+const U2028: &str = r#"\\u2028"#;
+const U2029: &str = r#"\\u2029"#;
 
-  for c in json_str.chars() {
-    if c == '\u{2028}' {
-      write!(&mut escaped, "\\\\u{:04X}", &0x2028).unwrap();
-    } else if c == '\u{2029}' {
-      write!(&mut escaped, "\\\\u{:04X}", &0x2029).unwrap();
-    } else {
-      escaped.push(c)
+pub fn escape_json(input: &str) -> Cow<'_, str> {
+  let mut vec = input
+    .match_indices('\u{2028}')
+    .chain(input.match_indices('\u{2029}'))
+    .collect::<Vec<_>>();
+  let ret = if !vec.is_empty() {
+    // The replace algorithm require our byte index is ordered,
+    // which we could not ensure in the two chaining `match_indices`,
+    // because `\u2028` and `\u2029` could place in any where in a string.
+    //  We need to sort the indices to make sure the byte index of
+    // ## Example
+    // ```
+    // "\u2028\u2029\u2028"
+    // ```
+    // The final vec would be `[(0, '\u{2028}'),(2, '\u{2028}'),(1, '\u{2029}'),]`
+    vec.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+    // Length of `\u2028` and `\u2029` are both 3 , and would be replaced by raw string `r#"\\u2028"#` or `r#"\\u2029"#`
+    // which length are 7.That's why we need to allocate extra `vec.len() * (7 - 3)` bytes.
+    let mut ret = String::with_capacity(input.len() + vec.len() * 4);
+    let mut last = 0;
+    // The replace algorithm is copied from rust std lib https://doc.rust-lang.org/src/alloc/str.rs.html#288
+    for (i, str) in vec.into_iter() {
+      ret.push_str(unsafe { input.get_unchecked(last..i) });
+      ret.push_str(if str == "\u{2028}" { U2028 } else { U2029 });
+      last = i + ESCAPE_STRING_LENGTH;
     }
-  }
-
-  escaped
+    ret.push_str(unsafe { input.get_unchecked(last..) });
+    Cow::Owned(ret)
+  } else {
+    Cow::Borrowed(input)
+  };
+  ret
 }
-
 mod test {
   #[test]
   fn test_escape_json() {
@@ -23,6 +44,10 @@ mod test {
       (
         r#"{"LS":" ","PS":" ","escaped":"\\u2028"}"#,
         r#"{"LS":"\\u2028","PS":"\\u2029","escaped":"\\u2028"}"#,
+      ),
+      (
+        r#"{"LS":" ","PS":" ","escaped":"\\u2028"}"#,
+        r#"{"LS":"\\u2029","PS":"\\u2028","escaped":"\\u2028"}"#,
       ),
       (r#"{"na\ me": "\ntest"}"#, r#"{"na\ me": "\ntest"}"#),
       (r#"{"a": \n\r\t"a"}"#, r#"{"a": \n\r\t"a"}"#),
