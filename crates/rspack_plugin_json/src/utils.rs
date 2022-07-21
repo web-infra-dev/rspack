d830b31db1f94aeb3b644179ac978b2c7468fbd5
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-07-21 12:17:12 +0800

    docs: ✏️ update doc

--- crates/rspack_plugin_json/src/utils.rs
+++ crates/rspack_plugin_json/src/utils.rs
@@ -13,15 +13,15 @@ pub fn escape_json(input: &str) -> Cow<'_, str> {
     // The replace algorithm require our byte index is ordered,
     // which we could not ensure in the two chaining `match_indices`,
     // because `\u2028` and `\u2029` could place in any where in a string.
-    //  We need to sort the indices to make sure the byte index of
+    //  We need to sort the indices to make sure the byte index is ordered.
     // ## Example
-    // ```
+    // ```json
     // "\u2028\u2029\u2028"
     // ```
     // The final vec would be `[(0, '\u{2028}'),(2, '\u{2028}'),(1, '\u{2029}'),]`
     vec.sort_unstable_by(|a, b| a.0.cmp(&b.0));
     // Length of `\u2028` and `\u2029` are both 3 , and would be replaced by raw string `r#"\\u2028"#` or `r#"\\u2029"#`
-    // which length are 7.That's why we need to allocate extra `vec.len() * (7 - 3)` bytes.
+    // which length is 7.That's why we need to allocate extra `vec.len() * (7 - 3)` bytes.
     let mut ret = String::with_capacity(input.len() + vec.len() * 4);
     let mut last = 0;
     // The replace algorithm is copied from rust std lib https://doc.rust-lang.org/src/alloc/str.rs.html#288
