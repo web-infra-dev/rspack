cf46a946996a8a3bfce9b86fc31cd7685405afc4
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-07-29 17:18:02 +0800

    fix: 🐛 fix cr

--- crates/rspack_plugin_css/src/visitors/mod.rs
+++ crates/rspack_plugin_css/src/visitors/mod.rs
@@ -22,26 +22,39 @@ impl VisitMut for DependencyScanner {
       .filter(|rule| match rule {
         Rule::AtRule(at_rule) => {
           if let Some(AtRulePrelude::ImportPrelude(prelude)) = &at_rule.prelude {
-            let href_string = match &prelude.href {
-              swc_css::ast::ImportPreludeHref::Url(url) => url
-                .value
-                .as_ref()
-                .map(|value| match value {
-                  UrlValue::Str(str) => str.value.to_string(),
-                  UrlValue::Raw(raw) => raw.value.to_string(),
-                })
-                .unwrap_or_default(),
-              swc_css::ast::ImportPreludeHref::Str(str) => str.value.to_string(),
+            let (kind, href_string) = match &prelude.href {
+              swc_css::ast::ImportPreludeHref::Url(url) => {
+                let href_string = url
+                  .value
+                  .as_ref()
+                  .map(|value| match value {
+                    UrlValue::Str(str) => str.value.clone(),
+                    UrlValue::Raw(raw) => raw.value.clone(),
+                  })
+                  .unwrap_or_default();
+                (ResolveKind::UrlToken, href_string)
+              }
+              swc_css::ast::ImportPreludeHref::Str(str) => {
+                (ResolveKind::AtImport, str.value.clone())
+              }
             };
-            // TODO: This just naive checking for http:// and https://, but it's not enough.
-            // Because any scheme is valid in `ImportPreludeHref::Url`, like `url(chrome://xxxx)`
-            // We need to find a better way to handle this.
-            if href_string.starts_with("http://") || href_string.starts_with("https://") {
-              return true;
-            }
+            match kind {
+              ResolveKind::AtImport => {}
+              ResolveKind::UrlToken => {
+                // TODO: This just naive checking for http:// and https://, but it's not enough.
+                // Because any scheme is valid in `ImportPreludeHref::Url`, like `url(chrome://xxxx)`
+                // We need to find a better way to handle this.
+                if href_string.starts_with("http://") || href_string.starts_with("https://") {
+                  return true;
+                }
+              }
+              _ => {
+                unreachable!("ResolveKind in CssPlugin could either be `AtImport` or `UrlToken`")
+              }
+            };
             self.dependencies.push(ModuleDependency {
-              specifier: href_string,
-              kind: ResolveKind::AtImport,
+              specifier: href_string.to_string(),
+              kind,
             });
             false
           } else {
