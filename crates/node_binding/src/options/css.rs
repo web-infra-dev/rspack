1cc4f84da3d1d87b75ffa31ff892efb91692b338
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-08-06 22:26:40 +0800

    refactor: 💡 cfg_attr

--- crates/node_binding/src/options/css.rs
+++ crates/node_binding/src/options/css.rs
@@ -5,25 +5,7 @@ use napi_derive::napi;
 
 #[derive(Deserialize, Debug, Default)]
 #[serde(rename_all = "camelCase", default = "Default::default")]
-#[cfg(not(feature = "test"))]
-#[napi(object)]
-pub struct RawCssOptions {
-  /// ## Example
-  /// ```rust,ignore
-  /// RawCssOptions {
-  ///   preset_env: vec![
-  ///
-  ///          "Firefox > 10".into(),
-  ///    "chrome >=20".into(),
-  /// ]
-  /// }
-  /// ```
-  pub preset_env: Vec<String>,
-}
-
-#[derive(Deserialize, Debug, Default)]
-#[serde(rename_all = "camelCase", default = "Default::default")]
-#[cfg(feature = "test")]
+#[cfg_attr(not(feature = "test"), napi(object))]
 pub struct RawCssOptions {
   /// ## Example
   /// ```rust,ignore
