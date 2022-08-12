908713d5279ef97d3240330ef21959c3e9ba8bf7
Chao <chao.guo@bytedance.com>
2022-08-12 13:22:50 -0700

    chore: add dev cli and dev-server

--- packages/rspack-dev-server/src/server/index.ts
+++ packages/rspack-dev-server/src/server/index.ts
@@ -1,20 +1,16 @@
 import express from 'express';
-import { Rspack } from "@rspack/core";
 
 export async function createServer(options): Promise<express.Express> {
-  const { dev: {
-    static : {
-      directory = 'dist'
-    } = {},
-  } = {}} =  options || {}
-
-
-  const rspack = new Rspack(options);
-  await rspack.build();
+  const {
+    dev: {
+      static: {
+        directory = 'dist'
+      } = {}
+    } = {}
+  } = options || {};
 
   const app = express();
-
-
+  
   app.use(express.static(directory));
 
   return app;
