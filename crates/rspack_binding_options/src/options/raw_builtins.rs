f93a1f8a02ffbb48848a8f30086b56873bdf0c6e
hardfist <yangjianzju@gmail.com>
2022-08-17 14:26:26 +0800

    refactor: move html options to builtin (#563)

---
+++ crates/rspack_binding_options/src/options/raw_builtins.rs
@@ -0,0 +1,74 @@
+use napi_derive::napi;
+use serde::Deserialize;
+use serde::Serialize;
+pub type HtmlPluginConfigScriptLoading = String;
+pub type HtmlPluginConfigInject = String;
+pub type HtmlSriHashFunction = String;
+
+// fn default_template() -> String {
+//   String::from("index.html")
+// }
+fn default_filename() -> String {
+  String::from("index.html")
+}
+
+fn default_script_loading() -> HtmlPluginConfigScriptLoading {
+  String::from("defer")
+}
+
+/**
+ * It seems napi not support enum well
+ */
+// #[derive(Deserialize, Debug, Clone, Copy)]
+// #[serde(rename_all = "snake_case")]
+// pub enum HtmlSriHashFunction {
+//   Sha256,
+//   Sha384,
+//   Sha512,
+// }
+// #[derive(Deserialize, Debug, Serialize)]
+// #[serde(rename_all = "snake_case")]
+// #[napi]
+// pub enum HtmlPluginConfigInject {
+//   Head,
+//   Body,
+// }
+// #[derive(Deserialize, Debug, Serialize)]
+// #[serde(rename_all = "snake_case")]
+// #[napi]
+// pub enum HtmlPluginConfigScriptLoading {
+//   Blocking,
+//   Defer,
+//   Module,
+// }
+
+#[derive(Deserialize, Debug, Serialize)]
+#[napi(object)]
+pub struct RawHtmlPluginConfig {
+  /// emitted file name in output path
+  #[serde(default = "default_filename")]
+  pub filename: String,
+  /// template html file
+  pub template: Option<String>,
+  /// `head`, `body` or None
+  #[napi(ts_type = "string | void")]
+  pub inject: Option<HtmlPluginConfigInject>,
+  /// path or `auto`
+  pub public_path: Option<String>,
+  /// `blocking`, `defer`, or `module`
+  #[serde(default = "default_script_loading")]
+  #[napi(ts_type = "string | void")]
+  pub script_loading: HtmlPluginConfigScriptLoading,
+
+  /// entry_chunk_name (only entry chunks are supported)
+  pub chunks: Option<Vec<String>>,
+  pub excluded_chunks: Option<Vec<String>>,
+  #[napi(ts_type = "string | void")]
+  pub sri: Option<HtmlSriHashFunction>,
+}
+
+#[derive(Debug, Deserialize)]
+#[napi(object)]
+pub struct RawBuiltins {
+  pub html: Option<Vec<RawHtmlPluginConfig>>,
+}
