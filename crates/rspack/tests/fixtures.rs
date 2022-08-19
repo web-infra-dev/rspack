1455d8131627ffbcd69949fc59fd852b15e3b437
Hana <andywangsy@gmail.com>
2022-08-19 14:35:26 +0800

    test: add back example tests (#579)

--- crates/rspack/tests/fixtures.rs
+++ crates/rspack/tests/fixtures.rs
@@ -1,8 +1,35 @@
+use std::fs;
 use std::path::PathBuf;
+
+use rspack_binding_options::{normalize_bundle_options, RawOptions};
 use temp_test_utils::test_fixture;
 use testing_macros::fixture;
 
 #[fixture("tests/fixtures/*")]
 fn rspack(fixture_path: PathBuf) {
   test_fixture(&fixture_path);
+}
+
+#[tokio::main]
+async fn run(context: PathBuf) {
+  let config_path = context
+    .join("test.config.json")
+    .to_string_lossy()
+    .to_string();
+  let config = fs::read_to_string(config_path).unwrap();
+  let options: RawOptions = serde_json::from_str(&config).expect("load config failed");
+  let mut compiler = rspack::rspack(
+    normalize_bundle_options(RawOptions {
+      context: Some(context.to_string_lossy().to_string()),
+      ..options
+    })
+    .unwrap(),
+    vec![],
+  );
+  compiler.compile().await.unwrap();
+}
+
+#[fixture("../../examples/react")]
+fn react(fixture_path: PathBuf) {
+  run(fixture_path);
 }
