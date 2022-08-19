d740af6a88c1c53bf3ce02be6ad6f57ab1cdbf2c
h-a-n-a <andywangsy@gmail.com>
2022-08-19 16:45:18 +0800

    feat: optimize macro

--- crates/rspack_binding_options/src/lib.rs
+++ crates/rspack_binding_options/src/lib.rs
@@ -1,46 +1,5 @@
 // mod options;
 // pub use options::*;
-// mod macros;
-// pub use macros::*;
 
-#[cfg(feature = "node-api")]
-use napi_derive::napi;
-
-macro_rules! generate_struct {
-  (
-    #[derive($($d_meta:meta),*)]
-    #[napi($($napi_meta:meta),*)]
-    struct $name:ident {
-      $(
-        $(
-          #[$attr:meta]
-        )*
-        $field:ident: $field_type:ty,
-      )*
-    }
-  ) => {
-    #[cfg(feature = "node-api")]
-    use napi_derive::napi;
-
-    #[cfg(feature = "node-api")]
-    #[derive($($d_meta), *)]
-    #[napi($($napi_meta), *)]
-    pub struct $name {
-      $(
-        $(
-          #[$attr]
-        )*
-        pub $field: $field_type,
-      )+
-    }
-  }
-}
-
-generate_struct!(
-  #[derive(Debug, Clone)]
-  #[napi(object)]
-  struct Foo {
-    #[napi(ts_type = "any")]
-    a: i32,
-  }
-);
+mod macros;
+pub use macros::*;
--- crates/rspack_binding_options/src/macros.rs
+++ crates/rspack_binding_options/src/macros.rs
@@ -1,78 +1,60 @@
 #[macro_export]
-macro_rules! generate_struct {
+macro_rules! define_napi_object {
   (
+    $(#[derive($($derive:meta),*)])*
+    $(#[serde($($serde:meta),*)])*
     $(#[doc=$($s_doc:tt)*])*
-    $name: ident {
+    $_vis_struct:vis $name: ident {
       $(
         $(#[doc=$($doc:tt)*])*
-        $field: ident: $type: ty,
-        #[napi($($napi_attr:meta), *)]
+        $(#[serde($($serde_attr:meta),*)])*
+        $(#[napi($($napi_attr:meta),*)])*
+        $_vis_field:vis $field: ident: $type: ty
       ),*
     }
  ) => {
+    #[cfg(feature = "node-api")]
+    $(#[derive($($derive),*)])*
+    $(#[serde($($serde),*)])*
+    $(#[doc=$($s_doc)*])*
+    #[napi(object)]
+    pub struct $name {
+      $(
+        $(#[doc=$($doc)*])*
+        $(#[serde($($serde_attr),*)])*
+        $(#[napi($($napi_attr),*)])*
+        pub $field: $type
+      ),*
+    }
+
+    #[cfg(not(feature = "node-api"))]
+    $(#[derive($($derive),*)])*
+    $(#[serde($($serde),*)])*
     $(#[doc=$($s_doc)*])*
     pub struct $name {
       $(
         $(#[doc=$($doc)*])*
+        $(#[serde($($serde_attr),*)])*
         pub $field: $type
       ),*
     }
   };
 
-  (
-    $(#[doc=$($s_doc:tt)*])*
-    $name: ident {
-      $(
-        $(#[doc=$($doc:tt)*])*
-        $field: ident: $type: ty,
-        #[napi($($napi_attr:meta), *)],
-        #[serde($($serde_attr:meta), *)],
-      ),*
-    }
-  ) => {
-    $(#[doc=$($s_doc)*])*
-    pub struct $name {
-      $(
-        $(#[doc=$($doc)*])*
-        pub $field: $type
-      ),*
-    }
-  };
+  ($($rest:tt)*) => {
+    compile_error!(r#"Unable to expand macro `define_napi_object`
+Please check your syntax. Note that the order of the proc-macros and attribute-macros is important!
+Supported proc-macros: `derive` / `serde`.
+Supported attribute-macros: `doc`(or, ///) / `serde` / `napi`.
 
-  (
-    $(#[doc=$($s_doc:tt)*])*
-    $name: ident {
-      $(
-        $(#[doc=$($doc:tt)*])*
-        $field: ident: $type: ty,
-        #[serde($($serde_attr:meta), *)],
-      ),*
-    }
-  ) => {
-    $(#[doc=$($s_doc)*])*
-    pub struct $name {
-      $(
-        $(#[doc=$($doc)*])*
-        pub $field: $type
-      ),*
-    }
-  };
-
-  (
-    $(#[doc=$($s_doc:tt)*])*
-    $name: ident, {
-      $(
-        $(#[doc=$($doc:tt)*])*
-        $field: ident: $type: ty
-      ),*
-    }
-  ) => {
-    // $(#[doc=$($s_doc)*])*
-    pub struct $name {
-      $(
-        $(#[doc=$($doc)*])*
-        pub $field: $type
-      ),*
-    }
+  Example:
+  define_napi_object!(
+      #[derive(...)]
+      #[serde(...)]
+      MyObject {
+        #[serde(...)]
+        #[napi(ts_type = "...")]
+        field: Type
+      }
+  });"#);
   };
 }
