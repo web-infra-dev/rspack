42e3ed9287c8b365e6bde3226db7bb82d50fb5dc
Hana <andywangsy@gmail.com>
2022-08-26 15:38:56 +0800

    feat: support debug for loader name (#623)
    * feat: support debug for loader name
    
    * feat: support display name for loader
    
    * feat: change naming to unknown-loader
    
    * feat: better experience
    
    * chore: prettier
    
    * chore: pnpm cleanup
    
    * feat: make fn safe to use

--- crates/node_binding/src/utils.rs
+++ crates/node_binding/src/utils.rs
@@ -1,14 +1,76 @@
-use napi::Env;
+use std::ffi::CStr;
+use std::io::Write;
+use std::ptr;
+
+use napi::{check_status, Env, Error, NapiRaw, Result};
 use napi_derive::napi;
 use once_cell::sync::OnceCell;
 
 static CUSTOM_TRACE_SUBSCRIBER: OnceCell<bool> = OnceCell::new();
 
+/// Try to resolve the string value of a given named property
+pub fn get_named_property_value_string<T: NapiRaw>(
+  env: Env,
+  object: T,
+  property_name: &str,
+) -> Result<String> {
+  let mut bytes_with_nul: Vec<u8> = Vec::with_capacity(property_name.len() + 1);
+
+  write!(&mut bytes_with_nul, "{}", property_name)?;
+  write!(&mut bytes_with_nul, "{}", '\0')?;
+
+  let mut value_ptr = ptr::null_mut();
+
+  check_status!(
+    unsafe {
+      napi_sys::napi_get_named_property(
+        env.raw(),
+        object.raw(),
+        CStr::from_bytes_with_nul_unchecked(&bytes_with_nul).as_ptr(),
+        &mut value_ptr,
+      )
+    },
+    "failed to get the value"
+  )?;
+
+  let mut str_len = 0;
+  check_status!(
+    unsafe {
+      napi_sys::napi_get_value_string_utf8(env.raw(), value_ptr, ptr::null_mut(), 0, &mut str_len)
+    },
+    "failed to get the value"
+  )?;
+
+  str_len += 1;
+  let mut buf = Vec::with_capacity(str_len);
+  let mut copied_len = 0;
+
+  check_status!(
+    unsafe {
+      napi_sys::napi_get_value_string_utf8(
+        env.raw(),
+        value_ptr,
+        buf.as_mut_ptr(),
+        str_len,
+        &mut copied_len,
+      )
+    },
+    "failed to get the value"
+  )?;
+
+  // Vec<i8> -> Vec<u8> See: https://stackoverflow.com/questions/59707349/cast-vector-of-i8-to-vector-of-u8-in-rust
+  let mut buf = std::mem::ManuallyDrop::new(buf);
+
+  let buf = unsafe { Vec::from_raw_parts(buf.as_mut_ptr() as *mut u8, copied_len, copied_len) };
+
+  String::from_utf8(buf).map_err(|_| Error::from_reason("failed to get property"))
+}
+
 #[napi]
 pub fn init_custom_trace_subscriber(
   mut env: Env,
   // trace_out_file_path: Option<String>,
-) -> napi::Result<()> {
+) -> Result<()> {
   CUSTOM_TRACE_SUBSCRIBER.get_or_init(|| {
     let guard = rspack_core::log::enable_tracing_by_env_with_chrome_layer();
     if let Some(guard) = guard {
