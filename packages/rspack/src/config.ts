76d1d1a1cf88a0cd6bd920738c98fcf7503930c6
liuliudada-w <18245019206@163.com>
2022-08-26 14:14:50 +0800

    fix: output path dont work

--- packages/rspack/src/config.ts
+++ packages/rspack/src/config.ts
@@ -6,64 +6,66 @@ import { createRawModuleRuleUses } from ".";
 export type Plugin = string | [string] | [string, unknown];
 
 export interface RspackOptions {
-	/**
-	 * Entry points of compilation.
-	 */
-	entry?: RawOptions["entry"];
-	/**
-	 * An **absolute** path pointed the
-	 */
-	context?: RawOptions["context"];
-	/**
-	 * An array of plugins
-	 */
-	plugins?: Plugin[];
-	/**
-	 * dev server
-	 */
-	dev?: {
-		port?: Number;
-		static?: {
-			directory?: string;
-		};
-	};
-	/**
-	 * Module configuration.
-	 */
-	module?: {
-		rules?: ModuleRule[];
-		parser?: RawOptions["module"]["parser"];
-	};
-	define?: RawOptions["define"];
-	target?: RawOptions["target"];
-	mode?: RawOptions["mode"];
-	external?: RawOptions["external"];
+  /**
+   * Entry points of compilation.
+   */
+  entry?: RawOptions["entry"];
+  /**
+   * An **absolute** path pointed the
+   */
+  context?: RawOptions["context"];
+  /**
+   * An array of plugins
+   */
+  plugins?: Plugin[];
+  /**
+   * dev server
+   */
+  dev?: {
+    port?: Number;
+    static?: {
+      directory?: string;
+    };
+  };
+  /**
+   * Module configuration.
+   */
+  module?: {
+    rules?: ModuleRule[];
+    parser?: RawOptions["module"]["parser"];
+  };
+  define?: RawOptions["define"];
+  target?: RawOptions["target"];
+  mode?: RawOptions["mode"];
+  external?: RawOptions["external"];
+  output?: RawOptions["output"];
 }
 
 export function normalizePlugins(plugins: Plugin[]) {
-	return plugins.map(plugin => {
-		if (typeof plugin === "string") {
-			return [plugin];
-		}
-	});
+  return plugins.map(plugin => {
+    if (typeof plugin === "string") {
+      return [plugin];
+    }
+  });
 }
 
 export function User2Native(config: RspackOptions): RawOptions {
-	return {
-		entry: config.entry ?? {},
-		context: config.context,
-		define: config.define,
-		target: config.target,
-		plugins: normalizePlugins(config.plugins ?? []),
-		external: config.external,
-		module: {
-			// TODO: support mutliple rules to support `Module Type`
-			rules: (config?.module?.rules ?? []).map(rule => {
-				return {
-					...rule,
-					uses: createRawModuleRuleUses(rule.uses || [])
-				};
-			})
-		}
-	};
+  return {
+    entry: config.entry ?? {},
+    context: config.context,
+    output: config.output,
+    define: config.define,
+    target: config.target,
+    plugins: normalizePlugins(config.plugins ?? []),
+    external: config.external,
+    module: {
+      // TODO: support mutliple rules to support `Module Type`
+      rules: (config?.module?.rules ?? []).map(rule => {
+        return {
+          ...rule,
+          uses: createRawModuleRuleUses(rule.uses || [])
+        };
+      })
+    }
+  };
 }
