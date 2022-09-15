b0538cf07445ea25364dc8aed8b1f3802bb99dec
hardfist <yangjianzju@gmail.com>
2022-09-15 17:57:48 +0800

    chore: migrate to jest (#724)
    * chore: migrate to jest
    
    * fix(crates/rspack_plugin_runtime): should generate correct entry modu… (#726)
    
    fix(crates/rspack_plugin_runtime): should generate correct entry module id for node target
    
    * chore: remove unnecerray file
    
    Co-authored-by: HeYunfei <i.heyunfei@gmail.com>

--- packages/rspack/tests/config.test.ts
+++ packages/rspack/tests/config.test.ts
@@ -1,46 +1,41 @@
-import { test } from "uvu";
-import * as assert from "uvu/assert";
+import assert from "assert";
 import { Rspack } from "@rspack/core";
 import path from "path";
+describe("config", () => {
+	it("default config snapshot", () => {
+		const resolvedOptions = new Rspack({}).options;
+		assert.equal(resolvedOptions.context, process.cwd());
+		assert.equal(
+			resolvedOptions.dev.static.directory,
+			path.resolve(process.cwd(), "./dist")
+		);
 
-test("default config snapshot", () => {
-	const resolvedOptions = new Rspack({}).options;
-
-	assert.equal(resolvedOptions.context, process.cwd());
-	assert.equal(
-		resolvedOptions.dev.static.directory,
-		path.resolve(process.cwd(), "./dist")
-	);
-
-	// TypeScript will throw `The operand of a 'delete' operator must be optional`.
-	// But we remove these configurations with absolute paths.
-	// @ts-expect-error
-	delete resolvedOptions.context;
-	// @ts-expect-error
-	delete resolvedOptions.dev.static.directory;
-	assert.snapshot(
-		JSON.stringify(resolvedOptions),
-		JSON.stringify({
-			mode: "development",
-			dev: { port: 8080, static: {} },
-			entry: {},
-			output: {},
-			define: {},
-			target: ["web"],
-			external: {},
-			plugins: [],
-			builtins: [],
-			module: { rules: [] },
-			resolve: {
-				preferRelative: false,
-				extensions: [".tsx", ".jsx", ".ts", ".js", ".json", ".d.ts"],
-				mainFiles: ["index"],
-				mainFields: ["module", "main"],
-				browserField: true,
-				conditionNames: ["module", "import"]
-			}
-		})
-	);
+		// TypeScript will throw `The operand of a 'delete' operator must be optional`.
+		// But we remove these configurations with absolute paths.
+		delete (resolvedOptions as any).context;
+		delete (resolvedOptions as any).dev.static.directory;
+		assert.equal(
+			JSON.stringify(resolvedOptions),
+			JSON.stringify({
+				mode: "development",
+				dev: { port: 8080, static: {} },
+				entry: {},
+				output: {},
+				define: {},
+				target: ["web"],
+				external: {},
+				plugins: [],
+				builtins: [],
+				module: { rules: [] },
+				resolve: {
+					preferRelative: false,
+					extensions: [".tsx", ".jsx", ".ts", ".js", ".json", ".d.ts"],
+					mainFiles: ["index"],
+					mainFields: ["module", "main"],
+					browserField: true,
+					conditionNames: ["module", "import"]
+				}
+			})
+		);
+	});
 });
-
-test.run();
