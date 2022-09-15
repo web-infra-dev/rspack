e7a0d02aef65324ab8bc06711e043c26896f47f8
hardfist <yangjianzju@gmail.com>
2022-09-16 07:34:05 +0800

    chore: migrate post-css and hook test (#728)

---
+++ packages/rspack/tests/configCases/plugins/internal/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	plugins: [
+		{
+			name: "test",
+			apply(compiler) {
+				compiler.hooks.compilation.tap("compilation", compilation => {
+					compilation.hooks.processAssets.tapPromise(
+						"processAssets1",
+						async assets => {
+							for (const [key, value] of Object.entries(assets)) {
+								compilation.updateAsset(key, {
+									source: "//banner;\n" + value.source()
+								});
+							}
+						}
+					);
+				});
+			}
+		}
+	]
+};
