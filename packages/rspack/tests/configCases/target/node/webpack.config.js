4e40c1c83cda5407cbbac5faedb90f2fd5ec40da
liuliudada-w <18245019206@163.com>
2022-09-19 18:29:02 +0800

    chore: add config case

---
+++ packages/rspack/tests/configCases/target/node/webpack.config.js
@@ -0,0 +1,6 @@
+const externalModule = ["uvu", "path", "fs", "expect", "os"];
+const external = Object.fromEntries(externalModule.map(x => [x, x]));
+module.exports = {
+	target: "node",
+	externals: external
+};
