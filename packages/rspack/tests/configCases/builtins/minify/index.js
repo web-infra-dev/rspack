11e8c888661b86df3b28501098dcfb3bfb7a1f94
jerrykingxyz <jerrykingxyz@gmail.com>
2022-09-19 18:16:29 +0800

    feat: move minify to processassets

--- packages/rspack/tests/configCases/builtins/minify/index.js
+++ packages/rspack/tests/configCases/builtins/minify/index.js
@@ -10,5 +10,5 @@ it("basic", () => {
 
 it("format", () => {
 	const content = fs.readFileSync(__filename, "utf-8");
-	expect(content).toMatch(/function\ test\(\)\{return 123\}/);
+	expect(content).not.toMatch("\n");
 });
