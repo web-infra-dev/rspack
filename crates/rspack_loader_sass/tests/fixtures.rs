887409c11c902d9c284d5b42a33d69711792e193
ahabhgk <ahabhgk@gmail.com>
2022-09-22 18:16:06 +0800

    remove sass source map test

--- crates/rspack_loader_sass/tests/fixtures.rs
+++ crates/rspack_loader_sass/tests/fixtures.rs
@@ -13,14 +13,9 @@ use rspack_test::{fixture, test_fixture};
 use sass_embedded::Url;
 
 // UPDATE_SASS_LOADER_TEST=1 cargo test --package rspack_loader_sass test_fn_name -- --exact --nocapture
-async fn loader_test(
-  actual: impl AsRef<Path>,
-  expected: impl AsRef<Path>,
-  expected_source_map: Option<&str>,
-) {
+async fn loader_test(actual: impl AsRef<Path>, expected: impl AsRef<Path>) {
   let tests_path = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"))).join("tests");
   let expected_path = tests_path.join(expected);
-  let expected_source_map = expected_source_map.map(|expected| tests_path.join(expected));
   let actual_path = tests_path.join(actual);
 
   let url = Url::from_file_path(&actual_path.to_string_lossy().to_string()).unwrap();
@@ -42,7 +37,7 @@ async fn loader_test(
           entry: std::collections::HashMap::default(),
           context: rspack_core::Context::default(),
           dev_server: rspack_core::DevServerOptions::default(),
-          devtool: expected_source_map.is_some(),
+          devtool: false,
           output: rspack_core::OutputOptions {
             path: Default::default(),
             public_path: Default::default(),
@@ -67,38 +62,19 @@ async fn loader_test(
   )
   .await
   .unwrap();
-  dbg!(&result);
-  let source_map = result.source_map.map(|s| s.to_json().unwrap());
   let result = result.content.try_into_string().unwrap();
 
   if env::var("UPDATE_SASS_LOADER_TEST").is_ok() {
     fs::write(expected_path, result).unwrap();
-    if let Some(expected_source_map) = expected_source_map {
-      fs::write(expected_source_map, source_map.unwrap()).unwrap();
-    }
   } else {
     let expected = fs::read_to_string(expected_path).unwrap();
     assert_eq!(result, expected);
-    if let Some(expected_source_map) = expected_source_map {
-      let expected_source_map = fs::read_to_string(expected_source_map).unwrap();
-      assert_eq!(source_map.unwrap(), expected_source_map);
-    }
   }
 }
 
 #[tokio::test]
 async fn rspack_importer() {
-  loader_test("scss/language.scss", "expected/rspack_importer.css", None).await;
-}
-
-#[tokio::test]
-async fn devtool_true() {
-  loader_test(
-    "scss/language.scss",
-    "expected/devtool_true.css",
-    Some("expected/devtool_true.css.map"),
-  )
-  .await;
+  loader_test("scss/language.scss", "expected/rspack_importer.css").await;
 }
 
 #[fixture("tests/fixtures/*")]
