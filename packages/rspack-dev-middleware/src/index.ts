f743740126663dda879ab0f52d8e05b6f6374662
bvanjoi <bohan-zhang@foxmail.com>
2022-09-26 21:34:49 +0800

    packages(rspack-dev-server): align to webpack-dev

--- packages/rspack-dev-middleware/src/index.ts
+++ packages/rspack-dev-middleware/src/index.ts
@@ -1,13 +1,25 @@
 import type { Rspack, ResolvedRspackOptions } from "@rspack/core";
 import type { RequestHandler as ExpressRequestHandler } from "express";
+import { ready } from "webpack-dev-middleware/dist/utils/ready";
 
-export type DevMiddleware = ExpressRequestHandler;
+export type RspackDevMiddleware = ExpressRequestHandler & {
+	invalidate(callback: Function): void;
+	close(callback: Function): void;
+};
 
 export function rdm(
 	compiler: Rspack,
 	options: ResolvedRspackOptions["dev"]["devMiddleware"]
-): DevMiddleware {
-	const instance = wrapper();
+): RspackDevMiddleware {
+	// @ts-ignore
+	const instance: RspackDevMiddleware = wrapper();
+	instance.invalidate = function (callback) {
+		// TODO:
+	};
+	instance.close = function (callback) {
+		// TODO:
+	};
+
 	const watching = compiler.watch({});
 	return instance;
 }
