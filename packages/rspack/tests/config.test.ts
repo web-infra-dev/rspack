61126b4f5d17917ce5fd217413e0f07103e6134f
liuliudada-w <18245019206@163.com>
2022-09-26 14:38:40 +0800

    chore: format

--- packages/rspack/tests/config.test.ts
+++ packages/rspack/tests/config.test.ts
@@ -2,56 +2,56 @@ import assert from "assert";
 import { Rspack } from "@rspack/core";
 import path from "path";
 describe("config", () => {
-  it("default config snapshot", () => {
-    const resolvedOptions = new Rspack({}).options;
-    assert.deepStrictEqual(resolvedOptions.context, process.cwd());
-    assert.deepStrictEqual(
-      resolvedOptions.dev.static.directory,
-      path.resolve(process.cwd(), "./dist")
-    );
+	it("default config snapshot", () => {
+		const resolvedOptions = new Rspack({}).options;
+		assert.deepStrictEqual(resolvedOptions.context, process.cwd());
+		assert.deepStrictEqual(
+			resolvedOptions.dev.static.directory,
+			path.resolve(process.cwd(), "./dist")
+		);
 
-    // TypeScript will throw `The operand of a 'delete' operator must be optional`.
-    // But we remove these configurations with absolute paths.
-    delete (resolvedOptions as any).context;
-    delete (resolvedOptions as any).dev.static.directory;
-    assert.deepStrictEqual(resolvedOptions, {
-      mode: "development",
-      dev: {
-        port: 8080,
-        static: {
-          watch: false
-        },
-        devMiddleware: {},
-        open: true,
-        hmr: false,
-        liveReload: true
-      },
-      entry: {},
-      output: {
-        assetModuleFilename: undefined,
-        chunkFilename: undefined,
-        filename: undefined,
-        path: undefined,
-        publicPath: undefined,
-        uniqueName: undefined
-      },
-      define: {},
-      target: ["web"],
-      external: {},
-      externalType: "",
-      plugins: [],
-      builtins: { browserslist: [] },
-      module: { rules: [] },
-      resolve: {
-        alias: {},
-        preferRelative: false,
-        extensions: [".tsx", ".jsx", ".ts", ".js", ".json", ".d.ts"],
-        mainFiles: ["index"],
-        mainFields: ["module", "main"],
-        browserField: true,
-        conditionNames: ["module", "import"]
-      },
-      devtool: false
-    });
-  });
+		// TypeScript will throw `The operand of a 'delete' operator must be optional`.
+		// But we remove these configurations with absolute paths.
+		delete (resolvedOptions as any).context;
+		delete (resolvedOptions as any).dev.static.directory;
+		assert.deepStrictEqual(resolvedOptions, {
+			mode: "development",
+			dev: {
+				port: 8080,
+				static: {
+					watch: false
+				},
+				devMiddleware: {},
+				open: true,
+				hmr: false,
+				liveReload: true
+			},
+			entry: {},
+			output: {
+				assetModuleFilename: undefined,
+				chunkFilename: undefined,
+				filename: undefined,
+				path: undefined,
+				publicPath: undefined,
+				uniqueName: undefined
+			},
+			define: {},
+			target: ["web"],
+			external: {},
+			externalType: "",
+			plugins: [],
+			builtins: { browserslist: [] },
+			module: { rules: [] },
+			resolve: {
+				alias: {},
+				preferRelative: false,
+				extensions: [".tsx", ".jsx", ".ts", ".js", ".json", ".d.ts"],
+				mainFiles: ["index"],
+				mainFields: ["module", "main"],
+				browserField: true,
+				conditionNames: ["module", "import"]
+			},
+			devtool: false
+		});
+	});
 });
