a2b235023c37fedad367e834adee55267c860fab
h-a-n-a <andywangsy@gmail.com>
2022-09-27 21:22:34 +0800

    feat: finish json migration

--- crates/rspack_plugin_json/src/lib.rs
+++ crates/rspack_plugin_json/src/lib.rs
@@ -7,10 +7,103 @@ use rspack_error::{
 
 use rspack_core::{
   rspack_sources::{BoxSource, RawSource, Source, SourceExt},
-  BoxModule, Module, ModuleType, Parser, Plugin, SourceType,
+  BoxModule, Module, ModuleType, Parser, ParserAndGenerator, Plugin, SourceType,
 };
 
 mod utils;
+
+#[derive(Debug)]
+struct JsonParserAndGenerator {}
+
+impl ParserAndGenerator for JsonParserAndGenerator {
+  fn source_types(&self) -> &[SourceType] {
+    &[SourceType::JavaScript]
+  }
+
+  fn parse(
+    &mut self,
+    parse_context: rspack_core::ParseContext,
+  ) -> Result<TWithDiagnosticArray<rspack_core::ParseResult>> {
+    let rspack_core::ParseContext {
+      source: box_source,
+      resource_data,
+      ..
+    } = parse_context;
+    let source = box_source.source();
+
+    json::parse(&source).map_err(|e| {
+      match e {
+        UnexpectedCharacter { ch, line, column } => {
+          let rope = ropey::Rope::from_str(&source);
+          let line_offset = rope.try_line_to_byte(line - 1).unwrap();
+          let start_offset = source[line_offset..]
+            .chars()
+            .take(column)
+            .fold(line_offset, |acc, cur| acc + cur.len_utf8());
+          Error::TraceableError(
+            TraceableError::from_path(
+              resource_data.resource_path.to_owned(),
+              // one character offset
+              start_offset,
+              start_offset + 1,
+              "Json parsing error".to_string(),
+              format!("Unexpected character {}", ch),
+            )
+            .with_kind(DiagnosticKind::Json),
+          )
+        }
+        ExceededDepthLimit | WrongType(_) | FailedUtf8Parsing => {
+          Error::InternalError(format!("{}", e))
+        }
+        UnexpectedEndOfJson => {
+          // End offset of json file
+          let offset = source.len();
+          Error::TraceableError(
+            TraceableError::from_path(
+              resource_data.resource_path.to_owned(),
+              offset,
+              offset,
+              "Json parsing error".to_string(),
+              format!("{}", e),
+            )
+            .with_kind(DiagnosticKind::Json),
+          )
+        }
+      }
+    })?;
+
+    Ok(
+      rspack_core::ParseResult {
+        dependencies: vec![],
+        ast_or_source: box_source.into(),
+      }
+      .with_empty_diagnostic(),
+    )
+  }
+
+  fn generate(
+    &self,
+    requested_source_type: SourceType,
+    ast_or_source: &rspack_core::AstOrSource,
+    _module: &rspack_core::ModuleGraphModule,
+    _compilation: &rspack_core::Compilation,
+  ) -> Result<rspack_core::GenerationResult> {
+    match requested_source_type {
+      SourceType::JavaScript => Ok(rspack_core::GenerationResult {
+        ast_or_source: RawSource::from(format!(
+          r#"module.exports = {};"#,
+          utils::escape_json(&ast_or_source.as_source().unwrap().source())
+        ))
+        .boxed()
+        .into(),
+      }),
+      _ => Err(Error::InternalError(format!(
+        "Unsupported source type {:?} for plugin Json",
+        requested_source_type,
+      ))),
+    }
+  }
+}
 
 #[derive(Debug)]
 pub struct JsonPlugin {}
@@ -24,9 +117,14 @@ impl Plugin for JsonPlugin {
     &mut self,
     ctx: rspack_core::PluginContext<&mut rspack_core::ApplyContext>,
   ) -> Result<()> {
-    ctx
-      .context
-      .register_parser(rspack_core::ModuleType::Json, Box::new(JsonParser::new()));
+    // ctx
+    //   .context
+    //   .register_parser(rspack_core::ModuleType::Json, Box::new(JsonParser::new()));
+
+    ctx.context.register_parser_and_generator_builder(
+      rspack_core::ModuleType::Json,
+      Box::new(|| Box::new(JsonParserAndGenerator {})),
+    );
 
     Ok(())
   }
