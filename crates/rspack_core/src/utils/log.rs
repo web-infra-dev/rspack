0a964ca42194a9322df3b777fbd55a6f18f17d6f
hardfist <yangjianzju@gmail.com>
2022-09-27 18:28:36 +0800

    chore: disable event tracing

--- crates/rspack_core/src/utils/log.rs
+++ crates/rspack_core/src/utils/log.rs
@@ -2,10 +2,21 @@ use std::sync::atomic::AtomicBool;
 
 use tracing::Level;
 use tracing_chrome::FlushGuard;
+use tracing_subscriber::layer::Filter;
 // use tracing_chrome::FlushGuard;
 
 static IS_TRACING_ENABLED: AtomicBool = AtomicBool::new(false);
-
+// skip event because it's not useful for performance analysis
+struct FilterEvent {}
+impl<S> Filter<S> for FilterEvent {
+  fn enabled(
+    &self,
+    meta: &tracing::Metadata<'_>,
+    _cx: &tracing_subscriber::layer::Context<'_, S>,
+  ) -> bool {
+    !meta.is_event()
+  }
+}
 pub fn enable_tracing_by_env() {
   let is_enable_tracing = std::env::var("TRACE").map_or(false, |x| {
     matches!(x.as_str(), "TRACE" | "DEBUG" | "INFO" | "WARN" | "ERROR")
@@ -41,7 +52,7 @@ pub fn enable_tracing_by_env_with_chrome_layer() -> Option<FlushGuard> {
     // If we don't do this. chrome_layer will collect nothing.
     // std::mem::forget(guard);
     tracing_subscriber::registry()
-      .with(chrome_layer)
+      .with(chrome_layer.with_filter(FilterEvent {}))
       .with(
         tracing_subscriber::filter::Targets::new().with_targets(vec![
           ("rspack_core", Level::TRACE),
