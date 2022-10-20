7428baf27bf41b2bfb3af089f972f53e2e867add
likui <likui.underfin@bytedance.com>
2022-10-20 16:34:46 +0800

    refactor: move define to builtins(close: #867)

--- crates/rspack_core/src/options/builtins.rs
+++ crates/rspack_core/src/options/builtins.rs
@@ -1,6 +1,11 @@
+use std::collections::HashMap;
+
+pub type Define = HashMap<String, String>;
+
 #[derive(Debug, Clone, Default)]
 pub struct Builtins {
   pub minify: bool,
   pub polyfill: bool,
   pub browserslist: Vec<String>,
+  pub define: Define,
 }
