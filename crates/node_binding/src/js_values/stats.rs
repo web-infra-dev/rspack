web-infra-dev/rspack

commit 292f79a27d0c3e539000094b6b388504e1501f74

feat: stats fields for bundle-analyzer (#886)
* feat: stats fields for bundle-analyzer

* rebase test snapshot path

* delete slate comments

crates/node_binding/src/js_values/stats.rs
@@ -26,26 +26,96 @@ pub struct DiffStat {
 }
 
 #[napi(object)]
-pub struct RspackError {
+pub struct StatsError {
   pub message: String,
 }
 
-#[napi(object)]
-pub struct Stats {
-  pub errors: Vec<RspackError>,
+impl From<rspack_core::StatsError> for StatsError {
+  fn from(stats: rspack_core::StatsError) -> Self {
+    Self {
+      message: stats.message,
+    }
+  }
 }
 
-impl<'a> From<rspack_core::Stats<'a>> for Stats {
-  fn from(rspack_stats: rspack_core::Stats) -> Self {
+#[napi(object)]
+pub struct StatsAsset {
+  pub r#type: &'static str,
+  pub name: String,
+  pub size: f64,
+  pub chunks: Vec<String>,
+}
+
+impl From<rspack_core::StatsAsset> for StatsAsset {
+  fn from(stats: rspack_core::StatsAsset) -> Self {
     Self {
-      errors: rspack_stats
-        .compilation
-        .diagnostic
-        .iter()
-        .map(|d| RspackError {
-          message: d.message.clone(),
-        })
-        .collect(),
+      r#type: stats.r#type,
+      name: stats.name,
+      size: stats.size,
+      chunks: stats.chunks,
+    }
+  }
+}
+
+#[napi(object)]
+pub struct StatsModule {
+  pub r#type: &'static str,
+  pub module_type: String,
+  pub identifier: String,
+  pub name: String,
+  pub id: String,
+  pub chunks: Vec<String>,
+  pub size: f64,
+}
+
+impl From<rspack_core::StatsModule> for StatsModule {
+  fn from(stats: rspack_core::StatsModule) -> Self {
+    Self {
+      r#type: stats.r#type,
+      name: stats.name,
+      size: stats.size,
+      chunks: stats.chunks,
+      module_type: stats.module_type.to_string(),
+      identifier: stats.identifier,
+      id: stats.id,
+    }
+  }
+}
+
+#[napi(object)]
+pub struct StatsChunk {
+  pub r#type: &'static str,
+  pub files: Vec<String>,
+  pub id: String,
+}
+
+impl From<rspack_core::StatsChunk> for StatsChunk {
+  fn from(stats: rspack_core::StatsChunk) -> Self {
+    Self {
+      r#type: stats.r#type,
+      files: Vec::from_iter(stats.files.into_iter()),
+      id: stats.id,
+    }
+  }
+}
+
+#[napi(object)]
+pub struct StatsCompilation {
+  pub assets: Vec<StatsAsset>,
+  pub modules: Vec<StatsModule>,
+  pub chunks: Vec<StatsChunk>,
+  pub errors: Vec<StatsError>,
+  pub errors_count: u32,
+}
+
+impl From<rspack_core::StatsCompilation> for StatsCompilation {
+  fn from(stats: rspack_core::StatsCompilation) -> Self {
+    Self {
+      assets: stats.assets.into_iter().map(Into::into).collect(),
+      modules: stats.modules.into_iter().map(Into::into).collect(),
+      chunks: stats.chunks.into_iter().map(Into::into).collect(),
+      errors: stats.errors.into_iter().map(Into::into).collect(),
+      errors_count: stats.errors_count as u32,
     }
   }
 }

crates/rspack_core/src/stats.rs
@@ -1,10 +1,11 @@
-use crate::{Compilation, CompilationAssets, PATH_START_BYTE_POS_MAP};
+use crate::{Chunk, Compilation, ModuleType, SourceType, PATH_START_BYTE_POS_MAP};
+use hashbrown::{HashMap, HashSet};
 use rspack_error::{
   emitter::{DiagnosticDisplay, StdioDiagnosticDisplay, StringDiagnosticDisplay},
   Result,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Stats<'compilation> {
   pub compilation: &'compilation Compilation,
 }
@@ -32,38 +33,155 @@ impl<'compilation> Stats<'compilation> {
 }
 
 impl<'compilation> Stats<'compilation> {
-  // This function is only used for tests compatible.
-  pub fn __should_only_used_in_tests_assets(&self) -> &CompilationAssets {
-    &self.compilation.assets
-  }
+  pub fn to_description(&self) -> StatsCompilation {
+    let mut compilation_file_to_chunks: HashMap<&String, Vec<&Chunk>> = HashMap::new();
+    for (_, chunk) in &self.compilation.chunk_by_ukey {
+      for file in &chunk.files {
+        // TODO: avoid runtime.js in every chunk.files, delete this once runtime refacted.
+        if file == "runtime.js" {
+          continue;
+        }
+        let chunks = compilation_file_to_chunks.entry(file).or_default();
+        chunks.push(chunk);
+      }
+    }
+    let mut assets: HashMap<&String, StatsAsset> =
+      HashMap::from_iter(self.compilation.assets.iter().map(|(name, asset)| {
+        (
+          name,
+          StatsAsset {
+            r#type: "asset",
+            name: name.clone(),
+            size: asset.get_source().size() as f64,
+            chunks: Vec::new(),
+          },
+        )
+      }));
+    for (_, asset) in &self.compilation.assets {
+      if let Some(source_map) = &asset.get_info().related.source_map {
+        assets.remove(source_map);
+      }
+    }
+    for (name, asset) in &mut assets {
+      if let Some(chunks) = compilation_file_to_chunks.get(name) {
+        asset.chunks = chunks.iter().map(|chunk| chunk.id.clone()).collect();
+        asset.chunks.sort();
+      }
+    }
+    let mut assets: Vec<StatsAsset> = assets.into_values().collect();
+    assets.sort_by(|a, b| {
+      // SAFETY: size should not be NAN.
+      b.size.partial_cmp(&a.size).unwrap()
+    });
 
-  pub fn to_description(self) -> StatsDescription {
-    StatsDescription {
-      assets: self
+    let mut modules: Vec<StatsModule> = self
+      .compilation
+      .module_graph
+      .modules()
+      .map(|module| {
+        let identifier = module.identifier();
+        let mgm = self
+          .compilation
+          .module_graph
+          .module_graph_module_by_identifier(&identifier)
+          .unwrap();
+        let mut chunks: Vec<String> = self
+          .compilation
+          .chunk_graph
+          .get_chunk_graph_module(&mgm.uri)
+          .chunks
+          .iter()
+          .map(|k| self.compilation.chunk_by_ukey.get(k).unwrap().id.clone())
+          .collect();
+        chunks.sort();
+        StatsModule {
+          r#type: "module",
+          module_type: module.module_type(),
+          identifier,
+          name: module.identifier(), // TODO: short it with requestShortener
+          id: mgm.id.clone(),
+          chunks,
+          size: module.size(&SourceType::JavaScript),
+        }
+      })
+      .collect();
+    modules.sort_by_cached_key(|m| m.identifier.to_string()); // TODO: sort by module.depth
+
+    let errors: Vec<StatsError> = self
+      .compilation
+      .get_errors()
+      .map(|d| StatsError {
+        message: d.message.clone(),
+      })
+      .collect();
+
+    StatsCompilation {
+      assets,
+      modules,
+      chunks: self
         .compilation
-        .assets
-        .iter()
-        .map(|(filename, _asset)| StatsAsset {
-          name: filename.clone(),
+        .chunk_by_ukey
+        .values()
+        .into_iter()
+        .map(|c| StatsChunk {
+          r#type: "chunk",
+          files: c.files.clone(),
+          id: c.id.clone(),
         })
         .collect(),
+      errors_count: errors.len(),
+      errors,
     }
   }
 }
 
-pub struct StatsDescription {
+#[derive(Debug)]
+pub struct StatsCompilation {
   pub assets: Vec<StatsAsset>,
+  pub modules: Vec<StatsModule>,
+  pub chunks: Vec<StatsChunk>,
+  pub errors: Vec<StatsError>,
+  pub errors_count: usize,
   // pub entrypoints: HashMap<String, StatsEntrypoint>,
 }
 
-pub struct StatsAsset {
-  pub name: String,
+#[derive(Debug)]
+pub struct StatsError {
+  pub message: String,
 }
 
+#[derive(Debug)]
+pub struct StatsAsset {
+  pub r#type: &'static str,
+  pub name: String,
+  pub size: f64,
+  pub chunks: Vec<String>,
+}
+
+#[derive(Debug)]
+pub struct StatsModule {
+  pub r#type: &'static str,
+  pub module_type: ModuleType,
+  pub identifier: String,
+  pub name: String,
+  pub id: String,
+  pub chunks: Vec<String>,
+  pub size: f64,
+}
+
+#[derive(Debug)]
+pub struct StatsChunk {
+  pub r#type: &'static str,
+  pub files: HashSet<String>,
+  pub id: String,
+}
+
+#[derive(Debug)]
 pub struct StatsAssetReference {
   pub name: String,
 }
 
+#[derive(Debug)]
 pub struct StatsEntrypoint {
   pub name: String,
   pub assets: Vec<StatsAssetReference>,

crates/node_binding/src/lib.rs
@@ -75,7 +75,7 @@ impl Rspack {
     })
   }
 
-  #[napi(ts_return_type = "Promise<Stats>")]
+  #[napi(ts_return_type = "Promise<StatsCompilation>")]
   pub fn build(&self, env: Env) -> Result<JsObject> {
     let inner = self.inner.clone();
     env.execute_tokio_future(
@@ -87,7 +87,7 @@ impl Rspack {
           .await
           .map_err(|e| Error::new(napi::Status::GenericFailure, format!("{:?}", e)))?;
 
-        let stats: Stats = rspack_stats.into();
+        let stats: StatsCompilation = rspack_stats.to_description().into();
         if stats.errors.is_empty() {
           tracing::info!("build success");
         } else {

crates/node_binding/src/plugins/mod.rs
@@ -9,11 +9,12 @@ use serde::{Deserialize, Serialize};
 
 use rspack_core::rspack_sources::{RawSource, SourceExt};
 use rspack_core::{
-  Compilation, Plugin, PluginBuildEndHookOutput, PluginContext, PluginProcessAssetsHookOutput,
-  ProcessAssetsArgs,
+  Compilation, DoneArgs, Plugin, PluginBuildEndHookOutput, PluginContext,
+  PluginProcessAssetsHookOutput, ProcessAssetsArgs,
 };
 use rspack_error::Error;
 
+use crate::js_values::StatsCompilation;
 use crate::threadsafe_function::{ThreadsafeFunction, ThreadsafeFunctionCallMode};
 use crate::{AssetContent, UpdateAssetOptions};
 
@@ -23,7 +24,7 @@ pub use utils::*;
 pub type BoxedClosure = Box<dyn Fn(CallContext<'_>) -> napi::Result<JsUndefined>>;
 
 pub struct RspackPluginNodeAdapter {
-  pub done_tsfn: ThreadsafeFunction<(), ()>,
+  pub done_tsfn: ThreadsafeFunction<StatsCompilation, ()>,
   pub process_assets_tsfn: ThreadsafeFunction<(String, BoxedClosure), ()>,
 }
 
@@ -99,10 +100,17 @@ impl Plugin for RspackPluginNodeAdapter {
   }
 
   #[tracing::instrument(skip_all)]
-  async fn done(&mut self) -> PluginBuildEndHookOutput {
+  async fn done<'s, 'c>(
+    &mut self,
+    _ctx: PluginContext,
+    args: DoneArgs<'s, 'c>,
+  ) -> PluginBuildEndHookOutput {
     self
       .done_tsfn
-      .call((), ThreadsafeFunctionCallMode::Blocking)
+      .call(
+        args.stats.to_description().into(),
+        ThreadsafeFunctionCallMode::Blocking,
+      )
       .map_err(Error::from)?
       .await
       .map_err(|err| Error::InternalError(format!("{:?}", err)))?;

crates/node_binding/src/plugins/utils.rs
@@ -1,6 +1,6 @@
-use napi::{bindgen_prelude::*, Env, NapiRaw};
+use napi::{bindgen_prelude::*, Env, JsObject, NapiRaw, NapiValue};
 
-use crate::{BoxedClosure, PluginCallbacks};
+use crate::{js_values::StatsCompilation, BoxedClosure, PluginCallbacks};
 
 pub fn create_node_adapter_from_plugin_callbacks(
   env: Env,
@@ -12,12 +12,17 @@ pub fn create_node_adapter_from_plugin_callbacks(
          done_callback,
          process_assets_callback,
        }| {
-        let done_tsfn: crate::threadsafe_function::ThreadsafeFunction<(), ()> = {
+        let done_tsfn: crate::threadsafe_function::ThreadsafeFunction<StatsCompilation, ()> = {
           let cb = unsafe { done_callback.raw() };
 
           crate::threadsafe_function::ThreadsafeFunction::create(env.raw(), cb, 0, |ctx| {
             let (ctx, resolver) = ctx.split_into_parts();
-            let result = ctx.callback.call_without_args(None)?;
+            let stats_json = unsafe {
+              let raw =
+                napi::bindgen_prelude::ToNapiValue::to_napi_value(ctx.env.raw(), ctx.value)?;
+              JsObject::from_raw_unchecked(ctx.env.raw(), raw)
+            };
+            let result = ctx.callback.call(None, &[stats_json])?;
 
             resolver.resolve::<()>(result, |_| Ok(()))
           })

crates/rspack_core/src/chunk_graph.rs
@@ -53,6 +53,13 @@ impl ChunkGraph {
       .expect("Module should be added before")
   }
 
+  pub(crate) fn get_chunk_graph_module(&self, module_uri: &str) -> &ChunkGraphModule {
+    self
+      .chunk_graph_module_by_module_url
+      .get(module_uri)
+      .expect("Module should be added before")
+  }
+
   pub(crate) fn get_chunk_graph_chunk_mut(
     &mut self,
     chunk_ukey: ChunkUkey,

crates/rspack_core/src/compiler/compilation.rs
@@ -9,7 +9,7 @@ use hashbrown::HashMap;
 use tokio::sync::mpsc::UnboundedSender;
 use tracing::instrument;
 
-use rspack_error::{Diagnostic, IntoTWithDiagnosticArray, Result};
+use rspack_error::{Diagnostic, IntoTWithDiagnosticArray, Result, Severity};
 use rspack_sources::BoxSource;
 
 use crate::{
@@ -17,7 +17,7 @@ use crate::{
   ChunkGroupUkey, ChunkKind, ChunkUkey, CompilerOptions, Dependency, EntryItem, Entrypoint,
   LoaderRunnerRunner, ModuleDependency, ModuleGraph, ModuleIdentifier, ModuleRule, Msg,
   NormalModule, NormalModuleFactory, NormalModuleFactoryContext, ProcessAssetsArgs,
-  RenderManifestArgs, RenderRuntimeArgs, ResolveKind, Runtime, SharedPluginDriver,
+  RenderManifestArgs, RenderRuntimeArgs, ResolveKind, Runtime, SharedPluginDriver, Stats,
   VisitedModuleIdentity,
 };
 
@@ -84,6 +84,13 @@ impl Compilation {
 
   pub fn push_batch_diagnostic(&mut self, mut diagnostic: Vec<Diagnostic>) {
     self.diagnostic.append(&mut diagnostic);
+  }
+
+  pub fn get_errors(&self) -> impl Iterator<Item = &Diagnostic> {
+    self
+      .diagnostic
+      .iter()
+      .filter(|d| matches!(d.severity, Severity::Error))
   }
 
   pub(crate) fn add_chunk(
@@ -452,7 +459,8 @@ impl Compilation {
       .ok();
   }
   pub async fn done(&mut self, plugin_driver: SharedPluginDriver) -> Result<()> {
-    plugin_driver.write().await.done().await?;
+    let stats = &mut Stats::new(self);
+    plugin_driver.write().await.done(stats).await?;
     Ok(())
   }
   #[instrument(name = "compilation:render_runtime")]

crates/rspack_core/src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(iter_intersperse)]
 
-use std::sync::Arc;
+use std::{fmt, sync::Arc};
 
 use dashmap::DashSet;
 use hashbrown::HashMap;
@@ -86,6 +86,27 @@ impl ModuleType {
   }
   pub fn is_jsx_like(&self) -> bool {
     matches!(self, ModuleType::Tsx | ModuleType::Jsx)
+  }
+}
+
+impl fmt::Display for ModuleType {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(
+      f,
+      "{}",
+      match self {
+        ModuleType::Js => "js",
+        ModuleType::Jsx => "jsx",
+        ModuleType::Ts => "ts",
+        ModuleType::Tsx => "tsx",
+        ModuleType::Css => "css",
+        ModuleType::Json => "json",
+        ModuleType::Asset => "asset",
+        ModuleType::AssetSource => "asset/source",
+        ModuleType::AssetResource => "asset/resource",
+        ModuleType::AssetInline => "asset/inline",
+      }
+    )
   }
 }
 

commit 9ad9aaba4cd006403e04af2c8b8b255575204e84

fix: broken ci caused by #886

crates/node_binding/src/js_values/stats.rs
@@ -93,7 +93,7 @@ impl From<rspack_core::StatsChunk> for StatsChunk {
   fn from(stats: rspack_core::StatsChunk) -> Self {
     Self {
       r#type: stats.r#type,
-      files: Vec::from_iter(stats.files.into_iter()),
+      files: stats.files,
       id: stats.id,
     }
   }

crates/rspack_core/src/stats.rs
@@ -1,5 +1,5 @@
 use crate::{Chunk, Compilation, ModuleType, SourceType, PATH_START_BYTE_POS_MAP};
-use hashbrown::{HashMap, HashSet};
+use hashbrown::HashMap;
 use rspack_error::{
   emitter::{DiagnosticDisplay, StdioDiagnosticDisplay, StringDiagnosticDisplay},
   Result,
@@ -123,10 +123,14 @@ impl<'compilation> Stats<'compilation> {
         .chunk_by_ukey
         .values()
         .into_iter()
-        .map(|c| StatsChunk {
-          r#type: "chunk",
-          files: c.files.clone(),
-          id: c.id.clone(),
+        .map(|c| {
+          let mut files = Vec::from_iter(c.files.iter().cloned());
+          files.sort();
+          StatsChunk {
+            r#type: "chunk",
+            files,
+            id: c.id.clone(),
+          }
         })
         .collect(),
       errors_count: errors.len(),
@@ -172,7 +176,7 @@ pub struct StatsModule {
 #[derive(Debug)]
 pub struct StatsChunk {
   pub r#type: &'static str,
-  pub files: HashSet<String>,
+  pub files: Vec<String>,
   pub id: String,
 }
 