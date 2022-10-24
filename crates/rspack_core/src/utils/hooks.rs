1bf0cb4e3d17d47a9653edbc3cc6f32ebf2e9258
bvanjoi <bohan-zhang@foxmail.com>
2022-10-24 11:44:57 +0800

    refactor(core): remove `parse_to_url` in factorize

--- crates/rspack_core/src/utils/hooks.rs
+++ crates/rspack_core/src/utils/hooks.rs
@@ -8,7 +8,7 @@ pub async fn resolve(
   args: ResolveArgs<'_>,
   plugin_driver: &SharedPluginDriver,
   _job_context: &mut NormalModuleFactoryContext,
-) -> Result<String> {
+) -> Result<ResolveResult> {
   let plugin_driver = plugin_driver.read().await;
   let base_dir = if let Some(importer) = args.importer {
     Path::new(importer)
@@ -17,43 +17,38 @@ pub async fn resolve(
   } else {
     Path::new(plugin_driver.options.context.as_str())
   };
-  Ok({
-    tracing::trace!(
-      "resolved importer:{:?},specifier:{:?}",
-      args.importer,
-      args.specifier
-    );
-    match plugin_driver
-      .resolver
-      .resolve(base_dir, args.specifier)
-      .map_err(|error| {
-        let is_overflow = matches!(error, Error::InternalError(_));
-        let is_failed_tag = matches!(error, Error::BatchErrors(_));
-        if !is_failed_tag && !is_overflow {
-          error
-        } else if let Some(importer) = args.importer {
-          let span = args.span.unwrap_or_default();
-          let message = if is_overflow {
-            format!(
-              "Can't resolve {:?}, maybe it had cycle alias",
-              args.specifier
-            )
-          } else {
-            format!("Failed to resolve {}", args.specifier)
-          };
-          Error::TraceableError(TraceableError::from_path(
-            importer.to_string(),
-            span.start as usize,
-            span.end as usize,
-            "Resolve error".to_string(),
-            message,
-          ))
+  tracing::trace!(
+    "resolved importer:{:?},specifier:{:?}",
+    args.importer,
+    args.specifier
+  );
+  plugin_driver
+    .resolver
+    .resolve(base_dir, args.specifier)
+    .map_err(|error| {
+      let is_overflow = matches!(error, Error::InternalError(_));
+      let is_failed_tag = matches!(error, Error::BatchErrors(_));
+      if !is_failed_tag && !is_overflow {
+        error
+      } else if let Some(importer) = args.importer {
+        let span = args.span.unwrap_or_default();
+        let message = if is_overflow {
+          format!(
+            "Can't resolve {:?}, maybe it had cycle alias",
+            args.specifier
+          )
         } else {
-          error
-        }
-      })? {
-      ResolveResult::Info(info) => info.path.to_string_lossy().to_string(),
-      ResolveResult::Ignored => format!("UnReachable:{}", args.specifier),
-    }
-  })
+          format!("Failed to resolve {}", args.specifier)
+        };
+        Error::TraceableError(TraceableError::from_path(
+          importer.to_string(),
+          span.start as usize,
+          span.end as usize,
+          "Resolve error".to_string(),
+          message,
+        ))
+      } else {
+        error
+      }
+    })
 }
