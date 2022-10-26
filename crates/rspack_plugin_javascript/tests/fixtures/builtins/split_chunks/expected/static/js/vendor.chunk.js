261b77c5da847c448d3b8505a31beb468ede8a7a
YunfeiHe <i.heyunfei@gmail.com>
2022-10-26 15:55:07 +0800

    Fix test

--- crates/rspack_plugin_javascript/tests/fixtures/builtins/split_chunks/expected/static/js/vendor.chunk.js
+++ crates/rspack_plugin_javascript/tests/fixtures/builtins/split_chunks/expected/static/js/vendor.chunk.js
@@ -1,5 +1,5 @@
 self["__rspack_runtime__"].__rspack_register__(["vendor"], {
-"./foo.js": function (module, exports, __rspack_require__, __rspack_dynamic_require__) {
+"./foo.js": function (module, exports, __rspack_require__, __rspack_dynamic_require__, __rspack_runtime__) {
 "use strict";
 console.log('foo.js');
 },
