e0506e929c43846a9e26c1fc1c3f5746fe66ed73
jinrui <jerrykingxyz@gmail.com>
2022-10-27 11:50:39 +0800

    fix: export star helper bug (#908)
    * fix: export star helper bug
    
    * fix: ci
    
    * fix: cr
    
    Co-authored-by: h-a-n-a <andywangsy@gmail.com>

---
+++ crates/rspack_plugin_javascript/src/runtime/helpers/_export_star.js
@@ -0,0 +1,14 @@
+(function () {
+	runtime.exportStar = function (from, to) {
+		Object.keys(from).forEach(function (k) {
+			if (k !== "default" && !Object.prototype.hasOwnProperty.call(to, k))
+				Object.defineProperty(to, k, {
+					enumerable: true,
+					get: function () {
+						return from[k];
+					}
+				});
+		});
+		return from;
+	};
+})();
--- crates/rspack_plugin_javascript/src/visitors/clear_mark.rs
+++ crates/rspack_plugin_javascript/src/visitors/clear_mark.rs
@@ -1,11 +1,12 @@
 use swc_common::SyntaxContext;
-use {
-  swc_ecma_ast::Ident,
-  swc_ecma_visit::{noop_visit_mut_type, VisitMut},
-};
+use swc_ecma_ast::Ident;
+use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut};
 
-#[derive(Clone, Copy)]
-pub struct ClearMark;
+pub fn clear_mark() -> impl Fold {
+  as_folder(ClearMark {})
+}
+
+struct ClearMark;
 impl VisitMut for ClearMark {
   noop_visit_mut_type!();
 
