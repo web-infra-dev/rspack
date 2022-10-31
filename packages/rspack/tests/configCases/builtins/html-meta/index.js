8325d66f7e65bb322c32dee37b39bd4c9fce79c1
likui <likui.underfin@bytedance.com>
2022-10-31 20:30:50 +0800

    fix: fix test

--- packages/rspack/tests/configCases/builtins/html-meta/index.js
+++ packages/rspack/tests/configCases/builtins/html-meta/index.js
@@ -1,14 +1,7 @@
-const fs = require("fs");
-
 function test() {
 	return 123;
 }
 
 it("basic", () => {
 	expect(test()).toBe(123);
-});
-
-it("format", () => {
-	const content = fs.readFileSync(__filename, "utf-8");
-	expect(content).not.toMatch("\n");
 });
