6532e43bbfd76ac266153dd0b5c47ba05bbb876f
likui <likui.underfin@bytedance.com>
2022-10-31 21:20:52 +0800

    fix: fix test

--- packages/rspack/tests/configCases/builtins/html-meta/index.js
+++ packages/rspack/tests/configCases/builtins/html-meta/index.js
@@ -3,7 +3,11 @@ const path = require("path");
 
 it("html meta", () => {
 	const htmlPath = path.join(__dirname, "./index.html");
-	expect(fs.readFileSync(htmlPath, "utf-8")).toMatch(
-		'<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
-	);
+	expect(
+		fs
+			.readFileSync(htmlPath, "utf-8")
+			.includes(
+				'<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
+			)
+	).toBe(true);
 });
