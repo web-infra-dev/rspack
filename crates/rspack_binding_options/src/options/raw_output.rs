aaf702e0afcb973dc4b7e9240ff6cf2e6f7bab28
YunfeiHe <i.heyunfei@gmail.com>
2022-10-31 15:49:55 +0800

    feat: Supports output#chunkFilename

--- crates/rspack_binding_options/src/options/raw_output.rs
+++ crates/rspack_binding_options/src/options/raw_output.rs
@@ -59,23 +59,39 @@ pub struct RawOutputOptions {
 
 impl RawOption<OutputOptions> for RawOutputOptions {
   fn to_compiler_option(self, options: &CompilerOptionsBuilder) -> anyhow::Result<OutputOptions> {
-    // let is_prod = matches!(mode, Mode::Production);
-    let filename = self.filename.unwrap_or(format!(
-      "{}{}{}",
-      NAME_PLACEHOLDER,
-      // todo need add hash
-      // if is_prod {
-      //   CONTENT_PLACEHOLDER
-      // } else {
-      //   HASH_PLACEHOLDER
-      // },
-      "",
-      EXT_PLACEHOLDER
-    ));
+    // Align with https://github.com/webpack/webpack/blob/9fcaa243573005d6fdece9a3f8d89a0e8b399613/lib/config/defaults.js#L750
+    let filename = self
+      .filename
+      .unwrap_or(format!("{}{}", NAME_PLACEHOLDER, ".js",));
 
-    let chunk_filename = self
-      .chunk_filename
-      .unwrap_or_else(|| filename.replace(NAME_PLACEHOLDER, &format!("{}.chunk", ID_PLACEHOLDER)));
+    let chunk_filename = self.chunk_filename.unwrap_or_else(|| {
+      // Align with https://github.com/webpack/webpack/blob/9fcaa243573005d6fdece9a3f8d89a0e8b399613/lib/config/defaults.js#L754
+      let has_name = filename.contains(NAME_PLACEHOLDER);
+      let has_id = filename.contains(ID_PLACEHOLDER);
+      let has_chunk_hash = filename.contains("[chunkhash]");
+      let hash_content_hash = filename.contains("[contenthash]");
+      if has_name || has_id || has_chunk_hash || hash_content_hash {
+        filename.clone()
+      } else {
+        let re = regex::Regex::new(r#"(^|\/)([^/]*(?:\?|$))"#).unwrap();
+        let captures = re.captures(&filename);
+        format!(
+          "{}[id].{}",
+          captures
+            .as_ref()
+            .unwrap()
+            .get(1)
+            .map(|m| m.as_str())
+            .unwrap_or(""),
+          captures
+            .as_ref()
+            .unwrap()
+            .get(2)
+            .map(|m| m.as_str())
+            .unwrap_or(""),
+        )
+      }
+    });
     let path = generate_path(
       self.path,
       options.context.as_ref().context("should have context")?,
