5161bd69bbe2cbb37349838c3bd7564fb27d29ea
bohanzh <bohan-zhang@foxmail.com>
2022-11-01 11:29:51 +0800

    Bench entry config (#977)
    bench: modify bench entry to array

--- benchcases/lodash-with-simple-css/test.config.js
+++ benchcases/lodash-with-simple-css/test.config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index.js",
-  },
-  enhanced: {},
-  builtins: {
-    css: {
-      presetEnv: ["chrome >= 40", "firefox > 10"],
-    },
-    html: [
-      {
-        template: "index.html",
-        sri: "sha384",
-        filename: "chunk1.html",
-      },
-      {
-        template: "index.html",
-        sri: "sha256",
-        filename: "chunk2.html",
-      },
-      {
-        template: "index.html",
-        sri: "sha512",
-        filename: "chunk3.html",
-      },
-    ],
-  },
+	mode: "development",
+	entry: {
+		index: ["./src/index.js"]
+	},
+	enhanced: {},
+	builtins: {
+		css: {
+			presetEnv: ["chrome >= 40", "firefox > 10"]
+		},
+		html: [
+			{
+				template: "index.html",
+				sri: "sha384",
+				filename: "chunk1.html"
+			},
+			{
+				template: "index.html",
+				sri: "sha256",
+				filename: "chunk2.html"
+			},
+			{
+				template: "index.html",
+				sri: "sha512",
+				filename: "chunk3.html"
+			}
+		]
+	}
 };
