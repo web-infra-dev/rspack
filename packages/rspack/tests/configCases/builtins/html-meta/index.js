b11b7b031dc80ac371c432cfeda40be5a9d17938
likui <likui.underfin@bytedance.com>
2022-11-01 11:24:19 +0800

    fix: fix test

--- packages/rspack/tests/configCases/builtins/html-meta/index.js
+++ packages/rspack/tests/configCases/builtins/html-meta/index.js
@@ -7,7 +7,7 @@ it("html meta", () => {
 		fs
 			.readFileSync(htmlPath, "utf-8")
 			.includes(
-				'<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
+				'<meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport" />'
 			)
 	).toBe(true);
 });
