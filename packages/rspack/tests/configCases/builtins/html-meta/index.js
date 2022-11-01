a62ba9a799823c815ca3f672376431cfd895556c
likui <likui.underfin@bytedance.com>
2022-11-01 17:21:58 +0800

    chore: remove console

--- packages/rspack/tests/configCases/builtins/html-meta/index.js
+++ packages/rspack/tests/configCases/builtins/html-meta/index.js
@@ -4,7 +4,6 @@ const path = require("path");
 it("html meta", () => {
 	const htmlPath = path.join(__dirname, "./index.html");
 	const htmlContent = fs.readFileSync(htmlPath, "utf-8");
-	console.log(htmlContent);
 	expect(
 		htmlContent.includes(
 			'<meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport" />'
