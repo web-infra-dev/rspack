af8c7b783fd30af1276697a31011eabe3289f4ad
h-a-n-a <andywangsy@gmail.com>
2022-11-02 02:32:59 +0800

    chore: polish comment

--- crates/node_binding/src/plugins/utils.rs
+++ crates/node_binding/src/plugins/utils.rs
@@ -21,7 +21,8 @@ pub fn create_node_adapter_from_plugin_callbacks(
         // For example:
         // tsfn1: [call-in-js-task1, call-in-js-task2]
         // tsfn2: [call-in-js-task3, call-in-js-task4]
-        // If the tsfn1 is created before tsfn2, and task1 is created before task2(single tsfn level), tasks will be called on main thread in the order of `task1` `task2` `task3` `task4`
+        // If the tsfn1 is created before tsfn2, and task1 is created(via `tsfn.call`) before task2(single tsfn level),
+        // and *if these tasks are created in the same tick*, tasks will be called on main thread in the order of `task1` `task2` `task3` `task4`
         //
         // In practice:
         // The creation of callback `this_compilation` is placed before the callback `compilation` because we want the JS hooks `this_compilation` to be called before the JS hooks `compilation`.
