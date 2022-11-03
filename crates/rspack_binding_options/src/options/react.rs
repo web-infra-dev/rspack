adc272369dff0dda2b62352210a0b696da465a52
bvanjoi <bohan-zhang@foxmail.com>
2022-11-03 17:35:13 +0800

    test: browserField

--- crates/rspack_binding_options/src/options/react.rs
+++
@@ -1,9 +0,0 @@
-use napi_derive::napi;
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug, Default)]
-#[serde(rename_all = "camelCase")]
-#[napi(object)]
-pub struct RawReactOptions {
-  pub fast_refresh: Option<bool>,
-}
--- packages/rspack-dev-server/tests/normalizeOptions.test.ts
+++ packages/rspack-dev-server/tests/normalizeOptions.test.ts
@@ -2,7 +2,7 @@ import type { RspackOptions } from "@rspack/core";
 import { RspackDevServer } from "@rspack/dev-server";
 import { createCompiler } from "@rspack/core";
 import serializer from "jest-serializer-path";
-
+import os from "os";
 expect.addSnapshotSerializer(serializer);
 
 describe("normalize options snapshot", () => {
@@ -23,6 +23,17 @@ describe("normalize options snapshot", () => {
 			entry: ["something"]
 		});
 	});
+
+	it("react-refresh added when react/refresh enabled", () => {
+		matchAdditionEntries({
+			entry: ["something"],
+			builtins: {
+				react: {
+					refresh: true
+				}
+			}
+		});
+	});
 });
 
 function match(config: RspackOptions) {
@@ -34,5 +45,16 @@ function match(config: RspackOptions) {
 function matchAdditionEntries(config: RspackOptions) {
 	const compiler = createCompiler(config);
 	const server = new RspackDevServer(compiler);
-	expect(compiler.options.entry).toMatchSnapshot();
+	const entires = Object.entries(compiler.options.entry);
+	// some hack for snapshot
+	const value = Object.fromEntries(
+		entires.map(([key, item]) => {
+			const replaced = item.map(entry => {
+				const array = entry.replace(/\\/g, "/").split("/");
+				return "<prefix>" + "/" + array.slice(-3).join("/");
+			});
+			return [key, replaced];
+		})
+	);
+	expect(value).toMatchSnapshot();
 }
