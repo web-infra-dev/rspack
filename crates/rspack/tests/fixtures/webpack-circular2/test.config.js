c3060df722866c1c5a4c31c8b288a99017924513
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-11-03 20:27:54 +0800

    test: 💍 finish circular2

---
+++ crates/rspack/tests/fixtures/webpack-circular2/test.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import('webpack').Configuration}
+ */
+module.exports = {
+	mode: "development",
+	entry: {
+		main: ["./index.js"],
+	},
+	define: {
+		"process.env.NODE_ENV": "'development'",
+	},
+	builtins: {
+		treeShaking: true,
+	},
+};
