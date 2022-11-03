74bbf1767833c287806caaec22c84de7a6eeb78e
ahabhgk <ahabhgk@gmail.com>
2022-11-03 11:20:28 +0800

    fmt

--- packages/rspack/src/config/stats.ts
+++ packages/rspack/src/config/stats.ts
@@ -3,20 +3,20 @@ import * as binding from "@rspack/binding";
 export type ResolvedStatsOptions = binding.RawStatsOptions;
 
 export interface StatsOptions {
-  colors?: boolean;
+	colors?: boolean;
 }
 
 export function resolveStatsOptions(
-  options: StatsOptions = {}
+	options: StatsOptions = {}
 ): ResolvedStatsOptions {
-  const colors = optionsOrFallback(options.colors, false);
-  return {
-    colors
-  };
+	const colors = optionsOrFallback(options.colors, false);
+	return {
+		colors
+	};
 }
 
 const optionsOrFallback = (...args) => {
-  let optionValues = [];
-  optionValues.push(...args);
-  return optionValues.find(optionValue => optionValue !== undefined);
+	let optionValues = [];
+	optionValues.push(...args);
+	return optionValues.find(optionValue => optionValue !== undefined);
 };
