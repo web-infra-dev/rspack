b172703d529cee87bc29326db243f1570d742919
hardfist <1562502418@qq.com>
2022-11-05 20:23:24 +0800

    chore: fix snapshot

--- crates/cargo-rst/src/rst.rs
+++ crates/cargo-rst/src/rst.rs
@@ -436,7 +436,7 @@ impl Rst {
             FailedCase::MissingActualFile(_) => unreachable!(),
             FailedCase::MissingExpectedDir(dir) => {
               // Expected dir should not exist
-              fs::remove_dir_all(&dir).expect("Remove dir failed");
+              fs::remove_dir_all(&dir).expect(&format!("Remove {:?} dir failed", dir));
             }
             FailedCase::MissingExpectedFile(file) => {
               // Expected file should not exist
