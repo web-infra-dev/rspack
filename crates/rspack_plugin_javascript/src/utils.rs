47ef98d4b0d916c034bfabfa3cc35ef725ec3e55
likui <likui.underfin@bytedance.com>
2022-11-14 20:31:26 +0800

    fix: add cached sources

--- crates/rspack_plugin_javascript/src/utils.rs
+++ crates/rspack_plugin_javascript/src/utils.rs
@@ -4,7 +4,7 @@ use hashbrown::hash_map::DefaultHashBuilder;
 use once_cell::sync::Lazy;
 use pathdiff::diff_paths;
 use rspack_core::rspack_sources::{
-  BoxSource, ConcatSource, MapOptions, RawSource, Source, SourceExt,
+  BoxSource, CachedSource, ConcatSource, MapOptions, RawSource, Source, SourceExt,
 };
 use rspack_core::{Compilation, ErrorSpan, ModuleType, TargetPlatform};
 use rspack_error::{DiagnosticKind, Error};
@@ -151,7 +151,7 @@ pub fn wrap_module_function(source: BoxSource, module_id: &str) -> BoxSource {
    * {source}
    * },
    */
-  ConcatSource::new([
+  CachedSource::new(ConcatSource::new([
     RawSource::from("\"").boxed(),
     RawSource::from(module_id.to_string()).boxed(),
     RawSource::from("\": ").boxed(),
@@ -163,7 +163,7 @@ pub fn wrap_module_function(source: BoxSource, module_id: &str) -> BoxSource {
     RawSource::from("\"use strict\";\n").boxed(),
     source,
     RawSource::from("},\n").boxed(),
-  ])
+  ]))
   .boxed()
 }
 
