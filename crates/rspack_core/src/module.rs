cc9b91c388dbe159122ed871a9f4355a418d9440
YunfeiHe <i.heyunfei@gmail.com>
2022-11-17 14:21:51 +0800

    feat(crates/core): add name_for_condition to Module trait

--- crates/rspack_core/src/module.rs
+++ crates/rspack_core/src/module.rs
@@ -56,6 +56,11 @@ pub trait Module: Debug + Send + Sync + AsAny {
   ) -> Result<TWithDiagnosticArray<BuildResult>>;
 
   fn code_generation(&self, _compilation: &Compilation) -> Result<CodeGenerationResult>;
+
+  fn name_for_condition(&self) -> Option<String> {
+    // Align with https://github.com/webpack/webpack/blob/8241da7f1e75c5581ba535d127fa66aeb9eb2ac8/lib/Module.js#L852
+    None
+  }
 }
 
 pub trait ModuleExt {
