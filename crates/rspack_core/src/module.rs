788829f34ab8d7c53cc9f2a1ff07bb6fea6c41ee
YunfeiHe <i.heyunfei@gmail.com>
2022-11-20 14:19:21 +0800

    Fix format

--- crates/rspack_core/src/module.rs
+++ crates/rspack_core/src/module.rs
@@ -86,7 +86,7 @@ pub trait Module: Debug + Send + Sync + AsAny + DynHash + DynEq {
     // Align with https://github.com/webpack/webpack/blob/8241da7f1e75c5581ba535d127fa66aeb9eb2ac8/lib/Module.js#L852
     None
   }
-  
+
   fn update_hash(&self, state: &mut dyn std::hash::Hasher) {
     self.dyn_hash(state);
   }
