3f0a90ce35dfccfd9ecaf3202f8a8455068f2dbe
underfin <likui6666666@gmail.com>
2022-11-21 13:29:20 +0800

    fix: __webpack_require__.O.j is not defined (#1221)

--- crates/rspack_plugin_runtime/src/runtime_module/jsonp_chunk_loading.rs
+++ crates/rspack_plugin_runtime/src/runtime_module/jsonp_chunk_loading.rs
@@ -1,12 +1,23 @@
 use crate::runtime_module::utils::{get_initial_chunk_ids, stringify_chunks};
+use hashbrown::HashSet;
 use rspack_core::{
-  rspack_sources::{BoxSource, RawSource, SourceExt},
-  ChunkUkey, Compilation, RuntimeModule,
+  rspack_sources::{BoxSource, ConcatSource, RawSource, SourceExt},
+  runtime_globals, ChunkUkey, Compilation, RuntimeModule,
 };
 
 #[derive(Debug, Default)]
 pub struct JsonpChunkLoadingRuntimeModule {
   chunk: Option<ChunkUkey>,
+  runtime_requirements: HashSet<String>,
+}
+
+impl JsonpChunkLoadingRuntimeModule {
+  pub fn new(runtime_requirements: HashSet<String>) -> Self {
+    Self {
+      chunk: None,
+      runtime_requirements,
+    }
+  }
 }
 
 impl RuntimeModule for JsonpChunkLoadingRuntimeModule {
@@ -23,14 +34,25 @@ impl RuntimeModule for JsonpChunkLoadingRuntimeModule {
     //   chunk_hash_js,
     // );
     let initial_chunks = get_initial_chunk_ids(self.chunk, compilation);
-    RawSource::from(
+    let mut source = ConcatSource::default();
+    source.add(RawSource::from(
       include_str!("runtime/jsonp_chunk_loading.js")
         .to_string()
         .replace("INSTALLED_CHUNKS", &stringify_chunks(&initial_chunks, 0))
         // TODO
         .replace("JS_MATCHER", "chunkId"),
-    )
-    .boxed()
+    ));
+
+    if self
+      .runtime_requirements
+      .contains(runtime_globals::ON_CHUNKS_LOADED)
+    {
+      source.add(RawSource::from(
+        include_str!("runtime/jsonp_chunk_loading_with_on_chunk_load.js").to_string(),
+      ));
+    }
+
+    source.boxed()
   }
 
   fn attach(&mut self, chunk: ChunkUkey) {
---
+++ crates/rspack_plugin_runtime/src/runtime_module/runtime/jsonp_chunk_loading_with_on_chunk_load.js
@@ -0,0 +1,5 @@
+(function () {
+	__webpack_require__.O.j = function (chunkId) {
+		installedChunks[chunkId] === 0;
+	};
+})();
