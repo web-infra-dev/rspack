fccd55816472961f8ef8b354fb954bf5bb9ac228
h-a-n-a <andywangsy@gmail.com>
2022-11-24 01:55:13 +0800

    test: polish test

--- packages/rspack/tests/configCases/module/match-all/index.js
+++ packages/rspack/tests/configCases/module/match-all/index.js
@@ -3,7 +3,10 @@ import RESOURCE from "./logo.svg";
 
 const RESOURCE_REGEX = /^\/assets\/[^/.]+\.svg$/;
 
-it("should generate urls based on loader options", () => {
+it("should generate inline svg if both `test` and `resourceQuery` matches", () => {
 	expect(INLINE.startsWith("data:image/svg+xml;base64,")).toBeTruthy();
+});
+
+it("should generate svg as a resource if only `test` matches", () => {
 	expect(RESOURCE_REGEX.test(RESOURCE)).toBeTruthy();
 });
