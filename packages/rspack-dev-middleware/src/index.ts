23e7c4c09a0dd35ff19fc70cd6a325e94e0aec4d
bvanjoi <bohan-zhang@foxmail.com>
2022-11-23 16:18:59 +0800

    refactor(dev-middleware): delete comments

--- packages/rspack-dev-middleware/src/index.ts
+++ packages/rspack-dev-middleware/src/index.ts
@@ -1,34 +1,9 @@
-import type { Compiler } from "@rspack/core";
 import type { RequestHandler as ExpressRequestHandler } from "express";
-// // import { ready } from "webpack-dev-middleware/dist/utils/ready";
 
 export type DevMiddleware = ExpressRequestHandler & {
 	invalidate(callback: Function): void;
 	close(callback: Function): void;
 };
-
-// export function rdm(
-// 	compiler: Compiler,
-// 	options: RspackOptionsNormalized["devServer"]["devMiddleware"]
-// ): RspackDevMiddleware {
-// 	// @ts-ignore
-// 	const instance: RspackDevMiddleware = wrapper();
-// 	instance.invalidate = function (callback) {
-// 		// TODO:
-// 	};
-// 	instance.close = function (callback) {
-// 		// TODO:
-// 	};
-
-// 	const watching = compiler.watch({});
-// 	return instance;
-// }
-
-// function wrapper(): ExpressRequestHandler {
-// 	return function middleware(req, res, next) {
-// 		return next();
-// 	};
-// }
 
 import wdm from "webpack-dev-middleware";
 
