7f660b6659e2e737df1b0443a29625de12a9bceb
zoolsher <zoolsher@gmail.com>
2022-11-23 22:48:51 +0800

    fix(cli): move webpack and webpack-dev-server to devDependencies
    fix(cli): install to update pnpm-lock

--- packages/rspack-cli/src/types.ts
+++ packages/rspack-cli/src/types.ts
@@ -1,8 +1,8 @@
 import { Colorette } from "colorette";
 import { RspackCLI } from "./rspack-cli";
-import { WebpackOptionsNormalized } from "webpack";
+import type { WebpackOptionsNormalized } from "webpack";
 export type { Configuration } from "@rspack/core";
-import { Configuration as DevServerConfig } from "webpack-dev-server";
+import type { Configuration as DevServerConfig } from "webpack-dev-server";
 export interface IRspackCLI {
 	runRspack(): Promise<void>;
 }
