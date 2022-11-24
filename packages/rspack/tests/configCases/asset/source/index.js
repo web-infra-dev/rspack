488334f9be9192ae4ebf6d15f73ec447bea1567f
h-a-n-a <andywangsy@gmail.com>
2022-11-24 17:24:14 +0800

    chore: typo

--- packages/rspack/tests/configCases/asset/source/index.js
+++ packages/rspack/tests/configCases/asset/source/index.js
@@ -1,5 +1,5 @@
 import data from "./data.txt";
 
-it("should return the raw data if `rule.type` is st to `asset/source`", () => {
+it("should return the raw data if `rule.type` is sat to `asset/source`", () => {
 	expect(data).toBe("- Isn't Rspack a gamechanging bundler?\n  - Hella yeah!");
 });
