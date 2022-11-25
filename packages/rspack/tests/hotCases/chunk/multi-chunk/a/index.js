3332e1a10874c0e8085e22d7883f6b9d7a3ec51e
bvanjoi <bohan-zhang@foxmail.com>
2022-11-25 10:33:45 +0800

    test(hmr): multi chunk should work

---
+++ packages/rspack/tests/hotCases/chunk/multi-chunk/a/index.js
@@ -0,0 +1,5 @@
+import file from '../file';
+
+it("should bundled in a", () => {
+  expect(typeof file).toBe('number');
+})
---
+++ packages/rspack/tests/hotCases/chunk/multi-chunk/rspack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: { 
+    a: './a/index.js',
+    b: './b/index.js',
+    main: './main/index.js'
+  },
+}
