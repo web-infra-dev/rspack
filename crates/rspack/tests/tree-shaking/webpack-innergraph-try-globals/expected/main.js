6d5fda0032745bfc64e0384e4c0ee74dde78c40a
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-12-10 18:44:51 +0800

    test: 💍 try globals

---
+++ crates/rspack/tests/tree-shaking/webpack-innergraph-try-globals/expected/main.js
@@ -0,0 +1,50 @@
+(self['webpackChunkwebpack'] = self['webpackChunkwebpack'] || []).push([["main"], {
+"./import-module.js": function (module, exports, __webpack_require__) {
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+const _module = __webpack_require__("./module.js");
+expect(_module.ok).toBe(true);
+expect(_module.ok2).toBe(true);
+},
+"./index.js": function (module, exports, __webpack_require__) {
+"use strict";
+it("should not threat globals as pure", ()=>{
+    __webpack_require__("./import-module.js");
+});
+},
+"./module.js": function (module, exports, __webpack_require__) {
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+function _export(target, all) {
+    for(var name in all)Object.defineProperty(target, name, {
+        enumerable: true,
+        get: all[name]
+    });
+}
+_export(exports, {
+    ok: ()=>ok,
+    ok2: ()=>ok2
+});
+try {
+    NOT_DEFINED;
+    var ok = false;
+} catch (e) {
+    var yep = true;
+    var ok = yep;
+}
+try {
+    var ok2 = false;
+    eval("");
+} catch (e) {
+    var ok2 = true;
+}
+},
+
+},function(__webpack_require__) {
+__webpack_require__("./index.js");
+}
+]);
