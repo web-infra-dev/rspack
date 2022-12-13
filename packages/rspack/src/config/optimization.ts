b1d06ff056ac7d95c4d167c16f436e86a539626e
hardfist <yangjianzju@gmail.com>
2022-12-13 23:19:09 +0800

    chore: add @rspack/plugin-minify (#1397)

--- packages/rspack/src/config/optimization.ts
+++ packages/rspack/src/config/optimization.ts
@@ -1,16 +1,24 @@
+import { PluginInstance } from "./plugin";
+
 export interface Optimization {
 	moduleIds?: "named" | "deterministic";
+	minimize?: boolean;
+	minimizer?: ("..." | PluginInstance)[];
 }
 
 export interface ResolvedOptimization {
 	moduleIds: "named" | "deterministic";
+	minimize?: boolean;
+	minimizer?: ("..." | PluginInstance)[];
 }
-
 export function resolveOptimizationOptions(
 	op: Optimization,
 	mode: string
 ): ResolvedOptimization {
 	return {
-		moduleIds: op.moduleIds ?? mode === "production" ? "deterministic" : "named"
+		moduleIds:
+			op.moduleIds ?? mode === "production" ? "deterministic" : "named",
+		minimize: op.minimize,
+		minimizer: op.minimizer
 	};
 }
