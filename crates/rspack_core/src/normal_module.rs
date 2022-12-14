27cf88308b67fa7850bf218597d1648b72c2eb45
likui <likui.underfin@bytedance.com>
2022-12-14 20:11:10 +0800

    chore: update

--- crates/rspack_core/src/normal_module.rs
+++ crates/rspack_core/src/normal_module.rs
@@ -544,7 +544,6 @@ impl NormalModule {
 impl Hash for NormalModule {
   fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
     "__rspack_internal__NormalModule".hash(state);
-    self.identifier().hash(state);
     if let Some(original_source) = &self.original_source {
       original_source.hash(state);
     }
