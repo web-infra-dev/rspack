f6cdc7fc95bf064ac355b57a08b8836512aa53ff
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-12-20 22:28:26 +0800

    test: 💍 add test case for css modules with tree-shaking

---
+++ packages/rspack/tests/configCases/postcss/css-modules-with-tree-shaking/index.js
@@ -0,0 +1,10 @@
+import { a } from "./a";
+it("basic", () => {
+	a;
+	const css = require("./index.css");
+	const style = require("./index.module.css");
+	expect(css).toEqual({});
+	expect(style).toEqual({
+		body: "_body_toys1_1"
+	});
+});
