58f860f18728c2c88465ac482a340f7932dadb51
bohan <bohan-zhang@foxmail.com>
2023-01-10 20:34:08 +0800

    fix: merge `resolve.alias`

--- packages/rspack/tests/configCases/resolve/alias/webpack.config.js
+++ packages/rspack/tests/configCases/resolve/alias/webpack.config.js
@@ -4,7 +4,20 @@ module.exports = {
 	resolve: {
 		alias: {
 			"@b": path.resolve(__dirname, "./a"),
-			xx: path.resolve(__dirname, "./a")
+			xx: path.resolve(__dirname, "./a"),
+			ignored: path.resolve(__dirname, "./a")
 		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				resolve: {
+					alias: {
+						ignored: false
+					}
+				}
+			}
+		]
 	}
 };
