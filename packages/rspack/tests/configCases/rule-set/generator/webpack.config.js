b5c0dfc67b4d9d3bb6d676be35b88abe6f69b3bf
h-a-n-a <andywangsy@gmail.com>
2023-01-10 22:19:08 +0800

    test: add test cases

---
+++ packages/rspack/tests/configCases/rule-set/generator/webpack.config.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('@rspack/core').RspackOptions}
+ */
+module.exports = {
+	context: __dirname,
+	output: {
+		assetModuleFilename: "asset/[name][ext]"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.png$/,
+				resourceQuery: /custom/,
+				type: "asset/resource",
+				generator: {
+					filename: "custom-asset/[name][ext]"
+				}
+			},
+			{
+				test: /\.svg$/,
+				resourceQuery: /non-custom/,
+				type: "asset/resource"
+			}
+		]
+	}
+};
