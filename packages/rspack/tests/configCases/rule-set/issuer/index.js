e472b544cc528ec91a93fbe4fd512ea3a047d770
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-01-31 20:31:02 +0800

    fix: add test comment

--- packages/rspack/tests/configCases/rule-set/issuer/index.js
+++ packages/rspack/tests/configCases/rule-set/issuer/index.js
@@ -1,6 +1,9 @@
 import { lib } from "./lib";
 
 it("`issuer` should work", () => {
+	// avoid being processed by loader
 	const target = "cbaa".split("").reverse().join("");
+
+	// should run loader and not run loader1
 	expect(lib).toEqual(target);
 });
