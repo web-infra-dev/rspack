a7d238bc26aaf8941b9ef1c47216a6361fa87520
h-a-n-a <andywangsy@gmail.com>
2023-02-03 18:44:59 +0800

    feat: add ast-viewer

---
+++ crates/rspack_ast_viewer/src/main.rs
@@ -0,0 +1,106 @@
+use std::{io::Read, sync::Arc};
+
+use anyhow::Result;
+use clap::Parser;
+use swc_core::{
+  self,
+  base::try_with_handler,
+  common::{errors::HANDLER, FileName, Globals, GLOBALS},
+  ecma::{
+    ast::*,
+    parser::{parse_file_as_module, Syntax, TsConfig},
+  },
+};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+  /// Module type: "js" | "css" (default: "js")
+  #[arg(short, long)]
+  r#type: Option<ModuleType>,
+
+  /// Whether to keep span (default: false)
+  #[arg(short, long)]
+  keep_span: Option<bool>,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum ModuleType {
+  JavaScript,
+}
+
+impl<'s> From<&'s str> for ModuleType {
+  fn from(value: &'s str) -> Self {
+    match &*value.to_ascii_lowercase() {
+      "js" => Self::JavaScript,
+      "css" => todo!("CSS module type is not supported yet"),
+      _ => panic!("Unknown module type: {value}"),
+    }
+  }
+}
+
+fn handle_javascript(input: String, keep_span: bool) -> Result<()> {
+  let cm: Arc<swc_core::common::SourceMap> = Default::default();
+  let fm = cm.new_source_file(FileName::Anon, input);
+  let mut errors = Default::default();
+
+  let ast = try_with_handler(cm, Default::default(), |handler| {
+    GLOBALS
+      .set(&Globals::default(), || {
+        HANDLER.set(handler, || {
+          parse_file_as_module(
+            &fm,
+            Syntax::Typescript(TsConfig {
+              tsx: true,
+              decorators: true,
+              ..Default::default()
+            }),
+            EsVersion::Es2022,
+            None,
+            &mut errors,
+          )
+          .map(Program::Module)
+        })
+      })
+      .map_err(|err| anyhow::anyhow!("{err:?}"))
+  })?;
+
+  if !errors.is_empty() {
+    anyhow::bail!("{errors:?}")
+  }
+
+  let output = {
+    let output = format!("{ast:#?}");
+
+    if keep_span {
+      output
+    } else {
+      let reg = regex::Regex::new(r#"\s*span: Span \{[^}]*?\},"#)?;
+      reg.replace_all(&output, "").into_owned()
+    }
+  };
+
+  println!("{output}");
+
+  Ok(())
+}
+
+fn main() {
+  let args = Args::parse();
+  let module_type = args.r#type.unwrap_or(ModuleType::JavaScript);
+  let keep_span = args.keep_span.unwrap_or(false);
+
+  let mut input = String::new();
+  std::io::stdin()
+    .read_to_string(&mut input)
+    .expect("Failed to read from stdin");
+
+  let res = match module_type {
+    ModuleType::JavaScript => handle_javascript(input, keep_span),
+  };
+
+  if let Err(err) = res {
+    eprintln!("{err}");
+    std::process::exit(1);
+  }
+}
