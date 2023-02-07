8ea8c2d50a28261c7dafd369c89d12d858638125
chenjiahan <chenjiahan.jait@bytedance.com>
2023-02-07 14:36:03 +0800

    chore(CI): add CI to check changeset type

---
+++ scripts/check_changeset.js
@@ -0,0 +1,38 @@
+import path from "path";
+import readChangesets from "@changesets/read";
+
+async function run() {
+	const cwd = process.cwd();
+	const changesets = await readChangesets(
+		path.join(cwd, "../"),
+		process.env.BASE_BRANCH
+	);
+
+	const errors = [];
+	for (const changeset of changesets) {
+		const { releases } = changeset;
+		releases.forEach(release => {
+			if (release.type === "major" || release.type === "minor") {
+				errors.push(
+					`- Packages "${release.name}" are not allowed to bump "${release.type}" version.`
+				);
+			}
+		});
+	}
+
+	return errors;
+}
+
+run().then(errors => {
+	if (errors.length) {
+		const messages = [
+			'Rspack is currently using "0.x" as the version number, so major or minor version upgrades are not allowed yet.',
+			"",
+			...errors,
+			"",
+			'Please open your changeset file and modify the version type to "patch".'
+		];
+		console.log(messages.join("\n"));
+		process.exit(1);
+	}
+});
