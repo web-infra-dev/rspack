84d3fe9a8a9d1f08894f3b5e669f3869686a8957
YunfeiHe <i.heyunfei@gmail.com>
2023-02-08 20:07:46 +0800

    feat: add cache in remove_parent_modules

--- crates/rspack_core/src/build_chunk_graph/remove_parent_modules.rs
+++ crates/rspack_core/src/build_chunk_graph/remove_parent_modules.rs
@@ -3,6 +3,7 @@
 
 use std::sync::Arc;
 
+use dashmap::DashMap;
 use rayon::prelude::ParallelBridge;
 use rayon::prelude::*;
 use rustc_hash::{FxHashMap, FxHashSet};
@@ -11,7 +12,7 @@ use super::code_splitter::CodeSplitter;
 use crate::{ChunkUkey, Compilation, IdentifierSet, ModuleIdentifier};
 
 type ChunkRelationGraph = petgraph::graphmap::DiGraphMap<ChunkUkey, ()>;
-type DefinitelyLoadedModules = IdentifierSet;
+type DefinitelyLoadedModules = Arc<IdentifierSet>;
 
 #[derive(Debug, Default)]
 pub(super) struct RemoveParentModulesContext {
@@ -39,6 +40,7 @@ impl<'me> CodeSplitter<'me> {
       root_chunks: &'a FxHashSet<ChunkUkey>,
       compilation: &'a Compilation,
       chunk_relation_graph: &'a ChunkRelationGraph,
+      cache: &'a DashMap<ChunkUkey, DefinitelyLoadedModules>,
     }
 
     impl<'a> AnalyzeContext<'a> {
@@ -52,7 +54,11 @@ impl<'me> CodeSplitter<'me> {
 
     #[tracing::instrument(skip_all)]
     fn analyze_loaded_modules(mut ctx: AnalyzeContext) -> Option<DefinitelyLoadedModules> {
-      if ctx.analyzing_chunks.contains(&ctx.target_ukey) {
+      if let Some(loaded_modules) = ctx.cache.try_get(&ctx.target_ukey).try_unwrap() {
+        return Some(loaded_modules.clone());
+      }
+
+      let ret = if ctx.analyzing_chunks.contains(&ctx.target_ukey) {
         // We are in a circle.
         // For case: ChunkA[a.js, shared.js] <--> ChunkB[b.js, shared.js]
         // Just return a empty vec.
@@ -60,10 +66,12 @@ impl<'me> CodeSplitter<'me> {
       } else if ctx.root_chunks.contains(&ctx.target_ukey) {
         // we are in a root chunk.
         // Just return the modules itself.
-        Some(ctx.chunk_modules(&ctx.target_ukey).clone())
+        Some(Arc::new(ctx.chunk_modules(&ctx.target_ukey).clone()))
       } else {
+        // Try hit the cache
+
         ctx.analyzing_chunks.insert(ctx.target_ukey);
-        let loaded_modules = ctx
+        let mut loaded_modules_of_parents = ctx
           .chunk_relation_graph
           .edges_directed(ctx.target_ukey, petgraph::Direction::Incoming)
           .par_bridge()
@@ -74,6 +82,15 @@ impl<'me> CodeSplitter<'me> {
               ..ctx.clone()
             })
           })
+          .collect::<Vec<_>>();
+
+        // If hit cache, return it.
+        if let Some(loaded_modules) = ctx.cache.try_get(&ctx.target_ukey).try_unwrap() {
+          return Some(loaded_modules.clone());
+        }
+
+        let loaded_modules = loaded_modules_of_parents
+          .into_par_iter()
           .fold(IdentifierSet::default, |mut acc, cur| {
             // The word `Definitely` in `DefinitelyLoadedModules` infers that
             // we need the intersection of all parent loaded modules.
@@ -85,8 +102,12 @@ impl<'me> CodeSplitter<'me> {
           .chain(ctx.chunk_modules(&ctx.target_ukey).par_iter().cloned())
           .collect::<IdentifierSet>();
 
-        Some(loaded_modules)
+        Some(Arc::new(loaded_modules))
+      };
+      if let Some(ret) = &ret {
+        ctx.cache.insert(ctx.target_ukey, ret.clone());
       }
+      ret
     }
 
     self
@@ -101,6 +122,7 @@ impl<'me> CodeSplitter<'me> {
           root_chunks: &self.remove_parent_modules_context.root_chunks,
           compilation: self.compilation,
           chunk_relation_graph: &self.remove_parent_modules_context.chunk_relation_graph,
+          cache: &DashMap::default(),
         })
         .unwrap_or_default();
 
@@ -112,7 +134,7 @@ impl<'me> CodeSplitter<'me> {
   #[tracing::instrument(skip_all)]
   fn analyze_modules_should_be_removed(
     &mut self,
-    loaded_modules_map: FxHashMap<ChunkUkey, DefinitelyLoadedModules>,
+    loaded_modules_map: &FxHashMap<ChunkUkey, DefinitelyLoadedModules>,
   ) -> Vec<(ChunkUkey, ModuleIdentifier)> {
     self
       .compilation
@@ -176,7 +198,10 @@ impl<'me> CodeSplitter<'me> {
   #[tracing::instrument(skip_all)]
   pub(super) fn remove_parent_modules(&mut self) {
     let loaded_modules_map = self.prepare_remove_parent_modules();
-    let modules_should_be_removed = self.analyze_modules_should_be_removed(loaded_modules_map);
+    let modules_should_be_removed = self.analyze_modules_should_be_removed(&loaded_modules_map);
+    std::thread::spawn(move || {
+      loaded_modules_map;
+    });
     self.remove_modules(modules_should_be_removed)
   }
 }
