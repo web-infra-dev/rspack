425db05b2fd77fd69a4992961f3b6f3f9e6fd3a8
YunfeiHe <i.heyunfei@gmail.com>
2023-02-13 20:26:21 +0800

    Enable

--- crates/rspack_binding_options/src/options/mod.rs
+++ crates/rspack_binding_options/src/options/mod.rs
@@ -224,12 +224,15 @@ pub fn normalize_bundle_options(raw_options: RawOptions) -> anyhow::Result<Compi
       Ok(options)
     })?
     .then(|mut options| {
-      if let Some(optimization) = raw_options.optimization {
-        let split_chunks = RawOption::raw_to_compiler_option(optimization.split_chunks, &options)?;
-        options.plugins.get_or_insert_default().push(Box::new(
-          rspack_plugin_split_chunks::SplitChunksPlugin::new(split_chunks),
-        ))
-      }
+      // if let Some(optimization) = raw_options.optimization {
+      //   let split_chunks = RawOption::raw_to_compiler_option(optimization.split_chunks, &options)?;
+      //   options.plugins.get_or_insert_default().push(Box::new(
+      //     rspack_plugin_split_chunks::SplitChunksPlugin::new(split_chunks),
+      //   ))
+      // }
+      options.plugins.get_or_insert_default().push(Box::new(
+        rspack_plugin_dev_friendly_split_chunks::DevFriendlySplitChunksPlugin::new(),
+      ));
       Ok(options)
     })?
     .then(|mut options| {
