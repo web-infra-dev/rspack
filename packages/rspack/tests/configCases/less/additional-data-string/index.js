875ec4c8730c25266aa9d08ad8f25d9828bba567
yangjian.fe <yangjian.fe@bytedance.com>
2023-02-15 15:09:28 +0800

    chore: delete less-loader

--- packages/rspack/tests/configCases/less/additional-data-string/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-it("should work as string", () => {
-	require("./index.less");
-	const css = fs.readFileSync(path.resolve(__dirname, "main.css"), "utf-8");
-	expect(css.includes("coral")).toBe(true);
-});
