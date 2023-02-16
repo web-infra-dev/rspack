a552f7e4571e9c86c6ffa14bf6dc12131420c73f
h-a-n-a <andywangsy@gmail.com>
2023-02-16 23:52:46 +0800

    chore: i really hate you clippy

--- crates/rspack/src/lib.rs
+++ crates/rspack/src/lib.rs
@@ -50,7 +50,7 @@ pub fn rspack(mut options: CompilerOptions, mut plugins: Vec<Box<dyn Plugin>>) -
   if options.experiments.lazy_compilation {
     plugins.push(Box::new(rspack_plugin_runtime::LazyCompilationPlugin {}));
   }
-  plugins.push(Box::<rspack_plugin_externals::ExternalPlugin>::default());
+  plugins.push(Box::<rspack_plugin_resolve::Resolve>::default());
   plugins.append(&mut options.plugins);
   plugins.push(Box::new(rspack_plugin_resolve::Resolve::default()));
   plugins.push(Box::new(rspack_plugin_javascript::JsPlugin::new()));
