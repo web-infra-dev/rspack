bc79809f47e0d0211f6ffcbfdf289f0dbf4dec70
yangjian.fe <yangjian.fe@bytedance.com>
2023-02-16 10:36:22 +0800

    chore: fix basic example & add cra example

--- examples/basic/a/index.js
+++
@@ -1,7 +0,0 @@
-import {bbb} from './bbb'
-export * from './aaa'
-
-var Index = function() {}
-Index.prototype.bbb = bbb;
-
-export default Index;
--- examples/basic/app.js
+++
@@ -1,2 +0,0 @@
-export {myanswer} from './lib'
-export {default as aaa} from './a'
