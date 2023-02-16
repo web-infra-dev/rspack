11f7744b7875fae1d05ec0f505f3a7740a7269cb
likui <likui.underfin@bytedance.com>
2023-02-16 11:50:15 +0800

    refactor: code

--- crates/rspack_plugin_javascript/src/dependency/esm/dynamic_import.rs
+++ crates/rspack_plugin_javascript/src/dependency/esm/dynamic_import.rs
@@ -122,65 +122,14 @@ impl CodeGeneratable for EsmDynamicImportDependency {
           decl_mappings.insert(id, val);
         }
 
-        let mut chunk_ids = {
-          let chunk_group = compilation.chunk_graph.get_block_chunk_group(
-            &referenced_module.module_identifier,
-            &compilation.chunk_group_by_ukey,
-          );
-          chunk_group
-            .chunks
-            .iter()
-            .map(|chunk_ukey| {
-              let chunk = compilation
-                .chunk_by_ukey
-                .get(chunk_ukey)
-                .unwrap_or_else(|| panic!("chunk should exist"));
-              chunk.expect_id().to_string()
-            })
-            .collect::<Vec<_>>()
-        };
-        chunk_ids.sort();
-
         // Add interop require to runtime requirements, as dynamic imports have been transformed so `inject_runtime_helper` will not be able to detect this.
         runtime_requirements.insert(runtime_globals::INTEROP_REQUIRE);
+        runtime_requirements.insert(runtime_globals::LOAD_CHUNK_WITH_MODULE);
 
         code_gen.visitors.push(
           create_javascript_visitor!(exact &self.ast_path, visit_mut_call_expr(n: &mut CallExpr) {
             if let Some(import) = n.args.get_mut(0) {
               if import.spread.is_none() && let Expr::Lit(Lit::Str(_)) = import.expr.as_mut() {
-                  // let call_expr = if chunk_ids.len() == 1 {
-                  //   CallExpr {
-                  //     span: DUMMY_SP,
-                  //     callee: Ident::new(runtime_globals::ENSURE_CHUNK.into(), DUMMY_SP).as_callee(),
-                  //     args: vec![Expr::Lit(Lit::Str(chunk_ids.first().expect("should have chunk id").to_string().into())).as_arg()],
-                  //     type_args: None,
-                  //   }
-                  // } else {
-                  //   CallExpr {
-                  //     span: DUMMY_SP,
-                  //     callee: quote_ident!("Promise.all").as_callee(),
-                  //     args: vec![ArrayLit {
-                  //       span: DUMMY_SP,
-                  //       elems: chunk_ids
-                  //         .iter()
-                  //         .map(|chunk_id| {
-                  //           Some(
-                  //             Expr::Call(CallExpr {
-                  //               span: DUMMY_SP,
-                  //               callee: Ident::new(runtime_globals::ENSURE_CHUNK.into(), DUMMY_SP)
-                  //                 .as_callee(),
-                  //               args: vec![Expr::Lit(Lit::Str(chunk_id.to_string().into())).as_arg()],
-                  //               type_args: None,
-                  //             })
-                  //             .as_arg(),
-                  //           )
-                  //         })
-                  //         .collect(),
-                  //     }
-                  //     .as_arg()],
-                  //     type_args: None,
-                  //   }
-                  // };
                   let call_expr = CallExpr {
                         span: DUMMY_SP,
                         callee: Ident::new(runtime_globals::LOAD_CHUNK_WITH_MODULE.into(), DUMMY_SP).as_callee(),
