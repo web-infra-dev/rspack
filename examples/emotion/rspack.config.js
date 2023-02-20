34014eba7f65118d1c1369e979be94c954de5aa7
yangjian.fe <yangjian.fe@bytedance.com>
2023-02-20 17:20:33 +0800

    chore: add emotion example

---
+++ examples/emotion/rspack.config.js
@@ -0,0 +1,20 @@
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+module.exports = {
+  entry: {
+    main: './src/index.jsx'
+  },
+  builtins: {
+    html: [
+      {
+        template: './index.html'
+      }
+    ],
+    emotion: true,
+    react: {
+      importSource: '@emotion/react',
+      runtime: 'automatic',
+    },
+  }
+}
