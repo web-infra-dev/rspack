14eec3c5e246d994a02e364d3191f497f1f827da
yangjian.fe <yangjian.fe@bytedance.com>
2023-02-23 14:21:11 +0800

    chore: remove webpack examples

--- webpack-examples/commonjs/example.js
+++
@@ -1,3 +0,0 @@
-const inc = require('./increment').increment;
-const a = 1;
-inc(a); // 2
--- webpack-examples/commonjs/increment.js
+++
@@ -1,4 +0,0 @@
-const add = require('./math').add;
-exports.increment = function(val) {
-    return add(val, 1);
-};
