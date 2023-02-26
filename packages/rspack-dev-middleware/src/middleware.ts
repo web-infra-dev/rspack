b39afcf507f3558d643277fadcaf3950e82b351d
yangjian.fe <yangjian.fe@bytedance.com>
2023-02-26 23:46:22 +0800

    chore: fix html index fallback

--- packages/rspack-dev-middleware/src/middleware.ts
+++ packages/rspack-dev-middleware/src/middleware.ts
@@ -24,14 +24,19 @@ export function getRspackMemoryAssets(
 					typeof index === "undefined" || typeof index === "boolean"
 						? "index.html"
 						: index;
-				return compiler.getAsset(filename + "/" + indexValue);
+				return compiler.getAsset(filename + indexValue);
 			})();
 		if (!buffer) {
 			return next();
 		}
+		let contentType;
+		if (filename === "") {
+			contentType = "text/html; charset=utf-8";
+		} else {
+			contentType =
+				mime.contentType(extname(path)) || "text/plain; charset=utf-8";
+		}
 
-		let contentType =
-			mime.contentType(extname(path)) || "text/plain; charset=utf-8";
 		res.setHeader("Content-Type", contentType);
 		res.send(buffer);
 	};
