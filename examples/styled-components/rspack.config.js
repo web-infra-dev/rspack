61a0416cbd5dc545749266287d66b7a59be3bb4f
yangjian.fe <yangjian.fe@bytedance.com>
2023-03-01 17:34:09 +0800

    chore: add styled-components

---
+++ examples/styled-components/rspack.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+module.exports = {
+  entry: {
+    main: './src/index.tsx'
+  },
+  builtins: {
+    html: [
+      {
+        template: './index.html'
+      }
+    ]
+  }
+}
