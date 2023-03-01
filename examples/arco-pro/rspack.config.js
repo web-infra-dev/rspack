06cd36b954f6251016e42666c7d8207a9633b975
ahabhgk <ahabhgk@gmail.com>
2023-03-01 18:31:49 +0800

    chore: fix bundle-stats ci

--- examples/arco-pro/rspack.config.js
+++ examples/arco-pro/rspack.config.js
@@ -1,72 +1,69 @@
-const path = require('path');
-const { default: HtmlPlugin } = require('@rspack/plugin-html');
+const path = require("path");
+const { default: HtmlPlugin } = require("@rspack/plugin-html");
 
-const prod = process.env.NODE_ENV === 'production';
+const prod = process.env.NODE_ENV === "production";
 
 /**
- * @type {import('webpack').Configuration}
+ * @type {import('@rspack/cli').Configuration}
  */
 module.exports = {
-  context: __dirname,
-  entry: { main: './src/index.tsx' },
-  devServer: {
-    port: 5555,
-    webSocketServer: 'sockjs',
-    historyApiFallback:true
-  },
-  mode: prod ? 'production' : 'development',
-  devtool: prod ? false : 'source-map',
-  builtins: {
-    progress: {},
-    treeShaking: true,
-    sideEffects: true,
-    noEmitAssets: false,
-  },
-  cache: false,
-  module: {
-    rules:
-      [
-        {
-          test: /\.less$/,
-          use:
-            [
-              { loader: 'less-loader' },
-            ],
-          type: 'css'
-        },
-        {
-          test: /\.module\.less$/,
-          use:
-            [
-              { loader: 'less-loader' },
-            ],
-          type: 'css/module'
-        },
-        { test: /\.svg$/, use: [{ loader: '@svgr/webpack' }], type: 'javascript/auto' }
-      ]
-  },
-  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
-  output: {
-    publicPath: '/'
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        someVendor: {
-          chunks: 'all',
-          minChunk: 2,
-        }
-      }
-    }
-  },
-  plugins: [
-    new HtmlPlugin({
-      title: 'Arco Pro App',
-      template: path.join(__dirname, 'index.html'),
-      favicon: path.join(__dirname, 'public', 'favicon.ico'),
-    }),
-  ],
-  infrastructureLogging: {
-    debug: false
-  }
-}
+	context: __dirname,
+	entry: { main: "./src/index.tsx" },
+	devServer: {
+		port: 5555,
+		webSocketServer: "sockjs",
+		historyApiFallback: true
+	},
+	mode: prod ? "production" : "development",
+	devtool: prod ? false : "source-map",
+	builtins: {
+		progress: {},
+		treeShaking: true,
+		sideEffects: true,
+		noEmitAssets: false
+	},
+	cache: false,
+	module: {
+		rules: [
+			{
+				test: /\.less$/,
+				use: [{ loader: "less-loader" }],
+				type: "css"
+			},
+			{
+				test: /\.module\.less$/,
+				use: [{ loader: "less-loader" }],
+				type: "css/module"
+			},
+			{
+				test: /\.svg$/,
+				use: [{ loader: "@svgr/webpack" }],
+				type: "javascript/auto"
+			}
+		]
+	},
+	resolve: { alias: { "@": path.resolve(__dirname, "src") } },
+	output: {
+		publicPath: "/"
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				someVendor: {
+					chunks: "all",
+					minChunk: 2
+				}
+			}
+		}
+	},
+	plugins: [
+		new HtmlPlugin({
+			title: "Arco Pro App",
+			template: path.join(__dirname, "index.html"),
+			favicon: path.join(__dirname, "public", "favicon.ico")
+		})
+	],
+	infrastructureLogging: {
+		debug: false
+	}
+};
