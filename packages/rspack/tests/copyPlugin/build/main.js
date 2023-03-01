147405569e9e9b7e12d500602f3fa98dc342864f
ahabhgk <ahabhgk@gmail.com>
2023-03-01 17:26:42 +0800

    rm tmp-sass-embedded

--- packages/rspack/tests/copyPlugin/build/main.js
+++ packages/rspack/tests/copyPlugin/build/main.js
@@ -1,24 +1,27 @@
-(function () {
-	var __webpack_modules__ = {
-		"../helpers/enter.js": function (module, exports, __webpack_require__) {}
-	};
-	// The module cache
-	var __webpack_module_cache__ = {};
-	function __webpack_require__(moduleId) {
-		// Check if module is in cache
-		var cachedModule = __webpack_module_cache__[moduleId];
-		if (cachedModule !== undefined) {
-			return cachedModule.exports;
-		}
-		// Create a new module (and put it into the cache)
-		var module = (__webpack_module_cache__[moduleId] = {
-			// no module.loaded needed
-			exports: {}
-		});
-		// Execute the module function
-		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-		// Return the exports of the module
-		return module.exports;
-	}
-	__webpack_require__("../helpers/enter.js");
+(function() {
+var __webpack_modules__ = {
+"../helpers/enter.js": function (module, exports, __webpack_require__) {
+},
+
+}
+// The module cache
+ var __webpack_module_cache__ = {};
+function __webpack_require__(moduleId) {
+// Check if module is in cache
+        var cachedModule = __webpack_module_cache__[moduleId];
+        if (cachedModule !== undefined) {
+      return cachedModule.exports;
+      }
+      // Create a new module (and put it into the cache)
+      var module = (__webpack_module_cache__[moduleId] = {
+      // no module.loaded needed
+          exports: {}
+        });
+        // Execute the module function
+      __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+// Return the exports of the module
+ return module.exports;
+
+}
+__webpack_require__("../helpers/enter.js");
 })();
