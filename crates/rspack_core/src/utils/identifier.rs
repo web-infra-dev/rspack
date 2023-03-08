ffe6e7861a21bab21d69434e8809ee53a62079c6
hana <andywangsy@gmail.com>
2023-03-08 19:46:25 +0800

    fix: fix module id on windows

--- crates/rspack_core/src/utils/identifier.rs
+++ crates/rspack_core/src/utils/identifier.rs
@@ -1,35 +1,12 @@
-use std::path::Path;
+use std::{borrow::Cow, path::Path};
 
-use sugar_path::SugarPath;
+use rspack_util::identifier::absolute_to_request;
 
 pub fn contextify(context: impl AsRef<Path>, request: &str) -> String {
   let context = context.as_ref();
   request
     .split('!')
-    .map(|r| absolute_to_request(context, r))
-    .collect::<Vec<String>>()
+    .map(|r| absolute_to_request(&context.to_string_lossy(), r))
+    .collect::<Vec<Cow<str>>>()
     .join("!")
-}
-
-fn absolute_to_request(context: &Path, maybe_absolute_path: &str) -> String {
-  if let Some((resource, query)) = maybe_absolute_path.split_once('?') {
-    let resource = relative_path_to_request(&Path::new(resource).relative(context));
-    resource + "?" + query
-  } else {
-    relative_path_to_request(&Path::new(maybe_absolute_path).relative(context))
-  }
-}
-
-fn relative_path_to_request(relative: &Path) -> String {
-  let r = relative.to_string_lossy();
-  if r == "" {
-    return "./.".to_string();
-  }
-  if r == ".." {
-    return "../.".to_string();
-  }
-  if r.starts_with("../") {
-    return r.to_string();
-  }
-  format!("./{r}")
 }
