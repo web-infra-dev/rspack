9494aa1279a25a9013905d81d7eb735bda5f6032
tuchg <ituchg@qq.com>
2023-03-14 13:43:06 +0800

    fix(testing): resolve context for `js` fixtures

--- crates/rspack_testing/src/eval_raw.rs
+++ crates/rspack_testing/src/eval_raw.rs
@@ -17,12 +17,15 @@ fn get_evaluate_code(config_path: &Path) -> String {
   let workspace_dir = PathBuf::from(env!("CARGO_WORKSPACE_DIR"));
   let rspack_path = workspace_dir.join("packages").join("rspack");
   let rspack_path = rspack_path.to_string_lossy();
+  let test_dir = config_path.parent().expect("TODO:").to_string_lossy();
   let config_path = config_path.to_string_lossy();
   format!(
     r#"
 const rspack = require("{rspack_path}");
 const config = require("{config_path}");
 const normalized = rspack.getNormalizedRspackOptions(config);
+normalized.context="{test_dir}";
+normalized.output.path = `{test_dir}/dist`; //TODO:
 rspack.applyRspackOptionsDefaults(normalized);
 const raw = rspack.getRawOptions(normalized);
 JSON.stringify(raw)
