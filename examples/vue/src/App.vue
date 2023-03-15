f6efa0161f3de8f7583c2a5aa6d9bcfe2788b09c
OrangeXC <orangexc1995@gmail.com>
2023-03-15 18:50:30 +0800

    chore:  update vue example rspack link

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -7,7 +7,7 @@ export default {
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <a href="https://www.rspack.dev/" target="_blank">
       <img src="./assets/rspack.svg" class="logo" alt="Rspack logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
