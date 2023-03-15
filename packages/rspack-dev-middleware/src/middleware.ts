f5f661a268bb69320ef1df6c0895dffdee61b842
Daniel Roe <daniel@roe.dev>
2023-03-15 00:14:05 +0000

    fix(dev-middleware): compatibility with native node handler (#2186)

--- packages/rspack-dev-middleware/src/middleware.ts
+++ packages/rspack-dev-middleware/src/middleware.ts
@@ -1,4 +1,5 @@
-import { extname, relative } from "path";
+import { extname } from "path";
+import type { IncomingMessage, ServerResponse } from "http";
 import type { Compiler } from "@rspack/core";
 import wdm from "webpack-dev-middleware";
 import type { RequestHandler } from "express";
@@ -8,14 +9,15 @@ export function getRspackMemoryAssets(
 	compiler: Compiler,
 	rdm: ReturnType<typeof wdm>
 ): RequestHandler {
-	return function (req, res, next) {
-		const { method, path } = req;
+	const publicPath = compiler.options.output.publicPath ? compiler.options.output.publicPath.replace(/\/$/, '') + '/' : '/';
+	return function (req: IncomingMessage, res: ServerResponse, next: () => void) {
+		const { method, url } = req;
 		if (method !== "GET") {
 			return next();
 		}
 
 		// asset name is not start with /, so path need to slice 1
-		const filename = path.slice(1);
+		const filename = url.startsWith(publicPath) ? url.slice(publicPath.length) : url.slice(1);
 		let buffer =
 			compiler.getAsset(filename) ??
 			(() => {
@@ -34,10 +36,10 @@ export function getRspackMemoryAssets(
 			contentType = "text/html; charset=utf-8";
 		} else {
 			contentType =
-				mime.contentType(extname(path)) || "text/plain; charset=utf-8";
+				mime.contentType(extname(url)) || "text/plain; charset=utf-8";
 		}
 
 		res.setHeader("Content-Type", contentType);
-		res.send(buffer);
+		res.end(buffer);
 	};
 }
