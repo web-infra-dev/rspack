cb2f4259816691309756f35742bdf5d562b63f16
so1ve <i@mk1.io>
2023-03-16 18:30:29 +0800

    fix: export helper

--- packages/rspack-cli/src/index.ts
+++ packages/rspack-cli/src/index.ts
@@ -1,3 +1,3 @@
 import { IRspackCLI } from "./types";
 export * from "./types";
-export { RspackCLI, defineConfig } from "./rspack-cli";
+export { RspackCLI, defineConfig, definePlugin } from "./rspack-cli";
