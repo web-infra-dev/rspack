063269f7d73fdc83f2bcd1d07a7c76dccf6f3468
ahabhgk <ahabhgk@gmail.com>
2023-03-22 18:36:32 +0800

    add timings for cli

--- packages/rspack-cli/src/rspack-cli.ts
+++ packages/rspack-cli/src/rspack-cli.ts
@@ -155,7 +155,7 @@ export class RspackCLI {
 			}
 
 			if (typeof item.stats === "undefined") {
-				item.stats = { preset: "errors-warnings" };
+				item.stats = { preset: "errors-warnings", timings: true };
 			} else if (typeof item.stats === "boolean") {
 				item.stats = item.stats ? { preset: "normal" } : { preset: "none" };
 			} else if (typeof item.stats === "string") {
