fff0c3d60d3990eedc05729ef53ba5ddb7691054
underfin <likui6666666@gmail.com>
2023-03-22 14:37:05 +0800

    fix: add test

--- packages/rspack/tests/configCases/library/umd/index.js
+++ packages/rspack/tests/configCases/library/umd/index.js
@@ -1,1 +1,6 @@
-it("should run", function () {});
+it("should run", function () {
+	var fs = require("fs");
+	var source = fs.readFileSync(__filename, "utf-8");
+
+	expect(source.includes("return __webpack_exports__")).toBe(true);
+});
