8df23740e8d443b93212e5ded6826649091f2ff7
ahabhgk <ahabhgk@gmail.com>
2023-03-27 15:34:15 +0800

    add test

--- packages/rspack/tests/cases/resolve/dep-conditions/index.js
+++ packages/rspack/tests/cases/resolve/dep-conditions/index.js
@@ -1,7 +1,9 @@
+import prod from "prod-condition";
 import v1 from "dep-condition";
 const v2 = require("dep-condition");
 
 it("should make different modules for resolve", function () {
+	expect(prod).toBe("production");
 	expect(v1).toBe("import");
 	expect(v2).toBe("require");
 });
