4ebd8b344db8ad244e81588954ee04da7043e196
zackarychapple <zackarychapple30@gmail.com>
2023-03-30 08:41:42 +0100

    fix: merging latest from main

--- packages/rspack-cli/src/utils/loadConfig.test.ts
+++ packages/rspack-cli/src/utils/loadConfig.test.ts
@@ -7,6 +7,7 @@ describe("loadRspackConfig", () => {
 			loadRspackConfig({
 				config: resolve(__dirname, ".", "./non-existent-config.js")
 			})
+		// 	@ts-ignore
 		).rejects.toThrow("config file");
 	});
 
