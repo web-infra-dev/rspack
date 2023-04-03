68d5006080c2758f8650098066568869ef88ad21
underfin <likui6666666@gmail.com>
2023-04-03 17:05:55 +0800

    chore: add comment

--- packages/rspack-dev-client/src/reactRefresh.ts
+++ packages/rspack-dev-client/src/reactRefresh.ts
@@ -1,10 +1,11 @@
-// Port from https://github.com/pmmmwh/react-refresh-webpack-plugin/blob/main/lib/runtime/RefreshUtils.js
+// Thanks https://github.com/pmmmwh/react-refresh-webpack-plugin
 // @ts-ignore
 const RefreshUtils = require('@pmmmwh/react-refresh-webpack-plugin/lib/runtime/RefreshUtils');
 const RefreshRuntime = require("react-refresh/runtime");
 
 RefreshRuntime.injectIntoGlobalHook(globalThis);
 
+// Port from https://github.com/pmmmwh/react-refresh-webpack-plugin/blob/main/loader/utils/getRefreshModuleRuntime.js#L29
 function refresh(moduleId, webpackHot) {
 	const currentExports = RefreshUtils.getModuleExports(moduleId);
 	const fn = () => {
