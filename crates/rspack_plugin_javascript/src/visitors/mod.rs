47c1387519f26affc9e7f3056395fa5b82d1efe6
underfin <likui6666666@gmail.com>
2023-04-07 17:18:01 +0800

    fix: clippy

--- crates/rspack_plugin_javascript/src/visitors/mod.rs
+++ crates/rspack_plugin_javascript/src/visitors/mod.rs
@@ -65,7 +65,7 @@ pub fn run_before_pass(
     rspack_core::TargetEsVersion::Esx(es_version) => Some(es_version),
     _ => None,
   };
-  let hash = build_info.hash.clone();
+  let hash = build_info.hash;
   let cm = ast.get_context().source_map.clone();
   // TODO: should use react-loader to get exclude/include
   let should_transform_by_react = module_type.is_jsx_like();
