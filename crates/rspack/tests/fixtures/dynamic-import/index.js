11a6a4d5b3496d604c51096294b2970cc3b18fc6
faga <lzc295592@163.com>
2023-04-12 23:10:13 +0800

    chore: decrease dist file count

--- crates/rspack/tests/fixtures/dynamic-import/index.js
+++ crates/rspack/tests/fixtures/dynamic-import/index.js
@@ -1,6 +1,6 @@
 const request = 'a'
-import('./a.js').then(({ a }) => console.log("Literal", a))
-import(`./b.js`).then(({b}) => console.log("Template Literal", b))
-import(`./${request}.js`).then(({a}) => console.log("context_module_tpl", a))
-import('./' + request + '.js').then(({a}) => console.log("context_module_bin", a))
-import("./".concat(request, ".js")).then(({a}) => console.log("context_module_concat", a))
+import('./child/a.js').then(({ a }) => console.log("Literal", a))
+import(`./child/b.js`).then(({b}) => console.log("Template Literal", b))
+import(`./child/${request}.js`).then(({a}) => console.log("context_module_tpl", a))
+import('./child/' + request + '.js').then(({a}) => console.log("context_module_bin", a))
+import("./child/".concat(request, ".js")).then(({a}) => console.log("context_module_concat", a))
