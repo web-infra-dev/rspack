17d0a3d2a11c4fadd471833ccb3937116f0659aa
Zakuru San <zelmarou@gmaiL.com>
2023-04-15 05:07:06 +0000

    Fixes following hyf0 comments

--- scripts/meta/setup.mjs
+++ scripts/meta/setup.mjs
@@ -45,14 +45,3 @@ runInContext(
 			encoding: "utf-8"
 		}).status
 );
-
-runInContext(
-	"corepack prepare pnpm@8.2.0",
-	() =>
-		spawnSync("corepack", ["prepare", "pnpm@8.2.0", "--activate"], {
-			cwd: process.cwd(),
-			env: process.env,
-			stdio: "inherit",
-			encoding: "utf-8"
-		}).status
-);
