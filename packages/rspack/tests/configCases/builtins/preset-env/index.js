473059157437ae028a191964cf7e5e00e7f876e2
underfin <likui6666666@gmail.com>
2023-04-18 19:22:10 +0800

    fix: preset env use unresolved_mark (#2807)

--- packages/rspack/tests/configCases/builtins/preset-env/index.js
+++ packages/rspack/tests/configCases/builtins/preset-env/index.js
@@ -9,3 +9,19 @@ it("builtins preset env code check", () => {
 	const content = fs.readFileSync(__filename, "utf-8");
 	expect(content).not.toMatch(/\=\>/);
 });
+
+it("should transfrom arrow", () => {
+	const obj = {};
+
+	function fn() {
+		expect(this).toBe(obj);
+		expect(arguments[0]).toBe(1);
+		const b = () => {
+			expect(this).toBe(obj);
+			expect(arguments[0]).toBe(1);
+		};
+		b();
+	}
+
+	a.apply(obj, [1]);
+});
