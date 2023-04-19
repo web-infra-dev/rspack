b6cb9e85eba6b8f853ac67e26a1ede7bad1ba3fc
suxin2017 <1107178482@qq.com>
2023-04-19 08:44:42 +0800

    done

--- crates/rspack_plugin_javascript/tests/fixtures/magic_comment/expected/main.js
+++ crates/rspack_plugin_javascript/tests/fixtures/magic_comment/expected/main.js
@@ -1,7 +1,9 @@
 (self['webpackChunkwebpack'] = self['webpackChunkwebpack'] || []).push([["main"], {
 "./index.js": function (module, exports, __webpack_require__) {
-console.log('hello, world');
-console.log(__webpack_require__.c);
+__webpack_require__.el("./normal.js").then(__webpack_require__.bind(__webpack_require__, "./normal.js")).then(__webpack_require__.ir);
+__webpack_require__.el("./sub_fold.js").then(__webpack_require__.bind(__webpack_require__, "./sub_fold.js")).then(__webpack_require__.ir);
+__webpack_require__.el("./single_quote.js").then(__webpack_require__.bind(__webpack_require__, "./single_quote.js")).then(__webpack_require__.ir);
+__webpack_require__.el("./other.js").then(__webpack_require__.bind(__webpack_require__, "./other.js")).then(__webpack_require__.ir);
 },
 
 },function(__webpack_require__) {
