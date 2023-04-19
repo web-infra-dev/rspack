c8f632dfb9f52f2a618346c3cac66a0366884b9f
suxin2017 <yishidixiashang@gmail.com>
2023-04-19 12:07:36 +0800

    feat: support complex magical comment `import(/* webpackChunkName: "/dist/d/js_d" */ "./d")` (#2812)
    * done
    
    * done
    
    * chore: code style
    
    * fix: ignore webpack chunk name bug

---
+++ crates/rspack_plugin_javascript/tests/fixtures/magic_comment/expected/main.js
@@ -0,0 +1,14 @@
+(self['webpackChunkwebpack'] = self['webpackChunkwebpack'] || []).push([["main"], {
+"./index.js": function (module, exports, __webpack_require__) {
+__webpack_require__.el("./normal.js").then(__webpack_require__.bind(__webpack_require__, "./normal.js")).then(__webpack_require__.ir);
+__webpack_require__.el("./sub_fold.js").then(__webpack_require__.bind(__webpack_require__, "./sub_fold.js")).then(__webpack_require__.ir);
+__webpack_require__.el("./single_quote.js").then(__webpack_require__.bind(__webpack_require__, "./single_quote.js")).then(__webpack_require__.ir);
+__webpack_require__.el("./other.js").then(__webpack_require__.bind(__webpack_require__, "./other.js")).then(__webpack_require__.ir);
+__webpack_require__.el("./bug_only_single_quote.js").then(__webpack_require__.bind(__webpack_require__, "./bug_only_single_quote.js")).then(__webpack_require__.ir);
+},
+
+},function(__webpack_require__) {
+var __webpack_exports__ = __webpack_require__('./index.js');
+
+}
+]);
---
+++ crates/rspack_plugin_javascript/tests/fixtures/magic_comment/index.js
@@ -0,0 +1,9 @@
+import(/* webpackChunkName: "normal" */ './normal')
+import(/* webpackChunkName: "./sub/fold" */ './sub_fold')
+import(/* webpackChunkName: './sub/single' */ './single_quote')
+
+import(/* webpackChunkName: `./sub/other` */ './other')
+import(/* 'bug_' */ './bug_only_single_quote.js')
+
+
+
