4b2b90b657cdfb418f93be162950360a6e2dcfb1
suxin2017 <1107178482@qq.com>
2023-04-19 08:43:55 +0800

    done

---
+++ crates/rspack_plugin_javascript/tests/fixtures/magic_comment/expected/sub/fold.js
@@ -0,0 +1,6 @@
+(self['webpackChunkwebpack'] = self['webpackChunkwebpack'] || []).push([["./sub/fold"], {
+"./sub_fold.js": function (module, exports, __webpack_require__) {
+console.log('123');
+},
+
+}]);
---
+++ crates/rspack_plugin_javascript/tests/fixtures/magic_comment/index.js
@@ -0,0 +1,8 @@
+import(/* webpackChunkName: "normal" */ './normal')
+import(/* webpackChunkName: "./sub/fold" */ './sub_fold')
+import(/* webpackChunkName: './sub/single' */ './single_quote')
+
+import(/* webpackChunkName: `./sub/other` */ './other')
+
+
+
