3b4c4cfb366a6aa81e0da82a3ecac4f7fd2891f3
GiveMe-A-Name <a455300764b@hotmail.com>
2023-04-20 11:41:25 +0800

    fix(plugin_javascript): the webpack_chunk_name_capture can not capture string like /user/[id]/page

---
+++ crates/rspack_plugin_javascript/tests/fixtures/magic_comment/expected/user/user/[id]/page/page.js
@@ -0,0 +1,6 @@
+(self['webpackChunkwebpack'] = self['webpackChunkwebpack'] || []).push([["user/[id]/page"], {
+"./user/page/2.js": function (module, exports, __webpack_require__) {
+console.log('user [id]/page2');
+},
+
+}]);
