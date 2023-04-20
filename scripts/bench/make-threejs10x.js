48a218bbf1d1b04a54a13950a7cab2044cba13d7
YunfeiHe <i.heyunfei@gmail.com>
2023-04-20 16:37:11 +0800

    chore: use `zx` to replace xtask

---
+++ scripts/bench/make-threejs10x.js
@@ -0,0 +1,48 @@
+import "zx/globals";
+
+await import("../meta/check_is_workspace_root.js");
+
+fs.ensureDir("./benchcases/threejs10x");
+
+for (const i in Array(10).fill(null)) {
+	await $`cp -r ./benchcases/.three/src ./benchcases/threejs10x/copy${i}`;
+}
+
+const entryCode = Array(10)
+	.fill(null)
+	.map((_, i) => i)
+	.map(
+		i => `import * as copy${i} from './copy${i}/Three.js'\nexport { copy${i} }`
+	)
+	.join("\n");
+
+fs.writeFile("./benchcases/threejs10x/entry.js", entryCode);
+
+// Create test.config.json
+
+const testConfig = `
+{
+    "entry": {
+        "index": {
+            "import": ["./src/entry.js"]
+        }
+    }
+}
+`;
+
+fs.writeFile("./benchcases/threejs10x/test.config.json", testConfig);
+
+// Create  webpack.config.js
+
+const webpackConfig = `
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: ['./benchcases/three/src/entry.js']
+    },
+    devtool: 'eval',
+    cache: {type: 'filesystem'}
+}
+`;
+
+fs.writeFile("./benchcases/threejs10x/webpack.config.js", webpackConfig);
---
+++ scripts/meta/check_is_workspace_root.js
@@ -0,0 +1,25 @@
+import * as path from "path";
+
+try {
+	// Make sure developers are at the workspace root
+	const { default: rootPkgJson } = await import(
+		path.join(process.cwd(), "package.json"),
+		{
+			assert: {
+				type: "json"
+			}
+		}
+	);
+	if (rootPkgJson.name != "monorepo") {
+		throw new Error(`Unexpected cwd ${process.cwd()}`);
+	}
+} catch (oldErr) {
+	const err = new Error(
+		`Make sure you are in workspace root to run this script`
+	);
+	// @ts-expect-error
+	err.cause = oldErr;
+	throw err;
+}
+
+export {};
