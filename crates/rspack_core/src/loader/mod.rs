c8abfc677f233c503c62c564764fccfa3b148333
Hana <andywangsy@gmail.com>
2023-04-22 02:49:16 +0800

    chore: cleanup

--- crates/rspack_core/src/loader/mod.rs
+++ crates/rspack_core/src/loader/mod.rs
@@ -1,16 +1,4 @@
 mod loader_runner;
-
 pub use loader_runner::*;
 mod process_resource;
 pub use process_resource::*;
-use rspack_identifier::{Identifiable, Identifier};
-
-pub struct EmptyLoader(Identifier);
-
-impl Loader<LoaderRunnerContext> for EmptyLoader {}
-
-impl Identifiable for EmptyLoader {
-  fn identifier(&self) -> rspack_identifier::Identifier {
-    self.0
-  }
-}
