cb4edf1b9b7e42485ce1a6bb258f9ed6cb653370
underfin <likui6666666@gmail.com>
2023-04-25 20:27:56 +0800

    fix: add test

--- examples/react-refresh/src/index.tsx
+++ examples/react-refresh/src/index.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import React from 'react';
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
-root.render(<App />)
