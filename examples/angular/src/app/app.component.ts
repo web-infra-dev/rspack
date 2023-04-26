a873c4fce00e3d6b89f86f1f1eb0286d97486f50
Zack Chapple <zackarychapple30@gmail.com>
2023-04-26 19:28:52 -0400

    feat: adding back more features that are available now.

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from "@angular/core";
 	templateUrl: "./app.component.html",
 	// uncommenting this freezes rspack
 	// styleUrls: ["./app.component.scss"]
+	// TODO: Figure out why inline styles as template string does not work
 })
 export class AppComponent {
 	title = "rspack-ngs";
