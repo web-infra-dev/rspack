71fc41783110cedb8fae51a979c4791d6b296fe2
HeYunfei <i.heyunfei@gmail.com>
2023-04-28 19:14:22 +0800

    fix(react-hmr): inject the runtime global function (#2977)
    * fix: react hmr
    
    * Apply fix
    
    * Apply fix

--- crates/rspack_plugin_javascript/src/visitors/swc_visitor/react.rs
+++ crates/rspack_plugin_javascript/src/visitors/swc_visitor/react.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use once_cell::sync::Lazy;
 use rspack_core::{ModuleType, ReactOptions};
 use swc_core::common::{comments::SingleThreadedComments, Mark, SourceMap};
-use swc_core::ecma::ast::{CallExpr, Callee, Expr, Ident, ModuleItem, Program, Script, Stmt};
+use swc_core::ecma::ast::{CallExpr, Callee, Expr, Ident, ModuleItem, Program, Script};
 use swc_core::ecma::transforms::react::RefreshOptions;
 use swc_core::ecma::transforms::react::{react as swc_react, Options};
 use swc_core::ecma::visit::{noop_visit_type, Fold, Visit, VisitWith};
@@ -81,25 +81,18 @@ impl Visit for ReactRefreshUsageFinder {
 }
 
 // __webpack_require__.$ReactRefreshRuntime$ is injected by the react-refresh additional entry
-static HMR_HEADER: &str = r#"var RefreshRuntime = __webpack_modules__.$ReactRefreshRuntime$;
-var $RefreshReg$ = function (type, id) {
-  RefreshRuntime.register(type, __webpack_module__.id + "_" + id);
-}
-var $RefreshSig$ = RefreshRuntime.createSignatureFunctionForTransform;"#;
-
 // See https://github.com/web-infra-dev/rspack/pull/2714 why we have a promise here
-static HMR_FOOTER: &str = r#"Promise.resolve().then(function(){ 
+static RUNTIME_CODE: &str = r#"
+function $RefreshReg$(type, id) {
+  __webpack_modules__.$ReactRefreshRuntime$.register(type, __webpack_module__.id+ "_" + id);
+}
+Promise.resolve().then(function(){ 
   __webpack_modules__.$ReactRefreshRuntime$.refresh(__webpack_module__.id, module.hot);
-})"#;
+})
+"#;
 
-static HMR_HEADER_AST: Lazy<Script> = Lazy::new(|| {
-  parse_js_code(HMR_HEADER.to_string(), &ModuleType::Js)
-    .expect("TODO:")
-    .expect_script()
-});
-
-static HMR_FOOTER_AST: Lazy<Script> = Lazy::new(|| {
-  parse_js_code(HMR_FOOTER.to_string(), &ModuleType::Js)
+static RUNTIME_CODE_AST: Lazy<Script> = Lazy::new(|| {
+  parse_js_code(RUNTIME_CODE.to_string(), &ModuleType::Js)
     .expect("TODO:")
     .expect_script()
 });
@@ -115,17 +108,15 @@ impl Fold for ReactHmrFolder {
       return program;
     }
 
-    let mut body = vec![];
-    body.extend(HMR_HEADER_AST.body.clone());
+    let rumtime_stmts = RUNTIME_CODE_AST.body.clone();
+
     match program {
-      Program::Module(ref mut m) => m.body.extend(to_module_body(body)),
-      Program::Script(ref mut s) => s.body.extend(body),
+      Program::Module(ref mut m) => m
+        .body
+        .extend(rumtime_stmts.into_iter().map(ModuleItem::Stmt)),
+      Program::Script(ref mut s) => s.body.extend(rumtime_stmts),
     };
 
     program
   }
-}
-
-fn to_module_body(script_body: Vec<Stmt>) -> Vec<ModuleItem> {
-  script_body.into_iter().map(ModuleItem::Stmt).collect()
 }
