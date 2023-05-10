5c8b32616b4c90ac6a4798463fd3a7c50e5e6b90
suxin2017 <1107178482@qq.com>
2023-05-10 15:51:23 +0800

    reset code

--- crates/rspack_testing/examples/test-cli.rs
+++ crates/rspack_testing/examples/test-cli.rs
@@ -2,15 +2,16 @@
 
 use std::{env, path::PathBuf};
 
-use rspack_testing::test_rebuild_fixture;
+use rspack_testing::test_fixture;
 
 fn main() {
-  let fixture = PathBuf::from("./simple");
+  let fixture = env::args().nth(1).expect("path");
+  let fixture = PathBuf::from(fixture);
   let fixture = if fixture.is_absolute() {
     fixture
   } else {
     let cwd = env::current_dir().expect("current_dir");
     cwd.join(fixture).canonicalize().expect("canonicalize")
   };
-  test_rebuild_fixture(&fixture);
+  test_fixture(&fixture);
 }
