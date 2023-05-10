50a05bb960ed74a4835b27c13caa1b5e49faa188
suxin2017 <1107178482@qq.com>
2023-05-10 08:04:28 +0800

    add hot update test

--- crates/rspack_testing/examples/test-hmr-cli.rs
+++ crates/rspack_testing/examples/test-hmr-cli.rs
@@ -5,11 +5,13 @@ use std::{env, path::PathBuf};
 use rspack_testing::test_hmr_fixture;
 
 fn main() {
-  let fixture = PathBuf::from("./simple");
+  let fixture = PathBuf::from("crates/rspack_testing/examples/simple");
+  dbg!(&fixture);
   let fixture = if fixture.is_absolute() {
     fixture
   } else {
     let cwd = env::current_dir().expect("current_dir");
+    dbg!(&cwd);
     cwd.join(fixture).canonicalize().expect("canonicalize")
   };
   test_hmr_fixture(&fixture);
--- crates/rspack_testing/src/run_fixture.rs
+++ crates/rspack_testing/src/run_fixture.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::path::{Path, PathBuf};
 
 use cargo_rst::{helper::make_relative_from, rst::RstBuilder};
@@ -39,6 +40,90 @@ pub async fn test_fixture(fixture_path: &Path) -> Compiler<AsyncNativeFileSystem
     .build()
     .await
     .unwrap_or_else(|e| panic!("failed to compile in fixtrue {fixture_path:?}, {e:#?}"));
+  let stats = compiler.compilation.get_stats();
+  let output_name = make_relative_from(&compiler.options.output.path, fixture_path);
+  let rst = RstBuilder::default()
+    .fixture(PathBuf::from(fixture_path))
+    .actual(output_name)
+    .build()
+    .expect("TODO:");
+
+  let errors = stats.get_errors();
+  if !errors.is_empty() {
+    panic!(
+      "Failed to compile in fixtrue {:?}, errors: {:?}",
+      fixture_path,
+      stats
+        .emit_diagnostics_string(true)
+        .expect("failed to emit diagnostics to string")
+    );
+  }
+  rst.assert();
+
+  compiler
+}
+
+fn read_dir_reverse(path: &PathBuf) -> Vec<String> {
+  let mut result = vec![];
+  if let Ok(changed_dir) = std::fs::read_dir(path) {
+    changed_dir.for_each(|entry| {
+      let entry = entry.unwrap();
+      if entry.path().is_file() {
+        result.push(entry.path().as_os_str().to_string_lossy().to_string());
+      }
+      if entry.path().is_dir() {
+        result.extend(read_dir_reverse(&entry.path()))
+      }
+    })
+  }
+  result
+}
+
+#[tokio::main]
+pub async fn test_hmr_fixture(fixture_path: &Path) -> Compiler<AsyncNativeFileSystem> {
+  enable_tracing_by_env();
+
+  let (mut options, plugins) = apply_from_fixture(fixture_path);
+  for (_, entry) in options.entry.iter_mut() {
+    entry.runtime = Some("runtime".to_string());
+  }
+  // clean output
+  if options.output.path.exists() {
+    std::fs::remove_dir_all(&options.output.path).expect("should remove output");
+  }
+  let mut compiler = Compiler::new(options, plugins, AsyncNativeFileSystem);
+  compiler
+    .build()
+    .await
+    .unwrap_or_else(|e| panic!("failed to compile in fixtrue {fixture_path:?}, {e:#?}"));
+
+  // let removed_files = Default::default();
+  // let changed_files = Default::default();
+  // test hot change
+  let mut oldFiles: HashMap<String, Vec<u8>> = HashMap::new();
+  let changed_files: HashSet<String> =
+    HashSet::from_iter(read_dir_reverse(&fixture_path.clone().join("changed")).into_iter());
+
+  changed_files.iter().for_each(|file_path| {
+    let old_path = file_path.replace("changed", "");
+    let old_raw = std::fs::read(old_path.clone());
+    oldFiles.insert(old_path.clone(), old_raw.expect("change file not found"));
+    let new_content = std::fs::read(file_path);
+    std::fs::write(old_path, new_content.expect("changed file do not read"))
+      .expect("TODO: panic message");
+  });
+  dbg!(&changed_files);
+  // if let Some(remove_dir) = std::fs::read_dir(fixture_path.clone().join("removed")){
+  //   remove_dir.
+  // }
+  compiler
+    .rebuild(changed_files, Default::default())
+    .await
+    .unwrap_or_else(|e| panic!("failed to rebuild in fixture {fixture_path:?}, {e:#?}"));
+
+  oldFiles.iter().for_each(|(old_path, old_content)| {
+    std::fs::write(old_path.clone(), old_content).expect("TODO: panic message");
+  });
   let stats = compiler.compilation.get_stats();
   let output_name = make_relative_from(&compiler.options.output.path, fixture_path);
   let rst = RstBuilder::default()
