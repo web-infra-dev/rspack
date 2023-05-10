737caacdf55ea011b51c5c136ca40013d5b964cc
YunfeiHe <i.heyunfei@gmail.com>
2023-05-10 15:57:18 +0800

    Fix

--- crates/rspack_plugin_javascript/src/visitors/mod.rs
+++ crates/rspack_plugin_javascript/src/visitors/mod.rs
@@ -53,7 +53,6 @@ macro_rules! either {
 }
 
 /// return (ast, top_level_mark, unresolved_mark, globals)
-/// too-many-arguments
 #[allow(clippy::too_many_arguments)]
 pub fn run_before_pass(
   resource_data: &ResourceData,
