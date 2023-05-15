e698c1ba969853b9fec3879932cc490c87c6adf0
suxin2017 <1107178482@qq.com>
2023-05-15 19:42:12 +0800

    print log

--- packages/rspack/tests/helpers/fakeSystem.js
+++ packages/rspack/tests/helpers/fakeSystem.js
@@ -94,7 +94,7 @@ const System = {
 	},
 	ensureExecuted: name => {
 		let m = System.registry[name];
-		console.log(m);
+		console.log(System.registry);
 		if (!m && System._require) {
 			const oldName = System._nextName;
 			System._nextName = name;
