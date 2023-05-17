5e5bf0e5cbf8e16b2b27896ff494a55cdb693909
YunfeiHe <i.heyunfei@gmail.com>
2023-05-17 21:21:10 +0800

    fix: use custom tokio runtime to customize stack size of tokio worker threads

---
+++ crates/node_binding/src/tokio_runtime.rs
@@ -0,0 +1,18 @@
+use once_cell::sync::Lazy;
+
+/// TODO(hyf0): we might need a way to cleanup this resource
+static TOKIO_RT: Lazy<tokio::runtime::Runtime> = Lazy::new(init_tokio_runtime);
+
+fn init_tokio_runtime() -> tokio::runtime::Runtime {
+  //
+  tokio::runtime::Builder::new_multi_thread()
+    // See https://github.com/web-infra-dev/rspack/pull/183
+    // 6mb+
+    .thread_stack_size(6777216)
+    .build()
+    .unwrap()
+}
+
+pub fn tokio_rt() -> &'static tokio::runtime::Runtime {
+  &TOKIO_RT
+}
