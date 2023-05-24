189e74bfabc67f67470fe1fa18f9344dcf7fc003
h-a-n-a <andywangsy@gmail.com>
2023-05-24 22:53:35 +0800

    chore: make sure to compile less files

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -22,13 +22,13 @@ export default {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
-}
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #eeea8caa);
-}
+  &:hover {
+    filter: drop-shadow(0 0 2em #eeea8caa);
+  }
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  &.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
 }
 </style>
