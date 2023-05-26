6ea26e5e91347b12fe3640dcbdd8be5af364f91a
ahabhgk <ahabhgk@gmail.com>
2023-05-26 15:49:51 +0800

    fix

--- packages/rspack/tests/configCases/builtins/css-modules-locals-convention-camelCaseOnly/index.js
+++ packages/rspack/tests/configCases/builtins/css-modules-locals-convention-camelCaseOnly/index.js
@@ -1,9 +1,9 @@
 it("css modules localsConvention with camelCaseOnly", () => {
 	const style = require("./index.css");
 	expect(style).toEqual({
-		btnInfoIsDisabled: "-index-css__btn-info_is-disabled",
-		btnInfoIsDisabled1: "-index-css__btn--info_is-disabled_1",
-		fooBar: "-index-css__foo_bar",
-		simple: "-index-css__simple"
+		btnInfoIsDisabled: "index-css__btn-info_is-disabled",
+		btnInfoIsDisabled1: "index-css__btn--info_is-disabled_1",
+		fooBar: "index-css__foo_bar",
+		simple: "index-css__simple"
 	});
 });
