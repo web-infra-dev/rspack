4bd92fc0e64f4c240c7bd1b27d93e480280b5182
yangjian.fe <yangjian.fe@bytedance.com>
2023-06-02 16:56:23 +0800

    chore: add vue2-ts example

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,40 +1,40 @@
 <script lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-let title: string = "Rspack + Vue"
+import HelloWorld from "./components/HelloWorld.vue";
+let title: string = "Rspack + Vue";
 export default {
-  components: { HelloWorld },
-  data() {
-    return {
-      title
-    }
-  }
-}
+	components: { HelloWorld },
+	data() {
+		return {
+			title
+		};
+	}
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://www.rspack.dev/" target="_blank">
-      <img src="./assets/rspack.svg" class="logo" alt="Rspack logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld :msg="title" />
+	<div>
+		<a href="https://www.rspack.dev/" target="_blank">
+			<img src="./assets/rspack.svg" class="logo" alt="Rspack logo" />
+		</a>
+		<a href="https://vuejs.org/" target="_blank">
+			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+		</a>
+	</div>
+	<HelloWorld :msg="title" />
 </template>
 
 <style scoped lang="less">
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
 
-  &:hover {
-    filter: drop-shadow(0 0 2em #eeea8caa);
-  }
+	&:hover {
+		filter: drop-shadow(0 0 2em #eeea8caa);
+	}
 
-  &.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-  }
+	&.vue:hover {
+		filter: drop-shadow(0 0 2em #42b883aa);
+	}
 }
 </style>
