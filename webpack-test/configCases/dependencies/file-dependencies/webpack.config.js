13013b14064c4831ed7c10556c25e77bbeeabe0c
jinrui <jerrykingxyz@gmail.com>
2023-06-06 19:54:50 +0800

    test: webpack cache deps test to file deps test (#3445)

---
+++ webpack-test/configCases/dependencies/file-dependencies/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	plugins: [
+		compiler => {
+			compiler.hooks.done.tap("Test", ({ compilation }) => {
+				const fileDeps = Array.from(compilation.fileDependencies);
+				expect(fileDeps).toContain(
+					path.resolve(__dirname, "node_modules/package/index.js")
+				);
+				expect(fileDeps).toContain(
+					path.resolve(__dirname, "node_modules/package/extra.js")
+				);
+				expect(fileDeps).toContain(path.resolve(__dirname, "extra.js"));
+				expect(fileDeps).toContain(path.resolve(__dirname, "index.js"));
+			});
+		}
+	]
+};
