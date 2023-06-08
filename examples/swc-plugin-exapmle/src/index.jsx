6d84f78b4ee8fdc61f96f6d816dfe7ab5ad5fad9
suxin2017 <1107178482@qq.com>
2023-06-08 11:17:23 +0800

    swc plugin exapmle

---
+++ examples/swc-plugin-exapmle/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`;
+console.log(Title)
