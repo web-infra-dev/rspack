5822d427efa041e6fecd7937c622fc881c614e2b
YunfeiHe <i.heyunfei@gmail.com>
2023-06-08 22:19:31 +0800

    Deal with recursive type

--- packages/rspack/src/config/zod/resolve.ts
+++ packages/rspack/src/config/zod/resolve.ts
@@ -7,21 +7,30 @@ const resolveAlias = z.record(
 		.or(z.array(z.literal(false).or(z.string())))
 );
 
-export function resolve() {
-	return z.strictObject({
-		alias: resolveAlias.optional(),
-		browserField: z.boolean().optional(),
-		conditionNames: z.string().array().optional(),
-		extensions: z.string().array().optional(),
-		fallback: resolveAlias.optional(),
-		mainFields: z.string().array().optional(),
-		mainFiles: z.string().array().optional(),
-		modules: z.string().array().optional(),
-		preferRelative: z.boolean().optional(),
-		tsConfigPath: z.string().optional(),
-		fullySpecified: z.boolean().optional(),
-		exportsFields: z.string().array().optional(),
-		extensionAlias: z.record(z.string().or(z.string().array())).optional(),
-		byDependency: z.record(z.lazy(resolve)).optional()
+const baseResolve = z.strictObject({
+	alias: resolveAlias.optional(),
+	browserField: z.boolean().optional(),
+	conditionNames: z.string().array().optional(),
+	extensions: z.string().array().optional(),
+	fallback: resolveAlias.optional(),
+	mainFields: z.string().array().optional(),
+	mainFiles: z.string().array().optional(),
+	modules: z.string().array().optional(),
+	preferRelative: z.boolean().optional(),
+	tsConfigPath: z.string().optional(),
+	fullySpecified: z.boolean().optional(),
+	exportsFields: z.string().array().optional(),
+	extensionAlias: z.record(z.string().or(z.string().array())).optional()
+});
+
+// Recursive types need to write this way.
+// See https://zod.dev/?id=recursive-types
+type ResolveConfig = z.infer<typeof baseResolve> & {
+	byDependency?: Record<string, ResolveConfig>;
+};
+
+export function resolve(): z.ZodType<ResolveConfig> {
+	return baseResolve.extend({
+		byDependency: z.lazy(() => z.record(resolve())).optional()
 	});
 }
