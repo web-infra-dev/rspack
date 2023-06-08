1ae79a0eb601b9161b433f4c395fe86a013f09ae
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-06-08 16:17:38 +0800

    feat: support override writeToDisk configuration

--- packages/rspack-dev-middleware/src/index.ts
+++ packages/rspack-dev-middleware/src/index.ts
@@ -2,8 +2,8 @@ import wdm from "webpack-dev-middleware";
 
 const rdm: typeof wdm = (compiler, options) => {
 	return wdm(compiler, {
-		...options,
-		writeToDisk: false
+		writeToDisk: false,
+		...options
 	});
 };
 
