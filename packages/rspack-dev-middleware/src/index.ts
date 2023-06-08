5ffc31b408d185e5e692c92a78134870b24f221f
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-06-08 16:23:40 +0800

    fix: use wdm default

--- packages/rspack-dev-middleware/src/index.ts
+++ packages/rspack-dev-middleware/src/index.ts
@@ -1,10 +1,7 @@
 import wdm from "webpack-dev-middleware";
 
 const rdm: typeof wdm = (compiler, options) => {
-	return wdm(compiler, {
-		writeToDisk: false,
-		...options
-	});
+	return wdm(compiler, options);
 };
 
 export default rdm;
