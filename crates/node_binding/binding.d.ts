d2946806e0ce7484a711920c4dee93eeb18a5583
Gengkun <ahabhgk@gmail.com>
2023-06-09 17:23:34 +0800

    fix: Base64Error caused by unclean base64 (#3494)
    * add test
    
    * fmt

--- crates/node_binding/binding.d.ts
+++ crates/node_binding/binding.d.ts
@@ -364,6 +364,12 @@ export interface JsStatsWarning {
   formatted: string
 }
 
+export interface NodeFS {
+  writeFile: (...args: any[]) => any
+  mkdir: (...args: any[]) => any
+  mkdirp: (...args: any[]) => any
+}
+
 export interface PathData {
   filename?: string
   hash?: string
@@ -916,5 +922,13 @@ export interface RawStyleConfig {
 
 export interface RawTrustedTypes {
   policyName?: string
+}
+
+export interface ThreadsafeNodeFS {
+  writeFile: (...args: any[]) => any
+  removeFile: (...args: any[]) => any
+  mkdir: (...args: any[]) => any
+  mkdirp: (...args: any[]) => any
+  removeDirAll: (...args: any[]) => any
 }
 
