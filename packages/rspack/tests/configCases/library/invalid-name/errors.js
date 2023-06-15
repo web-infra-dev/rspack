797cfdf7d3fadfef3e2de634624623a61166ae5c
underfin <likui6666666@gmail.com>
2023-06-15 12:03:58 +0800

    chore: rebase

--- packages/rspack/tests/configCases/library/invalid-name/errors.js
+++ packages/rspack/tests/configCases/library/invalid-name/errors.js
@@ -1,6 +1,6 @@
 module.exports = [
 	[
 		/Library name base \(123-hello world\) must be a valid identifier/,
-		/use a valid identifier \(e\. g\. 123_hello_world\)/
+		/use a valid identifier \(e\. g\. _123_hello_world\)/
 	]
 ];
