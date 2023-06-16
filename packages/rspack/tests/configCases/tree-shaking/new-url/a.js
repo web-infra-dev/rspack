bd338ca0ab2087c7e212a95656116b93bbb2f5f7
IWANABETHATGUY <iwanabethatguy@qq.com>
2023-06-17 00:10:14 +0800

    test: 💍 add test case

---
+++ packages/rspack/tests/configCases/tree-shaking/new-url/a.js
@@ -0,0 +1,7 @@
+export function a() {
+	new URL("./a.wasm", import.meta.url);
+}
+
+export function b() {
+	new URL("./b.wasm", import.meta.url);
+}
---
+++ packages/rspack/tests/configCases/tree-shaking/new-url/index.js
@@ -0,0 +1,31 @@
+import { a } from "./a";
+const fs = require("fs");
+
+it("should not shake the url import", () => {
+	a();
+	const file = fs.readFileSync(__filename).toString();
+	// 3 = 2 + 1 in assertion
+	expect(countSubstringOccurrences(file, "a.wasm")).toBe(3);
+	// TODO: here b should be two times
+	expect(countSubstringOccurrences(file, "b.wasm")).toBe(3);
+});
+
+function countSubstringOccurrences(string, substring) {
+	if (substring.length === 0) {
+		return 0;
+	}
+
+	let count = 0;
+	let index = 0;
+
+	while (index !== -1) {
+		index = string.indexOf(substring, index);
+
+		if (index !== -1) {
+			count++;
+			index += substring.length;
+		}
+	}
+
+	return count;
+}
