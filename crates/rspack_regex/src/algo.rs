1e10325a2482463eefadf83cdd520c3e095a5989
YunfeiHe <i.heyunfei@gmail.com>
2023-06-17 20:45:05 +0800

    Use enum instead of closure

--- crates/rspack_regex/src/algo.rs
+++ crates/rspack_regex/src/algo.rs
@@ -1,27 +1,18 @@
-use std::sync::Arc;
-
 use regex_syntax::hir::literal::ExtractKind;
 use regex_syntax::hir::{Hir, HirKind, Look};
 use rspack_error::{internal_error, Error};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum Algo {
-  FastCustom(Arc<dyn Fn(&str) -> bool + Send + Sync>),
+  /// See details at https://github.com/web-infra-dev/rspack/pull/3113
+  EndWith { pats: Vec<String> },
+  /// Regress is considered having the same behaviors as RegExp in JS.
   Regress(regress::Regex),
-}
-
-impl std::fmt::Debug for Algo {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    match self {
-      Self::FastCustom(_) => write!(f, "FastCustom(...)"),
-      Self::Regress(arg0) => f.debug_tuple("Regress").field(arg0).finish(),
-    }
-  }
 }
 
 impl Algo {
   pub fn new(expr: &str, flags: &str) -> Result<Algo, Error> {
-    if let Some(algo) = Self::try_fast_path(expr) {
+    if let Some(algo) = Self::try_compile_to_end_with_fast_path(expr) {
       Ok(algo)
     } else {
       regress::Regex::with_flags(expr, flags)
@@ -32,21 +23,19 @@ impl Algo {
     }
   }
 
-  pub fn try_fast_path(expr: &str) -> Option<Algo> {
+  pub fn try_compile_to_end_with_fast_path(expr: &str) -> Option<Algo> {
     let hir = regex_syntax::parse(expr).ok()?;
     let seq = regex_syntax::hir::literal::Extractor::new()
       .kind(ExtractKind::Suffix)
       .extract(&hir);
     if is_ends_with_regex(&hir) && seq.is_exact() {
-      let string_list = seq
+      let pats = seq
         .literals()?
         .iter()
         .map(|item| String::from_utf8_lossy(item.as_bytes()).to_string())
         .collect::<Vec<_>>();
 
-      Some(Algo::FastCustom(Arc::new(move |str: &str| {
-        string_list.iter().any(|item| str.ends_with(item))
-      })))
+      Some(Algo::EndWith { pats })
     } else {
       None
     }
@@ -54,14 +43,14 @@ impl Algo {
 
   pub(crate) fn test(&self, str: &str) -> bool {
     match self {
-      Algo::FastCustom(fast) => fast(str),
       Algo::Regress(regex) => regex.find(str).is_some(),
+      Algo::EndWith { pats } => pats.iter().any(|pat| str.ends_with(pat)),
     }
   }
 
   #[cfg(test)]
-  fn is_fast_custom(&self) -> bool {
-    matches!(self, Self::FastCustom(..))
+  fn is_end_with(&self) -> bool {
+    matches!(self, Self::EndWith { .. })
   }
 
   #[cfg(test)]
@@ -84,9 +73,9 @@ mod test_algo {
   use super::*;
   #[test]
   fn check_fast_path() {
-    assert!(Algo::new("\\.js$", "").unwrap().is_fast_custom());
-    assert!(Algo::new("\\.(jsx?|tsx?)$", "").unwrap().is_fast_custom());
-    assert!(Algo::new("\\.(svg|png)$", "").unwrap().is_fast_custom());
+    assert!(Algo::new("\\.js$", "").unwrap().is_end_with());
+    assert!(Algo::new("\\.(jsx?|tsx?)$", "").unwrap().is_end_with());
+    assert!(Algo::new("\\.(svg|png)$", "").unwrap().is_end_with());
   }
 
   #[test]
