2c7ac410ca8fc96e244e9665f161d4aae5deac2a
underfin <likui6666666@gmail.com>
2023-06-20 16:36:53 +0800

    fix: load chunk with module

--- crates/rspack_plugin_runtime/src/runtime_module/load_chunk_with_module.rs
+++ crates/rspack_plugin_runtime/src/runtime_module/load_chunk_with_module.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 use rayon::prelude::*;
 use rspack_core::{
   rspack_sources::{BoxSource, ConcatSource, RawSource, SourceExt},
-  Compilation, RuntimeModule,
+  ChunkUkey, Compilation, RuntimeModule,
 };
 use rspack_identifier::Identifier;
 use rspack_plugin_javascript::runtime::stringify_array;
@@ -14,12 +14,14 @@ use crate::impl_runtime_module;
 #[derive(Debug, Eq)]
 pub struct LoadChunkWithModuleRuntimeModule {
   id: Identifier,
+  chunk: Option<ChunkUkey>,
 }
 
 impl Default for LoadChunkWithModuleRuntimeModule {
   fn default() -> Self {
     Self {
       id: Identifier::from("webpack/runtime/load_chunk_with_module"),
+      chunk: None,
     }
   }
 }
@@ -30,68 +32,85 @@ impl RuntimeModule for LoadChunkWithModuleRuntimeModule {
   }
 
   fn generate(&self, compilation: &Compilation) -> BoxSource {
-    let async_modules = compilation
-      .module_graph
-      .module_graph_modules()
-      .par_iter()
-      .map(|(_, mgm)| mgm.dynamic_depended_modules(&compilation.module_graph))
-      .flatten()
-      .map(|(id, _)| id)
-      .collect::<HashSet<_>>();
-    let map = async_modules
-      .par_iter()
-      .map(|identifier| {
-        let mut chunk_ids = {
-          let chunk_group = compilation
-            .chunk_graph
-            .get_block_chunk_group(identifier, &compilation.chunk_group_by_ukey);
-          chunk_group
-            .chunks
-            .iter()
-            .map(|chunk_ukey| {
-              let chunk = compilation
-                .chunk_by_ukey
-                .get(chunk_ukey)
-                .expect("chunk should exist");
-              chunk.expect_id().to_string()
-            })
-            .collect::<Vec<_>>()
-        };
-        chunk_ids.sort_unstable();
-        let module = compilation
-          .module_graph
-          .module_graph_module_by_identifier(identifier)
-          .expect("no module found");
+    if let Some(chunk_ukey) = &self.chunk {
+      let runtime = &compilation
+        .chunk_by_ukey
+        .get(chunk_ukey)
+        .expect("should have chunk")
+        .runtime;
 
-        let module_id = module.id(&compilation.chunk_graph);
-        let module_id_expr = serde_json::to_string(module_id).expect("invalid module_id");
+      let async_modules = compilation
+        .module_graph
+        .module_graph_modules()
+        .par_iter()
+        .map(|(_, mgm)| mgm.dynamic_depended_modules(&compilation.module_graph))
+        .flatten()
+        .map(|(id, _)| id)
+        .collect::<HashSet<_>>();
+      let map = async_modules
+        .par_iter()
+        .map(|identifier| {
+          let mut chunk_ids = {
+            let chunk_group = compilation
+              .chunk_graph
+              .get_block_chunk_group(identifier, &compilation.chunk_group_by_ukey);
+            chunk_group
+              .chunks
+              .iter()
+              .filter_map(|chunk_ukey| {
+                let chunk = compilation
+                  .chunk_by_ukey
+                  .get(chunk_ukey)
+                  .expect("chunk should exist");
+                if &chunk.runtime == runtime {
+                  Some(chunk.expect_id().to_string())
+                } else {
+                  None
+                }
+              })
+              .collect::<Vec<_>>()
+          };
+          chunk_ids.sort_unstable();
+          let module = compilation
+            .module_graph
+            .module_graph_module_by_identifier(identifier)
+            .expect("no module found");
 
-        (module_id_expr, chunk_ids)
-      })
-      .collect::<HashMap<String, Vec<String>>>();
+          let module_id = module.id(&compilation.chunk_graph);
+          let module_id_expr = serde_json::to_string(module_id).expect("invalid module_id");
 
-    let mut source = ConcatSource::default();
-    source.add(RawSource::from(format!(
-      "var map = {};\n",
-      &stringify_map(&map)
-    )));
-    source.add(RawSource::from(
-      "
-    __webpack_require__.el = function(module) {
-        var chunkId = map[module];
-        if (chunkId === undefined) {
-            return Promise.resolve();
-        }
-        if (chunkId.length > 1) {
-          return Promise.all(chunkId.map(__webpack_require__.e));
-        } else {
-          return __webpack_require__.e(chunkId[0]);
-        };
+          // TODO if chunk_ids is empty, here should remove the filed
+          (module_id_expr, chunk_ids)
+        })
+        .collect::<HashMap<String, Vec<String>>>();
+
+      let mut source = ConcatSource::default();
+      source.add(RawSource::from(format!(
+        "var map = {};\n",
+        &stringify_map(&map)
+      )));
+      source.add(RawSource::from(
+        "__webpack_require__.el = function(module) {
+          var chunkId = map[module];
+          if (chunkId === undefined) {
+              return Promise.resolve();
+          }
+          if (chunkId.length > 1) {
+            return Promise.all(chunkId.map(__webpack_require__.e));
+          } else {
+            return __webpack_require__.e(chunkId[0]);
+          };
+      }\n",
+      ));
+
+      source.boxed()
+    } else {
+      unreachable!("should have chunk")
     }
-    ",
-    ));
+  }
 
-    source.boxed()
+  fn attach(&mut self, chunk: ChunkUkey) {
+    self.chunk = Some(chunk);
   }
 }
 
