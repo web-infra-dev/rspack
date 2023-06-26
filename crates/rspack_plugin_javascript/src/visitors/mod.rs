abd58f4c04e18db148daa50b0a13151c8f6ce03a
underfin <likui6666666@gmail.com>
2023-06-26 13:44:31 +0800

    fix: fix test

--- crates/rspack_plugin_javascript/src/visitors/mod.rs
+++ crates/rspack_plugin_javascript/src/visitors/mod.rs
@@ -148,6 +148,7 @@ pub fn run_before_pass(
           syntax.typescript()
         ),
         !resource_path.contains("@swc/helpers")
+          && !resource_path.contains("@swc\\helpers") // window path
           && !resource_path.contains("tslib")
           && !resource_path.contains("core-js")
       ),
