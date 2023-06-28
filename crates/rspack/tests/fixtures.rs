2a0676a68450e5b7eb09e8ee5ce0a73e8c18a2d3
underfin <likui6666666@gmail.com>
2023-06-28 20:59:30 +0800

    fix: remove uncessary change

--- crates/rspack/tests/fixtures.rs
+++ crates/rspack/tests/fixtures.rs
@@ -4,7 +4,7 @@ use rspack_testing::test_fixture;
 use rspack_tracing::enable_tracing_by_env;
 use testing_macros::fixture;
 
-#[fixture("tests/fixtures/static-import")]
+#[fixture("tests/fixtures/*")]
 fn rspack(fixture_path: PathBuf) {
   enable_tracing_by_env();
   test_fixture(&fixture_path);
