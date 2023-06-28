8de5814e26c165672468f5ec30305fb4d75c6719
underfin <likui6666666@gmail.com>
2023-06-29 00:03:09 +0800

    fix: test

--- crates/rspack/tests/fixtures/simple-with-query/expected/main.js
+++ crates/rspack/tests/fixtures/simple-with-query/expected/main.js
@@ -8,12 +8,12 @@ __webpack_require__.d(exports, {'a': function() { return a; }});
 "./index.js": function (module, exports, __webpack_require__) {
 'use strict';
 __webpack_require__.r(exports);
-/* harmony import */var _b_jsx__WEBPACK_IMPORTED_MODULE__ = __webpack_require__(/* ./b.jsx */"./b.jsx");
-/* harmony import */var _b_js_x__WEBPACK_IMPORTED_MODULE__ = __webpack_require__(/* ./b.js?x */"./b.js?x");
+/* harmony import */var _b_jsx__WEBPACK_IMPORTED_MODULE_0_ = __webpack_require__(/* ./b.jsx */"./b.jsx");
+/* harmony import */var _b_js_x__WEBPACK_IMPORTED_MODULE_1_ = __webpack_require__(/* ./b.js?x */"./b.js?x");
 
 
-_b_js_x__WEBPACK_IMPORTED_MODULE__["a"];
-_b_jsx__WEBPACK_IMPORTED_MODULE__["a"];
+_b_js_x__WEBPACK_IMPORTED_MODULE_1_["a"];
+_b_jsx__WEBPACK_IMPORTED_MODULE_0_["a"];
 console.log("hello, world");
 },
 "./b.jsx": function (module, exports, __webpack_require__) {
