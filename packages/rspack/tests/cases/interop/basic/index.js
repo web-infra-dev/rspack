650d64d1fb94beba6c8f4308a14f4eabc5485938
underfin <likui6666666@gmail.com>
2023-07-03 17:18:22 +0800

    fix: update test

--- packages/rspack/tests/cases/interop/basic/index.js
+++ packages/rspack/tests/cases/interop/basic/index.js
@@ -1,5 +1,10 @@
 import a_default from "./a";
 import * as a_all from "./a";
+import * as esm from "./esm";
+
+it("should exports __esModule", function () {
+	expect(esm.__esModule).toBe(true);
+});
 
 it("should have interop when import cjs", function () {
 	expect(a_default.test).toBe(a_all.test);
