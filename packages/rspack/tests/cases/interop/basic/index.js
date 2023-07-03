73ddc83978c788541e7dfe981547b4cb95537a2a
underfin <likui6666666@gmail.com>
2023-07-03 17:22:03 +0800

    fix: update test

--- packages/rspack/tests/cases/interop/basic/index.js
+++ packages/rspack/tests/cases/interop/basic/index.js
@@ -6,6 +6,11 @@ it("should exports __esModule", function () {
 	expect(esm.__esModule).toBe(true);
 });
 
+it("should hasn't exports at esm module", function () {
+	// because it run at node.js, so it always exit.
+	// expect(exports).toBe(undefined);
+});
+
 it("should have interop when import cjs", function () {
 	expect(a_default.test).toBe(a_all.test);
 });
