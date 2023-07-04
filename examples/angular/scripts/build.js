cb9cff4de4351f3b7717910ce7ea16a36dedf890
Hana <andywangsy@gmail.com>
2023-07-04 13:53:25 +0800

    fix(minifier): fix minifier options difference between minifications

--- examples/angular/scripts/build.js
+++ examples/angular/scripts/build.js
@@ -1,31 +1,37 @@
 const { spawn, spawnSync } = require("node:child_process");
 const ls = spawn("node", ["-v"]);
 
-ls.stdout.on("data", (data) => {
+ls.stdout.on("data", data => {
 	let res = data.toString().trim();
 	const [a, _] = res.split(".");
 	if (a >= "v16") {
 		build()
 			.then(console.log)
-			.catch((err) => {
+			.catch(err => {
 				console.error(err);
-				process.exit(-1);
+				process.exit(1);
 			});
 	} else {
-		console.log("The Angular CLI requires a minimum of v16.13")
+		console.log("The Angular CLI requires a minimum of v16.13");
 		// Working around angular not support node14, but we need to test node v14 in CI
 		process.exit(0);
 	}
 });
 
-ls.stderr.on("data", (data) => {
+ls.stderr.on("data", data => {
 	throw new Error(`${data}`);
 });
 
 function build() {
 	return new Promise((resolve, reject) => {
 		try {
-			spawnSync("npx", ["rspack", "build"], { stdio: "inherit" });
+			const { status, error, stderr } = spawnSync("npx", ["rspack", "build"], {
+				stdio: "inherit"
+			});
+			if (status !== 0) {
+				reject(stderr?.toString() || error);
+				return;
+			}
 			resolve();
 		} catch (err) {
 			reject(err);
