c5a57b28d69cb66c4131ada0d22e43a1300ee619
ahabhgk <ahabhgk@gmail.com>
2023-07-10 17:00:31 +0800

    comment

--- webpack-test/cases/wasm/wasm-explorer-examples-sync/test.filter.js
+++ webpack-test/cases/wasm/wasm-explorer-examples-sync/test.filter.js
@@ -3,5 +3,3 @@ var supportsWebAssembly = require("../../../helpers/supportsWebAssembly");
 module.exports = function(config) {
 	return supportsWebAssembly();
 };
-
-// module.exports = () => {return false}
