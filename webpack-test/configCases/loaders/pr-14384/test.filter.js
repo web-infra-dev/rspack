e7d14ab2c1d66a8339c7b15aaef1945cf64db092
Hana <andywangsy@gmail.com>
2023-07-10 17:28:29 +0800

    test: explain the current blockers

--- webpack-test/configCases/loaders/pr-14384/test.filter.js
+++ webpack-test/configCases/loaders/pr-14384/test.filter.js
@@ -1,1 +1,1 @@
-module.exports = () => {return false}
+module.exports = () => {return "Rspack does not export `NormalModule` and support `beforeLoaders` from JS side and whether to implemented is tracked here: https://github.com/orgs/web-infra-dev/projects/9"}
