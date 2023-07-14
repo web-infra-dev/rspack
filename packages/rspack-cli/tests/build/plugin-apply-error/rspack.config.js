4d99a8541a47e13aecad91c3e5286d6704cbee61
ahabhgk <ahabhgk@gmail.com>
2023-07-14 18:06:36 +0800

    add test

---
+++ packages/rspack-cli/tests/build/plugin-apply-error/rspack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	mode: "development",
+	devtool: false,
+	plugins: [
+		{
+			name: "test-plugin",
+			apply(compiler) {
+				throw new Error("error in plugin");
+			}
+		}
+	],
+	devServer: {}
+};
