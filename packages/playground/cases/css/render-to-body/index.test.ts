c2815788295d4973bcc6f4fbf74f543541dfca18
underfin <likui6666666@gmail.com>
2023-07-17 15:19:27 +0800

    fix: add e2e

---
+++ packages/playground/cases/css/render-to-body/index.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@/fixtures";
+
+test("should update body css", async ({ page, fileAction }) => {
+	await expect(page.locator("body")).toHaveCSS("display", "block");
+	fileAction.updateFile("src/index.css", content =>
+		content.replace("block", "flex")
+	);
+	await expect(page.locator("body")).toHaveCSS("display", "flex");
+});
