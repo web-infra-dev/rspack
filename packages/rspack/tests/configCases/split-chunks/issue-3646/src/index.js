a176d8ebe8a705a6a7915274a8a32c5e4eb986fe
Yunfei He <i.heyunfei@gmail.com>
2023-07-18 14:09:24 +0800

    feat(splitChunks): support `splitChunks.{cacheGroup}.idHint` (#3783)
    * feat(splitChunks): support `splitChunks.{cacheGroup}.idHint`
    
    * Fix test

--- packages/rspack/tests/configCases/split-chunks/issue-3646/src/index.js
+++ packages/rspack/tests/configCases/split-chunks/issue-3646/src/index.js
@@ -4,8 +4,6 @@ import "./shared";
 export default "index.js";
 
 it("issue-3646", () => {
-	expect(fs.existsSync(path.resolve(__dirname, "./defaultVendors.js"))).toBe(
-		true
-	);
+	expect(fs.existsSync(path.resolve(__dirname, "./vendors.js"))).toBe(true);
 	expect(fs.existsSync(path.resolve(__dirname, "./default.js"))).toBe(false);
 });
