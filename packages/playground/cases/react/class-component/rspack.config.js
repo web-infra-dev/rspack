dfee9aaf3a283b83c7ef51fa8f2be1a53706788e
underfin <likui6666666@gmail.com>
2023-07-20 11:34:45 +0800

    fix: react class component hmr

---
+++ packages/playground/cases/react/class-component/rspack.config.js
@@ -0,0 +1,27 @@
+/** @type { import('@rspack/core').RspackOptions } */
+module.exports = {
+	context: __dirname,
+	mode: "development",
+	entry: ["@rspack/dev-client/react-refresh-entry", "./src/index.jsx"],
+	devServer: {
+		hot: true
+	},
+	cache: false,
+	stats: "none",
+	infrastructureLogging: {
+		debug: false
+	},
+	builtins: {
+		provide: {
+			$ReactRefreshRuntime$: [require.resolve("./react-refresh.js")]
+		},
+		html: [
+			{
+				template: "./src/index.html"
+			}
+		]
+	},
+	watchOptions: {
+		poll: 1000
+	}
+};
