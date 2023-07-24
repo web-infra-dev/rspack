21cb30e5d7a91c12b269ed7b245e09a6f86cade8
Sg <suica@users.noreply.github.com>
2023-07-24 17:58:07 +0800

    add type checking for tests

--- packages/rspack/tests/configCases/type/multiple-options/rspack.config.js
+++ packages/rspack/tests/configCases/type/multiple-options/rspack.config.js
@@ -8,4 +8,8 @@ export const config2 = [{}];
 /** @type {import("@rspack/core").Configuration} */
 export const config3 = [{}, {}];
 
+/** @type {import("@rspack/core").Configuration} */
+//@ts-expect-error
+export const config4 = [{}, { devtool: true }];
+
 module.exports = config;
