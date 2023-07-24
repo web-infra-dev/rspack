1889e790aa598a6403cb0a09e3f05f0be17c4e97
Sg <suica@users.noreply.github.com>
2023-07-24 16:47:10 +0800

    add test cases

---
+++ packages/rspack/tests/configCases/type/multiple-options/respack.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+/** @type {import("@rspack/core").Configuration} */
+const config = [];
+
+/** @type {import("@rspack/core").Configuration} */
+export const config2 = [{}];
+
+/** @type {import("@rspack/core").Configuration} */
+export const config3 = [{}, {}];
+
+module.exports = config;
