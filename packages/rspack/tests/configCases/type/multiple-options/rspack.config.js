c1fe72929a7c946f116ab21dc4465917c7cbe78d
Sg <suica@users.noreply.github.com>
2023-07-24 18:06:42 +0800

    add detail cases

--- packages/rspack/tests/configCases/type/multiple-options/rspack.config.js
+++ packages/rspack/tests/configCases/type/multiple-options/rspack.config.js
@@ -1,15 +1,60 @@
 // @ts-check
-/** @type {import("@rspack/core").Configuration} */
-const config = [];
 
 /** @type {import("@rspack/core").Configuration} */
-export const config2 = [{}];
+// empty object is a valid configuration
+export const shouldAcceptEmptyObject = {};
 
 /** @type {import("@rspack/core").Configuration} */
-export const config3 = [{}, {}];
+export const shouldAcceptEmptyArr = [];
 
 /** @type {import("@rspack/core").Configuration} */
-//@ts-expect-error
-export const config4 = [{}, { devtool: true }];
+export const shouldAcceptArrWithRealWorldConfig = [
+	{
+		context: __dirname,
+		module: {
+			rules: [
+				{
+					test: /\.svg$/,
+					type: "asset/resource"
+				}
+			]
+		},
+		builtins: {
+			treeShaking: true
+		},
+		optimization: {
+			sideEffects: true
+		},
+		externalsPresets: {
+			node: true
+		}
+	}
+];
 
-module.exports = config;
+/** @type {import("@rspack/core").Configuration} */
+export const shouldRejectArrWithWrongConfig = [
+	{
+		context: __dirname,
+		module: {
+			rules: [
+				{
+					// @ts-expect-error should be a regexp
+					test: 1,
+					type: "asset/resource"
+				}
+			]
+		},
+		builtins: {
+			// @ts-expect-error should be boolean
+			treeShaking: "1"
+		},
+		// @ts-expect-error should not be an array
+		optimization: [],
+		externalsPresets: {
+			// @ts-expect-error should be string
+			node: 1
+		}
+	}
+];
+
+module.exports = shouldAcceptArrWithRealWorldConfig;
