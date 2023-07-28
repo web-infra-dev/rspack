556ed6bacfb3b1a3470fb3cbc5e0a04f4da4e0ec
underfin <likui6666666@gmail.com>
2023-07-28 14:23:30 +0800

    fix: should replace _webpack_require to __nested_webpack_require_ at _webpack_require usaged before declation

--- crates/rspack_plugin_javascript/src/visitors/dependency/compatibility_scanner.rs
+++ crates/rspack_plugin_javascript/src/visitors/dependency/compatibility_scanner.rs
@@ -23,6 +23,17 @@ impl<'a> CompatibilityScanner<'a> {
       name_map: Default::default(),
     }
   }
+
+  fn get_nested_webpack_require_(&mut self, ctxt: SyntaxContext) -> String {
+    let count = if let Some(count) = self.name_map.get(&ctxt) {
+      count
+    } else {
+      self.count += 1;
+      self.name_map.insert(ctxt, self.count);
+      &self.count
+    };
+    format!("__nested_webpack_require_{}__", count)
+  }
 }
 
 impl Visit for CompatibilityScanner<'_> {
@@ -30,32 +41,29 @@ impl Visit for CompatibilityScanner<'_> {
 
   fn visit_ident(&mut self, ident: &Ident) {
     if &ident.sym == "__webpack_require__" && ident.span.ctxt != *self.unresolved_ctxt {
-      if let Some(count) = self.name_map.get(&ident.span.ctxt) {
-        self
-          .presentational_dependencies
-          .push(Box::new(ConstDependency::new(
-            ident.span.real_lo(),
-            ident.span.real_hi(),
-            format!("__nested_webpack_require_{count}__").into(),
-            None,
-          )));
-      }
+      let value = self.get_nested_webpack_require_(ident.span.ctxt);
+      self
+        .presentational_dependencies
+        .push(Box::new(ConstDependency::new(
+          ident.span.real_lo(),
+          ident.span.real_hi(),
+          value.into(),
+          None,
+        )));
     }
   }
 
   fn visit_fn_decl(&mut self, fn_decl: &FnDecl) {
     if &fn_decl.ident.sym == "__webpack_require__"
       && fn_decl.ident.span.ctxt != *self.unresolved_ctxt
-      && !self.name_map.contains_key(&fn_decl.ident.span.ctxt)
     {
-      self.count += 1;
-      self.name_map.insert(fn_decl.ident.span.ctxt, self.count);
+      let value = self.get_nested_webpack_require_(fn_decl.ident.span.ctxt);
       self
         .presentational_dependencies
         .push(Box::new(ConstDependency::new(
           fn_decl.ident.span.real_lo(),
           fn_decl.ident.span.real_hi(),
-          format!("__nested_webpack_require_{}__", self.count).into(),
+          value.into(),
           None,
         )));
     }
