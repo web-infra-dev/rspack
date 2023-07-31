37eafb252fd3af6b760af91f8bcbba27e459b28c
underfin <likui6666666@gmail.com>
2023-07-31 11:49:54 +0800

    fix: should replace _webpack_require to __nested_webpack_require_ at … (#3857)
    * fix: should replace _webpack_require to __nested_webpack_require_ at _webpack_require usaged before declation
    
    * fix: test
    
    * fix: test
    
    * fix: test

--- packages/rspack/tests/cases/parsing/nested-webpack-require/index.js
+++ packages/rspack/tests/cases/parsing/nested-webpack-require/index.js
@@ -1,7 +1,9 @@
+__webpack_require__.d = 1;
 function __webpack_require__() {}
 __webpack_require__.n = 1;
 
 it("__nested_webpack_require_", function () {
 	expect(typeof __webpack_require__).toBe("function");
 	expect(__webpack_require__.n).toBe(1);
+	expect(__webpack_require__.d).toBe(1);
 });
