0980837eb8994648771d2506f56466736e781eaf
Yunfei He <i.heyunfei@gmail.com>
2023-08-03 16:04:27 +0800

    perf(css): only re-parse source code while compiling css modules (#3877)
    * perf(css): only re-parse source code while compiling css modules
    
    * Update tests
    
    * Fix test
    
    * Fix test
    
    * Fix test

--- crates/node_binding/binding.d.ts
+++ crates/node_binding/binding.d.ts
@@ -491,7 +491,6 @@ export interface RawBannerConfig {
 export interface RawBuiltins {
   html?: Array<RawHtmlPluginConfig>
   css?: RawCssPluginConfig
-  postcss?: RawPostCssConfig
   minifyOptions?: RawMinification
   presetEnv?: RawPresetEnv
   define: Record<string, string>
@@ -894,10 +893,6 @@ export interface RawPluginImportConfig {
   ignoreStyleComponent?: Array<string>
 }
 
-export interface RawPostCssConfig {
-  pxtorem?: RawPxToRemConfig
-}
-
 export interface RawPresetEnv {
   targets: Array<string>
   mode?: 'usage' | 'entry'
@@ -906,16 +901,6 @@ export interface RawPresetEnv {
 
 export interface RawProgressPluginConfig {
   prefix?: string
-}
-
-export interface RawPxToRemConfig {
-  rootValue?: number
-  unitPrecision?: number
-  selectorBlackList?: Array<string>
-  propList?: Array<string>
-  replace?: boolean
-  mediaQuery?: boolean
-  minPixelValue?: number
 }
 
 export interface RawReactOptions {
