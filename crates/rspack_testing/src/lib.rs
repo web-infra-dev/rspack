3047c308cac6c52b5fc1faab0517ae45526f2cb4
yangjian.fe <yangjian.fe@bytedance.com>
2023-08-30 10:58:05 +0800

    chore: fix check

--- crates/rspack_testing/src/lib.rs
+++ crates/rspack_testing/src/lib.rs
@@ -4,8 +4,8 @@ mod run_fixture;
 mod test_config;
 pub use eval_raw::evaluate_to_json;
 pub use run_fixture::{
-  apply_from_fixture, test_fixture, test_fixture_css, test_fixture_html, test_fixture_js,
-  test_rebuild_fixture,
+  apply_from_fixture, test_fixture, test_fixture_css, test_fixture_html, test_fixture_insta,
+  test_fixture_js, test_rebuild_fixture,
 };
 pub use test_config::TestConfig;
 pub use testing_macros::{self, fixture};
