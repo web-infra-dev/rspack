72ad1dcee4b8fe563b957ae6cebe248db7bcf13b
JA+ <601286825@qq.com>
2023-08-31 20:20:47 +0800

    fix: make dev-server  globalThis =>self

--- packages/rspack-dev-client/src/reactRefreshEntry.js
+++ packages/rspack-dev-client/src/reactRefreshEntry.js
@@ -1,10 +1,4 @@
 const RefreshRuntime = require("react-refresh/runtime");
 
-let _globalThis;
-try {
-	_globalThis = globalThis;
-} catch (e) {
-	_globalThis = self;
-}
-RefreshRuntime.injectIntoGlobalHook(_globalThis);
-_globalThis.$RefreshSig$ = RefreshRuntime.createSignatureFunctionForTransform;
+RefreshRuntime.injectIntoGlobalHook(self);
+self.$RefreshSig$ = RefreshRuntime.createSignatureFunctionForTransform;
