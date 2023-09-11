3fc1c99a58a76ef58a6c3bcd168434bfaf102e96
Hana <andywangsy@gmail.com>
2023-09-11 17:04:32 +0800

    chore: more comments

--- crates/rspack_plugin_javascript/src/visitors/mod.rs
+++ crates/rspack_plugin_javascript/src/visitors/mod.rs
@@ -19,7 +19,7 @@ use swc_core::ecma::parser::Syntax;
 use swc_core::ecma::transforms::base::pass::{noop, Optional};
 
 macro_rules! either {
-  ($config: expr, $f: expr) => {
+  ($config:expr, $f:expr) => {
     if let Some(config) = &$config {
       Either::Left($f(config))
     } else {
@@ -28,8 +28,16 @@ macro_rules! either {
   };
 }
 
+/// Rspack builtins
+/// - `react`: a port of `react-refresh/babel`, and `react-fast-refresh-loader`
+/// - `relay`: a port of `babel-plugin-relay`
+/// - `import`: a port of `babel-plugin-import`
+/// - `emotion`: a port of `babel-plugin-emotion`
+#[deprecated(
+  note = "Builtin transform is deprecated and will be removed in JS version 0.5.0, see: https://github.com/web-infra-dev/rspack/pull/4133"
+)]
 #[allow(clippy::too_many_arguments)]
-fn chain_builtins_transform<'b>(
+fn builtins_transform<'b>(
   resource_data: &'b ResourceData,
   options: &'b CompilerOptions,
   module_type: &'b ModuleType,
@@ -83,13 +91,17 @@ fn chain_builtins_transform<'b>(
   )
 }
 
-fn chain_builtins_webpack_plugin<'b>(
-  options: &'b CompilerOptions,
-  unresolved_mark: Mark,
-) -> impl Fold + 'b {
+/// Webpack builtin plugins
+/// - `define`: a port of `DefinePlugin`
+/// - `provide`: a port of `ProvidePlugin`
+fn builtins_webpack_plugin(options: &CompilerOptions, unresolved_mark: Mark) -> impl Fold + '_ {
   chain!(
     Optional::new(
       rspack_swc_visitors::define(&options.builtins.define),
+      !options.builtins.define.is_empty()
+    ),
+    Optional::new(
+      builtins_webpack_plugin_define_optimizer(unresolved_mark),
       !options.builtins.define.is_empty()
     ),
     Optional::new(
@@ -99,8 +111,34 @@ fn chain_builtins_webpack_plugin<'b>(
   )
 }
 
+/// Removing this will **not** cause semantic difference, but will avoid overheads that are not necessary.
+/// Expression simplifier and dead branch remover are working together to avoid parsing not necessary path.
+///
+/// Example:
+/// ```js
+/// if(process.env.NODE_ENV === "development") {
+///   module.exports = require(".../xxx.development.js")
+/// } else {
+///   module.exports = require(".../xxx.production.js")
+/// }
+/// ```
+/// The ordering of these two is important, `expr_simplifier` goes first and `dead_branch_remover` goes second.
+///
+/// `if(process.env.NODE_ENV === "development")` -> <define> -> `if("development" === "development")`
+/// `if("development" === "development") {} else {}` -> <expr_simplifier> -> `if(true) {} else {}`
+/// `if(true) {} else {}` -> <dead_branch_remover> -> `else` branch is removed
+fn builtins_webpack_plugin_define_optimizer(unresolved_mark: Mark) -> impl Fold {
+  chain!(
+    swc_visitor::expr_simplifier(unresolved_mark, Default::default()),
+    swc_visitor::dead_branch_remover(unresolved_mark)
+  )
+}
+
 #[allow(clippy::too_many_arguments)]
-fn chain_compat_transform<'b>(
+#[deprecated(
+  note = "Compat transform is deprecated and will be removed in JS version 0.5.0, see: https://github.com/web-infra-dev/rspack/discussions/4070"
+)]
+fn compat_transform<'b>(
   resource_data: &'b ResourceData,
   options: &'b CompilerOptions,
   top_level_mark: Mark,
@@ -108,32 +146,37 @@ fn chain_compat_transform<'b>(
   assumptions: Assumptions,
   syntax: Syntax,
 ) -> impl Fold + 'b {
-  let transform_by_default = options.should_transform_by_default();
   let es_version = match options.target.es_version {
     rspack_core::TargetEsVersion::Esx(es_version) => Some(es_version),
     _ => None,
   };
-
   let resource_path = resource_data.resource_path.to_string_lossy();
 
-  Optional::new(
-    swc_visitor::compat(
-      options.builtins.preset_env.clone(),
-      es_version,
-      assumptions,
-      top_level_mark,
-      unresolved_mark,
-      None,
-      syntax.typescript(),
+  chain!(
+    Optional::new(
+      swc_visitor::export_default_from(),
+      syntax.export_default_from()
     ),
-    transform_by_default
-      && !resource_path.contains("@swc/helpers")
-      && !resource_path.contains("tslib")
-      && !resource_path.contains("core-js"),
+    swc_visitor::paren_remover(None),
+    Optional::new(
+      swc_visitor::compat(
+        options.builtins.preset_env.clone(),
+        es_version,
+        assumptions,
+        top_level_mark,
+        unresolved_mark,
+        None,
+        syntax.typescript(),
+      ),
+      !resource_path.contains("@swc/helpers")
+        && !resource_path.contains("tslib")
+        && !resource_path.contains("core-js"),
+    ),
+    swc_visitor::inject_helpers(unresolved_mark),
+    swc_visitor::reserved_words(),
   )
 }
 
-/// return (ast, top_level_mark, unresolved_mark, globals)
 #[allow(clippy::too_many_arguments)]
 pub fn run_before_pass(
   resource_data: &ResourceData,
@@ -144,8 +187,6 @@ pub fn run_before_pass(
   module_type: &ModuleType,
   source: &str,
 ) -> Result<()> {
-  let transform_by_default = options.should_transform_by_default();
-
   let cm = ast.get_context().source_map.clone();
   ast.transform_with_handler(cm.clone(), |_handler, program, context| {
     let top_level_mark = context.top_level_mark;
@@ -162,43 +203,44 @@ pub fn run_before_pass(
       swc_visitor::resolver(unresolved_mark, top_level_mark, syntax.typescript()),
       Optional::new(
         swc_visitor::decorator(assumptions, &options.builtins.decorator),
-        syntax.decorators()
+        // Decorator transformation varies from `ModuleType`,
+        // - TypeScript-like: decorators will be transformed by default, with legacy settings and will emit meta data.
+        //      Since this is a default behavior with tsc.
+        // - JavaScript-like: decorators will not be transformed, the syntax will be kept as-is.
+        // Difference between Rspack and Webpack:
+        //   Webpack's parser does not support an input with decorator. The parser will complain.
+        //   Still, the overall principle of JavaScript-like syntax is to keep the minimal amount of the transformation.
+        //   (i.e transform only when it is necessary)
+        (options.should_transform_by_default() || syntax.typescript()) && syntax.decorators()
       ),
       Optional::new(
         swc_visitor::typescript(assumptions, top_level_mark, comments, &cm),
         syntax.typescript()
       ),
-      chain_builtins_transform(
-        resource_data,
-        options,
-        module_type,
-        source,
-        top_level_mark,
-        unresolved_mark,
-        cm
-      ),
-      chain_builtins_webpack_plugin(options, unresolved_mark),
       Optional::new(
-        swc_visitor::export_default_from(),
-        syntax.export_default_from()
+        builtins_transform(
+          resource_data,
+          options,
+          module_type,
+          source,
+          top_level_mark,
+          unresolved_mark,
+          cm
+        ),
+        options.should_transform_by_default()
       ),
-      swc_visitor::paren_remover(comments.map(|v| v as &dyn Comments)),
-      chain_compat_transform(
-        resource_data,
-        options,
-        top_level_mark,
-        unresolved_mark,
-        assumptions,
-        syntax
-      ),
-      Optional::new(swc_visitor::reserved_words(), transform_by_default),
       Optional::new(
-        swc_visitor::inject_helpers(unresolved_mark),
-        transform_by_default
+        compat_transform(
+          resource_data,
+          options,
+          top_level_mark,
+          unresolved_mark,
+          assumptions,
+          syntax
+        ),
+        options.should_transform_by_default()
       ),
-      // The ordering of these two is important, `expr_simplifier` goes first and `dead_branch_remover` goes second.
-      swc_visitor::expr_simplifier(unresolved_mark, Default::default()),
-      swc_visitor::dead_branch_remover(unresolved_mark),
+      builtins_webpack_plugin(options, unresolved_mark),
       swc_visitor::hygiene(false, top_level_mark),
       swc_visitor::fixer(comments.map(|v| v as &dyn Comments)),
     );
