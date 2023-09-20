d62987439b02e3b6a43d26c16f93a3a607e474c5
ahabhgk <ahabhgk@gmail.com>
2023-09-20 19:03:59 +0800

    fix

--- crates/rspack_plugin_runtime/src/runtime_module/jsonp_chunk_loading.rs
+++ crates/rspack_plugin_runtime/src/runtime_module/jsonp_chunk_loading.rs
@@ -87,7 +87,7 @@ impl RuntimeModule for JsonpChunkLoadingRuntimeModule {
         include_str!("runtime/jsonp_chunk_loading_with_hmr.js")
           .replace("$globalObject$", &compilation.options.output.global_object)
           .replace(
-            "$hotUpdateGloabl$",
+            "$hotUpdateGlobal$",
             &serde_json::to_string(&compilation.options.output.hot_update_global)
               .expect("failed to serde_json::to_string(hot_update_global)"),
           ),
