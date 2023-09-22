2ead61c9db4f20f683845d68f9103c0786f6ca4a
Hana <andywangsy@gmail.com>
2023-09-22 18:03:54 +0800

    fix: fix react-refresh `self` is not defined (#4232)

--- packages/rspack-plugin-react-refresh/client/reactRefreshEntry.js
+++ packages/rspack-plugin-react-refresh/client/reactRefreshEntry.js
@@ -1,4 +1,50 @@
-const RefreshRuntime = require("react-refresh/runtime");
+/**
+ * The following code is modified based on
+ * https://github.com/pmmmwh/react-refresh-webpack-plugin/blob/0b960573797bf38926937994c481e4fec9ed8aa6/client/ReactRefreshEntry.js
+ *
+ * MIT Licensed
+ * Author Michael Mok
+ * Copyright (c) 2019 Michael Mok
+ * https://github.com/pmmmwh/react-refresh-webpack-plugin/blob/0b960573797bf38926937994c481e4fec9ed8aa6/LICENSE
+ */
 
-RefreshRuntime.injectIntoGlobalHook(self);
-self.$RefreshSig$ = RefreshRuntime.createSignatureFunctionForTransform;
+var RefreshRuntime = require("react-refresh/runtime");
+var safeThis = (function () {
+	// copied from core-js-pure/features/global-this
+	"use strict";
+
+	var check = function (it) {
+		return it && it.Math == Math && it;
+	};
+
+	// https://github.com/zloirock/core-js/issues/86#issuecomment-115759028
+	// eslint-disable-next-line es/no-global-this -- safe
+	return (
+		check(typeof globalThis == "object" && globalThis) ||
+		check(typeof window == "object" && window) ||
+		// eslint-disable-next-line no-restricted-globals -- safe
+		check(typeof self == "object" && self) ||
+		check(typeof __webpack_require__.g == "object" && __webpack_require__.g) ||
+		// eslint-disable-next-line no-new-func -- fallback
+		(function () {
+			return this;
+		})() ||
+		this ||
+		Function("return this")()
+	);
+})();
+
+if (process.env.NODE_ENV !== "production") {
+	if (typeof safeThis !== "undefined") {
+		var $RefreshInjected$ = "__reactRefreshInjected";
+		// Only inject the runtime if it hasn't been injected
+		if (!safeThis[$RefreshInjected$]) {
+			RefreshRuntime.injectIntoGlobalHook(safeThis);
+			safeThis.$RefreshSig$ =
+				RefreshRuntime.createSignatureFunctionForTransform;
+
+			// Mark the runtime as injected to prevent double-injection
+			safeThis[$RefreshInjected$] = true;
+		}
+	}
+}
