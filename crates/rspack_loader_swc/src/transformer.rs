01a07ef7135b0abbf3f5417ff708ef1ad8c4caf8
Asuka109 <asuka109@foxmail.com>
2023-10-07 21:02:22 +0800

    feat: use `once_cell::sync::Lazy` to compute content hash lazily

--- crates/rspack_loader_swc/src/transformer.rs
+++ crates/rspack_loader_swc/src/transformer.rs
@@ -2,11 +2,13 @@ use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 use either::Either;
+use once_cell::sync::Lazy;
 use rspack_core::CompilerOptions;
 use rspack_swc_visitors::{styled_components, StyledComponentsOptions};
 use swc_core::common::FileName;
 use swc_core::common::{chain, comments::Comments, Mark, SourceMap};
 use swc_core::ecma::{transforms::base::pass::noop, visit::Fold};
+use xxhash_rust::xxh32::xxh32;
 
 use crate::options::RspackExperiments;
 
@@ -35,19 +37,18 @@ pub(crate) fn transform<'a>(
   _top_level_mark: Mark,
   unresolved_mark: Mark,
   cm: Arc<SourceMap>,
-  content_hash: Option<u32>,
+  content: String,
   rspack_experiments: &'a RspackExperiments,
 ) -> impl Fold + 'a {
   use rspack_swc_visitors::EmotionOptions;
+  let content_hash = Lazy::new(|| xxh32(content.as_bytes(), 0));
 
   chain!(
     either!(rspack_experiments.emotion, |options: &EmotionOptions| {
-      // SAFETY: Source content hash should always available if emotion is turned on.
-      let content_hash = content_hash.expect("Content hash should be available");
       rspack_swc_visitors::emotion(
         options.clone(),
         resource_path,
-        content_hash,
+        *content_hash,
         cm.clone(),
         comments,
       )
@@ -55,10 +56,9 @@ pub(crate) fn transform<'a>(
     either!(
       rspack_experiments.styled_components,
       |options: &StyledComponentsOptions| {
-        let content_hash = content_hash.expect("Content hash should be available");
         styled_components(
           FileName::Real(resource_path.into()),
-          content_hash.into(),
+          (*content_hash).into(),
           options.clone(),
         )
       }
