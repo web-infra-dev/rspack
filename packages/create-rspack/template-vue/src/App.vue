b6640f503811bfc4ef75a74fe4da4b7e6f286c08
btea <2356281422@qq.com>
2023-10-08 10:12:02 +0800

    chore: component style remains consistent

--- packages/create-rspack/template-vue/src/App.vue
+++ packages/create-rspack/template-vue/src/App.vue
@@ -25,15 +25,17 @@ export default {
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: filter 300ms;
 }
+
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+	filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+	filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
