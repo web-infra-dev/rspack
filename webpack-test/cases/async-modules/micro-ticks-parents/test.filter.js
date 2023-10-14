de54be599a29c2d2ac8d4d292dd130e522f06c15
Gengkun <ahabhgk@gmail.com>
2023-10-14 16:26:45 +0800

    feat: comment test markdown for webpack-test (#4316)
    * feat: emit test markdown for webpack-test
    
    * fix
    
    * fmt

--- webpack-test/cases/async-modules/micro-ticks-parents/test.filter.js
+++ webpack-test/cases/async-modules/micro-ticks-parents/test.filter.js
@@ -6,6 +6,6 @@ module.exports = function (config) {
 };
 
 */
-module.exports = () => {return false}
+module.exports = () => {return "https://github.com/web-infra-dev/rspack/issues/3889"}
 
 							
