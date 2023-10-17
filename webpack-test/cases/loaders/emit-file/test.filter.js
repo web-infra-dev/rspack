70fe2022acca4b31d57f03310edc4109a9b65fa4
Gengkun <ahabhgk@gmail.com>
2023-10-17 14:08:23 +0800

    fix: detect cjs assign esModule mark (#4330)
    * fix: detect cjs assign esModule mark
    
    * fix

--- webpack-test/cases/loaders/emit-file/test.filter.js
+++ webpack-test/cases/loaders/emit-file/test.filter.js
@@ -1,10 +1,3 @@
-
-/*
 module.exports = function (config) {
 	return !config.module;
 };
-
-*/
-module.exports = () => {return "blocked by https://github.com/web-infra-dev/rspack/issues/2408"}
-
-							
