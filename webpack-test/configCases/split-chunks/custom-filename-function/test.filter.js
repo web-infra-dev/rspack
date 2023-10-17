6295db5dc1d4fbe1f1f99fe4c5e1588061174b06
jserfeng <1114550440@qq.com>
2023-10-17 14:58:08 +0800

    chore: align webpack tests

--- webpack-test/configCases/split-chunks/custom-filename-function/test.filter.js
+++ webpack-test/configCases/split-chunks/custom-filename-function/test.filter.js
@@ -1,1 +1,1 @@
-module.exports = () => {return false}
+module.exports = () => 'not support function type of splitChunks https://github.com/web-infra-dev/rspack/issues/4333'
--- webpack-test/configCases/split-chunks/entry-point-error/test.filter.js
+++ webpack-test/configCases/split-chunks/entry-point-error/test.filter.js
@@ -1,1 +1,1 @@
-module.exports = () => {return false}
+module.exports = () => 'should emit error if entry point and splitted chunk have the same name https://github.com/web-infra-dev/rspack/issues/4332'
