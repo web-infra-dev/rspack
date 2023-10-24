7ab23f4d394de2a73c4a8fdccd8452e93a40e362
lingyucoder <lingyucoder@gmail.com>
2023-10-24 18:55:27 +0800

    feat(runtime-diff): alignment of global runtime module

--- crates/rspack_plugin_runtime/src/runtime_module/runtime/global.js
+++ crates/rspack_plugin_runtime/src/runtime_module/runtime/global.js
@@ -1,8 +1,8 @@
 __webpack_require__.g = (function () {
-	if (typeof globalThis === "object") return globalThis;
+	if (typeof globalThis === 'object') return globalThis;
 	try {
-		return this || new Function("return this")();
+		return this || new Function('return this')();
 	} catch (e) {
-		if (typeof window === "object") return window;
+		if (typeof window === 'object') return window;
 	}
 })();
