3cf060bcdd67830a652a2e84670237b21444c791
SyMind <dacongsama@live.com>
2023-10-25 16:36:48 +0800

    chore: stats case for LimitChunkCountPlugin

---
+++ packages/rspack/tests/statsCases/limit-chunk-count-plugin/webpack.config.js
@@ -0,0 +1,24 @@
+// TODO: In the future, when rspack supports `require.ensure`, this test case needs to be changed.
+
+var webpack = require("@rspack/core");
+/** @type {import("@rspack/core").Configuration[]} */
+module.exports = [1, 2, 3, 4].map(n => ({
+	name: `${n} chunks`,
+	mode: "production",
+	entry: "./index",
+	output: {
+		filename: `bundle${n}.js`
+	},
+	plugins: [
+		new webpack.optimize.LimitChunkCountPlugin({
+			maxChunks: n
+		})
+	],
+	stats: {
+		chunkModules: true,
+		// dependentModules: true,
+		chunkRelations: true,
+		modules: false,
+		chunks: true
+	}
+}));
