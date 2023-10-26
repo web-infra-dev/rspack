71a9c5657653309beda52f50f8ca4fcd2b0699cf
faga <lzc295592@163.com>
2023-10-26 16:15:36 +0800

    chore: update test case

--- packages/rspack/tests/fixtures/sourceFilename.js
+++
@@ -1,7 +0,0 @@
-const png = require('./empty.png');
-function render() {
-    document.getElementById('root').innerHTML = `The answer to the universe is ${answer}.`;
-    const img = document.createElement('img');
-    img.src = png;
-}
-render()
