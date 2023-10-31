2c8c6965d9e763a297b8c5d5db7ff952f4a56787
chenjiahan <chenjiahan.jait@bytedance.com>
2023-10-31 15:23:03 +0800

    fix(plugin-react-refresh): add type declaration for entry

---
+++ packages/rspack-plugin-react-refresh/src/index.d.ts
@@ -0,0 +1,20 @@
+import type { Compiler } from "@rspack/core";
+
+/**
+ * @typedef {Object} Options
+ * @property {(string | RegExp | (string | RegExp)[] | null)=} include included resourcePath for loader
+ * @property {(string | RegExp | (string | RegExp)[] | null)=} exclude excluded resourcePath for loader
+ */
+export default class ReactRefreshRspackPlugin {
+	/**
+	 * @param {Options} options
+	 */
+	constructor(options?: {
+		include?: string | RegExp | (string | RegExp)[] | null;
+		exclude?: string | RegExp | (string | RegExp)[] | null;
+	});
+	/**
+	 * @param {import("@rspack/core").Compiler} compiler
+	 */
+	apply(compiler: Compiler): void;
+}
