d808046b07900a8e360589e575f951449e9a7b2c
LingyuCoder <--global>
2023-11-03 11:49:11 +0800

    chore(runtime-diff): alignment of startup entrypoint runtime module

--- crates/rspack_plugin_runtime/src/runtime_module/runtime/startup_entrypoint_with_async.js
+++ crates/rspack_plugin_runtime/src/runtime_module/runtime/startup_entrypoint_with_async.js
@@ -1,12 +1,9 @@
-__webpack_require__.X = function(result, chunkIds, fn) {
+__webpack_require__.X = function (result, chunkIds, fn) {
     // arguments: chunkIds, moduleId are deprecated
     var moduleId = chunkIds;
-    if(!fn) {
-        chunkIds = result;
-        fn = function() {
-            return __webpack_require__(__webpack_require__.s = moduleId);
-        }
-    }
+    if (!fn) chunkIds = result, fn = function () {
+        return __webpack_require__(__webpack_require__.s = moduleId);
+    };
     return Promise.all(chunkIds.map(__webpack_require__.e, __webpack_require__)).then(function () {
         var r = fn();
         return r === undefined ? result : r;
