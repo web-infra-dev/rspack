8fd2f8616552516834a398c661011273f5fbf70b
Y80 <40132433+Y80@users.noreply.github.com>
2023-11-09 11:38:13 +0800

    feat(example): vue2-tsx (#4573)
    * feat(example): vue2-tsx
    
    * chore: update pnpm-lock.yaml

---
+++ examples/vue2-tsx/src/components/SFC_TSX_Counter.vue
@@ -0,0 +1,20 @@
+<script lang="tsx">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const count = ref<number>(0)
+
+    function add() {
+      count.value++
+    }
+
+    return () => (
+      <button onClick={add}>
+        <span>SFC_TSX_Counter: </span>
+        <strong>{count.value}</strong>
+      </button>
+    )
+  },
+})
+</script>
