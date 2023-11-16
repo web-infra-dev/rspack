c54540f8f4417663f074a2a4687a2c20b6b7c740
ahabhgk <ahabhgk@gmail.com>
2023-11-16 14:51:58 +0800

    comment

--- packages/rspack/src/index.ts
+++ packages/rspack/src/index.ts
@@ -1,7 +1,7 @@
 import { rspack as rspackFn } from "./rspack";
 import * as rspackExports from "./exports";
 
-// add
+// add exports on rspack() function
 const rspack = Object.assign(rspackFn, rspackExports);
 export { rspack };
 export { rspack as webpack };
