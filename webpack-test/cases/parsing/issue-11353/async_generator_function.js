2456a4f676600aec6a627cce7f537953381fe643
Boshen <boshenc@gmail.com>
2023-11-17 19:05:30 +0800

    fix(webpack-test): fix the slow 5 seconds test (#4694)

--- webpack-test/cases/parsing/issue-11353/async_generator_function.js
+++ webpack-test/cases/parsing/issue-11353/async_generator_function.js
@@ -3,7 +3,7 @@
 export default async function* asyncIdMaker(start = 1, end = 5){
 	for (let i = start; i <= end; i++) {
 
-		await new Promise(resolve => setTimeout(resolve, 1000));
+		await new Promise(resolve => setTimeout(resolve, 1));
 
 		yield i;
 	}
