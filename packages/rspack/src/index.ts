b42d0f2485ceb0f5a699a592a6812d887bb5e5f2
SyMind <dacongsama@live.com>
2023-11-19 12:08:35 +0800

    Revert "feat: support util.serialization"
    This reverts commit 5f99c39b6e8a43cbf1e1be9413ddc6ac4282d5fe.

--- packages/rspack/src/index.ts
+++ packages/rspack/src/index.ts
@@ -65,10 +65,4 @@ export const optimize = { LimitChunkCountPlugin };
 
 export const sources = require("webpack-sources");
 
-export const util = {
-	get serialization() {
-		return require("./util/serialization");
-	}
-};
-
 export { Watching };
