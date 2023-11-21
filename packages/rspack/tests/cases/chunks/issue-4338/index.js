5078b1ed4e7cd6b7d4724e77137bfe0f6327e3ce
bohan <bohan-zhang@foxmail.com>
2023-11-21 14:23:14 +0800

    fix: sort by chunk name before merge duplicate chunk

--- packages/rspack/tests/cases/chunks/issue-4338/index.js
+++ packages/rspack/tests/cases/chunks/issue-4338/index.js
@@ -7,8 +7,8 @@ it("should has correctly output", () => {
 	const dir = fs.readdirSync(__dirname);
 	expect(dir).toStrictEqual([
 		"0.js",
-		"3.js", // should be 2.js in webpack, https://github.com/web-infra-dev/rspack/discussions/4697
-		"c1.js", // should be c0.js in webpack
+		"2.js",
+		"c0.js",
 		"c2.js",
 		"main.js",
 		"one.js",
