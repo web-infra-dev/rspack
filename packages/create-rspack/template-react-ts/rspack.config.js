75b7317f8e6558ab83990875245875ab08b7ba56
hardfist <yangjian.fe@bytedance.com>
2023-11-23 11:58:30 +0800

    chore: upgrade template to 0.4.0

--- packages/create-rspack/template-react-ts/rspack.config.js
+++ packages/create-rspack/template-react-ts/rspack.config.js
@@ -1,4 +1,6 @@
 const rspack = require("@rspack/core");
+const refreshPlugin = require("@rspack/plugin-react-refresh");
+const isDev = process.env.NODE_ENV === "development";
 /**
  * @type {import('@rspack/cli').Configuration}
  */
@@ -12,12 +14,44 @@ module.exports = {
 			{
 				test: /\.svg$/,
 				type: "asset"
+			},
+			{
+				test: /\.(jsx?|tsx?)$/,
+				use: [
+					{
+						loader: "builtin:swc-loader",
+						options: {
+							sourceMap: true,
+							jsc: {
+								parser: {
+									syntax: "typescript",
+									tsx: true
+								},
+								transform: {
+									react: {
+										runtime: "automatic",
+										development: isDev,
+										refresh: isDev
+									}
+								}
+							},
+							env: {
+								targets: "Chrome >= 48"
+							}
+						}
+					}
+				]
 			}
 		]
 	},
 	plugins: [
+		new rspack.DefinePlugin({
+			"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+		}),
+		new rspack.ProgressPlugin({}),
 		new rspack.HtmlRspackPlugin({
 			template: "./index.html"
-		})
+		}),
+		new refreshPlugin()
 	]
 };
--- packages/create-rspack/template-vue/rspack.config.js
+++ packages/create-rspack/template-vue/rspack.config.js
@@ -1,6 +1,6 @@
 const rspack = require("@rspack/core");
 const { VueLoaderPlugin } = require("vue-loader");
-
+const isDev = process.env.NODE_ENV == "development";
 /** @type {import('@rspack/cli').Configuration} */
 const config = {
 	context: __dirname,
@@ -21,6 +21,33 @@ const config = {
 				options: {
 					experimentalInlineMatchResource: true
 				}
+			},
+			{
+				test: /\.(jsx?|tsx?)$/,
+				use: [
+					{
+						loader: "builtin:swc-loader",
+						options: {
+							sourceMap: true,
+							jsc: {
+								parser: {
+									syntax: "typescript",
+									tsx: true
+								},
+								transform: {
+									react: {
+										runtime: "automatic",
+										development: isDev,
+										refresh: isDev
+									}
+								}
+							},
+							env: {
+								targets: "Chrome >= 48"
+							}
+						}
+					}
+				]
 			},
 			{
 				test: /\.svg/,
