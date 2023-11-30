246bbdff03af6044ee767b2065801a094a8ae9e9
Boshen <boshenc@gmail.com>
2023-11-30 18:30:08 +0800

    fix(resolver): resolve query and fragments with unicode filenames (#4830)
    closes #4630
    
    Source: https://github.com/oxc-project/oxc/pull/1591

--- packages/rspack/tests/cases/resolve/query/index.js
+++ packages/rspack/tests/cases/resolve/query/index.js
@@ -11,4 +11,13 @@ it("should make different modules for query", function () {
 	expect(a === b).toBe(false);
 	expect(a === c).toBe(false);
 	expect(b === c).toBe(false);
+
+	var d = require("./测试");
+	var e = require("./测试?1");
+	var f = require("./测试.js");
+	var g = require("./测试.js?1");
+	expect(d).toBe("测试");
+	expect(e).toBe("测试");
+	expect(f).toBe("测试");
+	expect(g).toBe("测试");
 });
