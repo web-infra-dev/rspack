961d78511304e73c7af47d7f9766072835dcb427
LingyuCoder <--global>
2023-12-04 18:12:40 +0800

    chore: align wasm async chunk loading with webpack

--- crates/rspack_plugin_wasm/src/runtime.rs
+++ crates/rspack_plugin_wasm/src/runtime.rs
@@ -1,5 +1,8 @@
 use rspack_core::rspack_sources::{BoxSource, RawSource, SourceExt};
-use rspack_core::{impl_runtime_module, Compilation, RuntimeModule, RuntimeModuleStage};
+use rspack_core::{
+  get_filename_without_hash_length, impl_runtime_module, ChunkUkey, Compilation, PathData,
+  RuntimeModule, RuntimeModuleStage,
+};
 use rspack_identifier::Identifier;
 
 #[derive(Debug, Eq)]
@@ -7,14 +10,20 @@ pub struct AsyncWasmLoadingRuntimeModule {
   generate_load_binary_code: String,
   id: Identifier,
   supports_streaming: bool,
+  chunk: ChunkUkey,
 }
 
 impl AsyncWasmLoadingRuntimeModule {
-  pub fn new(generate_load_binary_code: String, supports_streaming: bool) -> Self {
+  pub fn new(
+    generate_load_binary_code: String,
+    supports_streaming: bool,
+    chunk: ChunkUkey,
+  ) -> Self {
     Self {
       generate_load_binary_code,
       id: Identifier::from("webpack/runtime/async_wasm_loading"),
       supports_streaming,
+      chunk,
     }
   }
 }
@@ -23,10 +32,35 @@ impl RuntimeModule for AsyncWasmLoadingRuntimeModule {
   fn name(&self) -> Identifier {
     self.id
   }
-  fn generate(&self, _compilation: &Compilation) -> BoxSource {
-    let path = "wasmModuleHash";
+  fn generate(&self, compilation: &Compilation) -> BoxSource {
+    let (fake_filename, hash_len_map) =
+      get_filename_without_hash_length(&compilation.options.output.webassembly_module_filename);
+
+    // Even use content hash when [hash] in webpack
+    let hash = match hash_len_map
+      .get("[contenthash]")
+      .or(hash_len_map.get("[hash]"))
+    {
+      Some(hash_len) => format!("\" + wasmModuleHash.slice(0, {}) + \"", hash_len),
+      None => "\" + wasmModuleHash + \"".to_string(),
+    };
+
+    let chunk = compilation
+      .chunk_by_ukey
+      .get(&self.chunk)
+      .expect("chunk not found");
+    let path = compilation.get_path(
+      &fake_filename,
+      PathData::default()
+        .hash(&hash)
+        .content_hash(&hash)
+        .id("\" + wasmModuleId + \"")
+        .runtime(&chunk.runtime),
+    );
     RawSource::from(get_async_wasm_loading(
-      &self.generate_load_binary_code.replace("$PATH", path),
+      &self
+        .generate_load_binary_code
+        .replace("$PATH", &format!("\"{}\"", path)),
       self.supports_streaming,
     ))
     .boxed()
