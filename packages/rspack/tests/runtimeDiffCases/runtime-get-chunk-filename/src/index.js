415fa8a9ea28bb317620f934347902453be5260f
LingyuCoder <--global>
2023-12-08 10:14:25 +0800

    chore: complex get chunk filename diff case

--- packages/rspack/tests/runtimeDiffCases/runtime-get-chunk-filename/src/index.js
+++ packages/rspack/tests/runtimeDiffCases/runtime-get-chunk-filename/src/index.js
@@ -1,3 +1,8 @@
-import("./a.js");
-import("./b.js");
-import("./c.js");
+import("./shared/0");
+import("./shared/1");
+
+import("./common/0");
+import("./common/1");
+
+import("./other/0");
+import("./other/1");
