3c931c483ce47ef9e3a0193371aadf13d023af95
ahabhgk <ahabhgk@gmail.com>
2023-12-08 16:04:24 +0800

    feat: add more chunk methods

---
+++ crates/rspack_binding_values/src/chunk_graph.rs
@@ -0,0 +1,75 @@
+use napi::Result;
+use napi_derive::napi;
+use rspack_core::{ChunkUkey, SourceType};
+
+use crate::{JsChunk, JsCompilation, JsModule, ToJsModule};
+
+#[napi(js_name = "__chunk_graph_inner_get_chunk_modules")]
+pub fn get_chunk_modules(js_chunk_ukey: u32, compilation: &JsCompilation) -> Vec<JsModule> {
+  let compilation = &compilation.inner;
+  let modules = compilation.chunk_graph.get_chunk_modules(
+    &ChunkUkey::from(js_chunk_ukey as usize),
+    &compilation.module_graph,
+  );
+
+  return modules
+    .iter()
+    .filter_map(|module| module.to_js_module().ok())
+    .collect::<Vec<_>>();
+}
+
+#[napi(js_name = "__chunk_graph_inner_get_chunk_entry_modules")]
+pub fn get_chunk_entry_modules(js_chunk_ukey: u32, compilation: &JsCompilation) -> Vec<JsModule> {
+  let compilation = &compilation.inner;
+  let modules = compilation
+    .chunk_graph
+    .get_chunk_entry_modules(&ChunkUkey::from(js_chunk_ukey as usize));
+
+  return modules
+    .iter()
+    .filter_map(|module| compilation.module_graph.module_by_identifier(module))
+    .filter_map(|module| module.to_js_module().ok())
+    .collect::<Vec<_>>();
+}
+
+#[napi(js_name = "__chunk_graph_inner_get_chunk_entry_dependent_chunks_iterable")]
+pub fn get_chunk_entry_dependent_chunks_iterable(
+  js_chunk_ukey: u32,
+  compilation: &JsCompilation,
+) -> Vec<JsChunk> {
+  let compilation = &compilation.inner;
+  let chunks = compilation
+    .chunk_graph
+    .get_chunk_entry_dependent_chunks_iterable(
+      &ChunkUkey::from(js_chunk_ukey as usize),
+      &compilation.chunk_by_ukey,
+      &compilation.chunk_group_by_ukey,
+    );
+
+  return chunks
+    .into_iter()
+    .map(|c| JsChunk::from(compilation.chunk_by_ukey.expect_get(&c)))
+    .collect::<Vec<_>>();
+}
+
+#[napi(js_name = "__chunk_graph_inner_get_chunk_modules_iterable_by_source_type")]
+pub fn get_chunk_modules_iterable_by_source_type(
+  js_chunk_ukey: u32,
+  source_type: String,
+  compilation: &JsCompilation,
+) -> Result<Vec<JsModule>> {
+  let compilation = &compilation.inner;
+  Ok(
+    compilation
+      .chunk_graph
+      .get_chunk_modules_iterable_by_source_type(
+        &ChunkUkey::from(js_chunk_ukey as usize),
+        SourceType::try_from(source_type.as_str())
+          .map_err(|e| napi::Error::from_reason(e.to_string()))?,
+        &compilation.module_graph,
+      )
+      .into_iter()
+      .filter_map(|module| module.to_js_module().ok())
+      .collect(),
+  )
+}
--- packages/rspack/src/ChunkGraph.ts
+++ packages/rspack/src/ChunkGraph.ts
@@ -1,6 +1,8 @@
 import {
-	__chunk_inner_get_chunk_entry_modules,
-	__chunk_inner_get_chunk_modules
+	__chunk_graph_inner_get_chunk_entry_dependent_chunks_iterable,
+	__chunk_graph_inner_get_chunk_entry_modules,
+	__chunk_graph_inner_get_chunk_modules,
+	__chunk_graph_inner_get_chunk_modules_iterable_by_source_type
 } from "@rspack/binding";
 import { Chunk } from "./Chunk";
 import { Compilation } from "./Compilation";
@@ -9,17 +11,52 @@ import { Module } from "./Module";
 export class ChunkGraph {
 	constructor(private compilation: Compilation) {}
 
-	getChunkModulesIterable(chunk: Chunk) {
-		return __chunk_inner_get_chunk_modules(
+	getChunkModules(chunk: Chunk): Module[] {
+		return __chunk_graph_inner_get_chunk_modules(
 			chunk.__internal_inner_ukey(),
 			this.compilation.__internal_getInner()
 		).map(m => Module.__from_binding(m));
 	}
 
-	getChunkEntryModulesIterable(chunk: Chunk) {
-		return __chunk_inner_get_chunk_entry_modules(
-			chunk.__internal_inner_ukey(),
-			this.compilation.__internal_getInner()
-		).map(m => Module.__from_binding(m));
+	getChunkModulesIterable(chunk: Chunk): Iterable<Module> {
+		return new Set(
+			__chunk_graph_inner_get_chunk_modules(
+				chunk.__internal_inner_ukey(),
+				this.compilation.__internal_getInner()
+			).map(m => Module.__from_binding(m))
+		);
+	}
+
+	getChunkEntryModulesIterable(chunk: Chunk): Iterable<Module> {
+		return new Set(
+			__chunk_graph_inner_get_chunk_entry_modules(
+				chunk.__internal_inner_ukey(),
+				this.compilation.__internal_getInner()
+			).map(m => Module.__from_binding(m))
+		);
+	}
+
+	getChunkEntryDependentChunksIterable(chunk: Chunk): Iterable<Chunk> {
+		return new Set(
+			__chunk_graph_inner_get_chunk_entry_dependent_chunks_iterable(
+				chunk.__internal_inner_ukey(),
+				this.compilation.__internal_getInner()
+			).map(c =>
+				Chunk.__from_binding(c, this.compilation.__internal_getInner())
+			)
+		);
+	}
+
+	getChunkModulesIterableBySourceType(
+		chunk: Chunk,
+		sourceType: string
+	): Iterable<Module> {
+		return new Set(
+			__chunk_graph_inner_get_chunk_modules_iterable_by_source_type(
+				chunk.__internal_inner_ukey(),
+				sourceType,
+				this.compilation.__internal_getInner()
+			).map(m => Module.__from_binding(m))
+		);
 	}
 }
