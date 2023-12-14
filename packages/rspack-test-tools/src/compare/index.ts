b689372369526004b59975d6e47a2f4f50c4f22e
天镶 <lingyucoder@gmail.com>
2023-12-14 10:35:12 +0800

    Revert "chore: add comparator for diff tester (#5002)"
    This reverts commit 8afe46b16850a8833b9812c7955d6a7653cd2ef7.

--- packages/rspack-test-tools/src/compare/index.ts
+++ packages/rspack-test-tools/src/compare/index.ts
@@ -2,4 +2,3 @@ export * from "./replace-module-argument";
 export * from "./replace-runtime-module-name";
 export * from "./format-code";
 export * from "./compare";
-export * from "./comparator";
--- packages/rspack-test-tools/src/plugin/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./rspack-diff-config-plugin";
-export * from "./webpack-diff-config-plugin";
