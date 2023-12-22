e6ce8d9297b4107b6df57670bbf19cec1943ce64
ahabhgk <ahabhgk@gmail.com>
2023-12-22 15:50:05 +0800

    clean

--- packages/rspack/tests/cases/css/rewrite-url-rule-public-path/index.js
+++ packages/rspack/tests/cases/css/rewrite-url-rule-public-path/index.js
@@ -6,5 +6,4 @@ it("should rewrite the css url()", function () {
 	const css = fs.readFileSync(path.resolve(__dirname, "main.css"), "utf-8");
 	const a = /a: url\((.*)\);/.exec(css)[1];
 	expect(a).toBe("https://test.rspack.dev/cdn/logo.png");
-	expect(a).toMatchSnapshot();
 });
