9a40e32249717621713c999971764e6d5171de98
Gengkun <ahabhgk@gmail.com>
2023-12-25 21:36:00 +0800

    fix(mf1.5): remoteInfos when moduleIds is not named (#5101)

--- packages/rspack/src/container/ModuleFederationPlugin.ts
+++ packages/rspack/src/container/ModuleFederationPlugin.ts
@@ -1,6 +1,8 @@
-import { ExternalsType } from "../config";
+import { ExternalsType, externalsType } from "../config";
 import { Compiler } from "../Compiler";
 import { type ModuleFederationPluginV1Options } from "./ModuleFederationPluginV1";
+import { parseOptions } from "./options";
+import { isValidate } from "../util/validate";
 
 export interface ModuleFederationPluginOptions
 	extends Omit<ModuleFederationPluginV1Options, "enhanced"> {
@@ -15,7 +17,7 @@ export class ModuleFederationPlugin {
 		const { webpack } = compiler;
 		new webpack.EntryPlugin(
 			compiler.context,
-			defaultImplementation(this._options, compiler),
+			getDefaultEntryRuntime(this._options, compiler),
 			{ name: undefined }
 		).apply(compiler);
 		new webpack.container.ModuleFederationPluginV1({
@@ -25,10 +27,21 @@ export class ModuleFederationPlugin {
 	}
 }
 
-function defaultImplementation(
-	options: ModuleFederationPluginOptions,
-	compiler: Compiler
-) {
+interface RemoteInfo {
+	alias: string;
+	name?: string;
+	entry?: string;
+	externalType: ExternalsType;
+	shareScope: string;
+}
+
+type RemoteInfos = Record<string, RemoteInfo[]>;
+
+function getRemoteInfos(options: ModuleFederationPluginOptions): RemoteInfos {
+	if (!options.remotes) {
+		return {};
+	}
+
 	function extractUrlAndGlobal(urlAndGlobal: string) {
 		const index = urlAndGlobal.indexOf("@");
 		if (index <= 0 || index === urlAndGlobal.length - 1) {
@@ -43,64 +56,104 @@ function defaultImplementation(
 	function getExternalTypeFromExternal(external: string) {
 		if (/^[a-z0-9-]+ /.test(external)) {
 			const idx = external.indexOf(" ");
-			return [external.slice(0, idx), external.slice(idx + 1)];
+			return [
+				external.slice(0, idx) as ExternalsType,
+				external.slice(idx + 1)
+			] as const;
 		}
 		return null;
 	}
 
-	function getExternal(external: string, defaultExternalType: ExternalsType) {
+	function getExternal(external: string) {
 		const result = getExternalTypeFromExternal(external);
 		if (result === null) {
-			return [defaultExternalType, external];
+			return [remoteType, external] as const;
 		}
 		return result;
 	}
 
-	const runtimeTemplate = compiler.webpack.Template.getFunctionContent(
-		require("./default.runtime.js")
-	);
-	const remotes = [];
 	const remoteType =
 		options.remoteType ||
-		(options.library ? (options.library.type as ExternalsType) : "script");
-	for (let [key, remote] of Object.entries(options.remotes ?? {})) {
-		const [externalType, external] = getExternal(
-			typeof remote === "string" ? remote : remote.external,
-			remoteType
-		);
-		const shareScope =
-			(typeof remote !== "string" && remote.shareScope) ||
-			options.shareScope ||
-			"default";
-		if (externalType === "script") {
-			const [url, global] = extractUrlAndGlobal(external)!;
-			remotes.push({
-				alias: key,
-				name: global,
-				entry: url,
-				externalType,
-				shareScope
-			});
-		} else {
-			remotes.push({
-				alias: key,
-				name: undefined,
-				entry: undefined,
-				externalType,
-				shareScope
-			});
+		(options.library && isValidate(options.library.type, externalsType)
+			? (options.library.type as ExternalsType)
+			: "script");
+	const remotes = parseOptions(
+		options.remotes,
+		item => ({
+			external: Array.isArray(item) ? item : [item],
+			shareScope: options.shareScope || "default"
+		}),
+		item => ({
+			external: Array.isArray(item.external) ? item.external : [item.external],
+			shareScope: item.shareScope || options.shareScope || "default"
+		})
+	);
+
+	const remoteInfos: Record<string, RemoteInfo[]> = {};
+	for (let [key, config] of remotes) {
+		for (let external of config.external) {
+			const [externalType, externalRequest] = getExternal(external);
+			remoteInfos[key] ??= [];
+			if (externalType === "script") {
+				const [url, global] = extractUrlAndGlobal(externalRequest)!;
+				remoteInfos[key].push({
+					alias: key,
+					name: global,
+					entry: url,
+					externalType,
+					shareScope: config.shareScope
+				});
+			} else {
+				remoteInfos[key].push({
+					alias: key,
+					name: undefined,
+					entry: undefined,
+					externalType,
+					shareScope: config.shareScope
+				});
+			}
 		}
 	}
-	const runtimePlugins = options.runtimePlugins ?? [];
-	const pluginImports = runtimePlugins.map(
-		p => `require(${JSON.stringify(p)})`
-	);
-	const implementationPath =
+	return remoteInfos;
+}
+
+function getRuntimePlugins(options: ModuleFederationPluginOptions) {
+	return options.runtimePlugins ?? [];
+}
+
+function getImplementation(options: ModuleFederationPluginOptions) {
+	return (
 		options.implementation ??
-		require.resolve("@module-federation/webpack-bundler-runtime");
-	let implementation = runtimeTemplate
-		.replace("$RUNTIME_PACKAGE_PATH$", JSON.stringify(implementationPath))
-		.replace("$ALL_REMOTES$", JSON.stringify(remotes))
-		.replace("$INITOPTIONS_PLUGINS$", `[${pluginImports.join(", ")}]`);
-	return `data:text/javascript,${implementation}`;
+		require.resolve("@module-federation/runtime-tools/webpack-bundler-runtime")
+	);
+}
+
+function getDefaultEntryRuntime(
+	options: ModuleFederationPluginOptions,
+	compiler: Compiler
+) {
+	const implementationPath = getImplementation(options);
+	const runtimePlugins = getRuntimePlugins(options);
+	const remoteInfos = getRemoteInfos(options);
+	const runtimePluginImports = [];
+	const runtimePluginVars = [];
+	for (let i = 0; i < runtimePlugins.length; i++) {
+		const runtimePluginVar = `__module_federation_runtime_plugin_${i}__`;
+		runtimePluginImports.push(
+			`import ${runtimePluginVar} from ${JSON.stringify(runtimePlugins[i])}`
+		);
+		runtimePluginVars.push(`${runtimePluginVar}()`);
+	}
+	const content = [
+		`import __module_federation_runtime__ from ${JSON.stringify(
+			implementationPath
+		)}`,
+		...runtimePluginImports,
+		`const __module_federation_runtime_plugins__ = [${runtimePluginVars.join(
+			", "
+		)}]`,
+		`const __module_federation_remote_infos__ = ${JSON.stringify(remoteInfos)}`,
+		compiler.webpack.Template.getFunctionContent(require("./default.runtime"))
+	].join("\n");
+	return `data:text/javascript,${content}`;
 }
--- packages/rspack/src/container/default.runtime.js
+++ packages/rspack/src/container/default.runtime.js
@@ -1,168 +1,240 @@
 // @ts-nocheck
+var __module_federation_runtime__,
+	__module_federation_runtime_plugins__,
+	__module_federation_remote_infos__;
 module.exports = function () {
 	if (
 		__webpack_require__.initializeSharingData ||
 		__webpack_require__.initializeExposesData
 	) {
-		const getDefaultExport = module =>
-			module.__esModule ? module.default : module;
-		const federation = require($RUNTIME_PACKAGE_PATH$);
-		const plugins = $INITOPTIONS_PLUGINS$.map(m => getDefaultExport(m)());
-		const allRemotes = $ALL_REMOTES$;
+		const override = (obj, key, value) => {
+			if (!obj) return;
+			if (obj[key]) obj[key] = value;
+		};
+		const merge = (obj, key, fn) => {
+			const value = fn();
+			if (Array.isArray(value)) {
+				obj[key] ??= [];
+				obj[key].push(...value);
+			} else if (typeof value === "object" && value !== null) {
+				obj[key] ??= {};
+				Object.assign(obj[key], value);
+			}
+		};
+		const early = (obj, key, initial) => {
+			obj[key] ??= initial();
+		};
 
-		const scopeToInitDataMapping =
+		const remotesLoadingChunkMapping =
+			__webpack_require__.remotesLoadingData?.chunkMapping ?? {};
+		const remotesLoadingModuleIdToRemoteDataMapping =
+			__webpack_require__.remotesLoadingData?.moduleIdToRemoteDataMapping ?? {};
+		const initializeSharingScopeToInitDataMapping =
 			__webpack_require__.initializeSharingData?.scopeToSharingDataMapping ??
 			{};
-		const shared = {};
-		for (let [scope, stages] of Object.entries(scopeToInitDataMapping)) {
-			for (let stage of stages) {
-				if (Array.isArray(stage)) {
-					const [name, version, factory, eager] = stage;
-					if (shared[name]) {
-						shared[name].scope.push(scope);
-					} else {
-						shared[name] = { version, get: factory, scope: [scope] };
-					}
-				}
-			}
-		}
-
-		const idToExternalAndNameMapping =
-			__webpack_require__.remotesLoadingData?.moduleIdToRemoteDataMapping ?? {};
-		const idToRemoteMap = {};
-		for (let [id, external] of Object.entries(idToExternalAndNameMapping)) {
-			for (let remote of allRemotes) {
-				if (
-					id.slice("webpack/container/remote/".length).startsWith(remote.alias)
-				) {
-					idToRemoteMap[id] = [
-						{
-							externalType: remote.externalType,
-							request: remote.entry,
-							remoteName: remote.name
-						}
-					];
-				}
-			}
-		}
-
-		const moduleToConsumeDataMapping =
+		const consumesLoadingChunkMapping =
+			__webpack_require__.consumesLoadingData?.chunkMapping ?? {};
+		const consumesLoadingModuleToConsumeDataMapping =
 			__webpack_require__.consumesLoadingData?.moduleIdToConsumeDataMapping ??
 			{};
-		const consumesLoadingModuleToHandlerMapping = {};
-		for (let [moduleId, data] of Object.entries(moduleToConsumeDataMapping)) {
-			consumesLoadingModuleToHandlerMapping[moduleId] = {
-				getter: data.fallback,
-				shareInfo: {
-					shareConfig: {
-						fixedDependencies: false,
-						requiredVersion: data.requiredVersion,
-						strictVersion: data.strictVersion,
-						singleton: data.singleton,
-						eager: data.eager
-					},
-					scope: [data.shareScope]
-				},
-				shareKey: data.shareKey
-			};
-		}
-
 		const consumesLoadinginstalledModules = {};
 		const initializeSharingInitPromises = [];
 		const initializeSharingInitTokens = [];
-		const remotesLoadingChunkMapping =
-			__webpack_require__.remotesLoadingData?.chunkMapping ?? {};
-		const consumesLoadingChunkMapping =
-			__webpack_require__.consumesLoadingData?.chunkMapping ?? {};
 		const containerShareScope =
 			__webpack_require__.initializeExposesData?.containerShareScope;
 
-		federation.initOptions = {
-			name: __webpack_require__.initializeSharingData?.uniqueName,
-			remotes: allRemotes.filter(remote => remote.externalType === "script"),
-			shared: shared,
-			plugins: plugins
-		};
-		federation.bundlerRuntimeOptions = {
-			remotes: {
-				idToRemoteMap,
-				chunkMapping: remotesLoadingChunkMapping,
-				idToExternalAndNameMapping,
-				webpackRequire: __webpack_require__
+		early(
+			__webpack_require__,
+			"federation",
+			() => __module_federation_runtime__
+		);
+
+		early(
+			__webpack_require__.federation,
+			"consumesLoadingModuleToHandlerMapping",
+			() => {
+				const consumesLoadingModuleToHandlerMapping = {};
+				for (let [moduleId, data] of Object.entries(
+					consumesLoadingModuleToConsumeDataMapping
+				)) {
+					consumesLoadingModuleToHandlerMapping[moduleId] = {
+						getter: data.fallback,
+						shareInfo: {
+							shareConfig: {
+								fixedDependencies: false,
+								requiredVersion: data.requiredVersion,
+								strictVersion: data.strictVersion,
+								singleton: data.singleton,
+								eager: data.eager
+							},
+							scope: [data.shareScope]
+						},
+						shareKey: data.shareKey
+					};
+				}
+				return consumesLoadingModuleToHandlerMapping;
 			}
-		};
+		);
 
-		if (__webpack_require__.f?.remotes) {
-			__webpack_require__.f.remotes = (chunkId, promises) =>
-				federation.bundlerRuntime.remotes({
-					chunkId,
-					promises,
-					chunkMapping: remotesLoadingChunkMapping,
-					idToExternalAndNameMapping,
-					idToRemoteMap,
-					webpackRequire: __webpack_require__
-				});
-		}
-		if (__webpack_require__.f?.consumes) {
-			__webpack_require__.f.consumes = (chunkId, promises) =>
-				federation.bundlerRuntime.consumes({
-					chunkId,
-					promises,
-					chunkMapping: consumesLoadingChunkMapping,
-					moduleToHandlerMapping: consumesLoadingModuleToHandlerMapping,
-					installedModules: consumesLoadinginstalledModules,
-					webpackRequire: __webpack_require__
-				});
-		}
-		if (__webpack_require__.I) {
-			__webpack_require__.I = (name, initScope) =>
-				federation.bundlerRuntime.I({
-					shareScopeName: name,
-					initScope,
-					initPromises: initializeSharingInitPromises,
-					initTokens: initializeSharingInitTokens,
-					webpackRequire: __webpack_require__
-				});
-		}
-		if (__webpack_require__.S) {
-			__webpack_require__.S = federation.bundlerRuntime.S;
-		}
-		if (__webpack_require__.initContainer) {
-			__webpack_require__.initContainer = (shareScope, initScope) =>
-				federation.bundlerRuntime.initContainerEntry({
-					shareScope,
-					initScope,
-					shareScopeKey: containerShareScope,
-					webpackRequire: __webpack_require__
-				});
-		}
-		if (__webpack_require__.getContainer) {
-			__webpack_require__.getContainer = (module, getScope) => {
-				var moduleMap = __webpack_require__.initializeExposesData.moduleMap;
-				__webpack_require__.R = getScope;
-				getScope = Object.prototype.hasOwnProperty.call(moduleMap, module)
-					? moduleMap[module]()
-					: Promise.resolve().then(() => {
-							throw new Error(
-								'Module "' + module + '" does not exist in container.'
-							);
-					  });
-				__webpack_require__.R = undefined;
-				return getScope;
-			};
-		}
+		early(__webpack_require__.federation, "initOptions", () => ({}));
+		early(
+			__webpack_require__.federation.initOptions,
+			"name",
+			() => __webpack_require__.initializeSharingData?.uniqueName
+		);
+		early(__webpack_require__.federation.initOptions, "shared", () => {
+			const shared = {};
+			for (let [scope, stages] of Object.entries(
+				initializeSharingScopeToInitDataMapping
+			)) {
+				for (let stage of stages) {
+					if (typeof stage === "object" && stage !== null) {
+						const { name, version, factory, eager } = stage;
+						if (shared[name]) {
+							shared[name].scope.push(scope);
+						} else {
+							shared[name] = { version, get: factory, scope: [scope] };
+						}
+					}
+				}
+			}
+			return shared;
+		});
+		merge(__webpack_require__.federation.initOptions, "remotes", () =>
+			Object.values(__module_federation_remote_infos__)
+				.flat()
+				.filter(remote => remote.externalType === "script")
+		);
+		merge(
+			__webpack_require__.federation.initOptions,
+			"plugins",
+			() => __module_federation_runtime_plugins__
+		);
 
-		__webpack_require__.federation = federation;
+		early(__webpack_require__.federation, "bundlerRuntimeOptions", () => ({}));
+		early(
+			__webpack_require__.federation.bundlerRuntimeOptions,
+			"remotes",
+			() => ({})
+		);
+		early(
+			__webpack_require__.federation.bundlerRuntimeOptions.remotes,
+			"chunkMapping",
+			() => remotesLoadingChunkMapping
+		);
+		early(
+			__webpack_require__.federation.bundlerRuntimeOptions.remotes,
+			"idToExternalAndNameMapping",
+			() => {
+				const remotesLoadingIdToExternalAndNameMappingMapping = {};
+				for (let [moduleId, data] of Object.entries(
+					remotesLoadingModuleIdToRemoteDataMapping
+				)) {
+					remotesLoadingIdToExternalAndNameMappingMapping[moduleId] = [
+						data.shareScope,
+						data.name,
+						data.externalModuleId,
+						data.remoteName
+					];
+				}
+				return remotesLoadingIdToExternalAndNameMappingMapping;
+			}
+		);
+		early(
+			__webpack_require__.federation.bundlerRuntimeOptions.remotes,
+			"webpackRequire",
+			() => __webpack_require__
+		);
+		merge(
+			__webpack_require__.federation.bundlerRuntimeOptions.remotes,
+			"idToRemoteMap",
+			() => {
+				const idToRemoteMap = {};
+				for (let [id, remoteData] of Object.entries(
+					remotesLoadingModuleIdToRemoteDataMapping
+				)) {
+					const info =
+						__module_federation_remote_infos__[remoteData.remoteName];
+					if (info) idToRemoteMap[id] = info;
+				}
+				return idToRemoteMap;
+			}
+		);
 
-		federation.instance = federation.runtime.init(federation.initOptions);
+		override(
+			__webpack_require__,
+			"S",
+			() => __module_federation_runtime__.bundlerRuntime.S
+		);
+		override(__webpack_require__.f, "remotes", (chunkId, promises) =>
+			__module_federation_runtime__.bundlerRuntime.remotes({
+				chunkId,
+				promises,
+				chunkMapping: remotesLoadingChunkMapping,
+				idToExternalAndNameMapping:
+					__webpack_require__.federation.bundlerRuntimeOptions.remotes
+						.idToExternalAndNameMapping,
+				idToRemoteMap:
+					__webpack_require__.federation.bundlerRuntimeOptions.remotes
+						.idToRemoteMap,
+				webpackRequire: __webpack_require__
+			})
+		);
+		override(__webpack_require__.f, "consumes", (chunkId, promises) =>
+			__module_federation_runtime__.bundlerRuntime.consumes({
+				chunkId,
+				promises,
+				chunkMapping: consumesLoadingChunkMapping,
+				moduleToHandlerMapping:
+					__webpack_require__.federation.consumesLoadingModuleToHandlerMapping,
+				installedModules: consumesLoadinginstalledModules,
+				webpackRequire: __webpack_require__
+			})
+		);
+		override(__webpack_require__, "I", (name, initScope) =>
+			__module_federation_runtime__.bundlerRuntime.I({
+				shareScopeName: name,
+				initScope,
+				initPromises: initializeSharingInitPromises,
+				initTokens: initializeSharingInitTokens,
+				webpackRequire: __webpack_require__
+			})
+		);
+		override(__webpack_require__, "initContainer", (shareScope, initScope) =>
+			__module_federation_runtime__.bundlerRuntime.initContainerEntry({
+				shareScope,
+				initScope,
+				shareScopeKey: containerShareScope,
+				webpackRequire: __webpack_require__
+			})
+		);
+		override(__webpack_require__, "getContainer", (module, getScope) => {
+			var moduleMap = __webpack_require__.initializeExposesData.moduleMap;
+			__webpack_require__.R = getScope;
+			getScope = Object.prototype.hasOwnProperty.call(moduleMap, module)
+				? moduleMap[module]()
+				: Promise.resolve().then(() => {
+						throw new Error(
+							'Module "' + module + '" does not exist in container.'
+						);
+				  });
+			__webpack_require__.R = undefined;
+			return getScope;
+		});
+
+		__webpack_require__.federation.instance =
+			__webpack_require__.federation.runtime.init(
+				__webpack_require__.federation.initOptions
+			);
 
 		if (__webpack_require__.consumesLoadingData?.initialConsumes) {
-			federation.bundlerRuntime.installInitialConsumes({
+			__webpack_require__.federation.bundlerRuntime.installInitialConsumes({
 				webpackRequire: __webpack_require__,
 				installedModules: consumesLoadinginstalledModules,
 				initialConsumes:
 					__webpack_require__.consumesLoadingData.initialConsumes,
-				moduleToHandlerMapping: consumesLoadingModuleToHandlerMapping
+				moduleToHandlerMapping:
+					__webpack_require__.federation.consumesLoadingModuleToHandlerMapping
 			});
 		}
 	}
