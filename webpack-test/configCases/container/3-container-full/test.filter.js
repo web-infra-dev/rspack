740f168bab5033f9daae3fd77673ed22c0d16a75
bohan <bohan-zhang@foxmail.com>
2023-12-27 15:15:47 +0800

    fix: library module renderStartup missing export (#4901)
    * wip: block by new treeshaking
    
    * add switch
    
    * enable test
    
    * fix
    
    * lint
    
    * fix
    
    ---------
    
    Co-authored-by: ahabhgk <ahabhgk@gmail.com>

--- webpack-test/configCases/container/3-container-full/test.filter.js
+++
@@ -1,3 +0,0 @@
-const { FilteredStatus } = require("../../../lib/util/filterUtil")
-// this test depend on 1-container-full
-module.exports = () => {return [FilteredStatus.PARTIAL_PASS, "https://github.com/web-infra-dev/rspack/issues/4784"]}
