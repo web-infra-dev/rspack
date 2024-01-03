329f68d01921ee18c800ac235458256d04cabd1a
Hana <andywangsy@gmail.com>
2024-01-03 18:10:45 +0800

    test: snap

--- crates/rspack_testing/src/eval_raw.rs
+++ crates/rspack_testing/src/eval_raw.rs
@@ -28,7 +28,12 @@ config.output ??= {{}};
 config.output.path ??= "{test_dir}/dist";
 const normalized = rspack.config.getNormalizedRspackOptions(config);
 rspack.config.applyRspackOptionsDefaults(normalized);
-const raw = rspack.config.__internal__getRawOptions(normalized);
+let fakeCompiler = {{
+  ruleSet: {{
+    references: new Map()
+  }}
+}};
+const raw = rspack.config.__internal__getRawOptions(normalized, fakeCompiler);
 JSON.stringify(raw, null, 2)
 "#
   )
