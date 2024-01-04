c7519d66fa990c55876c3049b1abcc3cb984e18f
Hana <andywangsy@gmail.com>
2024-01-04 14:32:49 +0800

    ci: fix flaky e2e tests (#5215)

--- packages/playground/cases/html/issue-4366/index.test.ts
+++ packages/playground/cases/html/issue-4366/index.test.ts
@@ -1,17 +1,12 @@
 import { test, expect } from "@/fixtures";
 
-test("html should refresh after reload", async ({
-	page,
-	fileAction,
-	rspack
-}) => {
-	await expect(page.title()).resolves.toBe("123");
+test("html should refresh after reload", async ({ page, fileAction }) => {
+	await expect(page).toHaveTitle("123");
 	fileAction.updateFile("./src/index.html", content =>
 		content.replace("123", "456")
 	);
-	await rspack.waitUntil(async () => {
+	await expect(async () => {
 		await page.reload();
-		const t2 = await page.title();
-		return t2 === "456";
-	});
+		expect(await page.title()).toBe("456");
+	}).toPass();
 });
