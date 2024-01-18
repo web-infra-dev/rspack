7ed2eaa1c74b1fef95aac8fb373f2d2174b4d03b
Hana <andywangsy@gmail.com>
2024-01-18 14:31:10 +0800

    feat: not printing error code in error

--- packages/rspack/tests/cases/parsing/issue-5120-binding/index.js
+++ packages/rspack/tests/cases/parsing/issue-5120-binding/index.js
@@ -3,6 +3,6 @@ const path = require("path");
 
 it("should throw error", () => {
 	expect(fs.readFileSync(path.join(__dirname, "fail.js"), "utf-8")).toContain(
-		"ModuleParseError"
+		"JavaScript parsing error: let is disallowed as a lexically bound name"
 	);
 });
