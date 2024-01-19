802ff6197f7d25159b051805c68504f96b6aa36f
Boshen <boshenc@gmail.com>
2024-01-19 23:35:43 +0800

    chore: enable some tests from webpack-examples

--- webpack-examples/wasm-complex/test.filter.js
+++
@@ -1,9 +0,0 @@
-
-/** var supportsWebAssembly = require("../../test/helpers/supportsWebAssembly");
-
-module.exports = function(config) {
-	return supportsWebAssembly();
-};
-*/
-module.exports = () =>{return false}
-			
