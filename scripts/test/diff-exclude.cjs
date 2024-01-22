6be71d5af2dec2a614f86f3c3fa817c71094825a
Hana <andywangsy@gmail.com>
2024-01-22 18:46:23 +0800

    chore: diff rspack/webpack test cases

---
+++ scripts/test/diff-exclude.cjs
@@ -0,0 +1,9 @@
+/**
+ * @type {Array<RegExp | string>}
+ */
+module.exports = [
+	// INTENDED TO BE DIFFERENT
+	"README.md"
+	// MISSING FEATURES OF RSPACK
+	// "cases/parsing/harmony-deep-exports"
+];
