0213dff69f2d40697b73e658c27f9b103d1aa3e7
bohan <bohan-zhang@foxmail.com>
2024-02-19 19:37:10 +0800

    refactor: mv `CallHooksName` to spearte module

---
+++ crates/rspack_plugin_javascript/src/visitors/dependency/parser/call_hooks_name.rs
@@ -0,0 +1,61 @@
+use swc_core::atoms::Atom;
+
+use super::{ExportedVariableInfo, JavascriptParser};
+use crate::visitors::scope_info::{FreeName, VariableInfo};
+
+/// callHooksForName/callHooksForInfo in webpack
+/// webpack use HookMap and filter at callHooksForName/callHooksForInfo
+/// we need to pass the name to hook to filter in the hook
+pub trait CallHooksName {
+  fn call_hooks_name(&self, parser: &mut JavascriptParser) -> Option<String>;
+}
+
+impl CallHooksName for &str {
+  fn call_hooks_name(&self, parser: &mut JavascriptParser) -> Option<String> {
+    if let Some(info) = parser.get_variable_info(self.as_ref()) {
+      call_hooks_info(info)
+    } else {
+      Some(self.to_string())
+    }
+  }
+}
+
+impl CallHooksName for String {
+  fn call_hooks_name(&self, parser: &mut JavascriptParser) -> Option<String> {
+    self.as_str().call_hooks_name(parser)
+  }
+}
+
+impl CallHooksName for Atom {
+  fn call_hooks_name(&self, parser: &mut JavascriptParser) -> Option<String> {
+    self.as_str().call_hooks_name(parser)
+  }
+}
+
+impl CallHooksName for VariableInfo {
+  fn call_hooks_name(&self, _parser: &mut JavascriptParser) -> Option<String> {
+    call_hooks_info(self)
+  }
+}
+
+impl CallHooksName for ExportedVariableInfo {
+  fn call_hooks_name(&self, parser: &mut JavascriptParser) -> Option<String> {
+    match self {
+      ExportedVariableInfo::Name(n) => n.call_hooks_name(parser),
+      ExportedVariableInfo::VariableInfo(v) => {
+        let info = parser.definitions_db.expect_get_variable(v);
+        call_hooks_info(info)
+      }
+    }
+  }
+}
+
+fn call_hooks_info(info: &VariableInfo) -> Option<String> {
+  // TODO: tag_info with hooks
+  if let Some(FreeName::String(free_name)) = &info.free_name {
+    Some(free_name.to_string())
+  } else {
+    // should run `defined ? defined() : None`
+    None
+  }
+}
