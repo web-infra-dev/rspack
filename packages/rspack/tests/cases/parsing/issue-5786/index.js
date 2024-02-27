f28df4b254a5b5393ac9c3c1345f7cb452dcde65
Neko <neko@qwq.icu>
2024-02-27 21:36:42 +0800

    test: add test cases

---
+++ packages/rspack/tests/cases/parsing/issue-5786/index.js
@@ -0,0 +1,15 @@
+class A {
+	constructor(
+		func = () => {
+			this.a = 1;
+		}
+	) {
+		func();
+	}
+}
+
+it("ensure top level is right in constructor parameters", () => {
+	const a = new A();
+	expect(a.a).toBe(1);
+	expect(typeof a).toBe("object");
+});
