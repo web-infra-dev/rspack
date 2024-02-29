91567548471f5cadab2b1ebaa9318b08207449a1
Hana <andywangsy@gmail.com>
2024-02-29 17:08:14 +0800

    chore: align jest config (#5815)

--- packages/rspack/jest.config.js
+++ packages/rspack/jest.config.js
@@ -1,26 +1,26 @@
 const path = require("path");
 
-/** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
+/** @type {import('jest').Config} */
 const config = {
 	testEnvironment: "../../scripts/test/patch-node-env.cjs",
 	setupFilesAfterEnv: ["<rootDir>/tests/setupTestFramework.js"],
 	testMatch: [
-		"<rootDir>/tests/*.test.ts",
 		"<rootDir>/tests/*.test.js",
-		"<rootDir>/tests/*.basictest.ts",
 		"<rootDir>/tests/*.basictest.js",
-		"<rootDir>/tests/*.longtest.ts",
 		"<rootDir>/tests/*.longtest.js",
-		"<rootDir>/tests/*.unittest.ts",
 		"<rootDir>/tests/*.unittest.js",
-		"<rootDir>/tests/copyPlugin/*.test.js",
-		"<rootDir>/tests/WatchSuspend.test.js"
+		"<rootDir>/tests/copyPlugin/*.test.js"
 	],
 	testTimeout: process.env.CI ? 60000 : 30000,
 	prettierPath: require.resolve("prettier-2"),
 	moduleNameMapper: {
 		// Fixed jest-serialize-path not working when non-ascii code contains.
 		slash: path.join(__dirname, "../../scripts/test/slash.cjs")
+	},
+	transformIgnorePatterns: ["<rootDir>/tests"],
+	snapshotFormat: {
+		escapeString: true,
+		printBasicPrototype: true
 	}
 };
 
