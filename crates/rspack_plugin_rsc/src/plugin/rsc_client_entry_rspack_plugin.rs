cdc8ce9b0c6e54beee1929840c14b590b77b1c53
JW <jiangweixian1994@gmail.com>
2024-03-12 10:40:48 +0800

    fix: support css

--- crates/rspack_plugin_rsc/src/plugin/rsc_client_entry_rspack_plugin.rs
+++ crates/rspack_plugin_rsc/src/plugin/rsc_client_entry_rspack_plugin.rs
@@ -2,7 +2,7 @@ use std::collections::{HashMap, HashSet};
 
 use async_trait::async_trait;
 use rspack_core::rspack_sources::{RawSource, SourceExt};
-use rspack_core::{AssetInfo, Compilation, CompilationAsset, Module, Plugin};
+use rspack_core::{AssetInfo, Compilation, CompilationAsset, Module, ModuleType, Plugin};
 use rspack_error::Result;
 use serde_json::to_string;
 
@@ -23,6 +23,7 @@ impl RSCClientEntryRspackPlugin {
     let data = module
       .as_normal_module()
       .and_then(|m| Some(m.resource_resolved_data()));
+    let module_type = module.module_type();
     if let Some(data) = data {
       let resource_path = &data.resource_path;
       let resource_path_str = resource_path.to_str().expect("Should exits");
@@ -30,11 +31,19 @@ impl RSCClientEntryRspackPlugin {
         return;
       }
       visited_modules.insert(String::from(resource_path_str));
+      let is_css = match module_type {
+        ModuleType::Css | ModuleType::CssModule | ModuleType::CssAuto => true,
+        _ => false,
+      };
+      // TODO: check css file is in used
+      // TODO: unique css files from other entry
       let use_client = String::from("use client");
+      let is_client_components = match module.build_info() {
+        Some(build_info) => build_info.directives.contains(&use_client),
+        None => false,
+      };
       // let og = module.original_source().unwrap();
-      if let Some(build_info) = module.build_info()
-        && build_info.directives.contains(&use_client)
-      {
+      if is_client_components || is_css {
         collect_client_imports.insert(String::from(resource_path_str));
       } else {
         for connection in compilation.module_graph.get_outgoing_connections(module) {
