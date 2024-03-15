9c25b4d51e36d8f8a734874318d460201ae38695
Hana <andywangsy@gmail.com>
2024-03-15 22:54:20 +0800

    refactor(node-binding): support `CompilerStateGuard` (#5956)
    refactor(node-binding): support

--- crates/node_binding/src/compiler.rs
+++ crates/node_binding/src/compiler.rs
@@ -1,6 +1,10 @@
 use std::{
   marker::PhantomPinned,
   ops::{Deref, DerefMut},
+  sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+  },
 };
 
 use rspack_fs_node::AsyncNodeWritableFileSystem;
@@ -27,5 +31,34 @@ impl Deref for Compiler {
 impl DerefMut for Compiler {
   fn deref_mut(&mut self) -> &mut Self::Target {
     &mut self.0
+  }
+}
+
+pub(crate) struct CompilerState(Arc<AtomicBool>);
+
+impl CompilerState {
+  pub(crate) fn init() -> Self {
+    Self(Arc::new(AtomicBool::new(false)))
+  }
+}
+
+impl CompilerState {
+  pub(crate) fn running(&self) -> bool {
+    self.0.load(Ordering::Relaxed)
+  }
+
+  pub(crate) fn enter(&self) -> CompilerStateGuard {
+    self.0.store(true, Ordering::Relaxed);
+    CompilerStateGuard(self.0.clone())
+  }
+}
+
+pub(crate) struct CompilerStateGuard(Arc<AtomicBool>);
+
+unsafe impl Send for CompilerStateGuard {}
+
+impl Drop for CompilerStateGuard {
+  fn drop(&mut self) {
+    self.0.store(false, Ordering::Relaxed);
   }
 }
