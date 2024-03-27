2baea0418e49a4a513eac83f344e6a5d37548ead
Hana <andywangsy@gmail.com>
2024-03-27 11:53:53 +0800

    fix: `Compiler` and `Compilation` in `LoaderContext` of a childCompiler should be updated (#6034)
    fix: `LoaderContext` in childCompiler should expose new `Compiler` and `Compilation`

--- packages/rspack/src/builtin-plugin/JsLoaderRspackPlugin.ts
+++ packages/rspack/src/builtin-plugin/JsLoaderRspackPlugin.ts
@@ -6,5 +6,6 @@ import { Compiler } from "../Compiler";
 export const JsLoaderRspackPlugin = create(
 	BuiltinPluginName.JsLoaderRspackPlugin,
 	(compiler: Compiler) => runLoaders.bind(null, compiler),
-	"compilation"
+	/* Not Inheretable */
+	"thisCompilation"
 );
