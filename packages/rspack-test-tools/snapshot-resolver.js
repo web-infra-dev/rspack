758573d0d1c6054c0cb12def0dec153859a34ab5
LingyuCoder <lingyucoder@gmail.com>
2024-03-28 00:39:50 +0800

    chore: remove integrate cases

--- packages/rspack-test-tools/snapshot-resolver.js
+++ packages/rspack-test-tools/snapshot-resolver.js
@@ -1,32 +1,17 @@
 const path = require("path");
 const testBasePath = path.resolve(__dirname, "./tests");
-const snapshotLegacyBasePath = path.resolve(
+const snapshotBasePath = path.resolve(
 	__dirname,
 	"../rspack/tests/__snapshots__"
 );
-const snapshotBasePath = path.resolve(__dirname, "./tests/__snapshots__");
-
-const MIGRATED_CASES = [
-	"Compiler.test.js",
-	"Defaults.unittest.js",
-	"Stats.test.js"
-];
 
 module.exports = {
 	resolveSnapshotPath: (testPath, snapshotExtension) => {
-		const snapshotPath = MIGRATED_CASES.includes(path.basename(testPath))
-			? snapshotBasePath
-			: snapshotLegacyBasePath;
 		const relative = testPath.replace(testBasePath, "");
-		return path.join(snapshotPath, relative) + snapshotExtension;
+		return path.join(snapshotBasePath, relative) + snapshotExtension;
 	},
 	resolveTestPath: (snapshotFilePath, snapshotExtension) => {
-		const snapshotPath = MIGRATED_CASES.includes(
-			path.basename(snapshotFilePath).slice(0, -snapshotExtension.length)
-		)
-			? snapshotBasePath
-			: snapshotLegacyBasePath;
-		const relative = snapshotFilePath.replace(snapshotPath + path.sep, "");
+		const relative = snapshotFilePath.replace(snapshotBasePath + path.sep, "");
 		return relative.slice(0, -snapshotExtension.length);
 	},
 	testPathForConsistencyCheck: `tests${path.sep}example.test.js`
