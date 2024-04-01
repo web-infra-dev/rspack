8eaa7c5fe8ca38f4c6273ee7d3e52ef8dc1ede47
quininer <quininer@live.com>
2024-04-01 22:00:04 +0800

    perf(core): Use unicase for compare_ids

--- crates/rspack_util/src/comparators.rs
+++ crates/rspack_util/src/comparators.rs
@@ -1,26 +1,11 @@
 use std::cmp::Ordering;
 
-#[allow(clippy::comparison_chain)]
 pub fn compare_ids(a: &str, b: &str) -> Ordering {
-  let a = a.to_lowercase();
-  let b = b.to_lowercase();
-  if a < b {
-    Ordering::Less
-  } else if a > b {
-    Ordering::Greater
-  } else {
-    Ordering::Equal
-  }
+  unicase::UniCase::new(a).cmp(&unicase::UniCase::new(b))
 }
-#[allow(clippy::comparison_chain)]
+
 pub fn compare_numbers(a: u32, b: u32) -> Ordering {
-  if a < b {
-    Ordering::Less
-  } else if a > b {
-    Ordering::Greater
-  } else {
-    Ordering::Equal
-  }
+  a.cmp(&b)
 }
 
 #[cfg(test)]
