71c4b98a2fb64cf15baa2b58b06966ca260a226e
Hana <andywangsy@gmail.com>
2024-04-11 13:51:26 +0800

    fix: should parse patterns in class (#6198)

--- packages/rspack/tests/configCases/module-variables/global-true/index.js
+++ packages/rspack/tests/configCases/module-variables/global-true/index.js
@@ -2,3 +2,22 @@ it("global true", function () {
 	global;
 	expect(__webpack_require__.g).not.toBe(undefined);
 });
+
+it("should polyfill `global` if `node.global` is `true`", function() {
+	class Example {
+		constructor(g = global) {
+			this.g = g;
+		}
+	}
+	expect(new Example().g).toBe(globalThis);
+})
+
+
+it("should only polyfill `global` if it's not declared", function() {
+	class Example {
+		constructor(global = global) {
+			this.global = global;
+		}
+	}
+	expect(() => new Example()).toThrow(/Cannot access '\w*' before initialization/);
+})
