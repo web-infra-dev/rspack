6494c19f50c696da1ab48a1b3e48166864e86b87
LingyuCoder <lingyucoder@gmail.com>
2024-04-11 15:15:41 +0800

    fix: favicon path of HtmlRspackPlugin

--- packages/rspack/tests/configCases/builtins/html-favicon-absolute-publicpath/index.js
+++ packages/rspack/tests/configCases/builtins/html-favicon-absolute-publicpath/index.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 const path = require("path");
 
 it("html favicon with absolute path and public path", () => {
+	const faviconPath = path.join(__dirname, "./favicon.ico");
+	expect(fs.existsSync(faviconPath)).toBe(true);
+
 	const htmlPath = path.join(__dirname, "./index.html");
 	const htmlContent = fs.readFileSync(htmlPath, "utf-8");
 	expect(
--- packages/rspack/tests/configCases/builtins/html-favicon-absolute-subdir/index.js
+++ packages/rspack/tests/configCases/builtins/html-favicon-absolute-subdir/index.js
@@ -2,9 +2,12 @@ const fs = require("fs");
 const path = require("path");
 
 it("html favicon with absolute path in subdirectory", () => {
+	const faviconPath = path.join(__dirname, "./favicon.ico");
+	expect(fs.existsSync(faviconPath)).toBe(true);
+
 	const htmlPath = path.join(__dirname, "./index.html");
 	const htmlContent = fs.readFileSync(htmlPath, "utf-8");
 	expect(
-		htmlContent.includes('<link rel="icon" href="/static/favicon.ico" />')
+		htmlContent.includes('<link rel="icon" href="/favicon.ico" />')
 	).toBe(true);
 });
