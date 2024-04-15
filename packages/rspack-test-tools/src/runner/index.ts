5b4cc6ddd192d35d9812b0bc7eda26dc20cc3fae
harpsealjs <lingyucoder@gmail.com>
2024-04-15 21:03:56 +0800

    chore: hot step test (#6182)
    * chore: hot step test
    
    * chore: add hot step snapshot
    
    * chore: add hot step snapshot
    
    * chore: add hot step snapshot
    
    * chore: add hot step snapshot
    
    * chore: add hot step snapshot
    
    * chore: add hot step snapshot
    
    * chore: add hot step snapshot
    
    * chore: add hot step snapshot
    
    * fix: format error
    
    * chore: add hot step snapshot
    
    * chore: add hot step snapshot
    
    * chore: add hot step snapshot
    
    * chore: add hot step snapshot

--- packages/rspack-test-tools/src/runner/index.ts
+++ packages/rspack-test-tools/src/runner/index.ts
@@ -1,5 +1,6 @@
 export * from "./basic";
 export * from "./hot";
+export * from "./hot-step";
 export * from "./normal";
 export * from "./watch";
 export * from "./type";
---
+++ packages/rspack-test-tools/tests/HotTestStepWeb.test.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const { describeByWalk, createHotStepCase } = require("../dist");
+
+const NAME = "HotStepTestCasesNode";
+const caseDir = path.resolve(__dirname, "../../rspack/tests/hotCases");
+const distDir = path.resolve(
+	__dirname,
+	`../../rspack/tests/js/hot-step-cases-web`
+);
+
+describeByWalk(NAME, caseDir, distDir, (name, src, dist) => {
+	createHotStepCase(name, src, dist, "web");
+});
