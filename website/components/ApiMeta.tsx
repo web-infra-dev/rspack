589771e6294175ce8491b76338c1918fa6dacff2
neverland <chenjiahan.jait@bytedance.com>
2024-04-17 17:56:43 +0800

    docs: improve style of API meta tags

--- website/components/ApiMeta.tsx
+++ website/components/ApiMeta.tsx
@@ -1,5 +1,5 @@
 import { useLang } from 'rspress/runtime';
-import './ApiMeta.scss';
+import styles from './ApiMeta.module.scss';
 
 /**
  * The Stability Index is learned from https://nodejs.org/api/documentation.html#stability-index
@@ -8,8 +8,6 @@ export enum Stability {
   Deprecated = 'Deprecated', // The feature may emit warnings. Backward compatibility is not guaranteed.
   Removed = 'Removed',
   Experimental = 'Experimental', // The feature is not subject to semantic versioning rules
-  Stable = 'Stable', // Compatibility with the npm ecosystem is a high priority.
-  Legacy = 'Legacy', // Although this feature is unlikely to be removed and is still covered by semantic versioning guarantees, it is no longer actively maintained, and other alternatives are available.
 }
 
 export interface ApiMetaProps {
@@ -22,41 +20,33 @@ export interface ApiMetaProps {
 export function ApiMeta(props: ApiMetaProps) {
   let lang = useLang();
   return (
-    <div className="api-meta">
-      {(!!props.addedVersion ||
-        !!props.deprecatedVersion ||
-        !!props.removedVersion) && (
-        <div className="api-meta-version">
-          {!!props.addedVersion && (
-            <span className="api-meta-version-added">
-              <a href={`/${lang}/misc/future`}>v{props.addedVersion}</a>
-            </span>
-          )}
-          {!!props.deprecatedVersion && (
-            <span className="api-meta-version-deprecated">
-              <a
-                href={`/${lang}/misc/future?deprecatedVersion=${props.deprecatedVersion}`}
-              >
-                v{props.deprecatedVersion}
-              </a>
-            </span>
-          )}
-          {!!props.removedVersion && (
-            <span className="api-meta-version-removed">
-              <a
-                href={`/${lang}/misc/future?removedVersion=${props.removedVersion}`}
-              >
-                v{props.removedVersion}
-              </a>
-            </span>
-          )}
-        </div>
+    <div className={styles.wrapper}>
+      {props.addedVersion && (
+        <span className={`${styles.tag} ${styles.added}`}>
+          <a href={`/${lang}/misc/future`}>Added in v{props.addedVersion}</a>
+        </span>
       )}
-      {!!props.stability && (
+      {props.deprecatedVersion && (
+        <span className={`${styles.tag} ${styles.deprecated}`}>
+          <a
+            href={`/${lang}/misc/future?deprecatedVersion=${props.deprecatedVersion}`}
+          >
+            Deprecated in v{props.deprecatedVersion}
+          </a>
+        </span>
+      )}
+      {props.removedVersion && (
+        <span className={`${styles.tag} ${styles.removed}`}>
+          <a
+            href={`/${lang}/misc/future?removedVersion=${props.removedVersion}`}
+          >
+            Removed in v{props.removedVersion}
+          </a>
+        </span>
+      )}
+      {props.stability && (
         <div
-          className={`api-meta-stability api-meta-stability-${
-            props.stability || ''
-          }`}
+          className={`${styles.tag} ${ props.stability ? styles[props.stability.toLowerCase()] : ''}`}
         >
           Stability: {props.stability}
         </div>
