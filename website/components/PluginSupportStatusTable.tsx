56c6350e4e4209324090aafdbec41d403600dc8c
Cong-Cong Pan <dacongsama@live.com>
2024-04-18 21:01:09 +0800

    docs: update plugin support status table (#6285)
    * docs: update plugin support status table
    
    * fix: check format

--- website/components/PluginSupportStatusTable.tsx
+++ website/components/PluginSupportStatusTable.tsx
@@ -152,6 +152,282 @@ const pluginSupportStatusList: PluginSupportStatus[] = [
     name: 'WatchIgnorePlugin',
     status: SupportStatus.NotSupported,
   },
+
+  // internal Rspack plugins
+  {
+    name: 'NodeEnvironmentPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'MemoryCachePlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'RecordIdsPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'EntryPlugin',
+    status: SupportStatus.PartiallySupported,
+    notes: {
+      en: '`layer` and `wasmLoading` options are not supported, and `filename` cannot accept a function as a value',
+      zh: '不支持 `layer`、`wasmLoading` 选项，`filename` 无法接受函数作为值',
+    },
+  },
+  {
+    name: 'JsonpTemplatePlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'NodeTemplatePlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'LibraryTemplatePlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'WebWorkerTemplatePlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'EvalDevToolModulePlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'APIPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'ConstPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'RequireJsStuffPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'NodeSourcePlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'NodeTargetPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'AMDPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'CommonJsPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'RequireContextPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'RequireEnsurePlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'RequireIncludePlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'MergeDuplicateChunksPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'RemoveEmptyChunksPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'FlagIncludedChunksPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'RealContentHashPlugin',
+    status: SupportStatus.PartiallySupported,
+    notes: {
+      en: '`hashFunction` and `hashDigest` options are not supported',
+      zh: '不支持 `hashFunction`、`hashDigest` 选项',
+    },
+  },
+
+  // not write in webpack docs
+  {
+    name: 'AbstractLibraryPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'AggressiveMergingPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'AggressiveSplittingPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'ChunkModuleIdRangePlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'CleanPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'ConsumeSharedPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'ContainerPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'ContainerReferencePlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'ContextExclusionPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'ContextReplacementPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'DelegatedPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'DeterministicChunkIdsPlugin',
+    status: SupportStatus.PartiallySupported,
+    notes: {
+      en: '`context` and `maxLength` options are not supported',
+      zh: '不支持 `context`、`maxLength` 选项',
+    },
+  },
+  {
+    name: 'DeterministicModuleIdsPlugin',
+    status: SupportStatus.PartiallySupported,
+    notes: {
+      en: '`context`, `test`, `maxLength`, `salt`, `fixedLength`, `failOnConflict` options are not supported',
+      zh: '不支持 `context`、`test`、`maxLength`、`salt`、`fixedLength`、`failOnConflict` 选项',
+    },
+  },
+  {
+    name: 'DllReferencePlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'DynamicEntryPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'ElectronTargetPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'EnableChunkLoadingPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'EnableLibraryPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'EnableWasmLoadingPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'EntryOptionPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'ExternalsPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'FetchCompileAsyncWasmPlugin',
+    status: SupportStatus.PartiallySupported,
+    notes: {
+      en: 'Temporarily not exported from the JavaScript side',
+      zh: '暂时未从 JavaScript 侧导出',
+    },
+  },
+  {
+    name: 'FetchCompileWasmPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'HttpUriPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'JavascriptModulesPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'LibManifestPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'LoaderOptionsPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'NamedChunkIdsPlugin',
+    status: SupportStatus.PartiallySupported,
+    notes: {
+      en: '`delimiter` and `context` options are not supported',
+      zh: '不支持 `delimiter`、`context` 选项',
+    },
+  },
+  {
+    name: 'NamedModuleIdsPlugin',
+    status: SupportStatus.PartiallySupported,
+    notes: {
+      en: '`context` options are not supported',
+      zh: '不支持 `context` 选项',
+    },
+  },
+  {
+    name: 'NaturalModuleIdsPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'OccurrenceChunkIdsPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'OccurrenceModuleIdsPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'ProvideSharedPlugin',
+    status: SupportStatus.PartiallySupported,
+    notes: {
+      en: 'Temporarily not exported from the JavaScript side',
+      zh: '暂时未从 JavaScript 侧导出',
+    },
+  },
+  {
+    name: 'ReadFileCompileWasmPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'RuntimeChunkPlugin',
+    status: SupportStatus.NotSupported,
+  },
+  {
+    name: 'SideEffectsFlagPlugin',
+    status: SupportStatus.FullySupported,
+  },
+  {
+    name: 'SyncModuleIdsPlugin',
+    status: SupportStatus.NotSupported,
+  },
 ];
 
 export const PluginSupportStatusTable: React.FC = () => {
