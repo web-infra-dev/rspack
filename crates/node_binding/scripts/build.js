e41992540e00a03ef48e0072c95cd7d0ebaa2a73
kfll <xc2@users.noreply.github.com>
2024-04-22 13:02:56 +0800

    fix(binding): fix type error of binding.d.ts; add test for binding.d.ts (#6274)
    * fix(binding): fix binding.d.ts; add type test; support manually maintained type decleration
    
    * fix: fix windows run
    
    * fix: fix windows run

--- crates/node_binding/scripts/build.js
+++ crates/node_binding/scripts/build.js
@@ -22,7 +22,10 @@ async function build() {
 			"--dts",
 			"binding.d.ts",
 			"--no-js",
-			"--no-const-enum"
+			"--no-const-enum",
+			"--no-dts-header",
+			"--pipe",
+			`"node ./scripts/dts-header.js"`
 		];
 		if (release) {
 			args.push("--release");
