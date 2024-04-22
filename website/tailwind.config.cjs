d917f6717e401d2b5c396f4006a7edc583bc1ed3
戴陈威 <daichenwei@bytedance.com>
2024-04-22 21:45:28 +0800

    chore: bump rspress to 1.18.3

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js,jsx,tx,tsx}',
     './theme/**/*.{html,js,jsx,tx,tsx}',
     './components/**/*.{js,jsx,tx,tsx}',
   ],
