dbe97187f3e441eeeb538a915f1fb071ba3f46fd
ahabhgk <ahabhgk@gmail.com>
2024-04-23 17:54:03 +0800

    doc

--- website/components/builtIns/mdx-components.tsx
+++ website/components/builtIns/mdx-components.tsx
@@ -15,7 +15,9 @@ export const Tr = (props: ComponentProps<'tr'>) => {
   return (
     <tr
       {...props}
-      className={`border border-solid transition-colors duration-500 even:bg-soft border-gray-light-2 ${props.className || ''}`}
+      className={`border border-solid transition-colors duration-500 even:bg-soft border-gray-light-2 ${
+        props.className || ''
+      }`}
     />
   );
 };
@@ -24,7 +26,9 @@ export const Td = (props: ComponentProps<'td'>) => {
   return (
     <td
       {...props}
-      className={`border border-solid  px-4 py-2 border-gray-light-2 ${props.className || ''}`}
+      className={`border border-solid  px-4 py-2 border-gray-light-2 ${
+        props.className || ''
+      }`}
     />
   );
 };
@@ -33,7 +37,9 @@ export const Th = (props: ComponentProps<'th'>) => {
   return (
     <th
       {...props}
-      className={`border border-solid px-4 py-2 text-text-1 text-base font-semibold border-gray-light-2 ${props.className || ''}`}
+      className={`border border-solid px-4 py-2 text-text-1 text-base font-semibold border-gray-light-2 ${
+        props.className || ''
+      }`}
     />
   );
 };
