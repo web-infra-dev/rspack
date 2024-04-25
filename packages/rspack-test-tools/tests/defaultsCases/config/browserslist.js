549825af333f5af65533c3a23e98652dd26a3379
Gengkun <ahabhgk@gmail.com>
2024-04-25 13:58:25 +0800

    fix: default resolve roots (#6340)

--- packages/rspack-test-tools/tests/defaultsCases/config/browserslist.js
+++ packages/rspack-test-tools/tests/defaultsCases/config/browserslist.js
@@ -6,23 +6,26 @@ module.exports = {
 	}),
 	diff: e =>
 		e.toMatchInlineSnapshot(`
-    - Expected
-    + Received
+		- Expected
+		+ Received
 
-    @@ ... @@
-    -   "context": "<cwd>",
-    +   "context": "<cwd>/tests/fixtures/browserslist",
-    @@ ... @@
-    -     "chunkLoadingGlobal": "webpackChunk_rspack_core",
-    +     "chunkLoadingGlobal": "webpackChunkbrowserslist_test",
-    @@ ... @@
-    -     "devtoolNamespace": "@rspack/core",
-    +     "devtoolNamespace": "browserslist-test",
-    @@ ... @@
-    -     "hotUpdateGlobal": "webpackHotUpdate_rspack_core",
-    +     "hotUpdateGlobal": "webpackHotUpdatebrowserslist_test",
-    @@ ... @@
-    -     "uniqueName": "@rspack/core",
-    +     "uniqueName": "browserslist-test",
-  `)
+		@@ ... @@
+		-   "context": "<cwd>",
+		+   "context": "<cwd>/tests/fixtures/browserslist",
+		@@ ... @@
+		-     "chunkLoadingGlobal": "webpackChunk_rspack_core",
+		+     "chunkLoadingGlobal": "webpackChunkbrowserslist_test",
+		@@ ... @@
+		-     "devtoolNamespace": "@rspack/core",
+		+     "devtoolNamespace": "browserslist-test",
+		@@ ... @@
+		-     "hotUpdateGlobal": "webpackHotUpdate_rspack_core",
+		+     "hotUpdateGlobal": "webpackHotUpdatebrowserslist_test",
+		@@ ... @@
+		-     "uniqueName": "@rspack/core",
+		+     "uniqueName": "browserslist-test",
+		@@ ... @@
+		-       "<cwd>",
+		+       "<cwd>/tests/fixtures/browserslist",
+	`)
 };
