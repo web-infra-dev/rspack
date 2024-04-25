6584022599892b18a7ee8efc259dc9d375c1fe4b
neverland <chenjiahan.jait@bytedance.com>
2024-04-25 16:17:24 +0800

    docs: improve homepage style

--- website/theme/pages/index.tsx
+++ website/theme/pages/index.tsx
@@ -1,5 +1,5 @@
-import { Hero, HomeHero } from '../components/HomeHero';
-import { HomeFeature, Feature } from '../components/HomeFeatures';
+import { type Hero, HomeHero } from '../components/HomeHero';
+import { HomeFeature, type Feature } from '../components/HomeFeatures';
 import { NoSSR, usePageData } from 'rspress/runtime';
 import { Benchmark } from '../components/Benchmark';
 import { HomeFooter } from '../components/HomeFooter/index';
@@ -11,11 +11,10 @@ export function HomeLayout() {
     <div>
       {/* Landing Page */}
       <div
-        className="relative border-b dark:border-dark-50"
+        className="relative border-b"
         style={{
-          background: 'var(--rp-home-bg)',
-          minHeight: 'calc(80rem - var(--rp-nav-height))',
           paddingBottom: '56px',
+          borderColor: 'var(--rp-c-divider-light)',
         }}
       >
         <div className="pt-14 pb-12">
