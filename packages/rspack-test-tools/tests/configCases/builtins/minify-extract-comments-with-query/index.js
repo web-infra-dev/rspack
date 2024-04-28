4da0b3a23c5cad30bfad8b51ab5e2cfde3fddabf
LingyuCoder <lingyucoder@gmail.com>
2024-04-28 17:52:28 +0800

    test: add case for extract comments

---
+++ packages/rspack-test-tools/tests/configCases/builtins/minify-extract-comments-with-query/index.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const path = require("path");
+/*! Legal Comment */
+
+/**
+ * @preserve Copyright 2009 SomeThirdParty.
+ * Here is the full license text and copyright
+ * notice for this file. Note that the notice can span several
+ * lines and is only terminated by the closing star and slash:
+ */
+
+/**
+ * Utility functions for the foo package.
+ * @license Apache-2.0
+ */
+
+/*! Legal Foo */
+
+// Foo
+
+/*
+ Foo Bar
+ */
+
+// @license
+
+/*
+ * Foo
+ */
+// @lic
+
+function getComments(content, regex) {
+	const comments = [];
+	while ((array1 = regex.exec(content)) !== null) {
+		comments.push(array1[1]);
+	}
+	return comments;
+}
+it("should minify and extract comments", () => {
+	const licenseFile = path.resolve(__dirname, "bundle0.js.LICENSE.txt");
+	expect(fs.existsSync(licenseFile));
+	const content = fs.readFileSync(licenseFile, "utf-8");
+	expect(getComments(content, /@preserve/g).length).toBe(1);
+	expect(getComments(content, /@license/g).length).toBe(2);
+});
