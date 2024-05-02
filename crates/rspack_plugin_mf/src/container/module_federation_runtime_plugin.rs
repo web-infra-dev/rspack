96e922820f43bb760c864fd62245b6b8d02ea51d
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-05-01 19:37:47 -0700

    fix(rspack_plugin_mf): add chunk js matcher runtime to federation runtime

--- crates/rspack_plugin_mf/src/container/module_federation_runtime_plugin.rs
+++ crates/rspack_plugin_mf/src/container/module_federation_runtime_plugin.rs
@@ -1,6 +1,6 @@
 use async_trait::async_trait;
 use rspack_core::{
-  impl_runtime_module,
+  compile_boolean_matcher, impl_runtime_module,
   rspack_sources::{BoxSource, RawSource, SourceExt},
   ApplyContext, ChunkUkey, Compilation, CompilationAdditionalTreeRuntimeRequirements,
   CompilerOptions, Plugin, PluginContext, RuntimeGlobals, RuntimeModule, RuntimeModuleStage,
@@ -9,6 +9,7 @@ use rspack_error::Result;
 use rspack_hook::{plugin, plugin_hook};
 use rspack_identifier::Identifier;
 use rspack_util::source_map::SourceMapKind;
+use runtime_module::utils::chunk_has_js;
 
 #[impl_runtime_module]
 #[derive(Debug, Eq)]
@@ -41,20 +42,29 @@ impl RuntimeModule for FederationRuntimeModule {
     RuntimeModuleStage::Normal
   }
 
-  fn generate(&self, _: &Compilation) -> rspack_error::Result<BoxSource> {
-    Ok(RawSource::from(federation_runtime_template()).boxed())
+  fn generate(&self, compilation: &Compilation) -> rspack_error::Result<BoxSource> {
+    Ok(RawSource::from(federation_runtime_template(compilation)).boxed())
   }
 }
 
-fn federation_runtime_template() -> String {
+fn federation_runtime_template(compilation: &Compilation) -> String {
   let federation_global = format!("{}.federation", RuntimeGlobals::REQUIRE);
+  let condition_map =
+    compilation
+      .chunk_graph
+      .get_chunk_condition_map(&chunk.ukey, compilation, chunk_has_js);
+  let has_js_matcher = compile_boolean_matcher(&condition_map);
+
   format!(
     r#"
 if(!{federation_global}){{
-  {federation_global} = {{}};
+    {federation_global} = {{
+        chunkMatcher: {has_js_matcher}
+    }};
 }}
 "#,
     federation_global = federation_global,
+    has_js_matcher = &has_js_matcher.render("chunkId")
   )
 }
 
