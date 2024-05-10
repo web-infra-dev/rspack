c5d7dd5cbad642b982703f2c09f471930803228a
jerrykingxyz <jerrykingxyz@gmail.com>
2024-05-10 11:33:06 +0800

    fix: make *_dependencies memory leak

--- crates/rspack_core/src/compiler/make/repair/factorize.rs
+++ crates/rspack_core/src/compiler/make/repair/factorize.rs
@@ -224,9 +224,13 @@ impl Task<MakeTaskContext> for FactorizeResultTask {
         .map(|d| d.with_module_identifier(original_module_identifier)),
     );
 
-    context.file_dependencies.extend(file_dependencies);
-    context.context_dependencies.extend(context_dependencies);
-    context.missing_dependencies.extend(missing_dependencies);
+    context.file_dependencies.add_batch_file(&file_dependencies);
+    context
+      .context_dependencies
+      .add_batch_file(&context_dependencies);
+    context
+      .missing_dependencies
+      .add_batch_file(&missing_dependencies);
     let module_graph =
       &mut MakeTaskContext::get_module_graph_mut(&mut context.module_graph_partial);
     let Some(factory_result) = factory_result else {
