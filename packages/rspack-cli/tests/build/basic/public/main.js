6cc1b4877534e7259148a101d9a2a2a148f62a3e
LingyuCoder <lingyucoder@gmail.com>
2024-05-15 15:23:07 +0800

    test: add test

--- packages/rspack-cli/tests/build/basic/public/main.js
+++ packages/rspack-cli/tests/build/basic/public/main.js
@@ -4,24 +4,10 @@
 			console.log("CONFIG");
 		}
 	};
-	// The module cache
-	var __webpack_module_cache__ = {};
-	function __webpack_require__(moduleId) {
-		// Check if module is in cache
-		var cachedModule = __webpack_module_cache__[moduleId];
-		if (cachedModule !== undefined) {
-			return cachedModule.exports;
-		}
-		// Create a new module (and put it into the cache)
-		var module = (__webpack_module_cache__[moduleId] = {
-			exports: {}
-		});
-		// Execute the module function
-		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-		// Return the exports of the module
-		return module.exports;
-	}
-	var __webpack_exports__ = __webpack_require__("./src/entry.js");
+	// startup
+	// Load entry module and return exports
+	var __webpack_exports__ = {};
+	__webpack_modules__["./src/entry.js"]();
 })();
 
 //# sourceMappingURL=main.js.map
