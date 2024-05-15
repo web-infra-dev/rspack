4adb0aa224b1d7ba0db6eb7a191ae10c7087b5a0
LingyuCoder <lingyucoder@gmail.com>
2024-05-15 18:14:14 +0800

    test: config type and rename

--- packages/rspack-test-tools/rspack.config.js
+++ packages/rspack-test-tools/rspack.config.js
@@ -1,21 +1,80 @@
-/** @type {import("@rspack/core").Configuration} */
+const rspack = require("@rspack/core");
+const path = require("path");
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
+const prod = process.env.NODE_ENV === "production";
+
 module.exports = {
-	target: "node",
-	entry: "./index.js",
-	output: {
-		filename: "[name].js"
+	entry: {
+		diff: "./viewer/entries/diff.tsx"
 	},
-	optimization: {
-		splitChunks: {
-			minSize: 1,
-			cacheGroups: {
-				splittedFoo: {
-					name: "splittedFoo",
-					test: /(foo|foo-2)\.js/,
-					priority: 0,
-					reuseExistingChunk: true
+	resolve: {
+		extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+		tsConfigPath: path.resolve(__dirname, "tsconfig.assets.json")
+	},
+	devtool: false,
+	output: {
+		globalObject: "self",
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "template")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(j|t)s$/,
+				exclude: [/[\\/]node_modules[\\/]/],
+				loader: "builtin:swc-loader",
+				options: {
+					sourceMaps: false,
+					jsc: {
+						parser: {
+							syntax: "typescript"
+						},
+						externalHelpers: true
+					},
+					env: {
+						targets: "Chrome >= 48"
+					}
 				}
+			},
+			{
+				test: /\.(j|t)sx$/,
+				loader: "builtin:swc-loader",
+				exclude: [/[\\/]node_modules[\\/]/],
+				options: {
+					sourceMaps: false,
+					jsc: {
+						parser: {
+							syntax: "typescript",
+							tsx: true
+						},
+						transform: {
+							react: {
+								runtime: "automatic",
+								development: !prod,
+								refresh: false
+							}
+						},
+						externalHelpers: true
+					},
+					env: {
+						targets: "Chrome >= 48"
+					}
+				}
+			},
+			{
+				test: /\.ttf$/,
+				type: "asset/resource"
 			}
-		}
-	}
+		]
+	},
+	plugins: [
+		new rspack.HtmlRspackPlugin({
+			template: "./viewer/templates/diff.html",
+			filename: "[name].html"
+		}),
+		new MonacoWebpackPlugin({
+			languages: ["javascript"]
+		})
+	]
 };
