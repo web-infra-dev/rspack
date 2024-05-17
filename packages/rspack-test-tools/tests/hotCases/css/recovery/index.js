bdafdf62c36ec138bd8716ba7d40ff99e3b2f1d2
jerrykingxyz <jerrykingxyz@gmail.com>
2024-05-17 13:47:25 +0800

    fix: ci

--- packages/rspack-test-tools/tests/hotCases/css/recovery/index.js
+++ packages/rspack-test-tools/tests/hotCases/css/recovery/index.js
@@ -2,9 +2,13 @@ import "./index.css";
 
 it("css recovery", done => {
 	NEXT(
-		require("../../update")(err => {
-			expect(String(err)).toContain("Module build failed");
-			NEXT(require("../../update")(done, true, () => done()));
-		})
+		require("../../update")(
+			err => {
+				expect(String(err)).toContain("Module build failed");
+				NEXT(require("../../update")(done, true, () => done()));
+			},
+			true,
+			() => done()
+		)
 	);
 });
