4178e8b08795ff319f8e5e174687305ca040cb5d
ahabhgk <ahabhgk@gmail.com>
2024-05-20 11:44:43 +0800

    t

--- webpack-test/configCases/resolve-merging/imports-fields/index.js
+++ webpack-test/configCases/resolve-merging/imports-fields/index.js
@@ -1,6 +1,6 @@
 import d1 from "./pkg.mjs";
 import d2 from "#internal";
-import { d3, d4 } from "lib1";
+import { d3, d4 } from "lib";
 
 it("imports field to resolve to the same", () => {
 	expect(d2).toBe(d1);
