427fb38e3870b4f658447f8d632ff568a5820e24
CPunisher <1343316114@qq.com>
2024-05-29 13:49:24 +0800

    Fix testcase

--- packages/rspack-test-tools/tests/configCases/loader/context-module/index.js
+++ packages/rspack-test-tools/tests/configCases/loader/context-module/index.js
@@ -1,7 +1,7 @@
 it("should exist request, rawRequest, and userRequest", async () => {
 	const result = require("./a");
 	const { request, userRequest, rawRequest } = result;
-	expect(request.endWiths("a.js")).toBe(true);
-	expect(userRequest.endWiths("a.js")).toBe(true);
-	expect(rawRequest.endWiths("a.js")).toBe(true);
+	expect(request.endsWith("a.js")).toBe(true);
+	expect(userRequest.endsWith("a.js")).toBe(true);
+	expect(rawRequest).toBe("./a");
 });
