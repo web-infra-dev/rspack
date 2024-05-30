b65aa27ecbe5278001160e4ee75c4d0c12716616
LingyuCoder <lingyucoder@gmail.com>
2024-05-30 11:57:35 +0800

    fix: replace \0 with \u200b

--- packages/rspack-test-tools/tests/configCases/loader-import-module/nul-byte/convert-loader.js
+++ packages/rspack-test-tools/tests/configCases/loader-import-module/nul-byte/convert-loader.js
@@ -4,8 +4,9 @@ module.exports = async function () {
   const importLoader = path.resolve(__dirname, 'import-loader.js');
   const sourceLoader = path.resolve(__dirname, 'source-loader.js');
   const empty = path.resolve(__dirname, 'empty.js');
+  const request = `${importLoader}!${sourceLoader}?{"content": "#b"}!${empty}`;
   return `
-    import value from '${importLoader}!${sourceLoader}?{"content": "#b"}!${empty}';
+    import value from ${JSON.stringify(request)};
     export default value;`
     ;
 }
