3697011321ae2f5010fb82e954a1a05f102266dd
ahabhgk <ahabhgk@gmail.com>
2024-05-30 15:43:32 +0800

    u

--- packages/rspack-test-tools/tests/configCases/container-1-5/lowing-syntax-with-loader/index.js
+++ packages/rspack-test-tools/tests/configCases/container-1-5/lowing-syntax-with-loader/index.js
@@ -1,4 +1,4 @@
-it("should not have es5 upper syntax", async () => {
+it("should not have es6 upper syntax", async () => {
 	await __webpack_init_sharing__("default");
 	const container = __non_webpack_require__("./container.js");
 	container.init(__webpack_share_scopes__.default);
