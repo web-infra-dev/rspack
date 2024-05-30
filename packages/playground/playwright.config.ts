4cffe920a988498a4394084de19986026ddc4d6a
Fy <1114550440@qq.com>
2024-05-30 20:29:40 +0800

    chore: fix testDir (#6679)

--- packages/playground/playwright.config.ts
+++ packages/playground/playwright.config.ts
@@ -5,7 +5,7 @@ const TIMEOUT = 2 * 60 * 1000;
 
 export default defineConfig<RspackOptions>({
 	// Look for test files in the "fixtures" directory, relative to this configuration file.
-	testDir: "./cases/lazy-compilation",
+	testDir: "./cases",
 
 	//	globalSetup: require.resolve("./scripts/globalSetup"),
 
