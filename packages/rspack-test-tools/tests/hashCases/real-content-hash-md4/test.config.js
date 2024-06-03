18a6c088ab82608425c9117d8638165a525246c4
LingyuCoder <lingyucoder@gmail.com>
2024-06-03 18:42:18 +0800

    fix: generate correct real content hash

---
+++ packages/rspack-test-tools/tests/hashCases/real-content-hash-md4/test.config.js
@@ -0,0 +1,12 @@
+// const crypto = require("node:crypto");
+
+/** @type {import('../../../dist').THashCaseConfig} */
+module.exports = {
+	validate(stats) {
+		const assets = stats.stats[0].toJson().assetsByChunkName.main;
+		assets.sort();
+
+		// `afc10c70ed4ce2b33593` = hash of src/file.svg
+		expect(assets).toEqual(['afc10c70ed4ce2b33593.svg', 'main.js']);
+	}
+};
