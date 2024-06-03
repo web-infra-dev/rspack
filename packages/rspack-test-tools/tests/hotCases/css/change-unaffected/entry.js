bd5727846cfc48ff7d58a2987c1f7ec8da5fe24c
harpsealjs <lingyucoder@gmail.com>
2024-06-03 19:04:05 +0800

    test: pass through cacheable (#6711)
    * test: pass cachable in css extract
    
    * test: pass cachable in css extract

--- packages/rspack-test-tools/tests/hotCases/css/change-unaffected/entry.js
+++
@@ -1,9 +0,0 @@
-import styles from "./color.module.css";
-module.hot.accept()
-// modify here
-export default styles
----
-import styles from "./color.module.css";
-module.hot.accept()
-// modify
-export default styles;
--- packages/rspack-test-tools/tests/hotCases/css/change-unaffected/index.js
+++
@@ -1,7 +0,0 @@
-it("css hmr", (done) => {
-	NEXT(require("../../update")(done, true, async () => {
-		const styles = await import('./entry.js')
-		expect(styles['default']).toHaveProperty("bar");
-		done();
-	}));
-}, 1000);
