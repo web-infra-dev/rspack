c500ce298b541656d36589f84fc3aaf02307fac0
JW <jiangweixian1994@gmail.com>
2024-06-04 20:36:27 +0800

    chore: resolve entry options

--- packages/rspack/src/builtin-plugin/RSCServerReferenceManifestRspackPlugin.ts
+++
@@ -1,9 +0,0 @@
-import { BuiltinPluginName } from "@rspack/binding";
-
-import { create } from "./base";
-
-export const RSCServerReferenceManifestRspackPlugin = create(
-	BuiltinPluginName.RSCServerReferenceManifestRspackPlugin,
-	() => {},
-	"compilation"
-);
