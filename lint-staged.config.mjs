e872f637c7bdfa6a0305eca311632551ebe967be
SoonIter <sooniter@gmail.com>
2024-06-06 17:21:12 +0800

    chore: rebase master

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -4,7 +4,7 @@ export default {
 	"*.toml": "npx @taplo/cli format",
 	"*.{ts,tsx,js,cts,cjs,mts,mjs}": () => [
 		"pnpm run lint:js",
-		"pnpm run lint:js-sort-imports-order --write",
+		"pnpm run lint:js-sort-imports-order",
 		"pnpm run api-extractor:ci"
 	]
 };
