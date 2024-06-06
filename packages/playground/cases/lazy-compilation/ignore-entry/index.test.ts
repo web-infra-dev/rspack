5b6fd1f9d3cd00e328779fa90a3199dbfc4e0f4c
Cong-Cong Pan <dacongsama@live.com>
2024-06-06 13:29:17 +0800

    chore: add test case for chunk graph (#6744)

--- packages/playground/cases/lazy-compilation/ignore-entry/index.test.ts
+++ packages/playground/cases/lazy-compilation/ignore-entry/index.test.ts
@@ -1,7 +1,6 @@
 import { expect, test } from "@/fixtures";
 
 test("should load success", async ({ page }) => {
-	await page.waitForTimeout(1000);
-	const body = await page.$("body");
-	expect(await body!.innerText()).toBe("hello world");
+	await page.getByText("Click me").click();
+	await expect(page).toHaveURL(/success/);
 });
