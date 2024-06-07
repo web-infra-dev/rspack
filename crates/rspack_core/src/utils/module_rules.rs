bebf02af6a8ae29833a12d3812c2f249330f8eee
Hana <andywangsy@gmail.com>
2024-06-08 00:10:55 +0800

    feat: init

--- crates/rspack_core/src/utils/module_rules.rs
+++ crates/rspack_core/src/utils/module_rules.rs
@@ -1,6 +1,7 @@
 use async_recursion::async_recursion;
 use rspack_error::Result;
 use rspack_loader_runner::ResourceData;
+use serde_json::Value;
 
 use crate::{DependencyCategory, ModuleRule};
 
@@ -26,8 +27,10 @@ pub async fn module_rule_matcher<'a>(
   dependency: &DependencyCategory,
   matched_rules: &mut Vec<&'a ModuleRule>,
 ) -> Result<bool> {
+  let resource = Value::String(resource_data.resource_path.to_string_lossy().to_string());
+
   if let Some(test_rule) = &module_rule.rspack_resource
-    && !test_rule.try_match(&resource_data.resource).await?
+    && !test_rule.try_match(&resource).await?
   {
     return Ok(false);
   }
@@ -35,38 +38,31 @@ pub async fn module_rule_matcher<'a>(
   // Include all modules that pass test assertion. If you supply a Rule.test option, you cannot also supply a `Rule.resource`.
   // See: https://webpack.js.org/configuration/module/#ruletest
   if let Some(test_rule) = &module_rule.test
-    && !test_rule
-      .try_match(&resource_data.resource_path.to_string_lossy())
-      .await?
+    && !test_rule.try_match(&resource).await?
   {
     return Ok(false);
   } else if let Some(resource_rule) = &module_rule.resource
-    && !resource_rule
-      .try_match(&resource_data.resource_path.to_string_lossy())
-      .await?
+    && !resource_rule.try_match(&resource).await?
   {
     return Ok(false);
   }
 
   if let Some(include_rule) = &module_rule.include
-    && !include_rule
-      .try_match(&resource_data.resource_path.to_string_lossy())
-      .await?
+    && !include_rule.try_match(&resource).await?
   {
     return Ok(false);
   }
 
   if let Some(exclude_rule) = &module_rule.exclude
-    && exclude_rule
-      .try_match(&resource_data.resource_path.to_string_lossy())
-      .await?
+    && exclude_rule.try_match(&resource).await?
   {
     return Ok(false);
   }
 
   if let Some(resource_query_rule) = &module_rule.resource_query {
     if let Some(resource_query) = &resource_data.resource_query {
-      if !resource_query_rule.try_match(resource_query).await? {
+      let resource_query = Value::String(resource_query.to_string());
+      if !resource_query_rule.try_match(&resource_query).await? {
         return Ok(false);
       }
     } else {
@@ -76,8 +72,9 @@ pub async fn module_rule_matcher<'a>(
 
   if let Some(resource_fragment_condition) = &module_rule.resource_fragment {
     if let Some(resource_fragment) = &resource_data.resource_fragment {
+      let resource_fragment = Value::String(resource_fragment.to_string());
       if !resource_fragment_condition
-        .try_match(resource_fragment)
+        .try_match(&resource_fragment)
         .await?
       {
         return Ok(false);
@@ -89,7 +86,8 @@ pub async fn module_rule_matcher<'a>(
 
   if let Some(mimetype_condition) = &module_rule.mimetype {
     if let Some(mimetype) = &resource_data.mimetype {
-      if !mimetype_condition.try_match(mimetype).await? {
+      let mimetype = Value::String(mimetype.to_string());
+      if !mimetype_condition.try_match(&mimetype).await? {
         return Ok(false);
       }
     } else {
@@ -102,20 +100,25 @@ pub async fn module_rule_matcher<'a>(
     if scheme.is_none() {
       return Ok(false);
     }
-    if !scheme_condition.try_match(&scheme.to_string()).await? {
+    let scheme = Value::String(scheme.to_string());
+    if !scheme_condition.try_match(&scheme).await? {
       return Ok(false);
     }
   }
 
   if let Some(issuer_rule) = &module_rule.issuer
     && let Some(issuer) = issuer
-    && !issuer_rule.try_match(issuer).await?
+    && !issuer_rule
+      .try_match(&Value::String(issuer.to_string()))
+      .await?
   {
     return Ok(false);
   }
 
   if let Some(dependency_rule) = &module_rule.dependency
-    && !dependency_rule.try_match(&dependency.to_string()).await?
+    && !dependency_rule
+      .try_match(&Value::String(dependency.as_str().to_string()))
+      .await?
   {
     return Ok(false);
   }
@@ -123,7 +126,10 @@ pub async fn module_rule_matcher<'a>(
   if let Some(description_data) = &module_rule.description_data {
     if let Some(resource_description) = &resource_data.resource_description {
       for (k, matcher) in description_data {
-        if let Some(v) = resource_description.json().get(k).and_then(|v| v.as_str()) {
+        if let Some(v) = k
+          .split('.')
+          .try_fold(resource_description.json(), |acc, key| acc.get(key))
+        {
           if !matcher.try_match(v).await? {
             return Ok(false);
           }
