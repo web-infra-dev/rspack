d2b8aea8cd75035db39209abf803add9844b6260
harpsealjs <lingyucoder@gmail.com>
2024-06-11 13:17:57 +0800

    test: export setup for plugin testing (#6765)

--- packages/rspack-test-tools/src/helper/setup-env.ts
+++ packages/rspack-test-tools/src/helper/setup-env.ts
@@ -1,55 +1,5 @@
 // @ts-nocheck
 
-const { toMatchFileSnapshot } = require("./jestFileSnapshot");
-
-expect.extend({
-	// CHANGE: new test matcher for `rspack-test-tools`
-	toMatchFileSnapshot,
-	toBeTypeOf(received, expected) {
-		const objType = typeof received;
-		const pass = objType === expected;
-
-		const message = pass
-			? () =>
-					this.utils.matcherHint(".not.toBeTypeOf") +
-					"\n\n" +
-					"Expected value to not be (using typeof):\n" +
-					`  ${this.utils.printExpected(expected)}\n` +
-					"Received:\n" +
-					`  ${this.utils.printReceived(objType)}`
-			: () =>
-					this.utils.matcherHint(".toBeTypeOf") +
-					"\n\n" +
-					"Expected value to be (using typeof):\n" +
-					`  ${this.utils.printExpected(expected)}\n` +
-					"Received:\n" +
-					`  ${this.utils.printReceived(objType)}`;
-
-		return { message, pass };
-	},
-	toEndWith(received, expected) {
-		const pass = typeof received === "string" && received.endsWith(expected);
-
-		const message = pass
-			? () =>
-					this.utils.matcherHint(".not.toEndWith") +
-					"\n\n" +
-					"Expected value to not end with:\n" +
-					`  ${this.utils.printExpected(expected)}\n` +
-					"Received:\n" +
-					`  ${this.utils.printReceived(received)}`
-			: () =>
-					this.utils.matcherHint(".toEndWith") +
-					"\n\n" +
-					"Expected value to end with:\n" +
-					`  ${this.utils.printExpected(expected)}\n` +
-					"Received:\n" +
-					`  ${this.utils.printReceived(received)}`;
-
-		return { message, pass };
-	}
-});
-
 if (process.env.ALTERNATIVE_SORT) {
 	const oldSort = Array.prototype.sort;
 
