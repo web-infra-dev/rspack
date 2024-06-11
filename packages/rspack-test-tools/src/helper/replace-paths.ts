ccf877dbcb5522ac8a2647e53719b9bee820db3e
LingyuCoder <lingyucoder@gmail.com>
2024-06-11 18:07:35 +0800

    test: fix win32 snapshot

--- packages/rspack-test-tools/src/helper/replace-paths.ts
+++ packages/rspack-test-tools/src/helper/replace-paths.ts
@@ -7,9 +7,9 @@ const rspackPath = path.resolve(__dirname, "../../../rspack");
 
 export function replacePaths(input: string) {
 	const rspackRoot: string = normalizePaths(rspackPath);
+	input = input.split("\\\\").join("\\");
 	if (os.platform() === "win32") {
-		const winRspackRoot = rspackRoot.split(path.posix.sep).join(path.win32.sep);
-		console.log(input);
+		const winRspackRoot = rspackRoot.split("\\\\").join(path.win32.sep);
 		console.log(winRspackRoot);
 		return normalizePaths(input)
 			.split(rspackRoot)
