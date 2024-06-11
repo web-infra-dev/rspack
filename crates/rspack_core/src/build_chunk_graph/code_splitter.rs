09ab804ba45c6374a8db91c5bcaa4ede59357711
jserfeng <1114550440@qq.com>
2024-06-11 17:28:07 +0800

    fix: should remove correct connections

--- crates/rspack_core/src/build_chunk_graph/code_splitter.rs
+++ crates/rspack_core/src/build_chunk_graph/code_splitter.rs
@@ -1522,6 +1522,8 @@ Or do you want to use the entrypoints '{name}' and '{runtime}' independently on 
             })
           })
         }
+
+        active_connections.reverse();
         for i in active_connections {
           cgi.skipped_module_connections.shift_remove_index(i);
         }
