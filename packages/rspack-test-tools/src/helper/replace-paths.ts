08ccf1195bb2a0c1d6e6c197729792abcd25f1db
LingyuCoder <lingyucoder@gmail.com>
2024-06-11 16:51:57 +0800

    test: fix win32 snapshot

--- packages/rspack-test-tools/src/helper/replace-paths.ts
+++ packages/rspack-test-tools/src/helper/replace-paths.ts
@@ -2,8 +2,12 @@ const path = require("path");
 const serializer = require("jest-serializer-path");
 const normalizePaths = serializer.normalizePaths;
 const rspackPath = path.resolve(__dirname, "../../../rspack");
+const os = require("os");
 
 export function replacePaths(input: string) {
 	const rspackRoot = normalizePaths(rspackPath);
+	if (os.platform() === "win32") {
+		input = input.split("\\\\").join(path.win32.sep);
+	}
 	return normalizePaths(input).split(rspackRoot).join("<RSPACK_ROOT>");
 }
