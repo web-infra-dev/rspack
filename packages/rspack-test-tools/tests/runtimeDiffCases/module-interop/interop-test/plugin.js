9b16a9e3f485b5bdd5aaba62856695ac07d34342
ahabhgk <ahabhgk@gmail.com>
2024-06-12 14:14:35 +0800

    fix win

--- packages/rspack-test-tools/tests/runtimeDiffCases/module-interop/interop-test/plugin.js
+++ packages/rspack-test-tools/tests/runtimeDiffCases/module-interop/interop-test/plugin.js
@@ -18,94 +18,94 @@ const print = (expr) =>
   `Promise.resolve().then(() => {}).then(() => console.log(util.inspect(${expr}, { showHidden: true, breakLength: Infinity, compact: true, getters: true })))`;
 
 const cases = (filename) => [
-  ["import x", `import x from "${filename}"; ${print("x")};`],
+  ["import x", `import x from ${filename}; ${print("x")};`],
   [
     "import { default as x }",
-    `import { default as x } from "${filename}"; ${print("x")};`,
+    `import { default as x } from ${filename}; ${print("x")};`,
   ],
   [
     "import * as x; x.default",
-    `import * as x from "${filename}"; ${print("x.default")};`,
+    `import * as x from ${filename}; ${print("x.default")};`,
   ],
   [
     "import * as x; ident(x).default",
-    `import * as x from "${filename}"; ${print("Object(x).default")};`,
+    `import * as x from ${filename}; ${print("Object(x).default")};`,
   ],
   [
     "import { named as x }",
-    `import { named as x } from "${filename}"; ${print("x")};`,
+    `import { named as x } from ${filename}; ${print("x")};`,
   ],
   [
     "import * as x; x.named",
-    `import * as x from "${filename}"; ${print("x.named")};`,
+    `import * as x from ${filename}; ${print("x.named")};`,
   ],
   [
     "import * as x; ident(x).named",
-    `import * as x from "${filename}"; ${print("Object(x).named")};`,
+    `import * as x from ${filename}; ${print("Object(x).named")};`,
   ],
   [
     "import { __esModule as x }",
-    `import { __esModule as x } from "${filename}"; ${print("x")};`,
+    `import { __esModule as x } from ${filename}; ${print("x")};`,
   ],
   [
     "import * as x; x.__esModule",
-    `import * as x from "${filename}"; ${print("x.__esModule")};`,
+    `import * as x from ${filename}; ${print("x.__esModule")};`,
   ],
   [
     "import * as x; ident(x).__esModule",
-    `import * as x from "${filename}"; ${print("Object(x).__esModule")};`,
+    `import * as x from ${filename}; ${print("Object(x).__esModule")};`,
   ],
-  ["import * as x", `import * as x from "${filename}"; ${print("x")};`],
+  ["import * as x", `import * as x from ${filename}; ${print("x")};`],
   [
     "import()",
-    `import("${filename}").then(x => { ${print(
+    `import(${filename}).then(x => { ${print(
       "x"
     )}; }).catch(err => { console.error(err); process.exitCode = 1; });`,
   ],
   [
     "x = require(); x.default",
-    `const x = require("${filename}"); ${print("x.default")};`,
+    `const x = require(${filename}); ${print("x.default")};`,
   ],
   [
     "x = require(); ident(x).default",
-    `const x = require("${filename}"); ${print("Object(x).default")};`,
+    `const x = require(${filename}); ${print("Object(x).default")};`,
   ],
   [
     "{ named } = require()",
-    `const { named } = require("${filename}"); ${print("named")};`,
+    `const { named } = require(${filename}); ${print("named")};`,
   ],
   [
     "x = require(); x.named",
-    `const x = require("${filename}"); ${print("x.named")};`,
+    `const x = require(${filename}); ${print("x.named")};`,
   ],
   [
     "x = require(); ident(x).named",
-    `const x = require("${filename}"); ${print("Object(x).named")};`,
+    `const x = require(${filename}); ${print("Object(x).named")};`,
   ],
   [
     "{ __esModule } = require()",
-    `const { __esModule } = require("${filename}"); ${print(
+    `const { __esModule } = require(${filename}); ${print(
       "__esModule"
     )};`,
   ],
   [
     "x = require(); x.__esModule",
-    `const x = require("${filename}"); ${print("x.__esModule")};`,
+    `const x = require(${filename}); ${print("x.__esModule")};`,
   ],
   [
     "x = require(); ident(x).__esModule",
-    `const x = require("${filename}"); ${print("Object(x).__esModule")};`,
+    `const x = require(${filename}); ${print("Object(x).__esModule")};`,
   ],
-  ["x = require()", `const x = require("${filename}"); ${print("x")};`],
+  ["x = require()", `const x = require(${filename}); ${print("x")};`],
   [
     "await import() === require()",
-    `import("${filename}").then(x => { const y = require("${filename}"); ${print(
+    `import(${filename}).then(x => { const y = require(${filename}); ${print(
       "x === y"
     )}; }).catch(err => { console.error(err); process.exitCode = 1; });`,
   ],
   [
     "import * as x; x === await import()",
-    `import * as x from "${filename}"; import("${filename}").then(y => { ${print(
+    `import * as x from ${filename}; import(${filename}).then(y => { ${print(
       "x === y"
     )}; }).catch(err => { console.error(err); process.exitCode = 1; });`,
   ],
@@ -134,7 +134,7 @@ module.exports = class GenerateCasesPlugin {
       await Promise.all(modules.map(moduleName => fs.promises.mkdir(path.resolve(extDir, moduleName))));
       const imports = await Promise.all(
         modules.flatMap(moduleName => {
-          return cases(path.resolve(src, `../modules/${moduleName}`)).map(
+          return cases(JSON.stringify(path.resolve(src, `../modules/${moduleName}`))).map(
             async ([name, content], idx) => {
               const requireTest = name.includes("require()");
               const testFilename = `index${idx}${requireTest ? ".js" : ext}`;
