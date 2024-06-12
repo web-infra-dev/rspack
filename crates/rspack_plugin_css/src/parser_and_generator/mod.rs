6da59c683ca46c2e439a2513e97a13476fb8cdba
ahabhgk <ahabhgk@gmail.com>
2024-06-12 15:18:58 +0800

    feat: use warning for css parsing

--- crates/rspack_plugin_css/src/parser_and_generator/mod.rs
+++ crates/rspack_plugin_css/src/parser_and_generator/mod.rs
@@ -13,7 +13,9 @@ use rspack_core::{
   ParserAndGenerator, RuntimeSpec, SourceType, TemplateContext, UsageState,
 };
 use rspack_core::{ModuleInitFragments, RuntimeGlobals};
-use rspack_error::{miette::Diagnostic, IntoTWithDiagnosticArray, Result, TWithDiagnosticArray};
+use rspack_error::{
+  miette::Diagnostic, IntoTWithDiagnosticArray, Result, Severity, TWithDiagnosticArray,
+};
 
 use crate::utils::export_locals_convention;
 use crate::utils::{css_modules_exports_to_string, LocalIdentOptions};
@@ -317,12 +319,19 @@ impl ParserAndGenerator for CssParserAndGenerator {
     }
     for warning in warnings {
       let range = warning.range();
-      diagnostics.push(Box::new(css_parsing_traceable_error(
+      let mut error = css_parsing_traceable_error(
         source_code.clone(),
         range.start,
         range.end,
         warning.to_string(),
-      )));
+      );
+      if !matches!(
+        warning.kind(),
+        css_module_lexer::WarningKind::NotPrecededAtImport
+      ) {
+        error = error.with_severity(Severity::Warn);
+      }
+      diagnostics.push(Box::new(error));
     }
 
     Ok(
