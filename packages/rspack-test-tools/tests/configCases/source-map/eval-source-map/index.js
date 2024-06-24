dc15a3bfe2f23361f66461dc4dce8c31eae2d4ec
Cong-Cong <dacongsama@live.com>
2024-06-24 19:13:39 +0800

    fix: test case

--- packages/rspack-test-tools/tests/configCases/source-map/eval-source-map/index.js
+++ packages/rspack-test-tools/tests/configCases/source-map/eval-source-map/index.js
@@ -6,5 +6,5 @@ it("basic", () => {
 			source
 		)[1];
 	const map = JSON.parse(Buffer.from(base64, "base64").toString("utf-8"));
-	expect(map.sources).toContain("./index.js");
+	expect(map.sources[0]).toMatch(/webpack:\/\/\/\.\/index.js?[a-zA-Z0-9]+/);
 });
