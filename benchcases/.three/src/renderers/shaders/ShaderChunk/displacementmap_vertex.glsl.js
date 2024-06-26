670f374e048991db324a7ceb2653389f09b189a9
neverland <chenjiahan.jait@bytedance.com>
2024-06-26 15:22:27 +0800

    chore: remove unused benchcases folder (#6936)
    * chore: remove unused bencases folder
    
    * fix: codeowners

--- benchcases/.three/src/renderers/shaders/ShaderChunk/displacementmap_vertex.glsl.js
+++
@@ -1,7 +0,0 @@
-export default /* glsl */`
-#ifdef USE_DISPLACEMENTMAP
-
-	transformed += normalize( objectNormal ) * ( texture2D( displacementMap, uv ).x * displacementScale + displacementBias );
-
-#endif
-`;
--- benchcases/.three/src/renderers/shaders/ShaderChunk/uv2_pars_vertex.glsl.js
+++
@@ -1,8 +0,0 @@
-export default /* glsl */`
-#if defined( USE_LIGHTMAP ) || defined( USE_AOMAP )
-
-	attribute vec2 uv2;
-	varying vec2 vUv2;
-
-#endif
-`;
