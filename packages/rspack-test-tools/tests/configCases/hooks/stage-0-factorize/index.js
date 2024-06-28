5255f1b89c5b58d79710dca6e754cdeec0fec3d6
Gengkun <ahabhgk@gmail.com>
2024-06-28 19:04:22 +0800

    test: stable stage-0-factorize test (#6981)

--- packages/rspack-test-tools/tests/configCases/hooks/stage-0-factorize/index.js
+++ packages/rspack-test-tools/tests/configCases/hooks/stage-0-factorize/index.js
@@ -5,13 +5,11 @@ require('./b')
 
 it("should not call factorize hook for requiest: 'fs' and 'path' (bailed by external)", () => {
 	const mainFile = fs.readFileSync(path.normalize(__filename), "utf-8");
-	expect(
-		mainFile.startsWith(
-`
-/* ./index.js */
-/* ./b */
-/* ./a */
-`.trim()
-		)
-	).toBeTruthy();
+	const requests = mainFile.match(/\/\*: .* :\*\//g);
+	const wrap = (request) => `/*: ${request} :*/`;
+	expect(requests).toContain(wrap('./index.js'));
+	expect(requests).toContain(wrap('./a'));
+	expect(requests).toContain(wrap('./b'));
+	expect(requests).not.toContain(wrap('fs'));
+	expect(requests).not.toContain(wrap('path'));
 });
