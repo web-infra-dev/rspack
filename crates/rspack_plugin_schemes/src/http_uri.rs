5ed79e2d9c9b495fc7ef313700531ee3d28dc954
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-06-29 21:44:47 -0700

    chore(rspack_plugin_schemes): url resolve

--- crates/rspack_plugin_schemes/src/http_uri.rs
+++ crates/rspack_plugin_schemes/src/http_uri.rs
@@ -103,7 +103,8 @@ async fn read_resource(&self, resource_data: &ResourceData) -> Result<Option<Con
     };
     let replaced_content = content_str
       .replace("import \"./", &format!("import \"{}/", origin))
-      .replace("import \"/", &format!("import \"{}/", origin));
+      .replace("import \"./", &format!("import \"{}/", origin));
+
     dbg!(&replaced_content);
     let final_content = replaced_content.into_bytes();
 
