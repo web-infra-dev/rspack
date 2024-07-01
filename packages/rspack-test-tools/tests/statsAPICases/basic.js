f90a738ad8362de919005a3a07326b7fdff87cbd
ahabhgk <ahabhgk@gmail.com>
2024-07-01 14:15:25 +0800

    feat: enable lightning css minimizer error recovery

--- packages/rspack-test-tools/tests/statsAPICases/basic.js
+++ packages/rspack-test-tools/tests/statsAPICases/basic.js
@@ -39,7 +39,7 @@ module.exports = {
 		  entry ./fixtures/a
 		  cjs self exports reference self [585]
 		  
-		Rspack compiled successfully (ee11b48ddc40a1c0f1c4)"
+		Rspack compiled successfully (c54fcee237a48276e019)"
 	`);
 	}
 };
