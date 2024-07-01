9858495e820d8171860fb366bdecb6ddb956fd3e
ahabhgk <ahabhgk@gmail.com>
2024-07-01 15:59:35 +0800

    fix

--- packages/rspack-test-tools/src/case/diagnostic.ts
+++ packages/rspack-test-tools/src/case/diagnostic.ts
@@ -14,7 +14,7 @@ const creator = new BasicCaseCreator({
 			format: (output: string) => {
 				// TODO: change to stats.errorStack
 				// TODO: add `errorStack: false`
-				return output;
+				return output.replace(/(│.* at ).*/g, "$1xxx");
 			}
 		})
 	]
