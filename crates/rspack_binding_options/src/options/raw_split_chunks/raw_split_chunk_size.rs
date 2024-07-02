1517e76afc5f6e76b9560cd251cc4652ad4ea1ef
wxiaoyun <w.xy020203@gmail.com>
2024-07-02 13:29:15 +0800

    fix: update rawsplitchunksizes struct

--- crates/rspack_binding_options/src/options/raw_split_chunks/raw_split_chunk_size.rs
+++ crates/rspack_binding_options/src/options/raw_split_chunks/raw_split_chunk_size.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use derivative::Derivative;
 use napi_derive::napi;
 use rspack_plugin_split_chunks::SplitChunkSizes;
@@ -6,15 +8,15 @@ use rspack_plugin_split_chunks::SplitChunkSizes;
 #[napi(object, object_to_js = false)]
 #[derivative(Debug)]
 pub struct RawSplitChunkSizes {
-  pub sizes: serde_json::Map<String, serde_json::Value>,
+  pub sizes: HashMap<String, f64>,
 }
 
 impl From<RawSplitChunkSizes> for SplitChunkSizes {
   fn from(sizes: RawSplitChunkSizes) -> Self {
-    let mut map = SplitChunkSizes::default();
-    for (k, v) in sizes.sizes {
-      map.insert((*k).into(), v.as_f64().unwrap_or_default());
+    let mut split_chunk_sizes = SplitChunkSizes::default();
+    for (chunk_name, size) in sizes.sizes {
+      split_chunk_sizes.insert((*chunk_name).into(), size);
     }
-    map
+    split_chunk_sizes
   }
 }
