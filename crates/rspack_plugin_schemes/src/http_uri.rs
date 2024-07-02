60615ff29cf55b9ca1adc9c09678c3f09f0c0169
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-07-01 21:53:51 -0700

    feat(rspack_core): resolve in scheme hook

--- crates/rspack_plugin_schemes/src/http_uri.rs
+++ crates/rspack_plugin_schemes/src/http_uri.rs
@@ -6,17 +6,9 @@ use once_cell::sync::Lazy;
 use regex::Regex;
 use reqwest::Client; // Add reqwest for HTTP requests
 use rspack_core::{
-  ApplyContext,
-  CompilerOptions,
-  Content,
-  ModuleFactoryCreateData,
-  NormalModuleFactoryResolveForScheme,
-  NormalModuleFactoryResolveInScheme,
-  NormalModuleReadResource,
-  Plugin,
-  PluginContext,
-  ResourceData,
-  Scheme, // Add this import
+  get_scheme, ApplyContext, CompilerOptions, Content, ModuleFactoryCreateData,
+  NormalModuleFactoryResolveForScheme, NormalModuleFactoryResolveInScheme,
+  NormalModuleReadResource, Plugin, PluginContext, ResourceData, Scheme,
 };
 use rspack_error::error;
 use rspack_error::{AnyhowError, Result}; // Removed `Error` import
@@ -39,7 +31,6 @@ async fn resolve_for_scheme(
 ) -> Result<Option<bool>> {
   if resource_data.get_scheme().is_http() && EXTERNAL_HTTP_REQUEST.is_match(&resource_data.resource)
   {
-    dbg!(&resource_data.resource);
     return Ok(None);
   }
   Ok(None)
@@ -48,54 +39,51 @@ async fn resolve_for_scheme(
 #[plugin_hook(NormalModuleFactoryResolveInScheme for HttpUriPlugin)]
 async fn resolve_in_scheme(
   &self,
-  _data: &mut ModuleFactoryCreateData,
+  data: &mut ModuleFactoryCreateData,
   resource_data: &mut ResourceData,
 ) -> Result<Option<bool>> {
-  dbg!(&resource_data);
-  dbg!(resource_data.get_scheme());
-  dbg!(_data.context.get(self));
-
-  // if _data.context.inner.is_empty() {
-  //   if resource_data.get_scheme().is_http() {
-  //     dbg!("is http context");
-  //   }
-  // }
-  if resource_data.get_scheme().is_http() {
-    // if matches!(_data.context, Scheme::Http) { // Ensure Scheme is imported
-    //   let base_url = url::Url::parse(&_data.context)?;
-    //   let full_url = base_url.join(&resource_data.resource)?;
-    //   resource_data.set_resource(full_url.to_string());
-    //   dbg!(&resource_data.resource);
-    //
-    //   return Ok(None);
-    // }
+  if !matches!(get_scheme(data.context.as_str()), Scheme::Http) {
+    return Ok(None);
   }
+
+  let base_url = match url::Url::parse(data.context.as_str()) {
+    Ok(url) => url,
+    Err(_) => return Ok(None),
+  };
+
+  let resource_url = match url::Url::parse(&resource_data.resource) {
+    Ok(url) if url.scheme() == "http" || url.scheme() == "https" => return Ok(None),
+    Ok(_) | Err(_) => resource_data.resource.clone(),
+  };
+
+  resource_data.set_resource(
+    base_url
+      .join(&resource_url)
+      .map(|url| url.to_string())
+      .unwrap_or_else(|_| resource_data.resource.clone()),
+  );
+
   Ok(None)
 }
-
 #[plugin_hook(NormalModuleReadResource for HttpUriPlugin)]
 async fn read_resource(&self, resource_data: &ResourceData) -> Result<Option<Content>> {
-  dbg!("reading resource before", &resource_data.resource);
   if resource_data.get_scheme().is_http() && EXTERNAL_HTTP_REQUEST.is_match(&resource_data.resource)
   {
-    dbg!(&resource_data.resource);
-
     // Check cache first
-    let cache_path = format!(
-      "{}/{}",
-      HTTP_CACHE_DIR,
-      resource_data.resource.replace("/", "_")
-    );
-    if Path::new(&cache_path).exists() {
-      dbg!("Cache hit");
-      let cached_content = fs::read(&cache_path)
-        .context("Failed to read cached content")
-        .map_err(|err| {
-          error!(err.to_string());
-          AnyhowError::from(err)
-        })?;
-      return Ok(Some(Content::Buffer(cached_content)));
-    }
+    // let cache_path = format!(
+    //   "{}/{}",
+    //   HTTP_CACHE_DIR,
+    //   resource_data.resource.replace("/", "_")
+    // );
+    // if Path::new(&cache_path).exists() {
+    //   let cached_content = fs::read(&cache_path)
+    //     .context("Failed to read cached content")
+    //     .map_err(|err| {
+    //       error!(err.to_string());
+    //       AnyhowError::from(err)
+    //     })?;
+    //   return Ok(Some(Content::Buffer(cached_content)));
+    // }
 
     let client = Client::new();
     let response = client
@@ -128,30 +116,21 @@ async fn read_resource(&self, resource_data: &ResourceData) -> Result<Option<Con
       Ok(url) => url.origin().ascii_serialization(),
       Err(_) => "".to_string(),
     };
-    let replaced_content = content_str
-      .replace("from \"/", &format!("from \"{}/", origin))
-      .replace("import \"/", &format!("import \"{}/", origin));
 
-    dbg!(&replaced_content);
+    // fs::create_dir_all(HTTP_CACHE_DIR)
+    //   .context("Failed to create cache directory")
+    //   .map_err(|err| {
+    //     error!(err.to_string());
+    //     AnyhowError::from(err)
+    //   })?;
+    // fs::write(&cache_path, &content_str)
+    //   .context("Failed to write cache content")
+    //   .map_err(|err| {
+    //     error!(err.to_string());
+    //     AnyhowError::from(err)
+    //   })?;
 
-    fs::create_dir_all(HTTP_CACHE_DIR)
-      .context("Failed to create cache directory")
-      .map_err(|err| {
-        error!(err.to_string());
-        AnyhowError::from(err)
-      })?;
-    fs::write(&cache_path, &replaced_content)
-      .context("Failed to write cache content")
-      .map_err(|err| {
-        error!(err.to_string());
-        AnyhowError::from(err)
-      })?;
-
-    // let final_content = replaced_content.into_bytes();
-
-    return Ok(Some(Content::Buffer(
-      replaced_content.to_string().into_bytes(),
-    )));
+    return Ok(Some(Content::Buffer(content_str.to_string().into_bytes())));
   }
   Ok(None)
 }
