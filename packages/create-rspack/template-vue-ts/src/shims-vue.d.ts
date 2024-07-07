164be050467dfe41b6f2e135d8b2d5bd11b44feb
hardfist <yangjianzju@gmail.com>
2024-07-07 13:31:01 +0800

    chore: migrate lint to biome

--- packages/create-rspack/template-vue-ts/src/shims-vue.d.ts
+++ packages/create-rspack/template-vue-ts/src/shims-vue.d.ts
@@ -1,6 +1,5 @@
 declare module "*.vue" {
 	import { DefineComponent } from "vue";
-	// biome-ignore lint/complexity/noBannedTypes: reason
 	const component: DefineComponent<{}, {}, any>;
 	export default component;
 }
