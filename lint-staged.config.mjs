b0f4343308c123a8623b982c36b0c806fa73708f
hardfist <yangjianzju@gmail.com>
2024-07-07 13:47:12 +0800

    chore: migrate lint to biome (#7061)
    Co-authored-by: neverland <chenjiahan.jait@bytedance.com>

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,9 +2,5 @@ export default {
 	"*.rs": "rustfmt --edition 2021",
 	"*.{ts,tsx,js}": "pnpm run format:js",
 	"*.toml": "npx @taplo/cli format",
-	"*.{ts,tsx,js,cts,cjs,mts,mjs}": () => [
-		"pnpm run lint:js",
-		"pnpm run lint:js-sort-imports-order",
-		"node ./x ae ci"
-	]
+	"*.{ts,tsx,js,cts,cjs,mts,mjs}": () => ["pnpm run lint:js", "node ./x ae ci"]
 };
