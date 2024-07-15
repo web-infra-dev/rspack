8ab9b0e5d41b07b70a54e68751543d9485a5003a
Gengkun <ahabhgk@gmail.com>
2024-07-15 11:52:45 +0800

    fix: unset cjs exports type on access exports directly (#7143)

---
+++ packages/rspack-test-tools/tests/normalCases/parsing/cjs-unset-exports-type/a.js
@@ -0,0 +1,4 @@
+// set to Flagged
+exports.__esModule = true;
+// set to Unset, `Object.defineProperty(exports, 'a', ...)` is not at the statement level
+(() => {})(Object.defineProperty(exports,'a',{enumerable:!0,get:function(){return 'a'}}))
