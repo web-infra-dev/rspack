1764b5f4d5921d99398139e5ea66bf1d29ff4fe5
neverland <chenjiahan.jait@bytedance.com>
2024-07-16 17:07:53 +0800

    docs: improve home page SSG and benchmark data (#7185)
    * docs: improve home page SSG and benchmark data
    
    * fix: SSR
    
    * fix: lock
    
    * Merge branch 'main' into home_page_ssg_0716

--- website/theme/components/Benchmark/ProgressBar.tsx
+++ website/theme/components/Benchmark/ProgressBar.tsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 import { useState } from 'react';
 import styles from './ProgressBar.module.scss';
+import { useInView } from 'react-intersection-observer';
 
 export function formatTime(time: number, totalTime: number) {
   if (totalTime < 1000) {
@@ -23,33 +24,32 @@ export function ProgressBar({
 }) {
   const [elapsedTime, setElapsedTime] = useState(0);
   const TOTAL_TIME = value * 1000;
-  const isSmallScreen = window.innerWidth < 1060;
-  const progressBarWidth = isSmallScreen ? 80 : 60;
   const variants = {
     initial: { width: 0 },
     animate: { width: `${(value / max) * 100}%` },
   };
+  const { ref, inView } = useInView();
 
   const formattedTime = formatTime(elapsedTime, TOTAL_TIME);
   return (
-    <div
-      className={`${styles.container} flex items-center sm:pr-4`}
-      style={{
-        width: `${progressBarWidth}vw`,
-      }}
-    >
-      <div className={`${styles['inner-container']} flex justify-between`}>
-        <motion.div
-          className={`${styles.bar} ${styles[color]}`}
-          initial="initial"
-          animate="animate"
-          variants={variants}
-          onUpdate={(latest: { width: string }) => {
-            const width = Number.parseFloat(latest.width);
-            setElapsedTime(width * max * 10);
-          }}
-          transition={{ duration: value, ease: 'linear' }}
-        />
+    <div className={`${styles.container} flex items-center sm:pr-4`}>
+      <div
+        ref={ref}
+        className={`${styles['inner-container']} flex justify-between`}
+      >
+        {inView ? (
+          <motion.div
+            className={`${styles.bar} ${styles[color]}`}
+            initial="initial"
+            animate="animate"
+            variants={variants}
+            onUpdate={(latest: { width: string }) => {
+              const width = Number.parseFloat(latest.width);
+              setElapsedTime(width * max * 10);
+            }}
+            transition={{ duration: value, ease: 'linear' }}
+          />
+        ) : null}
       </div>
       <div className={styles.desc}>
         <span className={styles.time}>{formattedTime}</span> {desc}
--- website/theme/components/Benchmark/index.tsx
+++ website/theme/components/Benchmark/index.tsx
@@ -1,5 +1,3 @@
-import { useInView } from 'react-intersection-observer';
-import { NoSSR } from 'rspress/runtime';
 import { useI18n } from '../../i18n';
 import { ProgressBar } from './ProgressBar';
 import styles from './index.module.scss';
@@ -13,6 +11,12 @@ const BENCHMARK_DATA = {
     coldStart: 0.49,
     coldBuild: 0.36,
     hmr: 0.09,
+  },
+  viteSwc: {
+    label: 'Vite + SWC',
+    coldStart: 1.58,
+    coldBuild: 1.37,
+    hmr: 0.05,
   },
   webpackSwc: {
     label: 'webpack + SWC',
@@ -31,13 +35,9 @@ const maxTime = 6.47;
 
 export function Benchmark() {
   const t = useI18n();
-  const { ref, inView } = useInView();
 
   return (
-    <div
-      ref={ref}
-      className="relative flex flex-col justify-center pt-24 pb-10 mt-18 h-auto"
-    >
+    <div className="relative flex flex-col justify-center pt-24 pb-10 mt-18 h-auto">
       <div className="flex flex-center flex-col">
         <h2 className={`${styles.title} font-bold text-3xl sm:text-5xl mt-16`}>
           {t('benchmarkTitle')}
@@ -48,53 +48,45 @@ export function Benchmark() {
           {t('benchmarkDesc')}
         </p>
       </div>
-      {inView && (
-        <NoSSR>
-          <div className="flex flex-col items-center my-4 z-1">
-            {Object.values(BENCHMARK_DATA).map(item => (
-              <div
-                key={item.label}
-                className={`${styles.item} flex flex-center justify-start m-5`}
-              >
-                {inView && (
-                  <>
-                    <p className={styles.progressName}>{item.label}</p>
-                    <div>
-                      <ProgressBar
-                        value={item.coldStart}
-                        max={maxTime}
-                        color="cyan"
-                        desc="dev"
-                      />
-                      <ProgressBar
-                        value={item.coldBuild}
-                        max={maxTime}
-                        color="blue"
-                        desc="build"
-                      />
-                      <ProgressBar
-                        value={item.hmr}
-                        max={maxTime}
-                        color="cyan"
-                        desc="HMR"
-                      />
-                    </div>
-                  </>
-                )}
-              </div>
-            ))}
+      <div className="flex flex-col items-center my-4 z-1">
+        {Object.values(BENCHMARK_DATA).map(item => (
+          <div
+            key={item.label}
+            className={`${styles.item} flex flex-center justify-start m-5`}
+          >
+            <p className={styles.progressName}>{item.label}</p>
             <div>
-              <a
-                href="misc/benchmark.html"
-                target="_blank"
-                className={`${styles['bottom-link']} hover:text-brand transition-colors duration-300 font-medium p-2`}
-              >
-                👉 {t('benchmarkDetail')}
-              </a>
+              <ProgressBar
+                value={item.coldStart}
+                max={maxTime}
+                color="cyan"
+                desc="dev"
+              />
+              <ProgressBar
+                value={item.coldBuild}
+                max={maxTime}
+                color="blue"
+                desc="build"
+              />
+              <ProgressBar
+                value={item.hmr}
+                max={maxTime}
+                color="cyan"
+                desc="HMR"
+              />
             </div>
           </div>
-        </NoSSR>
-      )}
+        ))}
+        <div>
+          <a
+            href="misc/benchmark.html"
+            target="_blank"
+            className={`${styles['bottom-link']} hover:text-brand transition-colors duration-300 font-medium p-2`}
+          >
+            👉 {t('benchmarkDetail')}
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
