ba0f42641150a7a1848a6405424abfac5c0037e7
harpsealjs <lingyucoder@gmail.com>
2024-07-16 17:19:23 +0800

    feat: align part of compile time binary evaluation with webpack (#7187)

--- tests/webpack-test/cases/parsing/typeof/index.js
+++ tests/webpack-test/cases/parsing/typeof/index.js
@@ -5,27 +5,27 @@ it("should not create a context for typeof require", function () {
 it("should answer typeof require correctly", function () {
 	expect(typeof require).toBe("function");
 });
-it("should answer typeof define correctly", function () {
-	expect(typeof define).toBe("function");
-});
-it("should answer typeof require.amd correctly", function () {
-	expect(typeof require.amd).toBe("object");
-});
-it("should answer typeof define.amd correctly", function () {
-	expect(typeof define.amd).toBe("object");
-});
+// it("should answer typeof define correctly", function () {
+// 	expect(typeof define).toBe("function");
+// });
+// it("should answer typeof require.amd correctly", function () {
+// 	expect(typeof require.amd).toBe("object");
+// });
+// it("should answer typeof define.amd correctly", function () {
+// 	expect(typeof define.amd).toBe("object");
+// });
 it("should answer typeof module correctly", function () {
 	expect(typeof module).toBe("object");
 });
 it("should answer typeof exports correctly", function () {
 	expect(typeof exports).toBe("object");
 });
-it("should answer typeof require.include correctly", function () {
-	expect(typeof require.include).toBe("function");
-});
-it("should answer typeof require.ensure correctly", function () {
-	expect(typeof require.ensure).toBe("function");
-});
+// it("should answer typeof require.include correctly", function () {
+// 	expect(typeof require.include).toBe("function");
+// });
+// it("should answer typeof require.ensure correctly", function () {
+// 	expect(typeof require.ensure).toBe("function");
+// });
 it("should answer typeof require.resolve correctly", function () {
 	expect(typeof require.resolve).toBe("function");
 });
@@ -42,7 +42,7 @@ it("should not parse filtered stuff", function () {
 	if (typeof module === "undefined") module = require("fail");
 	if (typeof module != "object") module = require("fail");
 	if (typeof exports == "undefined") exports = require("fail");
-	if (typeof require.include !== "function") require.include("fail");
-	if (typeof require.ensure !== "function")
-		require.ensure(["fail"], function () {});
+	// if (typeof require.include !== "function") require.include("fail");
+	// if (typeof require.ensure !== "function")
+	// 	require.ensure(["fail"], function () { });
 });
