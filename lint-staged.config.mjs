8f941c5a982fd087ee25cdc5b4f11d333f4d65be
Soon <sooniter@gmail.com>
2024-07-18 15:07:23 +0800

    chore(deps): add check-dependency-version for devDependencies (#6323)
    * chore: add check-dependency-version
    
    * chore(github): check-dependency-version when ci
    
    * chore: add lint-staged
    
    * chore: update
    
    * chore: update
    
    * chore: fix
    
    * chore: use ^
    
    * chore: ws use ^
    
    * chore: fix
    
    * chore: fix
    
    * chore: postcss-loader
    
    * chore: wast-loader

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,5 +2,6 @@ export default {
 	"*.rs": "rustfmt --edition 2021",
 	"*.{ts,tsx,js}": "pnpm run format:js",
 	"*.toml": "npx @taplo/cli format",
-	"*.{ts,tsx,js,cts,cjs,mts,mjs}": () => ["pnpm run lint:js", "node ./x ae ci"]
+	"*.{ts,tsx,js,cts,cjs,mts,mjs}": () => ["pnpm run lint:js", "node ./x ae ci"],
+	"package.json": "pnpm run check-dependency-version"
 };
