a19ca91a4fda7198de826565cc5f900f155d8973
Fy <1114550440@qq.com>
2024-07-19 17:43:01 +0800

    test: enable more available tests (#7234)

--- tests/plugin-test/css-extract/cases/dependOn/expected/entry1.js
+++ tests/plugin-test/css-extract/cases/dependOn/expected/entry1.js
@@ -1,38 +1,22 @@
 "use strict";
-(self["webpackChunk"] = self["webpackChunk"] || []).push([
-	[1],
-	[
-		,
-		,
-		/* 0 */ /* 1 */ /* 2 */
-		/***/ (
-			__unused_webpack_module,
-			__webpack_exports__,
-			__webpack_require__
-		) => {
-			__webpack_require__.r(__webpack_exports__);
-			/* harmony import */ var _styleA_css__WEBPACK_IMPORTED_MODULE_0__ =
-				__webpack_require__(3);
+(self['webpackChunk'] = self['webpackChunk'] || []).push([["entry1"], {
+"./styleA.css?6507": (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+__webpack_require__.r(__webpack_exports__);
+// extracted by css-extract-rspack-plugin
 
-			/***/
-		},
-		/* 3 */
-		/***/ (
-			__unused_webpack_module,
-			__webpack_exports__,
-			__webpack_require__
-		) => {
-			__webpack_require__.r(__webpack_exports__);
-			// extracted by mini-css-extract-plugin
 
-			/***/
-		}
-	],
-	/******/ __webpack_require__ => {
-		// webpackRuntimeModules
-		/******/ var __webpack_exec__ = moduleId =>
-			__webpack_require__((__webpack_require__.s = moduleId));
-		/******/ var __webpack_exports__ = __webpack_exec__(2);
-		/******/
-	}
+}),
+"./entryA.js": (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */var _styleA_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./styleA.css?6507");
+
+
+
+}),
+
+},function(__webpack_require__) {
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId) }
+var __webpack_exports__ = (__webpack_exec__("./entryA.js"));
+
+}
 ]);
