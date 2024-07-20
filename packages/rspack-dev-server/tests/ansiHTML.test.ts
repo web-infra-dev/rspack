bbd99abe79f1897856fc6dc60ec56eb06dd1ef54
ForAeons <w.xy020203@gmail.com>
2024-07-20 20:30:53 +0800

    test: update test

--- packages/rspack-dev-server/tests/ansiHTML.test.ts
+++ packages/rspack-dev-server/tests/ansiHTML.test.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import ansiHTML = require("../src/ansiHTML");
+import ansiHTML from "../src/ansiHTML";
 
 describe("ansi-html", () => {
 	it("should transform 24-bit rgb ansi colors", () => {
