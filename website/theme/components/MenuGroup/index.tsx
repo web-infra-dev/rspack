fee9fee326ec203db79facc07db28905d3299e4a
dalaoshu <laipichan@qq.com>
2024-07-23 11:00:22 +0800

    chore(infra/biome): rule noSvgWithoutTitle (#7262)

--- website/theme/components/MenuGroup/index.tsx
+++ website/theme/components/MenuGroup/index.tsx
@@ -8,6 +8,7 @@ interface MenuGroupProps {
 function Down() {
   return (
     <svg width="1em" height="1em" viewBox="0 0 32 32">
+      <title>ArrowDown</title>
       <path
         fill="currentColor"
         d="M16 22L6 12l1.4-1.4l8.6 8.6l8.6-8.6L26 12z"
