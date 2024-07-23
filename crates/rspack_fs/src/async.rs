de876babb550365b80cf904917471a58af72e541
hardfist <yangjianzju@gmail.com>
2024-07-23 14:45:33 +0800

    fix(fs): remove generic in `FileSystem` trait (#7275)

--- crates/rspack_fs/src/async.rs
+++ crates/rspack_fs/src/async.rs
@@ -15,27 +15,27 @@ pub trait AsyncWritableFileSystem {
   /// - User lacks permissions to create directory at path.
   /// - A parent of the given path doesn’t exist. (To create a directory and all its missing parents at the same time, use the create_dir_all function.)
   /// - Path already exists.
-  fn create_dir<P: AsRef<Path>>(&self, dir: P) -> BoxFuture<'_, Result<()>>;
+  fn create_dir(&self, dir: &Path) -> BoxFuture<'_, Result<()>>;
 
   /// Recursively create a directory and all of its parent components if they are missing.
-  fn create_dir_all<P: AsRef<Path>>(&self, dir: P) -> BoxFuture<'_, Result<()>>;
+  fn create_dir_all(&self, dir: &Path) -> BoxFuture<'_, Result<()>>;
 
   /// Write a slice as the entire contents of a file.
   /// This function will create a file if it does not exist, and will entirely replace its contents if it does.
-  fn write<P: AsRef<Path>, D: AsRef<[u8]>>(&self, file: P, data: D) -> BoxFuture<'_, Result<()>>;
+  fn write(&self, file: &Path, data: &[u8]) -> BoxFuture<'_, Result<()>>;
 
   /// Removes a file from the filesystem.
-  fn remove_file<P: AsRef<Path>>(&self, file: P) -> BoxFuture<'_, Result<()>>;
+  fn remove_file(&self, file: &Path) -> BoxFuture<'_, Result<()>>;
 
   /// Removes a directory at this path, after removing all its contents. Use carefully.
-  fn remove_dir_all<P: AsRef<Path>>(&self, dir: P) -> BoxFuture<'_, Result<()>>;
+  fn remove_dir_all(&self, dir: &Path) -> BoxFuture<'_, Result<()>>;
 }
 
 pub trait AsyncReadableFileSystem {
   /// Read the entire contents of a file into a bytes vector.
   ///
   /// Error: This function will return an error if path does not already exist.
-  fn read<P: AsRef<Path>>(&self, file: P) -> BoxFuture<'_, Result<Vec<u8>>>;
+  fn read(&self, file: &Path) -> BoxFuture<'_, Result<Vec<u8>>>;
 }
 
 /// Async readable and writable file system representation.
