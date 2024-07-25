70f1817f3494fc7d8379f589081b0272c431af8a
CPunisher <1343316114@qq.com>
2024-07-25 12:12:30 +0800

    Remove console.log

--- packages/rspack-test-tools/tests/configCases/output/library-async-module/lambda.js
+++ packages/rspack-test-tools/tests/configCases/output/library-async-module/lambda.js
@@ -2,11 +2,8 @@ const bb = async () => {
   return "asdsadasda";
 };
 
-console.log(await bb());
-
-console.log(123);
+await bb();
 
 export const handler = () => {
-  console.log("555")
   return 1;
 };
