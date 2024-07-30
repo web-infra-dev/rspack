3b49824c7a7d614aa1c2faf7f7d725303dc8a6f8
Hana <andywangsy@gmail.com>
2024-07-30 15:02:09 +0800

    perf: use mimalloc v2 for all

--- crates/rspack_allocator/src/lib.rs
+++ crates/rspack_allocator/src/lib.rs
@@ -1,15 +1,4 @@
-#[cfg(not(target_os = "linux"))]
-#[global_allocator]
-static GLOBAL: mimalloc_rust::GlobalMiMalloc = mimalloc_rust::GlobalMiMalloc;
+use mimalloc::MiMalloc;
 
-#[cfg(all(target_os = "linux", target_env = "musl"))]
 #[global_allocator]
-static GLOBAL: mimalloc_rust::GlobalMiMalloc = mimalloc_rust::GlobalMiMalloc;
-
-#[cfg(all(
-  target_os = "linux",
-  target_env = "gnu",
-  any(target_arch = "x86_64", target_arch = "aarch64")
-))]
-#[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+static GLOBAL: MiMalloc = MiMalloc;
