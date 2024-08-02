90c27643dda3738f0250186a0f2250577cdccee5
ahabhgk <ahabhgk@gmail.com>
2024-08-02 14:42:37 +0800

    test: enable innerGraph, concatModules for runtime condition diff test

--- packages/rspack-test-tools/tests/runtimeDiffCases/scope-hoisting/runtime-condition/test.config.js
+++ packages/rspack-test-tools/tests/runtimeDiffCases/scope-hoisting/runtime-condition/test.config.js
@@ -1,8 +1,6 @@
 /** @type {import("../../../../dist").TDiffCaseConfig} */
 module.exports = {
 	renameModule: (raw) => {
-		console.log(raw);
-		console.log(raw.split("|").slice(0, -1).join('|'));
 		return raw.split("|").slice(0, -1).join('|');
 	},
 	modules: true,
