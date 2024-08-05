da897a28a3b6b469bc381bb80aac54feb22ae3fd
SoonIter <sooniter@gmail.com>
2024-08-05 15:46:10 +0800

    chore: push

--- packages/rspack/src/index.ts
+++ packages/rspack/src/index.ts
@@ -3,7 +3,7 @@ import { rspack as rspackFn } from "./rspack";
 // add exports on rspack() function
 type Rspack = typeof rspackFn &
 	typeof rspackExports & { rspack: Rspack; webpack: Rspack };
-const fn: any = Object.assign(rspackFn, rspackExports);
+const fn = Object.assign(rspackFn, rspackExports) as Rspack;
 fn.rspack = fn;
 fn.webpack = fn;
 const rspack: Rspack = fn;
