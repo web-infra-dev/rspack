12f7989c839cdf4a8e7cc566967d3b060f925920
LingyuCoder <lingyucoder@gmail.com>
2024-08-08 17:27:15 +0800

    chore: move non-core packages to rspack-contrib

--- packages/rspack-plugin-preact-refresh/tests/hotCases/jsx/child/child.jsx
+++
@@ -1,9 +0,0 @@
-throw new Error("should not been imported");
----
-export function Child() {
-  return (<span>has child</span>);
-}
----
-export function Child() {
-  return (<span>child change</span>);
-}
