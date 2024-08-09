ce328ecea2e319c631286c037173283066ab503d
jserfeng <1114550440@qq.com>
2024-08-09 12:15:58 +0800

    fix: maxInitialRequests should exists in cache groups

---
+++ packages/rspack-test-tools/tests/configCases/split-chunks-common/rspack-issue-7509/rspack.config.js
@@ -0,0 +1,20 @@
+/** @type {import("@rspack/core").Configuration} */
+module.exports = {
+	optimization: {
+		chunkIds: 'named',
+		splitChunks: {
+			maxInitialRequests: 10,
+			cacheGroups: {
+				vendors: {
+					chunks: 'all',
+					test: /node_modules/,
+					minSize: 0,
+					filename: 'split-[name].js',
+
+					// should override the splitChunks.maxInitialRequests
+					maxInitialRequests: 1,
+				}
+			}
+		}
+	}
+}
