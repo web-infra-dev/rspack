490a53eab75ae530f4ba94bf6350a888ce966e0a
inottn <inottn@outlook.com>
2024-08-24 16:32:49 +0800

    fix: resolve issue with external array not working in UMD libraryTarget

---
+++ packages/rspack-test-tools/tests/configCases/externals/umd-array-externals/rspack.config.js
@@ -0,0 +1,13 @@
+/** @type {import("@rspack/core").Configuration} */
+module.exports = {
+	output: {
+		libraryTarget: "umd"
+	},
+	externals: {
+		external: {
+			root: ["a", "b"],
+			commonjs: "a",
+			commonjs2: "a"
+		}
+	}
+};
