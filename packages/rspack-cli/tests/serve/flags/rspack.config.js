db2cd3e2d503067da31878b64356c4ef4db1b0e2
shulaoda <laipichan@qq.com>
2024-08-28 19:24:15 +0800

    chore: add test case

---
+++ packages/rspack-cli/tests/serve/flags/rspack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	mode: "development",
+	devtool: false,
+	devServer: {
+		hot: false,
+		port: 8080,
+		host: "127.0.0.1",
+		onListening: function (devServer) {
+			const { hot, host, port } = devServer.options;
+			console.log(JSON.stringify({ hot, host, port }));
+		}
+	}
+};
