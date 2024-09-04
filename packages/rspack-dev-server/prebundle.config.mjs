3d6727bcfb79d8e1b96b1190a25144371b35d37a
hardfist <yangjianzju@gmail.com>
2024-09-04 13:47:31 +0800

    fix: fix p-retry dynamic import

--- packages/rspack-dev-server/prebundle.config.mjs
+++ packages/rspack-dev-server/prebundle.config.mjs
@@ -1,10 +1,32 @@
 // @ts-check
-
+import fs from 'node:fs';
+import {join} from 'node:path';
+function replaceFileContent(filePath, replaceFn) {
+  const content = fs.readFileSync(filePath, 'utf-8');
+  const newContent = replaceFn(content);
+  if (newContent !== content) {
+    fs.writeFileSync(filePath, newContent);
+  }
+}
 /** @type {import('prebundle').Config} */
 export default {
   dependencies: [{
     name: 'webpack-dev-server',
     ignoreDts: true,
+    // this is a trick to avoid ncc compiling the dynamic import syntax
+    // https://github.com/vercel/ncc/issues/935
+    beforeBundle(task) {
+      replaceFileContent(
+          join(task.depPath, 'lib/Server.js'),
+          (content) => content.replaceAll('await import', 'await __import'));
+    },
+    afterBundle(task) {
+      replaceFileContent(
+          join(task.distPath, 'index.js'),
+          (content) =>
+              `${content.replaceAll('await __import', 'await import')}`,
+      );
+    },
     externals: {
       // the following externals are from webpack-dev-server's dependencies
       // https://github.com/webpack/webpack-dev-server/blob/master/package.json#L48
