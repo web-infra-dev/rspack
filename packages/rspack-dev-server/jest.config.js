bcd005a648806a8654145004fe475886ba7e2284
LingyuCoder <lingyucoder@gmail.com>
2024-09-05 18:58:40 +0800

    test(dev-sever): add e2e test cases

--- packages/rspack-dev-server/jest.config.js
+++ packages/rspack-dev-server/jest.config.js
@@ -13,15 +13,15 @@ const config = {
 	testPathIgnorePatterns: isWin
 		? []
 		: [
-				// TODO: check why http proxy server throw error with websocket server
-				"<rootDir>/tests/e2e/allowed-hosts.test.js",
-				// TODO: check why this test timeout
-				"<rootDir>/tests/e2e/host.test.js",
-				// TODO: not support progress plugin event yet
-				"<rootDir>/tests/e2e/progress.test.js",
-				// TODO: check why this test throw error when run with other tests
-				"<rootDir>/tests/e2e/watch-files.test.js"
-			],
+			// TODO: check why http proxy server throw error with websocket server
+			"<rootDir>/tests/e2e/allowed-hosts.test.js",
+			// TODO: check why this test timeout
+			"<rootDir>/tests/e2e/host.test.js",
+			// TODO: not support progress plugin event yet
+			"<rootDir>/tests/e2e/progress.test.js",
+			// TODO: check why this test throw error when run with other tests
+			"<rootDir>/tests/e2e/watch-files.test.js"
+		],
 	cache: false,
 	testTimeout: process.env.CI ? 120000 : 30000,
 	transform: {
@@ -33,7 +33,7 @@ const config = {
 		]
 	},
 	// Add this to find out which test timeouts
-	testSequencer: "<rootDir>/tests/helpers/sequencer.js",
+	// testSequencer: "<rootDir>/tests/helpers/sequencer.js",
 	snapshotResolver: "<rootDir>/tests/helpers/snapshot-resolver.js",
 	setupFilesAfterEnv: ["<rootDir>/tests/helpers/setup-test.js"],
 	globalSetup: "<rootDir>/tests/helpers/global-setup-test.js",
