531fe3acffad38b5939e2289157587041bf9a343
jerrykingxyz <jerrykingxyz@gmail.com>
2024-09-18 19:31:05 +0800

    fix: make stage has_module_graph_change calculate incorrectly

--- crates/rspack_core/src/compiler/make/cutout/has_module_graph_change.rs
+++ crates/rspack_core/src/compiler/make/cutout/has_module_graph_change.rs
@@ -3,9 +3,7 @@ use rustc_hash::FxHashSet as HashSet;
 use swc_core::atoms::Atom;
 
 use super::super::MakeArtifact;
-use crate::{
-  AsyncDependenciesBlockIdentifier, DependencyId, GroupOptions, ModuleGraph, ModuleIdentifier,
-};
+use crate::{AsyncDependenciesBlockIdentifier, GroupOptions, ModuleGraph, ModuleIdentifier};
 
 #[derive(Debug, Default, Eq, PartialEq, Clone)]
 struct ModuleDeps {
@@ -62,6 +60,7 @@ impl ModuleDeps {
 #[derive(Debug, Default)]
 pub struct HasModuleGraphChange {
   disabled: bool,
+  expect_built_modules_len: usize,
   origin_module_deps: IdentifierMap<ModuleDeps>,
 }
 
@@ -78,12 +77,17 @@ impl HasModuleGraphChange {
     );
   }
 
-  pub fn analyze_force_build_deps(
-    &mut self,
-    deps: &HashSet<(DependencyId, Option<ModuleIdentifier>)>,
-  ) {
-    if deps.is_empty() {
+  pub fn analyze_artifact(&mut self, artifact: &MakeArtifact) {
+    if artifact.has_module_graph_change {
       self.disabled = true;
+      return;
+    }
+
+    self.expect_built_modules_len = artifact.built_modules.len();
+    for module_identifier in self.origin_module_deps.keys() {
+      if !artifact.built_modules.contains(module_identifier) {
+        self.expect_built_modules_len += 1;
+      }
     }
   }
 
@@ -92,23 +96,20 @@ impl HasModuleGraphChange {
     if self.disabled {
       return;
     }
-    if self.origin_module_deps.is_empty() {
-      // origin_module_deps empty means no force_build_module and no file changed
-      // this only happens when build from entry
+    if artifact.built_modules.len() != self.expect_built_modules_len {
+      // contain unexpected module built
       artifact.has_module_graph_change = true;
       return;
     }
-    // if artifact.has_module_graph_change is true, no need to recalculate
-    if !artifact.has_module_graph_change {
-      for (module_identifier, module_deps) in self.origin_module_deps {
-        if module_graph
-          .module_by_identifier(&module_identifier)
-          .is_none()
-          || ModuleDeps::from_module(module_graph, &module_identifier) != module_deps
-        {
-          artifact.has_module_graph_change = true;
-          return;
-        }
+
+    for (module_identifier, module_deps) in self.origin_module_deps {
+      if module_graph
+        .module_by_identifier(&module_identifier)
+        .is_none()
+        || ModuleDeps::from_module(module_graph, &module_identifier) != module_deps
+      {
+        artifact.has_module_graph_change = true;
+        return;
       }
     }
   }
