c380a8f393eac34e6b8d9cb3f0f263ac2443094f
SoonIter <sooniter@gmail.com>
2024-09-26 18:04:19 +0800

    chore: remove close

--- website/theme/components/Announcement/index.tsx
+++ website/theme/components/Announcement/index.tsx
@@ -1,28 +1,15 @@
-import { useState } from 'react';
-import IconCloseCircle from './close';
 import styles from './index.module.scss';
 
 export function Announcement({
   href,
   message,
-  // we don't need localStorageKey for persist to recommend upgrading
-  // localStorageKey,
   display = true,
 }: {
   href: string;
   message: string;
-  // localStorageKey: string;
   display?: boolean;
 }) {
   if (!display) {
-    return null;
-  }
-  const [disable, setDisable] = useState(
-    // window.localStorage.getItem(localStorageKey) ?? false,
-    false,
-  );
-
-  if (disable) {
     return null;
   }
 
@@ -31,13 +18,6 @@ export function Announcement({
       <a href={href} className={styles.link}>
         {message}
       </a>
-      <IconCloseCircle
-        onClick={() => {
-          setDisable(true);
-          // window.localStorage.setItem(localStorageKey, 'true');
-        }}
-        className={styles.close}
-      />
     </div>
   );
 }
