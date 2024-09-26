0033ab099f803703e05bfaf693508b6729e5de96
SoonIter <sooniter@gmail.com>
2024-09-26 15:17:19 +0800

    chore: fix typo

--- website/theme/index.tsx
+++ website/theme/index.tsx
@@ -2,8 +2,8 @@ import { RsfamilyNavIcon } from 'rsfamily-nav-icon';
 import Theme from 'rspress/theme';
 import { HomeLayout } from './pages';
 import 'rsfamily-nav-icon/dist/index.css';
-import { Announcement } from './components/Announcement';
 import { NoSSR, useLang } from 'rspress/runtime';
+import { Announcement } from './components/Announcement';
 
 const ANNOUNCEMENT_EN_URL = 'https://rspack.dev/blog/announcing-1-0';
 const ANNOUNCEMENT_ZH_URL = 'https://rspack.dev/zh/blog/announcing-1-0';
