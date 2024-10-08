e7b1681df6aa80e96dfb536a6d26f35a56f7b8b0
neverland <chenjiahan.jait@bytedance.com>
2024-10-08 18:55:35 +0800

    fix: external compiled bundles

--- packages/rspack/tsup.config.ts
+++ packages/rspack/tsup.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "tsup";
+import prebundleConfig from "./prebundle.config.mjs";
+
+const aliasCompiledPlugin = {
+	name: "alias-compiled-plugin",
+	setup(build) {
+		const { dependencies } = prebundleConfig;
+
+		for (const item of dependencies) {
+			const depName = typeof item === "string" ? item : item.name;
+			build.onResolve({ filter: new RegExp(`^${depName}$`) }, () => ({
+				path: `../compiled/${depName}/index.js`,
+				external: true
+			}));
+		}
+	}
+};
 
 export default defineConfig({
 	entry: ["./src/index.ts"],
 	format: ["cjs"],
-	target: "node16"
+	target: "node16",
+	esbuildPlugins: [aliasCompiledPlugin]
 });
