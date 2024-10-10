b23c029a7d1cee3722582f432fa851332b0960c0
jserfeng <1114550440@qq.com>
2024-10-10 20:23:48 +0800

    fix: css module should not calculate id

--- tests/plugin-test/css-extract/cases/dependOn-multiple-files-per-entry/expected/common.js
+++ tests/plugin-test/css-extract/cases/dependOn-multiple-files-per-entry/expected/common.js
@@ -1,13 +1,13 @@
 (() => { // webpackBootstrap
 "use strict";
 var __webpack_modules__ = ({
-"./styleC.css?839b": (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+"./styleC.css": (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 __webpack_require__.r(__webpack_exports__);
 // extracted by css-extract-rspack-plugin
 
 
 }),
-"./styleD.css?9e5d": (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+"./styleD.css": (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 __webpack_require__.r(__webpack_exports__);
 // extracted by css-extract-rspack-plugin
 
@@ -157,7 +157,7 @@ var __webpack_exports__ = {};
 (() => {
 var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */var _styleC_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./styleC.css?839b");
+/* harmony import */var _styleC_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./styleC.css");
 
 
 })();
@@ -165,7 +165,7 @@ __webpack_require__.r(__webpack_exports__);
 // This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
 (() => {
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */var _styleD_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./styleD.css?9e5d");
+/* harmony import */var _styleD_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./styleD.css");
 
 
 })();
