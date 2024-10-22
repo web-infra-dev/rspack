660d4b3f08a7537bfd18b3fa61f162dfd6f6090c
neverland <chenjiahan.jait@bytedance.com>
2024-10-22 15:40:09 +0800

    test: rename harmony to ESM (#8183)

--- packages/rspack-test-tools/tests/configCases/builtins/provide/esm.js
+++ packages/rspack-test-tools/tests/configCases/builtins/provide/esm.js
@@ -1,1 +1,3 @@
-module.exports = "esm";
+export default "ECMAScript 2015";
+export const alias = "ECMAScript modules";
+export const year = 2015;
--- packages/rspack-test-tools/tests/normalCases/parsing/esm-commonjs-mix/index.js
+++ packages/rspack-test-tools/tests/normalCases/parsing/esm-commonjs-mix/index.js
@@ -1,4 +1,4 @@
-it("should result in a warning when using module.exports in harmony module", function () {
+it("should result in a warning when using module.exports in ESM", function () {
 	var x = require("./module1");
 	expect(x).toEqual(
 		nsObj({
