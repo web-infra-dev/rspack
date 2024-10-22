9aa8c989bb4c3072a188354c854a8ab6a5994aa0
neverland <chenjiahan.jait@bytedance.com>
2024-10-22 15:15:37 +0800

    fix

--- packages/rspack-cli/rslib.config.ts
+++ packages/rspack-cli/rslib.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@rslib/core";
 export default defineConfig({
 	lib: [
 		{ format: "cjs", syntax: "es2021", dts: { bundle: false } },
-		{ format: "esm", syntax: "es2021", dts: { bundle: false } }
+		{ format: "esm", syntax: "es2021" }
 	],
 	source: {
 		tsconfigPath: "./tsconfig.build.json"
