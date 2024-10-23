f3d3725f1fdd4441df8bd6ad2aea7c3637c3b7c6
SoonIter <sooniter@gmail.com>
2024-10-23 16:44:59 +0800

    chore: update

--- packages/rspack/src/cjsIndex.ts
+++ packages/rspack/src/cjsIndex.ts
@@ -1,4 +1,5 @@
 // fix https://github.com/web-infra-dev/rspack/issues/8095
 // const rspack = require('@rspack/core');
 import { rspack } from "./index";
-export = rspack;
+export default rspack;
+export * from "./index";
