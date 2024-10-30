bce8b0c6a5c94ef25bce0df8e947ec505577db4f
hardfist <yangjianzju@gmail.com>
2024-10-30 12:20:43 +0800

    chore: set strict to false

--- crates/rspack_plugin_javascript/src/plugin/side_effects_flag_plugin.rs
+++ crates/rspack_plugin_javascript/src/plugin/side_effects_flag_plugin.rs
@@ -358,7 +358,7 @@ fn is_pure_call_expr(
     let expr = Expr::Call(call_expr.clone());
     !expr.may_have_side_effects(&ExprCtx {
       unresolved_ctxt,
-      in_strict: true,
+      in_strict: false,
       is_unresolved_ref_safe: false,
     })
   } else {
@@ -434,7 +434,7 @@ pub fn is_pure_expression<'a>(
       _ => !expr.may_have_side_effects(&ExprCtx {
         unresolved_ctxt,
         is_unresolved_ref_safe: true,
-        in_strict: true,
+        in_strict: false,
       }),
     }
   }
