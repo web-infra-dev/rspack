8edfc19295feda3aaf619bb87bfbe91191765e0b
Cong-Cong <dacongsama@live.com>
2024-10-30 16:47:01 +0800

    chore: test case

--- packages/rspack-test-tools/tests/configCases/output/source-map-filename/index.js
+++ packages/rspack-test-tools/tests/configCases/output/source-map-filename/index.js
@@ -1,8 +1,12 @@
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
+
 it("source-map-filename/name should same", async function () {
 	import("./two");
-	expect(
-		fs.readdirSync(path.resolve(__dirname, "")).includes("main.js.map")
-	).toBe(true);
+
+	expect(async () => await fs.stat(path.resolve(__dirname, "../maps/main.js.map"))).not.toThrow();
+
+	const outputCode = await fs.readFile(__filename, 'utf-8');
+	const sourceMapPath = outputCode.match(/\/\/# sourceMappingURL=(.*)/)?.[1];
+	expect(sourceMapPath).toBe("maps/main.js.map");
 });
