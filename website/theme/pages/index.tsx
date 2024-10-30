00e8b178e510bb213e1bfbe91a71942bca6462c2
neverland <chenjiahan.jait@bytedance.com>
2024-10-30 10:43:50 +0800

    docs: add license to website footer

--- website/theme/pages/index.tsx
+++ website/theme/pages/index.tsx
@@ -13,7 +13,13 @@ const CopyRight = () => {
   return (
     <footer className="bottom-0 mt-12 py-8 px-6 sm:p-8 w-full border-t border-solid border-divider-light">
       <div className="m-auto w-full text-center">
-        <div className="font-medium text-sm text-text-2">{message}</div>
+        <div className="font-medium text-sm text-text-2">
+          <p className="mb-2">
+            Rspack is free and open source software released under the MIT
+            license.
+          </p>
+          <p>© 2022-present ByteDance Inc.</p>
+        </div>
       </div>
     </footer>
   );
