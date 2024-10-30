c7a190e90436b95053ea89d6416733321105e082
neverland <chenjiahan.jait@bytedance.com>
2024-10-30 10:45:34 +0800

    fix

--- website/theme/pages/index.tsx
+++ website/theme/pages/index.tsx
@@ -1,15 +1,7 @@
-import { usePageData } from 'rspress/runtime';
 import { HomeFooter } from '../components/HomeFooter/index';
 import LandingPage from '../components/Landingpage';
 
 const CopyRight = () => {
-  const { siteData } = usePageData();
-  const { message } = siteData.themeConfig.footer || {};
-
-  if (!message) {
-    return null;
-  }
-
   return (
     <footer className="bottom-0 mt-12 py-8 px-6 sm:p-8 w-full border-t border-solid border-divider-light">
       <div className="m-auto w-full text-center">
