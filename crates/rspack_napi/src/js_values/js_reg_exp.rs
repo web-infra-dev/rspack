37a681d15d51c99be25da43e33258f89c2145836
dalaoshu <laipichan@qq.com>
2024-10-31 15:07:55 +0800

    refactor(rspack_napi): simplify the logic of `JsRegExp` (#8254)
    * refactor(rspack_napi): simplify the logic of JsRegExp
    
    * fix

--- crates/rspack_napi/src/js_values/js_reg_exp.rs
+++ crates/rspack_napi/src/js_values/js_reg_exp.rs
@@ -2,10 +2,8 @@ use std::fmt::Debug;
 
 use napi::{
   bindgen_prelude::{FromNapiValue, TypeName, ValidateNapiValue},
-  sys, Error, JsObject, NapiRaw, NapiValue, Result, Status,
+  sys, Env, Error, JsFunction, JsObject, NapiRaw, NapiValue, Result, Status,
 };
-
-use crate::utils::NapiTypeRef;
 
 pub struct JsRegExp(JsObject);
 
@@ -31,31 +29,6 @@ impl JsRegExp {
   }
 }
 
-impl NapiRaw for JsRegExp {
-  unsafe fn raw(&self) -> sys::napi_value {
-    unsafe { self.0.raw() }
-  }
-}
-
-impl FromNapiValue for JsRegExp {
-  unsafe fn from_napi_value(env: sys::napi_env, napi_val: sys::napi_value) -> Result<Self> {
-    // Safety: `JsObject::call_without_args` only leverages the `JsObject::raw` method.
-    // It's not necessarily have to be exactly an `JsObject` instance.
-    let js_object = unsafe { JsObject::from_raw_unchecked(env, napi_val) };
-    let ty = unsafe { NapiTypeRef::new(env, &js_object) }?;
-    if !ty.is_regex()? {
-      return Err(Error::new(
-        Status::InvalidArg,
-        format!(
-          "Expect value to be '[object RegExp]', but received {}",
-          ty.get_type()?
-        ),
-      ));
-    }
-    Ok(Self(unsafe { JsObject::from_napi_value(env, napi_val) }?))
-  }
-}
-
 impl ValidateNapiValue for JsRegExp {}
 
 impl TypeName for JsRegExp {
@@ -65,5 +38,42 @@ impl TypeName for JsRegExp {
 
   fn value_type() -> napi::ValueType {
     napi::ValueType::Object
+  }
+}
+
+impl NapiRaw for JsRegExp {
+  unsafe fn raw(&self) -> sys::napi_value {
+    unsafe { self.0.raw() }
+  }
+}
+
+impl FromNapiValue for JsRegExp {
+  unsafe fn from_napi_value(env: sys::napi_env, napi_val: sys::napi_value) -> Result<Self> {
+    let js_object = unsafe { JsObject::from_raw_unchecked(env, napi_val) };
+
+    let env = Env::from(env);
+    let global = env.get_global()?;
+    let object_prototype_to_string = global
+      .get_named_property_unchecked::<JsObject>("Object")?
+      .get_named_property::<JsObject>("prototype")?
+      .get_named_property::<JsFunction>("toString")?;
+
+    let js_string = object_prototype_to_string
+      .call_without_args(Some(&js_object))?
+      .coerce_to_string()?
+      .into_utf8()?;
+    let js_object_type = js_string.as_str()?;
+
+    if js_object_type == "[object RegExp]" {
+      Ok(Self(js_object))
+    } else {
+      Err(Error::new(
+        Status::ObjectExpected,
+        format!(
+          "Expect value to be '[object RegExp]', but received {}",
+          js_object_type
+        ),
+      ))
+    }
   }
 }
