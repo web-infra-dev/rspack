2b122406dd38c07200b0d657dbba1fc3847df76b
neverland <chenjiahan.jait@bytedance.com>
2024-11-04 15:28:20 +0800

    perf(cli): enable Node.js compile cache

--- packages/rspack-cli/bin/rspack.js
+++ packages/rspack-cli/bin/rspack.js
@@ -1,4 +1,17 @@
 #!/usr/bin/env node
+const nodeModule = require("node:module");
+
+// enable on-disk code caching of all modules loaded by Node.js
+// requires Nodejs >= 22.8.0
+const { enableCompileCache } = nodeModule;
+if (enableCompileCache && !process.env.NODE_DISABLE_COMPILE_CACHE) {
+	try {
+		enableCompileCache();
+	} catch {
+		// ignore errors
+	}
+}
+
 const { RspackCLI } = require("../dist/index");
 
 async function runCLI() {
