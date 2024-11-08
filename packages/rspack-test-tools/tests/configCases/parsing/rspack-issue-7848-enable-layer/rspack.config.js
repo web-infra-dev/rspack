5b63a8e8ae61506da8c0f7f7dbcf49cc9ec729d5
inottn <inottn@outlook.com>
2024-11-08 15:31:44 +0800

    fix: compile time evaluation of __webpack_layer__

---
+++ packages/rspack-test-tools/tests/configCases/parsing/rspack-issue-7848-enable-layer/rspack.config.js
@@ -0,0 +1,19 @@
+const rspack = require("@rspack/core");
+
+/** @type {import("@rspack/core").Configuration} */
+module.exports = {
+	entry: {
+		bundle0: {
+			import: "./index.js",
+			layer: "main"
+		}
+	},
+	experiments: {
+		layers: true
+	},
+	plugins: [
+		new rspack.DefinePlugin({
+			__RUNTIME_TYPE__: "__webpack_layer__"
+		})
+	]
+};
