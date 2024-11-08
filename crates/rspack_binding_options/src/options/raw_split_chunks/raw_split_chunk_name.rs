d928ac0ddd8fdebf0ec7f7c3bdcd25b6a16a15a3
Cong-Cong Pan <dacongsama@live.com>
2024-11-08 17:46:10 +0800

    refactor(napi): rm unreachable FromNapiValue (#8378)

--- crates/rspack_binding_options/src/options/raw_split_chunks/raw_split_chunk_name.rs
+++ crates/rspack_binding_options/src/options/raw_split_chunks/raw_split_chunk_name.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use napi::bindgen_prelude::{Either3, FromNapiValue};
+use napi::bindgen_prelude::Either3;
 use napi_derive::napi;
 use rspack_binding_values::{JsChunk, JsModuleWrapper};
 use rspack_core::Chunk;
@@ -21,15 +21,6 @@ pub struct RawChunkOptionNameCtx {
   pub module: JsModuleWrapper,
   pub chunks: Vec<JsChunk>,
   pub cache_group_key: String,
-}
-
-impl FromNapiValue for RawChunkOptionNameCtx {
-  unsafe fn from_napi_value(
-    _env: napi::sys::napi_env,
-    _napi_val: napi::sys::napi_value,
-  ) -> napi::Result<Self> {
-    unreachable!()
-  }
 }
 
 impl<'a> From<ChunkNameGetterFnCtx<'a>> for RawChunkOptionNameCtx {
