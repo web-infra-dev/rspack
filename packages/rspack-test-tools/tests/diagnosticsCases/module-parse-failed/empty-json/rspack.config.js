c0963a40bfc467ca0319f22f29a5cc6f486831ba
LingyuCoder <lingyucoder@gmail.com>
2024-11-11 12:47:48 +0800

    fix: panic when imported json file is empty

---
+++ packages/rspack-test-tools/tests/diagnosticsCases/module-parse-failed/empty-json/rspack.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('@rspack/core').RspackOptions}
+ */
+module.exports = {
+  context: __dirname,
+  devtool: false,
+  optimization: {
+    concatenateModules: true,
+    minimize: false
+  }
+};
