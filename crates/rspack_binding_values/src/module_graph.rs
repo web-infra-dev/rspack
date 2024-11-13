b9de707ffb1cd271dcaf23680167b79d171e916c
Cong-Cong <dacongsama@live.com>
2024-11-13 12:19:16 +0800

    feat: JsModuleGraph

--- crates/rspack_binding_values/src/module_graph.rs
+++ crates/rspack_binding_values/src/module_graph.rs
@@ -1,6 +1,6 @@
 use std::{ptr::NonNull, sync::Arc};
 
-use napi::{bindgen_prelude::Either3, Either};
+use napi::Either;
 use napi_derive::napi;
 use rspack_core::{Compilation, ModuleGraph, RuntimeSpec};
 use rustc_hash::FxHashSet;
@@ -13,21 +13,23 @@ pub struct JsModuleGraph {
 }
 
 impl JsModuleGraph {
-  fn as_ref(&self) -> napi::Result<ModuleGraph<'static>> {
+  fn as_ref(&self) -> napi::Result<(&'static Compilation, ModuleGraph<'static>)> {
     let compilation = unsafe { self.compilation.as_ref() };
     let module_graph = compilation.get_module_graph();
 
-    Ok(module_graph)
+    Ok((compilation, module_graph))
   }
 }
 
 #[napi]
 impl JsModuleGraph {
-  #[napi(ts_return_type = "JsModule")]
-  pub fn get_module(&self, js_dependency: &JsDependency) -> napi::Result<JsModuleWrapper> {
-    let module_graph = self.as_ref()?;
+  #[napi(ts_return_type = "JsModule | null")]
+  pub fn get_module(&self, js_dependency: &JsDependency) -> napi::Result<Option<JsModuleWrapper>> {
+    let (compilation, module_graph) = self.as_ref()?;
     let module = module_graph.get_module_by_dependency_id(&js_dependency.id());
-    todo!()
+    let js_module = module
+      .map(|module| JsModuleWrapper::new(module.as_ref(), compilation.id(), Some(&compilation)));
+    Ok(js_module)
   }
 
   #[napi]
@@ -35,8 +37,8 @@ impl JsModuleGraph {
     &self,
     js_module: &JsModule,
     js_runtime: Either<String, Vec<String>>,
-  ) -> napi::Result<Either3<bool, Vec<String>, ()>> {
-    let module_graph = self.as_ref()?;
+  ) -> napi::Result<Option<Either<bool, Vec<String>>>> {
+    let (_, module_graph) = self.as_ref()?;
 
     let mut runtime: FxHashSet<Arc<str>> = FxHashSet::default();
     match js_runtime {
@@ -50,14 +52,14 @@ impl JsModuleGraph {
     let used_exports =
       module_graph.get_used_exports(&js_module.identifier, Some(&RuntimeSpec::new(runtime)));
     Ok(match used_exports {
-      rspack_core::UsedExports::Null => Either3::C(()),
-      rspack_core::UsedExports::Bool(b) => Either3::A(b),
-      rspack_core::UsedExports::Vec(vec) => Either3::B(
+      rspack_core::UsedExports::Null => None,
+      rspack_core::UsedExports::Bool(b) => Some(Either::A(b)),
+      rspack_core::UsedExports::Vec(vec) => Some(Either::B(
         vec
           .into_iter()
           .map(|atom| atom.to_string())
           .collect::<Vec<_>>(),
-      ),
+      )),
     })
   }
 }
