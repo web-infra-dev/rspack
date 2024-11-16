1ca6b68892e06b7228bb16d3c85b6f8e1acee278
inottn <inottn@outlook.com>
2024-11-16 16:18:45 +0800

    fix: correct favicon path when output.publicPath is set to `auto` (#8415)

--- crates/rspack_plugin_html/src/asset.rs
+++ crates/rspack_plugin_html/src/asset.rs
@@ -111,21 +111,17 @@ impl HtmlPluginAssets {
         favicon_relative_path.to_string_lossy().to_string().as_str(),
       ));
 
-      let fake_html_file_name = compilation
-        .get_path(
-          html_file_name,
-          PathData::default().filename(output_path.as_str()),
-        )
-        .always_ok();
-
       if favicon_path.to_str().unwrap_or_default().is_empty() {
-        favicon_path = compilation
-          .options
-          .output
-          .path
-          .as_std_path()
-          .join(favicon_relative_path)
-          .relative(PathBuf::from(fake_html_file_name).join(".."));
+        let fake_html_file_name = compilation
+          .get_path(
+            html_file_name,
+            PathData::default().filename(output_path.as_str()),
+          )
+          .always_ok();
+        let output_path = compilation.options.output.path.as_std_path();
+        favicon_path = output_path
+          .relative(output_path.join(fake_html_file_name).join(".."))
+          .join(favicon_relative_path);
       } else {
         favicon_path.push(favicon_relative_path);
       }
---
+++ packages/rspack-test-tools/tests/configCases/builtins/html-favicon-absolute-publicpath-auto/rspack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const { rspack } = require("@rspack/core");
+
+/** @type {import("@rspack/core").Configuration} */
+module.exports = {
+	output: {
+	    publicPath: "auto",
+	},
+	plugins: [
+		new rspack.HtmlRspackPlugin({
+			favicon: path.resolve(__dirname, "favicon.ico")
+		})
+	]
+};
