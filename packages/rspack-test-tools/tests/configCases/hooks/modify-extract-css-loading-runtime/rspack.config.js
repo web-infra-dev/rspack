a3d9362c088cd266b093f6f3cdc211c3dcc6f601
ahabhgk <ahabhgk@gmail.com>
2024-11-19 19:04:21 +0800

    fix: modify css loading runtime panic

---
+++ packages/rspack-test-tools/tests/configCases/hooks/modify-extract-css-loading-runtime/rspack.config.js
@@ -0,0 +1,35 @@
+const webpack = require("@rspack/core");
+
+class Plugin {
+	apply(compiler) {
+		compiler.hooks.compilation.tap("TestFakePlugin", compilation => {
+			compilation.hooks.runtimeModule.tap("TestFakePlugin", (module, chunk) => {
+				if (module.constructorName === "CssLoadingRuntimeModule") {
+					const originSource = module.source.source.toString("utf-8");
+					module.source.source = Buffer.from(
+						`${originSource}\n__webpack_require__.f.miniCss.test = true;\n`,
+						"utf-8"
+					);
+				}
+			});
+		});
+	}
+}
+
+/**@type {import("@rspack/core").Configuration}*/
+module.exports = {
+	target: "web",
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [webpack.CssExtractRspackPlugin.loader, "css-loader"],
+				type: "javascript/auto"
+			}
+		]
+	},
+	plugins: [
+		new webpack.CssExtractRspackPlugin(),
+		new Plugin(),
+	]
+};
