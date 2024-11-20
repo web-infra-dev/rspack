edfd375523a04fff1128aab293045cabeb0ae523
ahabhgk <ahabhgk@gmail.com>
2024-11-20 20:19:12 +0800

    fix

--- packages/rspack-test-tools/tests/watchCases/provided-exports/reexport-chain-strict/0/index.js
+++ packages/rspack-test-tools/tests/watchCases/provided-exports/reexport-chain-strict/0/index.js
@@ -4,9 +4,8 @@ const nsObj = m => {
 	Object.defineProperty(m, Symbol.toStringTag, { value: "Module" });
 	return m;
 };
-debugger
+
 it("should have to correct exports", () => {
-	debugger
 	expect(a).toStrictEqual(nsObj({
 		[`x${WATCH_STEP}`]: WATCH_STEP
 	}));
