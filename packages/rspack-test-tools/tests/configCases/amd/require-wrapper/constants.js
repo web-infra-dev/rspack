35db167f54d9514c9d4b698e892e733f97d3f4ca
nilptr <nilptr.js@gmail.com>
2024-11-24 17:49:28 +0800

    chore(amd): update test cases

---
+++ packages/rspack-test-tools/tests/configCases/amd/require-wrapper/constants.js
@@ -0,0 +1,2 @@
+
+export const HELLO = 'hello';
--- packages/rspack-test-tools/tests/configCases/amd/require-wrapper/index.js
+++ packages/rspack-test-tools/tests/configCases/amd/require-wrapper/index.js
@@ -1,5 +1,8 @@
 it('require([], function(require) {}) should work well', function (done) {
-	require([], function (require) {
+	require(['./hello'], function (hello, require) {
+		expect(typeof hello).toBe('function');
+		expect(hello('world')).toBe('hello, world');
+
 		const add = require('./add');
 		expect(typeof add).toBe('function');
 		expect(add(1, 2)).toBe(3);
