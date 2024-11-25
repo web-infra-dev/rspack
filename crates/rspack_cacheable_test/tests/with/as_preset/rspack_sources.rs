3aecb2e40b62ffd72016978ccdfe103fac9afa85
jerrykingxyz <jerrykingxyz@gmail.com>
2024-11-25 19:42:49 +0800

    fix: ci

--- crates/rspack_cacheable_test/tests/with/as_preset/rspack_sources.rs
+++ crates/rspack_cacheable_test/tests/with/as_preset/rspack_sources.rs
@@ -5,18 +5,18 @@ use rspack_sources::{BoxSource, RawSource, SourceExt};
 #[derive(Debug)]
 struct Data(#[cacheable(with=AsPreset)] BoxSource);
 
-fn test_data(data: Data) {
-  let bytes = to_bytes(&data, &()).unwrap();
-  let new_data: Data = from_bytes(&bytes, &()).unwrap();
-  assert_eq!(data.0.buffer(), new_data.0.buffer());
-  assert_eq!(
-    data.0.map(&Default::default()),
-    new_data.0.map(&Default::default())
-  );
-}
-
 #[test]
 fn test_rspack_source() {
+  fn test_data(data: Data) {
+    let bytes = to_bytes(&data, &()).unwrap();
+    let new_data: Data = from_bytes(&bytes, &()).unwrap();
+    assert_eq!(data.0.buffer(), new_data.0.buffer());
+    assert_eq!(
+      data.0.map(&Default::default()),
+      new_data.0.map(&Default::default())
+    );
+  }
+
   test_data(Data(RawSource::from("123".as_bytes()).boxed()));
   test_data(Data(RawSource::from("123").boxed()));
 }
