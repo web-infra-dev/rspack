81f33c5eb5208bfb6f94bd76c258d396c8e6da10
Fy <1114550440@qq.com>
2024-12-02 16:18:44 +0800

    refactor: css hmr (#8587)

--- packages/rspack-test-tools/tests/hotCases/css/parser-and-generator-states/index.js
+++ packages/rspack-test-tools/tests/hotCases/css/parser-and-generator-states/index.js
@@ -1,9 +1,10 @@
 import style from './index.module.css';
 
+module.hot.accept('./index.module.css')
+
 it("should store and resume css parser and generator states", (done) => {
 	expect(style['btnInfoIsDisabled']).toBe('./index.module.css__btn-info_is-disabled');
 	NEXT(require("../../update")(done, true, () => {
-		const style = require('./index.module.css')
 		expect(style['btnInfoIsEnabled']).toBe('./index.module.css__btn-info_is-enabled');
 		done();
 	}));
