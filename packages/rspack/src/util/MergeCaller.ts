970ad72d975d7f83b864375e7fa67b5801bb79b1
Cong-Cong <dacongsama@live.com>
2024-12-02 18:36:32 +0800

    perf: use microtask for MergeCaller

--- packages/rspack/src/util/MergeCaller.ts
+++ packages/rspack/src/util/MergeCaller.ts
@@ -1,31 +1,27 @@
 type CallFn<D> = (args: D[]) => void;
 
 export default class MergeCaller<D> {
-	private timer: any = null;
+	private microtask: Promise<void> | null = null;
 	private callArgs: D[] = [];
 
-	// add in constructor
-	private debounceTime: number;
 	private callFn: CallFn<D>;
-	constructor(fn: CallFn<D>, debounceTime = 0) {
-		this.debounceTime = debounceTime;
+	constructor(fn: CallFn<D>) {
 		this.callFn = fn;
 	}
 
 	private finalCall = () => {
-		this.timer = null;
+		this.microtask = null;
 		const args = this.callArgs;
 		this.callArgs = [];
 		this.callFn(args);
 	};
 
 	push(...data: D[]) {
-		if (this.timer) {
-			clearTimeout(this.timer);
+		if (!this.microtask) {
+			this.microtask = Promise.resolve();
+			this.microtask.then(this.finalCall);
 		}
 
 		this.callArgs.push(...data);
-
-		this.timer = setTimeout(this.finalCall, this.debounceTime);
 	}
 }
