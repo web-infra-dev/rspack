44bea89ecabc373b79301b57d60362b656a58db1
ahabhgk <ahabhgk@gmail.com>
2024-12-10 20:07:51 +0800

    add warning

--- packages/rspack/src/builtin-plugin/NaturalChunkIdsPlugin.ts
+++ packages/rspack/src/builtin-plugin/NaturalChunkIdsPlugin.ts
@@ -1,9 +1,23 @@
-import { BuiltinPluginName } from "@rspack/binding";
+import { type BuiltinPlugin, BuiltinPluginName } from "@rspack/binding";
+import type { Compiler } from "../Compiler";
+import type { Incremental } from "../config";
+import { RspackBuiltinPlugin, createBuiltinPlugin } from "./base";
 
-import { create } from "./base";
+export class NaturalChunkIdsPlugin extends RspackBuiltinPlugin {
+	name = BuiltinPluginName.NaturalChunkIdsPlugin;
+	affectedHooks = "compilation" as const;
 
-export const NaturalChunkIdsPlugin = create(
-	BuiltinPluginName.NaturalChunkIdsPlugin,
-	() => {},
-	"compilation"
-);
+	raw(compiler: Compiler): BuiltinPlugin {
+		const incremental = compiler.options.experiments.incremental as Incremental;
+		const logger = compiler.getInfrastructureLogger(
+			"rspack.NaturalChunkIdsPlugin"
+		);
+		if (incremental.moduleIds) {
+			incremental.moduleIds = false;
+			logger.warn(
+				"`optimization.chunkIds = 'natural'` can't be used with `incremental.chunkIds` as natural chunk ids is a global effect. `incremental.chunkIds` has been overridden to false."
+			);
+		}
+		return createBuiltinPlugin(this.name, undefined);
+	}
+}
