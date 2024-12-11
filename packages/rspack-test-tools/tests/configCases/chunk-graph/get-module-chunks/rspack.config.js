fdd4baada3c40538866571a4d42ad1a8cea0bfbf
Cong-Cong <dacongsama@live.com>
2024-12-11 14:46:58 +0800

    chore: test case

---
+++ packages/rspack-test-tools/tests/configCases/chunk-graph/get-module-chunks/rspack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+class Plugin {
+    apply(compiler) {
+        compiler.hooks.compilation.tap("Test", compilation => {
+            compilation.hooks.processAssets.tap("Test", () => {
+                const chunk = Array.from(compilation.chunks)[0];
+                const modules = compilation.chunkGraph.getChunkModules(chunk);
+                expect(modules[0].resource).toBe(path.join(__dirname, "index.js"));
+            });
+        });
+    }
+}
+
+/** @type {import("@rspack/core").Configuration} */
+module.exports = {
+    target: 'web',
+    node: false,
+    entry: {
+        main: "./index.js"
+    },
+    output: {
+        filename: "[name].js"
+    },
+    optimization: {
+        sideEffects: false,
+    },
+    plugins: [
+        new Plugin()
+    ]
+};
