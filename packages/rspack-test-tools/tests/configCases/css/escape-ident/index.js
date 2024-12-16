e0b1a0f07a8f51de3e57759a7202dec0f7dd31b4
Fy <1114550440@qq.com>
2024-12-16 14:28:57 +0800

    fix: escape css (#8698)

--- packages/rspack-test-tools/tests/configCases/css/escape-ident/index.js
+++ packages/rspack-test-tools/tests/configCases/css/escape-ident/index.js
@@ -1,10 +1,17 @@
 import * as styles from "./index.module.css";
 
 it("should generate correct exports", () => {
+	const fs = __non_webpack_require__('fs')
+	const path = __non_webpack_require__('path')
 	expect(styles).toEqual(
 		nsObj({
 			a: '"aaa" 123',
-			b: "multiple lines  bbb"
+			b: "multiple lines  bbb",
+			'a/b': 'a/b-./'
 		})
 	);
+
+	const css = fs.readFileSync(path.resolve(__dirname, './bundle0.css')).toString()
+	const escape = css.replaceAll('\\', '')
+	expect(escape).toContain(styles['a/b'])
 });
