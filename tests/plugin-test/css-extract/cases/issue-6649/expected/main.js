9ba26cd8c920ac3d298dbdf18fe7ebbc39b42bdd
Hana <andywangsy@gmail.com>
2024-12-17 14:38:39 +0800

    perf: faster `substring` of `rspack-sources` (#8731)
    * perf: init
    
    * perf: update

--- tests/plugin-test/css-extract/cases/issue-6649/expected/main.js
+++ tests/plugin-test/css-extract/cases/issue-6649/expected/main.js
@@ -89,7 +89,7 @@ __webpack_require__.e = function (chunkId) {
           // return url for filenames not based on template
           
           // return url for filenames based on template
-          return "" + chunkId + ".$" + {"\\css\\chunk": "d0e08c0a6c7f57622c03","\\js\\chunk": "4417803b080d011de306",}[chunkId] + "$.js";
+          return "" + chunkId + ".$" + {"\\css\\chunk": "b1b844425027e2012f17","\\js\\chunk": "033244835a8df445c8ec",}[chunkId] + "$.js";
         };
       
 })();
@@ -107,7 +107,7 @@ __webpack_require__.e = function (chunkId) {
 // webpack/runtime/get_full_hash
 (() => {
 __webpack_require__.h = function () {
-	return "b509c1f0031feb6a88cc";
+	return "78ebdcf52651ae0b1318";
 };
 
 })();
