61dcc7b34fc114f6059e0e3b6bda3be5666e4ec5
Cong-Cong <dacongsama@live.com>
2024-12-18 18:53:42 +0800

    u

---
+++ packages/bench/fixtures/ts-react/rspack.config.ts
@@ -0,0 +1,67 @@
+import { defineConfig } from "@rspack/cli";
+import { rspack } from "@rspack/core";
+import * as RefreshPlugin from "@rspack/plugin-react-refresh";
+
+const isDev = process.env.NODE_ENV === "development";
+
+// Target browsers, see: https://github.com/browserslist/browserslist
+const targets = ["chrome >= 87", "edge >= 88", "firefox >= 78", "safari >= 14"];
+
+export default defineConfig({
+	context: __dirname,
+	entry: {
+		main: "./src/main.tsx"
+	},
+	resolve: {
+		extensions: ["...", ".ts", ".tsx", ".jsx"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svg$/,
+				type: "asset"
+			},
+			{
+				test: /\.(jsx?|tsx?)$/,
+				use: [
+					{
+						loader: "builtin:swc-loader",
+						options: {
+							jsc: {
+								parser: {
+									syntax: "typescript",
+									tsx: true
+								},
+								transform: {
+									react: {
+										runtime: "automatic",
+										development: isDev,
+										refresh: isDev
+									}
+								}
+							},
+							env: { targets }
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new rspack.HtmlRspackPlugin({
+			template: "./index.html"
+		}),
+		isDev ? new RefreshPlugin() : null
+	].filter(Boolean),
+	optimization: {
+		minimizer: [
+			new rspack.SwcJsMinimizerRspackPlugin(),
+			new rspack.LightningCssMinimizerRspackPlugin({
+				minimizerOptions: { targets }
+			})
+		]
+	},
+	experiments: {
+		css: true
+	}
+});
---
+++ packages/bench/ts-react.bench.ts
@@ -0,0 +1,21 @@
+import { bench, describe } from "vitest";
+import { rspack } from "@rspack/core";
+import rspackConfig from "./fixtures/ts-react/rspack.config";
+
+describe("TypeScript React project", () => {
+	bench(
+		"build single module project in production mode",
+		() =>
+			new Promise((resolve, reject) => {
+				rspack(rspackConfig, (err, stats) => {
+					if (err) {
+						reject(err);
+					}
+					if (stats?.hasErrors()) {
+						reject(new Error(stats.toString({})));
+					}
+					resolve();
+				});
+			})
+	);
+});
