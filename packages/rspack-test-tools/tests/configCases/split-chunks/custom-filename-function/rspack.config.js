1e95de2d1089a303a551f0b29254395264a60739
jserfeng <1114550440@qq.com>
2024-12-19 17:53:59 +0800

    feat: splitChunks.cacheGroups.filename supports function

---
+++ packages/rspack-test-tools/tests/configCases/split-chunks/custom-filename-function/rspack.config.js
@@ -0,0 +1,34 @@
+/** @type {import("@rspack/core").Configuration} */
+module.exports = {
+	mode: "development",
+	entry: {
+		a: "./a",
+		b: "./b"
+	},
+	output: {
+		filename: "[name].js",
+		libraryTarget: "commonjs2"
+	},
+	optimization: {
+		chunkIds: "named",
+		splitChunks: {
+			cacheGroups: {
+				shared: {
+					chunks: "all",
+					test: /shared/,
+					filename: (pathData, assetInfo) => {
+						expect(pathData).toBeDefined()
+						expect(typeof assetInfo).toBe('object')
+						return "shared-[name].js"
+					},
+					enforce: true
+				},
+				common: {
+					chunks: "all",
+					test: /common/,
+					enforce: true
+				}
+			}
+		}
+	}
+};
