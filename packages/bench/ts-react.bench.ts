2dc6ecd0a415affb4faa50be3a8a325564a53513
Cong-Cong <dacongsama@live.com>
2024-12-19 11:27:12 +0800

    chore: bench traverse chunk modules

--- packages/bench/ts-react.bench.ts
+++ packages/bench/ts-react.bench.ts
@@ -1,27 +1,60 @@
-import { rspack } from "@rspack/core";
-import { bench, describe } from "vitest";
+import { rspack, Compilation } from "@rspack/core";
+import { bench, describe, beforeAll } from "vitest";
 import rspackConfig from "./fixtures/ts-react/rspack.config";
 
-describe("TypeScript React project", () => {
-	bench(
-		"build single module project in production mode",
-		() =>
-			new Promise((resolve, reject) => {
-				rspack(
-					{
-						...rspackConfig,
-						mode: "production"
-					},
-					(err, stats) => {
-						if (err) {
-							reject(err);
-						}
-						if (stats?.hasErrors()) {
-							reject(new Error(stats.toString({})));
-						}
-						resolve();
+let theCompilation: Compilation;
+
+beforeAll(() => {
+	return new Promise((resolve, reject) =>
+		rspack(
+			{
+				...rspackConfig,
+				mode: "production",
+				plugins: [
+					...(rspackConfig.plugins ?? []),
+					compiler => {
+						compiler.hooks.compilation.tap("PLGUIN", compilation => {
+							theCompilation = compilation;
+						});
 					}
-				);
-			})
+				]
+			},
+			(err, stats) => {
+				if (err) {
+					reject(err);
+				}
+				if (stats?.hasErrors()) {
+					reject(new Error(stats.toString({})));
+				}
+				resolve(undefined);
+			}
+		)
 	);
+});
+
+describe("TypeScript React project", () => {
+	bench("Traverse chunk modules", () => {
+		const entries = theCompilation.entries.values();
+
+		const visitedModules = new Set();
+
+		function traverse(dependency) {
+			const module = theCompilation.moduleGraph.getModule(dependency);
+			if (module) {
+				if (visitedModules.has(module)) {
+					return;
+				}
+				visitedModules.add(module);
+				for (const dep of module.dependencies) {
+					traverse(dep);
+				}
+			}
+		}
+
+		for (const entry of entries) {
+			for (const dependency of entry.dependencies) {
+				traverse(dependency);
+			}
+		}
+	});
 });
