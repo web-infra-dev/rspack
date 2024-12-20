82171fe1c744c3c367f4fc80528d2859da03acd9
David Hales <david.hales@skybettingandgaming.com>
2024-12-20 09:27:00 +0000

    fix: updated tests

--- packages/rspack-test-tools/tests/configCases/css/rspack-issue-6435/index.js
+++ packages/rspack-test-tools/tests/configCases/css/rspack-issue-6435/index.js
@@ -2,6 +2,6 @@ import * as classes from "./style.module.css";
 import legacyClasses from "./legacy/index.css";
 
 it("should have consistent hash", () => {
-	expect(classes["container-main"]).toBe(`${/* md4("./style.module.css") */ "ea850e6088d2566f677"}-container-main`)
+	expect(classes["container-main"]).toBe(`${/* md4("./style.module.css") */ "_7ea850e6088d2566f677"}-container-main`)
 	expect(legacyClasses["legacy-main"]).toBe(`${/* md4("./legacy/index.css") */ "c15d43fe622e87bbf5d"}-legacy-main`)
 });
