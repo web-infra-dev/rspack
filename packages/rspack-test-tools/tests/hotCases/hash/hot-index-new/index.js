b09af3d7f8bc10503a3139462833fa70372626a9
LingyuCoder <lingyucoder@gmail.com>
2024-12-25 20:25:42 +0800

    test: easier way to write hmr test cases

--- packages/rspack-test-tools/tests/hotCases/hash/hot-index-new/index.js
+++ packages/rspack-test-tools/tests/hotCases/hash/hot-index-new/index.js
@@ -9,21 +9,3 @@ it("should accept a dependencies and require a new value", async () => {
 	await NEXT_HMR();
 	expect(value).toBe(3);
 });
-
-let children = [...module.children];
-let usedExports = children.reduce((res, child) => {
-	res[child] = __webpack_module_cache__[child].exports;
-	return res;
-}, {});
-module.hot.accept(children, () => {
-	children.forEach((child) => {
-		const reexports = __webpack_require__(child);
-		for (let key in reexports) {
-			Object.defineProperty(usedExports[child], key, {
-				configurable: true,
-				enumerable: true,
-				get: () => reexports[key]
-			});
-		}
-	});
-});
