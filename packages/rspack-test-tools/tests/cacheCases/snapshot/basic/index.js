dc9a4c37e88ef8aeea8a093486e19333be57b8b5
jerrykingxyz <jerrykingxyz@gmail.com>
2024-12-26 17:29:48 +0800

    test: update basic test case

--- packages/rspack-test-tools/tests/cacheCases/snapshot/basic/index.js
+++ packages/rspack-test-tools/tests/cacheCases/snapshot/basic/index.js
@@ -3,10 +3,12 @@ import value from "./file";
 it("should store and resume asset parser and generator states", async () => {
 	if (COMPILER_INDEX == 0) {
 		expect(value).toBe(1);
+		await NEXT_HMR();
+		expect(value).toBe(2);
 		await NEXT_START();
 	}
 	if (COMPILER_INDEX == 1) {
-		expect(value).toBe(2);
+		expect(value).toBe(3);
 	}
 
 	//		expect(value).toBe(1);
