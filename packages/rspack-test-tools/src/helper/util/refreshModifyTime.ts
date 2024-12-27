71d9ddca7e172f7e3bdb0eda52d9e39e1e103dbb
jerrykingxyz <jerrykingxyz@gmail.com>
2024-12-27 15:09:38 +0800

    fix: comment

--- packages/rspack-test-tools/src/helper/util/refreshModifyTime.ts
+++ packages/rspack-test-tools/src/helper/util/refreshModifyTime.ts
@@ -1,9 +1,6 @@
 import { readFile, stat, writeFile } from "fs-extra";
 
 export async function refreshModifyTime(file: string) {
-	const before = await stat(file);
 	const data = await readFile(file);
 	await writeFile(file, data);
-	const after = await stat(file);
-	console.log(before.mtime, after.mtime);
 }
