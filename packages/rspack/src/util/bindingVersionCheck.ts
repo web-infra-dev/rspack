e6b15773659610dcdef9e1ae5745c11147808937
Hao Cheng <hcheng636@gmail.com>
2024-12-30 16:10:00 +0100

    fix: include versions in error message for version mismatch

--- packages/rspack/src/util/bindingVersionCheck.ts
+++ packages/rspack/src/util/bindingVersionCheck.ts
@@ -133,8 +133,7 @@ export const checkVersion = () => {
 
 	if (!isMatch) {
 		return (result = new Error(
-			"Unmatched version @rspack/core@${CORE_VERSION}, @rspack/binding@${BINDING_VERSION}, @rspack/binding-${platformArchAbi}@${ADDON_VERSION}.\n" +
-				"Rspack requires these versions to be the same or you may have installed the wrong version. Otherwise, rspack may not work properly."
+			`Unmatched version @rspack/core@${CORE_VERSION}, @rspack/binding@${BINDING_VERSION}, @rspack/binding-${platformArchAbi}@${ADDON_VERSION}.\nRspack requires these versions to be the same or you may have installed the wrong version. Otherwise, rspack may not work properly.`
 		));
 	}
 
