657c9392a0a4b6c8ed1fd9dc5ecf9aade1bb88e4
GiveMe-A-Name <a455300764b@hotmail.com>
2025-01-13 14:33:39 +0800

    refactor: clean up unused code and improve template ID handling in runtime modules

--- crates/rspack_plugin_runtime/src/runtime_module/auto_public_path.rs
+++ crates/rspack_plugin_runtime/src/runtime_module/auto_public_path.rs
@@ -106,43 +106,4 @@ fn auto_public_path_template(
       "ASSIGN": assign
     })),
   )
-
-  // let script_url_template = if output.script_type.eq("module") {
-  //   format!(
-  //     r#"var scriptUrl;
-  //   if (typeof {import_meta_name}.url === "string") scriptUrl = {import_meta_name}.url
-  //   "#
-  //   )
-  //   .to_string()
-  // } else {
-  //   format!(
-  //     r#"var scriptUrl;
-  //   if ({global}.importScripts) scriptUrl = {global}.location + "";
-  //   var document = {global}.document;
-  //   if (!scriptUrl && document) {{
-  //     // Technically we could use `document.currentScript instanceof window.HTMLScriptElement`,
-  //     // but an attacker could try to inject `<script>HTMLScriptElement = HTMLImageElement</script>`
-  //     // and use `<img name="currentScript" src="https://attacker.controlled.server/"></img>`
-  //     if (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT') scriptUrl = document.currentScript.src;
-  //     if (!scriptUrl) {{
-  //       var scripts = document.getElementsByTagName("script");
-  //           if (scripts.length) {{
-  //             var i = scripts.length - 1;
-  //             while (i > -1 && (!scriptUrl || !/^http(s?):/.test(scriptUrl))) scriptUrl = scripts[i--].src;
-  //           }}
-  //     }}
-  //     }}
-  //   "#
-  //   )
-  // };
-  // format!(
-  //   r#"
-  //   {script_url_template}
-  //   // When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration",
-  //   // or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.',
-  //   if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-  //   scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-  //   {assign}
-  //   "#
-  // )
 }
