4d7b629c31de49d1011f404d278e560e560ec617
Hana <andywangsy@gmail.com>
2025-01-16 16:19:47 +0800

    feat: hoist compiler options builder to `Compiler::builder`

--- crates/rspack/tests/basic.rs
+++ crates/rspack/tests/basic.rs
@@ -1,23 +1,12 @@
 use rspack::builder::Builder as _;
-use rspack_core::{Compiler, CompilerOptions, EntryDescription, Optimization};
+use rspack_core::Compiler;
 use rspack_paths::Utf8Path;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn basic() {
-  let dir = Utf8Path::new(env!("CARGO_MANIFEST_DIR"));
   let mut compiler = Compiler::builder()
-    .compiler_options(
-      CompilerOptions::builder()
-        .context(Utf8Path::new(dir).join("tests/fixtures/basic"))
-        .entry(
-          "main".to_string(),
-          EntryDescription {
-            import: vec!["./src/index.js".to_string()],
-            ..Default::default()
-          },
-        )
-        .optimization(Optimization::builder().node_env("'development'".to_string())),
-    )
+    .context(Utf8Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/basic"))
+    .entry("main", "./src/index.js")
     .build();
 
   compiler.build().await.unwrap();
--- crates/rspack/tests/fixtures/basic/src/index.js
+++ crates/rspack/tests/fixtures/basic/src/index.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV === "development") {
+if(process.env.NODE_ENV === "production") {
 	console.log(123);
 } else {
 	console.log(456);
