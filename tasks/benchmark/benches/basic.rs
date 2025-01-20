1d7a2ea6c5624db6308bd3803f49c80337a0a76b
Hana <andywangsy@gmail.com>
2025-01-20 20:30:47 +0800

    test: add basic rust benchmark

--- tasks/benchmark/benches/basic.rs
+++ tasks/benchmark/benches/basic.rs
@@ -1,15 +1,56 @@
-use rspack_benchmark::{black_box, criterion_group, criterion_main, Criterion};
+#![feature(trait_upcasting)]
+use std::sync::Arc;
 
-fn fibonacci(n: u64) -> u64 {
-  match n {
-    0 => 1,
-    1 => 1,
-    n => fibonacci(n - 1) + fibonacci(n - 2),
-  }
+use rspack::builder::Builder as _;
+use rspack_benchmark::{criterion_group, criterion_main, Criterion};
+use rspack_core::Compiler;
+use rspack_fs::{MemoryFileSystem, ReadableFileSystem, WritableFileSystem};
+use tokio::runtime::Builder;
+
+trait FileSystem: ReadableFileSystem + WritableFileSystem + Send + Sync {}
+impl<T: ReadableFileSystem + WritableFileSystem + Send + Sync> FileSystem for T {}
+
+async fn basic(fs: Arc<dyn FileSystem>) {
+  let mut compiler = Compiler::builder()
+    .context("/")
+    .entry("main", "./src/index.js")
+    .input_filesystem(fs.clone())
+    .output_filesystem(fs.clone())
+    .build();
+
+  compiler.run().await.unwrap();
+  assert!(compiler
+    .compilation
+    .get_errors()
+    .collect::<Vec<_>>()
+    .is_empty());
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-  c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+  c.bench_function("basic", |b| {
+    let rt = Builder::new_multi_thread().build().unwrap();
+
+    let fs = MemoryFileSystem::default();
+    rt.block_on(async {
+      fs.create_dir_all("/src".into()).await.unwrap();
+      fs.write(
+        "/src/index.js".into(),
+        br#"if(process.env.NODE_ENV === "production") {
+    console.log(123);
+  } else {
+    console.log(456);
+  }
+  "#,
+      )
+      .await
+      .unwrap();
+    });
+    let fs = Arc::new(fs);
+    b.to_async(&rt).iter(|| {
+      let fs = fs.clone();
+      basic(fs)
+    });
+  });
 }
 
 criterion_group!(benches, criterion_benchmark);
