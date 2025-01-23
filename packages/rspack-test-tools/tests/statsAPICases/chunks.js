892d2058d924b8bc7d2200988fd9c36294586b93
ahabhgk <ahabhgk@gmail.com>
2025-01-23 15:29:06 +0800

    update snapshot

--- packages/rspack-test-tools/tests/statsAPICases/chunks.js
+++ packages/rspack-test-tools/tests/statsAPICases/chunks.js
@@ -150,7 +150,7 @@ module.exports = {
 		      main.js,
 		    ],
 		    filteredModules: undefined,
-		    hash: 277ca3c6aa3cd84e,
+		    hash: 8aed47a5120d1613,
 		    id: 909,
 		    idHints: Array [],
 		    initial: true,
