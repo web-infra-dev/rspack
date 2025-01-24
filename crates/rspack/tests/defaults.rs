89995f5a320177d9c5a4010bcea99b88b8bd003b
Hana <andywangsy@gmail.com>
2025-01-24 17:57:54 +0800

    test: init

---
+++ crates/rspack/tests/defaults.rs
@@ -0,0 +1,18 @@
+use rspack::builder::{BuilderContext, CompilerOptionsBuilder};
+use rspack_core::Mode;
+
+#[tokio::test(flavor = "multi_thread")]
+async fn default_options() {
+  let mut builder_context = BuilderContext::default();
+  let options = CompilerOptionsBuilder::default()
+    .mode(Mode::None)
+    .build(&mut builder_context);
+  let cwd = std::env::current_dir().unwrap();
+
+  let mut settings = insta::Settings::clone_current();
+  settings.add_filter(&cwd.to_string_lossy(), "<cwd>");
+  settings.bind(|| {
+    insta::assert_debug_snapshot!(options);
+    insta::assert_debug_snapshot!(builder_context);
+  });
+}
