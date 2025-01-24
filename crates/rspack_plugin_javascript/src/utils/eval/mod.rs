2f8578bcb0205d5d0ce8abd3e87d400548a76e7f
ahabhgk <ahabhgk@gmail.com>
2025-01-24 16:06:52 +0800

    fix: amd require array

--- crates/rspack_plugin_javascript/src/utils/eval/mod.rs
+++ crates/rspack_plugin_javascript/src/utils/eval/mod.rs
@@ -70,6 +70,7 @@ pub struct BasicEvaluatedExpression {
   string: Option<String>,
   bigint: Option<Bigint>,
   regexp: Option<Regexp>,
+  array: Option<Vec<String>>,
   identifier: Option<String>,
   root_info: Option<ExportedVariableInfo>,
   members: Option<Vec<Atom>>,
@@ -81,7 +82,6 @@ pub struct BasicEvaluatedExpression {
   prefix: Option<Box<BasicEvaluatedExpression>>,
   postfix: Option<Box<BasicEvaluatedExpression>>,
   wrapped_inner_expressions: Option<Vec<BasicEvaluatedExpression>>,
-  // array: Option<Array>
   template_string_kind: Option<TemplateStringKind>,
 
   options: Option<Vec<BasicEvaluatedExpression>>,
@@ -105,6 +105,7 @@ impl BasicEvaluatedExpression {
       boolean: None,
       number: None,
       bigint: None,
+      array: None,
       quasis: None,
       parts: None,
       identifier: None,
@@ -163,6 +164,10 @@ impl BasicEvaluatedExpression {
 
   pub fn is_array(&self) -> bool {
     matches!(self.ty, Ty::Array)
+  }
+
+  pub fn is_const_array(&self) -> bool {
+    matches!(self.ty, Ty::ConstArray)
   }
 
   pub fn is_wrapped(&self) -> bool {
@@ -398,6 +403,12 @@ impl BasicEvaluatedExpression {
     self.items = Some(items);
   }
 
+  pub fn set_array(&mut self, array: Vec<String>) {
+    self.ty = Ty::ConstArray;
+    self.side_effects = false;
+    self.array = Some(array);
+  }
+
   pub fn options(&self) -> &Vec<BasicEvaluatedExpression> {
     self.options.as_ref().expect("options should not empty")
   }
@@ -572,6 +583,14 @@ impl BasicEvaluatedExpression {
   pub fn items(&self) -> &Vec<BasicEvaluatedExpression> {
     assert!(self.is_array());
     self.items.as_ref().expect("items must exists for array")
+  }
+
+  pub fn array(&self) -> &Vec<String> {
+    assert!(self.is_const_array());
+    self
+      .array
+      .as_ref()
+      .expect("array must exists for const array")
   }
 
   pub fn number(&self) -> Number {
--- tests/webpack-test/cases/amd/namedModulesConstArrayDep/test.filter.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = () => {return "compile time eval split"}
-
-							
