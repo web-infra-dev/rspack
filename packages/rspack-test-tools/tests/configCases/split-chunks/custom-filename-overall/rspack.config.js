5a4df653b0207d3da023574edaa856c4980cc61f
ahabhgk <ahabhgk@gmail.com>
2025-02-11 17:31:22 +0800

    feat: split chunks overall filename

---
+++ packages/rspack-test-tools/tests/configCases/split-chunks/custom-filename-overall/rspack.config.js
@@ -0,0 +1,31 @@
+/** @type {import("@rspack/core").Configuration} */
+module.exports = {
+	mode: "development",
+	entry: {
+		a: "./a",
+		b: "./b"
+	},
+	output: {
+		filename: "[name].js",
+		libraryTarget: "commonjs2"
+	},
+	optimization: {
+		chunkIds: "named",
+		splitChunks: {
+			filename: "splitted-chunks/[name].js",
+			cacheGroups: {
+				shared: {
+					chunks: "all",
+					test: /shared/,
+					filename: "shared-[name].js",
+					enforce: true
+				},
+				common: {
+					chunks: "all",
+					test: /common/,
+					enforce: true
+				}
+			}
+		}
+	}
+};
