3f37001aee3978613e8076c75a8c7dbb5695327b
ahabhgk <ahabhgk@gmail.com>
2025-02-11 16:27:43 +0800

    fix

--- packages/rspack-test-tools/tests/configCases/module/function-use-match-resource/index.js
+++ packages/rspack-test-tools/tests/configCases/module/function-use-match-resource/index.js
@@ -3,14 +3,10 @@ import { A, B, __info__, __context__ } from "__label__?A=A#B=B!=!./lib";
 it("use entry arguments should be correct", () => {
 	expect(A).toBe('A');
 	expect(B).toBe('B');
-	expect(__info__).toMatchInlineSnapshot(`
-{
-  "resource": "__label__",
-  "realResource": "<TEST_TOOLS_ROOT>/tests/configCases/module/function-use-match-resource/lib.js",
-  "resourceQuery": "?A=A",
-  "resourceFragment": "#B=B",
-  "issuer": "<TEST_TOOLS_ROOT>/tests/configCases/module/function-use-match-resource/index.js",
-  "issuerLayer": ""
-}
-`);
+	expect(__info__.resource).toBe("__label__");
+	expect(__info__.realResource.includes("lib.js")).toBe(true);
+	expect(__info__.resourceQuery).toBe("?A=A");
+	expect(__info__.resourceFragment).toBe("#B=B");
+	expect(__info__.issuer.includes("index.js")).toBe(true);
+	expect(__info__.issuerLayer).toBe("");
 });
--- packages/rspack-test-tools/tests/configCases/module/function-use-match-resource/loader.js
+++ packages/rspack-test-tools/tests/configCases/module/function-use-match-resource/loader.js
@@ -2,6 +2,6 @@ module.exports = function (source) {
 	const info = this.getOptions();
 	delete info.compiler;
 	return source + `
-export const __info__ = ${JSON.stringify(JSON.stringify(info, null, 2))};
+export const __info__ = ${JSON.stringify(info, null, 2)};
 `
 };
