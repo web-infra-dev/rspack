64a1a138287235ae2e392d68555fca0c2416d41a
pshu <pishu.spf@antfin.com>
2025-02-12 12:09:45 +0800

    refactor: add mjs js format

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,12 +1,11 @@
-export default  
-	{
-    "*.rs": "rustfmt --edition 2021",
-    "*.{ts,tsx,js}": "pnpm run format:js",
-    "*.toml": "npx @taplo/cli format",
-    "*.{ts,tsx,js,cts,cjs,mts,mjs}": [
-      "pnpm run lint:js",
-      // ignore staged file list; execute x without extra args
-      ()=>'pnpm run x ae ci'
-    ],
-    "package.json": "pnpm run check-dependency-version"
-  }
+export default {
+	"*.rs": "rustfmt --edition 2021",
+	"*.{ts,tsx,js,mjs}": "pnpm run format:js",
+	"*.toml": "npx @taplo/cli format",
+	"*.{ts,tsx,js,cts,cjs,mts,mjs}": [
+		"pnpm run lint:js",
+		// ignore staged file list; execute x without extra args
+		() => "pnpm run x ae ci"
+	],
+	"package.json": "pnpm run check-dependency-version"
+};
