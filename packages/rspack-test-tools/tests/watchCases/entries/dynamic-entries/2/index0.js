ee278eed7cb2683345656bc446f6c5719af035a2
ahabhgk <ahabhgk@gmail.com>
2025-02-12 19:30:55 +0800

    fix: dynamic entry dependency reuse

--- packages/rspack-test-tools/tests/watchCases/entries/dynamic-entries/2/index0.js
+++ packages/rspack-test-tools/tests/watchCases/entries/dynamic-entries/2/index0.js
@@ -1,5 +1,5 @@
 it("should have correct entrypoints", function() {
-  expect(Object.keys(__STATS__.entrypoints)).toEqual(["bundle0", "bundle1"]);
+  expect(Object.keys(__STATS__.entrypoints)).toEqual(["bundle0", "bundle1", "bundle2"]);
 
   const index0 = __STATS__.modules.find(m => m.name === "./index0.js");
   expect(index0.built).toBe(true);
@@ -7,7 +7,12 @@ it("should have correct entrypoints", function() {
   expect(index0.reasons[0].type).toBe("entry");
 
   const index1 = __STATS__.modules.find(m => m.name === "./index1.js");
-  expect(index1.built).toBe(true);
+  expect(index1.built).toBe(false);
   expect(index1.reasons.length).toBe(1);
   expect(index1.reasons[0].type).toBe("entry");
+
+  const index2 = __STATS__.modules.find(m => m.name === "./index2.js");
+  expect(index2.built).toBe(true);
+  expect(index2.reasons.length).toBe(1);
+  expect(index2.reasons[0].type).toBe("entry");
 })
