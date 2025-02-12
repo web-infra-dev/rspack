e95733ce2f75554c2d2777b0146017ad62603162
pshu <pishu.spf@antfin.com>
2025-02-12 11:59:47 +0800

    refactor: use lint staged js config file

---
+++ lint-staged.config.mjs
@@ -0,0 +1,11 @@
+export default  
+	{
+    "*.rs": "rustfmt --edition 2021",
+    "*.{ts,tsx,js}": "pnpm run format:js",
+    "*.toml": "npx @taplo/cli format",
+    "*.{ts,tsx,js,cts,cjs,mts,mjs}": [
+      "pnpm run lint:js",
+      ()=>'pnpm run x ae ci'
+    ],
+    "package.json": "pnpm run check-dependency-version"
+  }
