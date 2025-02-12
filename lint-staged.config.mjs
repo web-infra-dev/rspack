6ab13e55172d77be2167514db0463ca5cf53289c
pshu <pishu.spf@antfin.com>
2025-02-12 12:03:05 +0800

    chore: 🔥

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -5,6 +5,7 @@ export default
     "*.toml": "npx @taplo/cli format",
     "*.{ts,tsx,js,cts,cjs,mts,mjs}": [
       "pnpm run lint:js",
+      // ignore staged file list; execute x without extra args
       ()=>'pnpm run x ae ci'
     ],
     "package.json": "pnpm run check-dependency-version"
--- scripts/utils/no-args.cjs
+++
@@ -1,4 +0,0 @@
-const { execSync } = require("node:child_process");
-const cmd = process.argv[2];
-
-execSync(cmd);
