8ea54cdb09ae6513cb7fe967f58e344dbc454170
neverland <chenjiahan.jait@bytedance.com>
2025-02-13 14:02:21 +0800

    test: remove copy-webpack-plugin v5 (#9274)

--- packages/rspack-test-tools/tests/configCases/plugins/copy-webpack-plugin-v5/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-it("copy-webpack-plugin-v5", () => {
-	const copied = fs.readFileSync(path.resolve(__dirname, "stuff.txt"), "utf-8");
-	expect(copied).toBe("from\nto");
-});
