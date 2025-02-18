930c9e94ed730c7c1733a2cb936efbf4ddffab9c
LingyuCoder <lingyucoder@gmail.com>
2025-02-18 15:54:08 +0800

    fix: runtime chunk check full hash

---
+++ packages/rspack-test-tools/tests/watchCases/hashes/runtimeChunkHash/rspack.config.js
@@ -0,0 +1,20 @@
+/** @type {import("@rspack/core").Configuration} */
+module.exports = {
+	entry: {
+		a: "./a/index.js",
+		b: "./b/index.js",
+		main: "./main/index.js"
+	},
+	output: {
+		clean: false,
+		filename: "[name].[chunkhash].[contenthash].js",
+		chunkFilename: "[name].[chunkhash].[contenthash].js"
+	},
+	optimization: {
+		runtimeChunk: "single"
+	},
+	experiments: {
+		// TODO: disable new incremental because it will panic
+		incremental: false
+	}
+};
