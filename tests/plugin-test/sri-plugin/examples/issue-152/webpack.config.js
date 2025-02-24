cf4d88222fc0fc93e1f78d51cd9d87ccd4a29d1e
harpsealjs <lingyucoder@gmail.com>
2025-02-24 18:51:16 +0800

    test: remove hash test in sri plugin cases (#9440)

--- tests/plugin-test/sri-plugin/examples/issue-152/webpack.config.js
+++ tests/plugin-test/sri-plugin/examples/issue-152/webpack.config.js
@@ -3,7 +3,7 @@ const { createIntegrityPlugin, getDist } = require("../wsi-test-helper");
 module.exports = {
   entry: "./index.js",
   output: {
-    filename: "[contenthash].js",
+    filename: "[name].js",
     chunkFilename: "[contenthash].chunk.js",
     crossOriginLoading: "anonymous",
     path: getDist(__dirname),
@@ -23,9 +23,8 @@ module.exports = {
     {
       apply: (compiler) => {
         compiler.hooks.done.tap("wsi-test", (stats) => {
-          expect(Object.keys(stats.compilation.assets)).toContain(
-            "85e3f6d0198e353b.js"
-          );
+          const mainAsset = stats.compilation.assets["main.js"];
+          expect(mainAsset.source()).toContain(`JSON.parse('{"key":"value","key2":"value2","key3":"value","key4":"value2","key5":"value","key6":"value2","key7":"value","key8":"value2","key9":"value","key10":"value2","key11":"value","key12":"value2","key13":"value","key14":"value2","key15":"value","key16":"value2","key17":"value","key18":"value2","key19":"value","key20":"value2","key21":"value","key22":"value2"}')`);
         });
       },
     },
