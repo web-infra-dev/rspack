de33abf7079ab2d1908cb086495f753d8e6d8663
ahabhgk <ahabhgk@gmail.com>
2025-02-25 15:08:26 +0800

    bad data structure cause werid bug

--- crates/rspack_core/src/chunk_graph/chunk_graph_module.rs
+++ crates/rspack_core/src/chunk_graph/chunk_graph_module.rs
@@ -5,6 +5,7 @@ use std::fmt;
 use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 
+use itertools::Itertools;
 use rspack_collections::{IdentifierSet, UkeySet};
 use rspack_hash::RspackHashDigest;
 use rspack_macros::cacheable;
@@ -14,9 +15,9 @@ use serde::{Serialize, Serializer};
 use tracing::instrument;
 
 use crate::{
-  AsyncDependenciesBlockIdentifier, ChunkByUkey, ChunkGroup, ChunkGroupByUkey, ChunkGroupUkey,
-  ChunkUkey, Compilation, ModuleGraph, ModuleIdentifier, ModuleIdsArtifact, RuntimeGlobals,
-  RuntimeSpec, RuntimeSpecMap, RuntimeSpecSet,
+  get_runtime_key, AsyncDependenciesBlockIdentifier, ChunkByUkey, ChunkGroup, ChunkGroupByUkey,
+  ChunkGroupUkey, ChunkUkey, Compilation, ModuleGraph, ModuleIdentifier, ModuleIdsArtifact,
+  RuntimeGlobals, RuntimeSpec, RuntimeSpecMap, RuntimeSpecSet,
 };
 use crate::{ChunkGraph, Module};
 
@@ -215,10 +216,14 @@ impl ChunkGraph {
     chunk_by_ukey: &'a ChunkByUkey,
   ) -> impl Iterator<Item = &'a RuntimeSpec> + use<'a, '_> {
     let cgm = self.expect_chunk_graph_module(module_identifier);
-    cgm.chunks.iter().map(|chunk_ukey| {
-      let chunk = chunk_by_ukey.expect_get(chunk_ukey);
-      chunk.runtime()
-    })
+    cgm
+      .chunks
+      .iter()
+      .map(|chunk_ukey| {
+        let chunk = chunk_by_ukey.expect_get(chunk_ukey);
+        chunk.runtime()
+      })
+      .unique_by(|&r| get_runtime_key(r))
   }
 
   pub fn get_module_id(
