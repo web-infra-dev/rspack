fd867cf57c418e0a426f7940012f8c4f493383b2
jinrui <jerrykingxyz@gmail.com>
2025-02-25 14:22:31 +0800

    fix: use persistent cache with module foundation (#9445)
    fix: persistent cache mf

--- packages/rspack-test-tools/src/helper/loaders/hot-update.ts
+++ packages/rspack-test-tools/src/helper/loaders/hot-update.ts
@@ -6,14 +6,17 @@ export default function (this: any, c: string) {
 		content = `
 			${content}
 			let __hmr_children__ = [...module.children];
-			let __hmr_used_exports__ = __hmr_children__.reduce((res, child) => {
-				res[child] = __webpack_module_cache__[child].exports;
+      let __hmr_used_exports__ = __hmr_children__.reduce((res, child) => {
+        if (__webpack_module_cache__[child]) {
+          res[child] = __webpack_module_cache__[child].exports;
+        }
 				return res;
 			}, {});
 			module.hot.accept(__hmr_children__, () => {
 				__hmr_children__.forEach((child) => {
 					const reexports = __webpack_require__(child);
-					for (let key in reexports) {
+          for (let key in reexports) {
+            if (!__hmr_used_exports__[child]) { continue; }
 						Object.defineProperty(__hmr_used_exports__[child], key, {
 							configurable: true,
 							enumerable: true,
