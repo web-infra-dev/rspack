09ac9ef3aad8201742741475955864c2fd060c4c
harpsealjs <lingyucoder@gmail.com>
2025-03-13 18:26:40 +0800

    chore: clean unused test dev dependencies (#9662)
    chore: clean unused dev dependencies

---
+++ packages/rspack-test-tools/tests/runtimeDiffCases/runtime-module/startup-chunk-dependencies/node_modules/lodash/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  VERSION: 1
+};
--- tests/webpack-examples/module-federation/build.js
+++
@@ -1,5 +0,0 @@
-global.NO_TARGET_ARGS = true;
-global.NO_REASONS = true;
-global.NO_STATS_OPTIONS = true;
-global.NO_PUBLIC_PATH = true;
-require("../build-common");
