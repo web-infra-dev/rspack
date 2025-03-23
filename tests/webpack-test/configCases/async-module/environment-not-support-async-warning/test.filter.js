a1e0489dbbafe2a68039f8db73759356ae72cb9f
Nicolas Le Cam <niko.lecam@gmail.com>
2025-03-23 13:55:03 +0100

    test(webpack): Sync new webpack configCases tests: batch #5

---
+++ tests/webpack-test/configCases/async-module/environment-not-support-async-warning/test.filter.js
@@ -0,0 +1,7 @@
+// var supportsWebAssembly = require("../../../helpers/supportsWebAssembly");
+// module.exports = function (config) {
+//        return supportsWebAssembly();
+// };
+
+// Expecting warnings `The generated code contains 'async\/await'`
+module.exports = () => false;
