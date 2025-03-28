c59cf0bb383d0ade41bf7c47cd34820b4f347acb
Cong-Cong Pan <dacongsama@live.com>
2025-03-28 17:23:52 +0800

    docs: add Rspack 1.3 release blog (#9817)
    * docs: 1.3.0 release note
    
    * docs: polish
    
    * docs: Rsbuild features
    
    * feat: import http url
    
    * fix: url
    
    * docs: add Re.Pack 5
    
    * docs: polish
    
    * docs: move module subtypes
    
    * docs: thanks
    
    * rewrite build http
    
    * docs: reorder
    
    * docs: update
    
    * docs: update
    
    * docs: update
    
    * docs: memory
    
    * docs: polish
    
    * docs: update
    
    * docs: lazy compilation
    
    * docs: polish
    
    * docs: intro
    
    * docs: Rslib 0.6
    
    * docs: RR v7
    
    * docs: more
    
    * docs
    
    * docs: og image
    
    ---------
    
    Co-authored-by: neverland <chenjiahan.jait@bytedance.com>
    Co-authored-by: ahabhgk <ahabhgk@gmail.com>

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -136,6 +136,9 @@ export default defineConfig({
         if (routePath.endsWith('blog/announcing-1-2')) {
           return 'assets/rspack-og-image-v1-2.png';
         }
+        if (routePath.endsWith('blog/announcing-1-3')) {
+          return 'assets/rspack-og-image-v1-3.png';
+        }
         // default
         return 'rspack-og-image.png';
       };
