f61c7ab7be41ebde3f92fa43045fa5361ab6533e
Zack Jackson <25274700+ScriptedAlchemy@users.noreply.github.com>
2025-03-27 23:18:17 -0700

    fix(plugin_http): implement respond_with_url_module (#9800)
    fix(plugin_http): implement respond_with_url_module
    
    ---------
    
    Co-authored-by: ahabhgk <ahabhgk@gmail.com>

--- packages/rspack-test-tools/tests/configCases/build-http/asset/index.js
+++ packages/rspack-test-tools/tests/configCases/build-http/asset/index.js
@@ -1,8 +1,11 @@
-const urlSvg = new URL(
+import url1 from "https://raw.githubusercontent.com/web-infra-dev/rspack/55d5d81/packages/rspack-test-tools/tests/configCases/asset/_images/file.png";
+
+const url2 = new URL(
 	"https://raw.githubusercontent.com/web-infra-dev/rspack/55d5d81/packages/rspack-test-tools/tests/configCases/asset/_images/file.jpg",
 	import.meta.url
 );
 
 it("should work", () => {
-	expect(/[\da-f]{16}\.jpg$/.test(urlSvg)).toBe(true);
+	expect(/[\da-f]{16}\.png$/.test(url1)).toBe(true);
+	expect(/[\da-f]{16}\.jpg$/.test(url2)).toBe(true);
 });
