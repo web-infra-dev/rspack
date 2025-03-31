f76fd2105ebe96c74ecb5b32d6f33ceefb464b88
GiveMe-A-Name <a455300764b@hotmail.com>
2025-03-31 16:49:46 +0800

    chore: update swc to 18.0.0

--- packages/rspack-test-tools/tests/statsAPICases/basic.js
+++ packages/rspack-test-tools/tests/statsAPICases/basic.js
@@ -37,7 +37,7 @@ module.exports = {
 		  entry ./fixtures/a
 		  cjs self exports reference self [585] ./fixtures/a.js
 		  
-		Rspack compiled successfully (23417a360dbe1de8)
+		Rspack compiled successfully (2a8e79da1ef69229)
 	`);
 	}
 };
