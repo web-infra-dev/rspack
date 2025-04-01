676940512c24fbd904db69730a31af6e592208de
harpsealjs <lingyucoder@gmail.com>
2025-04-01 15:17:47 +0800

    refactor: improve storage error and result (#9859)
    refactor: storage error result

--- crates/rspack_storage/tests/lock.rs
+++ crates/rspack_storage/tests/lock.rs
@@ -168,7 +168,7 @@ mod test_storage_lock {
 
   #[tokio::test]
   #[cfg_attr(miri, ignore)]
-  async fn test_consume_lock() {
+  async fn test_consume_lock() -> Result<()> {
     let cases = [
       (
         get_native_path("test_lock_native"),
@@ -188,7 +188,7 @@ mod test_storage_lock {
         .await
         .expect("should remove temp root");
 
-      let _ = test_generate_lock(
+      test_generate_lock(
         "xxx",
         &root,
         &temp_root,
@@ -198,10 +198,9 @@ mod test_storage_lock {
           break_on: 3,
         }),
       )
-      .await
-      .map_err(|e| panic!("{}", e));
+      .await?;
 
-      let _ = test_recovery_lock(
+      test_recovery_lock(
         "xxx",
         &root,
         &temp_root,
@@ -211,14 +210,14 @@ mod test_storage_lock {
           break_on: 9999,
         }),
       )
-      .await
-      .map_err(|e| panic!("{}", e));
+      .await?;
     }
+    Ok(())
   }
 
   #[tokio::test]
   #[cfg_attr(miri, ignore)]
-  async fn test_consume_lock_failed() {
+  async fn test_consume_lock_failed() -> Result<()> {
     let cases = [
       (
         get_native_path("test_lock_fail_native"),
@@ -238,7 +237,7 @@ mod test_storage_lock {
         .await
         .expect("should remove temp root");
 
-      let _ = test_generate_lock(
+      test_generate_lock(
         "xxx",
         &root,
         &temp_root,
@@ -248,10 +247,9 @@ mod test_storage_lock {
           break_on: 3,
         }),
       )
-      .await
-      .map_err(|e| panic!("{}", e));
+      .await?;
 
-      let _ = test_recovery_lock_failed(
+      test_recovery_lock_failed(
         "xxx",
         &root,
         &temp_root.join("other"),
@@ -261,8 +259,8 @@ mod test_storage_lock {
           break_on: 9999,
         }),
       )
-      .await
-      .map_err(|e| panic!("{}", e));
+      .await?;
     }
+    Ok(())
   }
 }
