96f761431c139fd997928c1524b05282cec17be4
ahabhgk <ahabhgk@gmail.com>
2025-04-07 20:12:49 +0800

    fix: injecting custom runtime globals in runtimeRequirementInTree

--- packages/rspack-test-tools/tests/configCases/hooks/runtime-requirement-in-tree/rspack.config.js
+++ packages/rspack-test-tools/tests/configCases/hooks/runtime-requirement-in-tree/rspack.config.js
@@ -16,6 +16,17 @@ class Plugin {
 			);
 
 			const once = new Set();
+			const customRuntimeGlobal = "__webpack_require__.custom";
+			const { RuntimeModule } = compiler.webpack;
+
+			class CustomRuntimeModule extends RuntimeModule {
+				constructor() {
+					super("CustomRuntimeModule", RuntimeModule.STAGE_NORMAL);
+				}
+				generate() {
+					return `__webpack_require__.custom = 42;`;
+				}
+			}
 
 			compilation.hooks.runtimeRequirementInTree
 				.for(RuntimeGlobals.chunkName)
@@ -35,6 +46,15 @@ class Plugin {
 					once.add(RuntimeGlobals.chunkName);
 					expect(chunk.name).toBe("main");
 					expect(set.has(RuntimeGlobals.hasOwnProperty)).toBeTruthy();
+					set.add(customRuntimeGlobal);
+				});
+
+			compilation.hooks.runtimeRequirementInTree
+				.for(customRuntimeGlobal)
+				.tap("TestFakePlugin", (chunk, set) => {
+					expect(once.has(customRuntimeGlobal)).toBe(false);
+					once.add(customRuntimeGlobal);
+					compilation.addRuntimeModule(chunk, new CustomRuntimeModule());
 				});
 		});
 	}
