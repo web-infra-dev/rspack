ebcd23101220144fc94b58197b4f2d0b6a4add89
fi3ework <fi3ework@gmail.com>
2025-04-08 11:54:05 +0800

    test: use errors.js to test expected error

--- packages/rspack-test-tools/tests/configCases/library/modern-module-dynamic-error/rspack.config.js
+++ packages/rspack-test-tools/tests/configCases/library/modern-module-dynamic-error/rspack.config.js
@@ -23,22 +23,5 @@ module.exports = {
 		concatenateModules: true,
 		avoidEntryIife: true,
 		minimize: false
-	},
-	plugins: [
-		{
-			apply(compiler) {
-				compiler.hooks.done.tap("TestPlugin", stats => {
-					const errors = stats.toJson({
-						errors: true,
-						ids: true,
-						moduleTrace: true
-					}).errors;
-					expect(errors.length).toBe(1);
-					expect(errors[0].message).toMatch(
-						/Module not found: Can't resolve 'non_exist_dep' in/
-					);
-				});
-			}
-		}
-	]
+	}
 };
