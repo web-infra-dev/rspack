b4de0cbdada5534354d36234ba6cee172f92a82d
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-04-16 14:02:25 -0700

    chore(next): align devDependencies with workspace for typescript and @types packages

---
+++ examples/next/next.config.ts
@@ -0,0 +1,9 @@
+import withRspack from "next-rspack";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	/* config options here */
+	reactStrictMode: true
+};
+
+export default withRspack(nextConfig);
