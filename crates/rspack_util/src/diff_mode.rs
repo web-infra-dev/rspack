395023e4b85e6fbfa354689dcd10ae2d7a4230d9
quininer <quininer@live.com>
2025-04-21 18:16:23 +0800

    chore: trivial improve

--- crates/rspack_util/src/diff_mode.rs
+++ crates/rspack_util/src/diff_mode.rs
@@ -1,8 +1,8 @@
 use std::sync::LazyLock;
 
-static IS_DIFF_MODE: LazyLock<String> =
-  LazyLock::new(|| std::env::var("RSPACK_DIFF").ok().unwrap_or_default());
+static IS_DIFF_MODE: LazyLock<bool> =
+  LazyLock::new(|| std::env::var("RSPACK_DIFF").ok().as_deref() == Some("true"));
 
 pub fn is_diff_mode() -> bool {
-  *IS_DIFF_MODE == "true"
+  *IS_DIFF_MODE
 }
