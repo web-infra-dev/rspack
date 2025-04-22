e25650b662373c1725b3c18a1bb825b925f1106c
hardfist <yangjianzju@gmail.com>
2025-04-22 17:00:06 +0800

    chore: fix prettier

--- scripts/build-npm.cjs
+++ scripts/build-npm.cjs
@@ -80,7 +80,7 @@ const NPM = path.resolve(__dirname, "../npm");
 try {
 	// Error tolerant if the directory already exists
 	fs.mkdirSync(NPM);
-} catch (e) { }
+} catch (e) {}
 
 // Releasing bindings
 const releasingPackages = [];
@@ -128,7 +128,7 @@ for (const binding of bindings) {
 	const output = path.join(NPM, platformArchABI);
 	try {
 		fs.mkdirSync(output);
-	} catch (e) { }
+	} catch (e) {}
 
 	const coreJson = require(
 		path.resolve(__dirname, "../packages/rspack/package.json")
