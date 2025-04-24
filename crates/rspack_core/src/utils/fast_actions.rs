710e8b58742957e05b5508be69daa2ac6f12d8ae
hardfist <yangjianzju@gmail.com>
2025-04-24 12:08:40 +0800

    refactor: remove dead_code and use spawn_blocking for fast_set

--- crates/rspack_core/src/utils/fast_actions.rs
+++ crates/rspack_core/src/utils/fast_actions.rs
@@ -1,4 +1,6 @@
-use std::{mem, thread};
+use std::mem;
+
+use tokio::task::spawn_blocking;
 
 /// Fast set `src` into the referenced `dest`, and drop the old value in other thread
 ///
@@ -8,22 +10,7 @@ where
   T: Send + 'static,
 {
   let old = mem::replace(dest, src);
-  thread::Builder::new()
-    .name("fast_set".to_string())
-    .spawn(move || {
-      mem::drop(old);
-    })
-    .expect("spawn fast_set thread failed");
-}
-
-pub fn fast_drop<T>(src: T)
-where
-  T: Send + 'static,
-{
-  thread::Builder::new()
-    .name("fast_drop".to_string())
-    .spawn(move || {
-      mem::drop(src);
-    })
-    .expect("spawn fast_drop thread failed");
+  spawn_blocking(|| {
+    mem::drop(old);
+  });
 }
