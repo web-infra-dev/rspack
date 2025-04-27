2a4fcd4a5061f9cff381eb9f1d301ca274f0429b
ahabhgk <ahabhgk@gmail.com>
2025-04-27 20:38:17 +0800

    feat(incremental): add more tracing

--- crates/rspack_core/src/compiler/rebuild.rs
+++ crates/rspack_core/src/compiler/rebuild.rs
@@ -150,9 +150,9 @@ impl Compiler {
         new_compilation.chunk_hashes_artifact =
           std::mem::take(&mut self.compilation.chunk_hashes_artifact);
       }
-      if new_compilation
+      if dbg!(new_compilation
         .incremental
-        .mutations_readable(IncrementalPasses::CHUNKS_RENDER)
+        .mutations_readable(IncrementalPasses::CHUNKS_RENDER))
       {
         new_compilation.chunk_render_artifact =
           std::mem::take(&mut self.compilation.chunk_render_artifact);
