f4990250a2b955e00bd112040744fc95e3d10363
pshu <pishu.spf@antfin.com>
2025-04-29 16:45:50 +0800

    revert: ⏪ example changes

--- examples/basic/rspack.config.cjs
+++ examples/basic/rspack.config.cjs
@@ -2,21 +2,5 @@ module.exports = {
 	context: __dirname,
 	entry: {
 		main: "./index.js"
-	},
-	module: {
-		parser: {
-			css: {
-				url: false
-			},
-			"css/auto": {
-				url: false
-			},
-			"css/module": {
-				url: false
-			}
-		}
-	},
-	experiments: {
-		css: true
 	}
 };
