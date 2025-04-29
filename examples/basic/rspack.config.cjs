4d3c5530e17fdae4eefb225222c03496f9a2b25e
pshu <pishu.spf@antfin.com>
2025-04-29 14:50:09 +0800

    chore: ✏️ update docs

--- examples/basic/rspack.config.cjs
+++ examples/basic/rspack.config.cjs
@@ -2,5 +2,21 @@ module.exports = {
 	context: __dirname,
 	entry: {
 		main: "./index.js"
+	},
+	module: {
+		parser: {
+			css: {
+				url: false
+			},
+			"css/auto": {
+				url: false
+			},
+			"css/module": {
+				url: false
+			}
+		}
+	},
+	experiments: {
+		css: true
 	}
 };
