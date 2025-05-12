ff0a23b132293a23c32e0cd75151fd303f14d8f5
Max Coplan <mchcopl@gmail.com>
2025-05-11 19:52:20 -0700

    fix(template-react-ts): work with newer Node (#10304)
    fix(create): fix starter template
    
    Co-authored-by: hardfist <yangjianzju@gmail.com>

--- packages/create-rspack/template-vue-js/rspack.config.mjs
+++ packages/create-rspack/template-vue-js/rspack.config.mjs
@@ -7,10 +7,9 @@ import { VueLoaderPlugin } from "vue-loader";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Target browsers, see: https://github.com/browserslist/browserslist
-const targets = ["last 2 versions", "> 0.2%",  "not dead",  "Firefox ESR"];
+const targets = ["last 2 versions", "> 0.2%", "not dead", "Firefox ESR"];
 
 export default defineConfig({
-	context: __dirname,
 	entry: {
 		main: "./src/main.js"
 	},
