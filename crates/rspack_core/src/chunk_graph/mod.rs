a060ffb5c233b97fcd2afc529dac9001ee11b188
jserfeng <1114550440@qq.com>
2025-05-13 19:34:11 +0800

    feat: add chunk link relation

--- crates/rspack_core/src/chunk_graph/mod.rs
+++ crates/rspack_core/src/chunk_graph/mod.rs
@@ -1,10 +1,11 @@
 use core::fmt;
 use std::{borrow::Cow, collections::HashSet};
 
+use indexmap::IndexSet;
 use itertools::Itertools;
-use rspack_collections::{IdentifierMap, UkeyMap};
-use rspack_util::env::has_query;
-use rustc_hash::FxHashMap as HashMap;
+use rspack_collections::{IdentifierMap, UkeyIndexMap, UkeyMap};
+use rspack_util::{atom::Atom, env::has_query};
+use rustc_hash::{FxHashMap as HashMap, FxHashSet as HashSet};
 
 use crate::{
   AsyncDependenciesBlockIdentifier, ChunkGroupUkey, ChunkUkey, Compilation, ModuleIdentifier,
@@ -16,6 +17,12 @@ pub use chunk_graph_chunk::{ChunkGraphChunk, ChunkSizeOptions};
 pub use chunk_graph_module::{ChunkGraphModule, ModuleId};
 
 #[derive(Debug, Clone, Default)]
+pub struct ChunkLink {
+  pub imports: UkeyIndexMap<ChunkUkey, IdentifierMap<IndexSet<Atom>>>,
+  pub exports: IdentifierMap<HashSet<Atom>>,
+}
+
+#[derive(Debug, Clone, Default)]
 pub struct ChunkGraph {
   /// If a module is imported dynamically, it will be assigned to a unique ChunkGroup
   pub(crate) block_to_chunk_group_ukey: HashMap<AsyncDependenciesBlockIdentifier, ChunkGroupUkey>,
@@ -24,6 +31,9 @@ pub struct ChunkGraph {
   chunk_graph_chunk_by_chunk_ukey: UkeyMap<ChunkUkey, ChunkGraphChunk>,
 
   runtime_ids: HashMap<String, Option<String>>,
+
+  // only used for esm output
+  pub link: Option<UkeyMap<ChunkUkey, ChunkLink>>,
 }
 
 impl ChunkGraph {
