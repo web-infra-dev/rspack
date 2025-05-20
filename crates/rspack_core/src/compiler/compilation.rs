c220aa4dc83aee10e8cb170043921f74b66cc462
jserfeng <1114550440@qq.com>
2025-05-20 14:18:14 +0800

    feat: render chunk

--- crates/rspack_core/src/compiler/compilation.rs
+++ crates/rspack_core/src/compiler/compilation.rs
@@ -1523,14 +1523,6 @@ impl Compilation {
         .generate_dot(compilation, "after-code-splitting");
       logger.time_end(start);
 
-      {
-        let mut count = 0;
-        for (ukey, chunk) in compilation.chunk_by_ukey.iter() {
-          let modules = compilation.chunk_graph.get_chunk_modules_identifier(ukey);
-          count += modules.len();
-        }
-        dbg!(count);
-      }
       Ok(compilation)
     })
     .await?;
