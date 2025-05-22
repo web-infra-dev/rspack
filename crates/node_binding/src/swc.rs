1890d8f93add47bfb86fc2eb1b6401521fcb11a8
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2025-05-22 14:22:47 +0800

    fix: swc compiler api using wasm plugin without tokio runtime panic. (#10438)
    * fix: async transform and minify fn in napi
    
    - napi will add tokio runtime auto in async fn, and swc wasm runtime need runnning in tokio runtime.
    
    * refactor: replace the closure with the function itself: `TransformOutput::from`
    
    * test: add swc api test case for @swc/plugin-remove-console

--- crates/node_binding/src/swc.rs
+++ crates/node_binding/src/swc.rs
@@ -1,4 +1,3 @@
-use napi::{bindgen_prelude::AsyncTask, Task};
 use rspack_javascript_compiler::{
   minify::JsMinifyOptions, transform::SwcOptions, JavaScriptCompiler,
   TransformOutput as CompilerTransformOutput,
@@ -24,80 +23,42 @@ impl From<CompilerTransformOutput> for TransformOutput {
   }
 }
 
-pub struct AsyncTransform {
-  source: String,
-  options: String,
+#[napi]
+pub async fn transform(source: String, options: String) -> napi::Result<TransformOutput> {
+  let options: SwcOptions = serde_json::from_str(&options)?;
+  let compiler = JavaScriptCompiler::new();
+  compiler
+    .transform(
+      source,
+      Some(swc_core::common::FileName::Anon),
+      options,
+      None,
+      |_| noop_pass(),
+    )
+    .map(TransformOutput::from)
+    .map_err(|e| napi::Error::new(napi::Status::GenericFailure, format!("{}", e)))
 }
 
-impl Task for AsyncTransform {
-  type Output = CompilerTransformOutput;
-  type JsValue = TransformOutput;
+#[napi]
+pub async fn minify(source: String, options: String) -> napi::Result<TransformOutput> {
+  let options: JsMinifyOptions = serde_json::from_str(&options)?;
+  let compiler = JavaScriptCompiler::new();
+  compiler
+    .minify(
+      swc_core::common::FileName::Anon,
+      source,
+      options,
+      None::<&dyn Fn(&swc_core::common::comments::SingleThreadedComments)>,
+    )
+    .map(TransformOutput::from)
+    .map_err(|e| {
+      let v = e.into_inner();
+      let err = v
+        .into_iter()
+        .map(|e| format!("{:?}", e))
+        .collect::<Vec<_>>()
+        .join("\n");
 
-  fn compute(&mut self) -> napi::Result<Self::Output> {
-    let compiler = JavaScriptCompiler::new();
-    let options: SwcOptions = serde_json::from_str(&self.options)?;
-    let source = std::mem::take(&mut self.source);
-    compiler
-      .transform(
-        source,
-        Some(swc_core::common::FileName::Anon),
-        options,
-        None,
-        |_| noop_pass(),
-      )
-      .map_err(|e| napi::Error::new(napi::Status::GenericFailure, format!("{}", e)))
-  }
-
-  fn resolve(&mut self, _env: napi::Env, output: Self::Output) -> napi::Result<Self::JsValue> {
-    let output = TransformOutput::from(output);
-    Ok(output)
-  }
-}
-
-pub struct AsyncMinifier {
-  source: String,
-  options: String,
-}
-
-impl Task for AsyncMinifier {
-  type Output = CompilerTransformOutput;
-  type JsValue = TransformOutput;
-
-  fn compute(&mut self) -> napi::Result<Self::Output> {
-    let options: JsMinifyOptions = serde_json::from_str(&self.options)?;
-    let compiler = JavaScriptCompiler::new();
-    let source = std::mem::take(&mut self.source);
-    compiler
-      .minify(
-        swc_core::common::FileName::Anon,
-        source,
-        options,
-        None::<&dyn Fn(&swc_core::common::comments::SingleThreadedComments)>,
-      )
-      .map_err(|e| {
-        let v = e.into_inner();
-        let err = v
-          .into_iter()
-          .map(|e| format!("{:?}", e))
-          .collect::<Vec<_>>()
-          .join("\n");
-
-        napi::Error::new(napi::Status::GenericFailure, err)
-      })
-  }
-
-  fn resolve(&mut self, _env: napi::Env, output: Self::Output) -> napi::Result<Self::JsValue> {
-    let output = TransformOutput::from(output);
-    Ok(output)
-  }
-}
-
-#[napi(ts_return_type = "Promise<TransformOutput>")]
-pub fn transform(source: String, options: String) -> AsyncTask<AsyncTransform> {
-  AsyncTask::new(AsyncTransform { source, options })
-}
-
-#[napi(ts_return_type = "Promise<TransformOutput>")]
-pub fn minify(source: String, options: String) -> AsyncTask<AsyncMinifier> {
-  AsyncTask::new(AsyncMinifier { source, options })
+      napi::Error::new(napi::Status::GenericFailure, err)
+    })
 }
