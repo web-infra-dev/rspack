40f1489418fc177cad98fa5036db4063d8f7a312
Cong-Cong Pan <dacongsama@live.com>
2025-05-30 18:52:02 +0800

    feat: support dependences in build info (#10531)

--- packages/rspack-test-tools/tests/configCases/module/build-info/loader.js
+++ packages/rspack-test-tools/tests/configCases/module/build-info/loader.js
@@ -2,5 +2,6 @@ module.exports = function (content) {
     this._module.buildInfo.loaded = true;
 
     this.emitFile("foo.txt", "foo");
+    this.addBuildDependency("./build.txt");
     return content;
 };
