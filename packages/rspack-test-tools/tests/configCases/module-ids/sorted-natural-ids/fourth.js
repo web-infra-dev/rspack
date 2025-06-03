86b174eb9ad957d7dcac9f732f3282cc125a159b
pshu <stormslowly@gmail.com>
2025-06-03 17:05:15 +0800

    Revert "fix(core): use module_id to sort module_code (#10516)"
    This reverts commit 8450da9865accdbbb999b39415a16c1992e3457e.

--- packages/rspack-test-tools/tests/configCases/module-ids/sorted-natural-ids/fourth.js
+++
@@ -1,1 +0,0 @@
-console.log('4 module');
--- packages/rspack-test-tools/tests/configCases/module-ids/sorted-natural-ids/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const webpack = require("@rspack/core");
-
-/** @type {import("@rspack/core").Configuration} */
-module.exports = {
-	optimization: {
-		moduleIds: "natural"
-	}
-};
