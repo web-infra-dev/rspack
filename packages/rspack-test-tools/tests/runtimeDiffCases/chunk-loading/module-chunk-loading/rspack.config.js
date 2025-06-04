513c75f3f4ab36d2341ca8f22d28c4867db266b1
GiveMe-A-Name <a455300764b@hotmail.com>
2025-06-04 10:36:43 +0800

    chore: bump swc version to 26.3.4

--- packages/rspack-test-tools/tests/runtimeDiffCases/chunk-loading/module-chunk-loading/rspack.config.js
+++ packages/rspack-test-tools/tests/runtimeDiffCases/chunk-loading/module-chunk-loading/rspack.config.js
@@ -7,12 +7,33 @@ module.exports = {
 		filename: "[name].js",
 		chunkLoading: "import",
 		chunkFormat: "module",
-		enabledChunkLoadingTypes: ["import"]
+		enabledChunkLoadingTypes: ["import"],
+		environment: {
+			arrowFunction: false,
+			bigIntLiteral: false,
+			const: false,
+			destructuring: false,
+			dynamicImport: false,
+			dynamicImportInWorker: false,
+			forOf: false,
+			globalThis: false,
+			module: false,
+			optionalChaining: false,
+			templateLiteral: false
+		}
 	},
 	optimization: {
 		runtimeChunk: {
 			name: "bundle"
-		}
+		},
+		minimize: false,
+		chunkIds: "named",
+		moduleIds: "named",
+		mangleExports: false,
+		concatenateModules: true
 	},
-	target: "node"
+
+	devtool: false,
+	target: "node",
+	mode: "development"
 };
