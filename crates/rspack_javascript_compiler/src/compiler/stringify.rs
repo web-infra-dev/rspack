0b4cbecae7818079920f0ea77d44674faaec5557
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2025-06-04 17:11:43 +0800

    chore: bump swc version to 26.3.4 (#10566)
    * chore: bump swc version to 26.3.4
    
    * refactor: update JSON parser options in CompilerOptions
    
    * refactor: remove unused configuration options in rspack.config.js

--- crates/rspack_javascript_compiler/src/compiler/stringify.rs
+++ crates/rspack_javascript_compiler/src/compiler/stringify.rs
@@ -166,7 +166,7 @@ impl JavaScriptCompiler {
 
     let map = if source_map_config.enable {
       let combined_source_map =
-        source_map.build_source_map_with_config(&src_map_buf, input_source_map, source_map_config);
+        source_map.build_source_map(&src_map_buf, input_source_map.cloned(), source_map_config);
 
       let mappings = encode_mappings(combined_source_map.tokens().map(|token| Mapping {
         generated_line: token.get_dst_line() + 1,
