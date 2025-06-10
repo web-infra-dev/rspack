6dc3414dc5df46e67554c1324d5a50fddd201746
ahabhgk <ahabhgk@gmail.com>
2025-06-10 19:24:23 +0800

    feat(incremental): enable advance-slient by default

--- packages/rspack-test-tools/tests/Incremental-web.test.js
+++ packages/rspack-test-tools/tests/Incremental-web.test.js
@@ -4,22 +4,22 @@ process.env.RSPACK_CONFIG_VALIDATE = "loose-silent";
 const path = require("path");
 const {
 	describeByWalk,
-	createHotNewIncrementalCase
+	createHotIncrementalCase
 } = require("@rspack/test-tools");
 
 function v(name) {
-	return path.join(__dirname, `new-incremental ${name}`);
+	return path.join(__dirname, `incremental ${name}`);
 }
 
 // Run tests rspack-test-tools/tests/hotCases in target web
 describeByWalk(
 	v("hot web"),
 	(name, src, dist) => {
-		createHotNewIncrementalCase(name, src, dist, "web", false);
+		createHotIncrementalCase(name, src, dist, "web", false);
 	},
 	{
 		source: path.resolve(__dirname, "./hotCases"),
-		dist: path.resolve(__dirname, `./js/new-incremental/hot-web`)
+		dist: path.resolve(__dirname, `./js/incremental/hot-web`)
 	}
 );
 
@@ -27,11 +27,11 @@ describeByWalk(
 describeByWalk(
 	v("hot web (webpack-test)"),
 	(name, src, dist) => {
-		createHotNewIncrementalCase(name, src, dist, "web", true);
+		createHotIncrementalCase(name, src, dist, "web", true);
 	},
 	{
 		source: path.resolve(__dirname, "../../../tests/webpack-test/hotCases"),
-		dist: path.resolve(__dirname, `./js/new-incremental/webpack-test/hot-web`),
+		dist: path.resolve(__dirname, `./js/incremental/webpack-test/hot-web`),
 		exclude: [
 			// there is a self reference module in this case causing the make phase didn't found the module is removed
 			/require-disposed-module-warning/
