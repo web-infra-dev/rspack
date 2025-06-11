c378699dec67555120da74480f1da675bbf03ddc
CPunisher <1343316114@qq.com>
2025-06-11 19:26:01 +0800

    Module 10000 -> module 1000

--- tasks/benchmark/benches/benches.rs
+++ tasks/benchmark/benches/benches.rs
@@ -1,8 +1,8 @@
 #![allow(clippy::unwrap_used)]
 
 use criterion::criterion_main;
-use groups::{basic_build::basic, build_chunk_graph::chunk_graph, modules_10000::modules_10000};
+use groups::{basic_build::basic, build_chunk_graph::chunk_graph, modules_1000::modules_1000};
 
 mod groups;
 
-criterion_main!(basic, chunk_graph, modules_10000);
+criterion_main!(basic, chunk_graph, modules_1000);
