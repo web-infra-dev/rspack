2fed29ad04eb08ad0df1a19bfe93610132a6edf2
Cong-Cong <dacongsama@live.com>
2025-06-12 18:45:45 +0800

    add test case

---
+++ packages/rspack-test-tools/tests/watchCases/compilation/remove-module/rspack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+class Plugin {
+    apply(compiler) {
+        const moduleMap = new Map();
+
+        compiler.hooks.compilation.tap("PLUGIN", compilation => {
+            compilation.hooks.finishModules.tap("PLUGIN", modules => {
+                for (const module of modules) {
+                    if (moduleMap.has(module.resource)) {
+                        const timestamp = moduleMap.get(module.resource);
+                        // index.js only run loader by once.
+                        expect(module.buildInfo.timestamp).toBe(timestamp);
+                    } else {
+                        moduleMap.set(module.resource, module.buildInfo.timestamp);
+                    }
+                }
+            });
+        });
+
+        compiler.hooks.done.tap("PLUGIN", stats => {
+            // Ignore: Module not found: Can't resolve "./foo.js"
+            stats.compilation.errors = [];
+        });
+    }
+}
+
+module.exports = {
+    plugins: [
+        new Plugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: path.join(__dirname, "loader.js"),
+                    }
+                ]
+            }
+        ]
+    }
+};
+
