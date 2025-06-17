44909f1d6ce33bebe7ea82fa38434816cde461af
CPunisher <1343316114@qq.com>
2025-06-17 16:21:51 +0800

    Refactor builder

--- tasks/benchmark/benches/groups/bundle.rs
+++ tasks/benchmark/benches/groups/bundle.rs
@@ -1,9 +1,9 @@
-use std::sync::Arc;
+use std::{path::PathBuf, sync::Arc};
 
 use criterion::{criterion_group, Criterion};
 use rspack::builder::{Builder, CompilerBuilder};
 use rspack_core::{
-  Compiler, Experiments, ModuleOptions, ModuleRule, ModuleRuleEffect, ModuleRuleUse,
+  Compiler, Experiments, Mode, ModuleOptions, ModuleRule, ModuleRuleEffect, ModuleRuleUse,
   ModuleRuleUseLoader, Resolve, RuleSetCondition,
 };
 use rspack_fs::{MemoryFileSystem, NativeFileSystem};
@@ -48,10 +48,24 @@ fn bundle_benchmark(c: &mut Criterion) {
   }
 }
 
-fn basic_compiler_builder() -> CompilerBuilder {
+struct BuilderOptions {
+  project: &'static str,
+  entry: &'static str,
+  is_production: bool,
+}
+
+fn basic_compiler_builder(options: BuilderOptions) -> CompilerBuilder {
   let mut builder = Compiler::builder();
 
+  let dir = PathBuf::from(env!("CARGO_WORKSPACE_DIR"))
+    .join(".bench/rspack-benchcases")
+    .canonicalize()
+    .unwrap()
+    .join(&options.project);
+
   builder
+    .context(dir.to_string_lossy().to_string())
+    .entry("main", options.entry)
     .module(ModuleOptions::builder().rule(ModuleRule {
       test: Some(RuleSetCondition::Regexp(
         RspackRegex::new("\\.(j|t)s(x)?$").unwrap(),
@@ -86,6 +100,12 @@ fn basic_compiler_builder() -> CompilerBuilder {
     .input_filesystem(Arc::new(NativeFileSystem::new(false)))
     .output_filesystem(Arc::new(MemoryFileSystem::default()))
     .enable_loader_swc();
+
+  if options.is_production {
+    builder.mode(Mode::Production);
+  } else {
+    builder.mode(Mode::Development);
+  }
 
   builder
 }
--- tasks/benchmark/benches/groups/bundle/threejs.rs
+++ tasks/benchmark/benches/groups/bundle/threejs.rs
@@ -1,27 +1,13 @@
-use std::path::PathBuf;
+use rspack_core::Compiler;
 
-use rspack_core::{Compiler, Mode};
+use crate::groups::bundle::{basic_compiler_builder, BuilderOptions};
 
-use crate::groups::bundle::basic_compiler_builder;
-
-pub fn compiler(production: bool) -> Compiler {
-  let mut builder = basic_compiler_builder();
-
-  let dir = PathBuf::from(env!("CARGO_WORKSPACE_DIR"))
-    .join(".bench/rspack-benchcases")
-    .canonicalize()
-    .unwrap()
-    .join("threejs");
-
-  builder
-    .context(dir.to_string_lossy().to_string())
-    .entry("main", "./src/Three.js");
-
-  if production {
-    builder.mode(Mode::Production);
-  } else {
-    builder.mode(Mode::Development);
-  }
+pub fn compiler(is_production: bool) -> Compiler {
+  let mut builder = basic_compiler_builder(BuilderOptions {
+    project: "threejs",
+    entry: "./src/Three.js",
+    is_production,
+  });
 
   builder.build().unwrap()
 }
