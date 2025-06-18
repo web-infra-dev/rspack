02970a51954c3beee291ca6e72f2b4ba197f0c72
Hana <andywangsy@gmail.com>
2025-06-18 11:37:22 +0800

    chore(benchmark): add rspack benchcases  (#9345)
    * test: init
    
    * chore: verbose
    
    * feat: split
    
    * Fix clippy and ra
    
    * Fix pnpm-lock
    
    * Remove workspace
    
    * Ignore rspack_cacheable
    
    * Fix test
    
    * Update setup.mjs
    
    * Module 10000 -> module 1000
    
    * Add dbg
    
    * println
    
    * set jobs=2 to avoid oom
    
    * set jobs=1
    
    * Remove flags
    
    * Use codspeed 2.7.2
    
    * Remove flags
    
    * Disable minimize
    
    * Test disable lto
    
    * Revert codspeed version
    
    * Use CARGO_WORKSPACE_DIR
    
    * env! CARGO_WORKSPACE_DIR
    
    * Add dbg
    
    * More println
    
    * Limit max blocking threads
    
    * Bundle
    
    * Add threejs
    
    * Refactor builder
    
    * 1000 -> Basic react
    
    * Derive projects
    
    * refactor: use self hosted
    
    * Clippy
    
    * Enable lto
    
    * Pass runner
    
    ---------
    
    Co-authored-by: CPunisher <1343316114@qq.com>
    Co-authored-by: pshu <pishu.spf@antfin.com>
    Co-authored-by: pshu <stormslowly@gmail.com>

---
+++ tasks/benchmark/benches/groups/bundle.rs
@@ -0,0 +1,39 @@
+use std::sync::Arc;
+
+use criterion::{criterion_group, Criterion};
+use tokio::runtime;
+
+use crate::groups::bundle::util::{derive_projects, CompilerBuilderGenerator};
+
+pub mod basic_react;
+pub mod threejs;
+pub mod util;
+
+criterion_group!(bundle, bundle_benchmark);
+
+fn bundle_benchmark(c: &mut Criterion) {
+  let mut group = c.benchmark_group("bundle");
+
+  #[cfg(feature = "codspeed")]
+  group.sample_size(10);
+
+  let projects: Vec<(&'static str, CompilerBuilderGenerator)> = vec![
+    ("basic-react", Arc::new(basic_react::compiler)),
+    ("threejs", Arc::new(threejs::compiler)),
+  ];
+
+  // Codspeed can only handle to up to 500 threads by default
+  let rt = runtime::Builder::new_multi_thread()
+    .max_blocking_threads(256)
+    .build()
+    .unwrap();
+
+  for (id, get_compiler) in derive_projects(projects) {
+    group.bench_function(format!("bundle@{id}"), |b| {
+      b.to_async(&rt).iter(|| async {
+        let mut compiler = get_compiler();
+        compiler.build().unwrap().run().await.unwrap();
+      });
+    });
+  }
+}
---
+++ tasks/benchmark/benches/groups/bundle/util.rs
@@ -0,0 +1,118 @@
+use std::{path::PathBuf, sync::Arc};
+
+use rspack::builder::{Builder, CompilerBuilder};
+use rspack_core::{
+  Compiler, Experiments, Mode, ModuleOptions, ModuleRule, ModuleRuleEffect, ModuleRuleUse,
+  ModuleRuleUseLoader, Resolve, RuleSetCondition,
+};
+use rspack_fs::{MemoryFileSystem, NativeFileSystem};
+use rspack_regex::RspackRegex;
+use serde_json::json;
+
+// Because `CompilerBuilder` is not `Clone`
+pub type CompilerBuilderGenerator = Arc<dyn Fn() -> CompilerBuilder + Send + Sync>;
+
+pub struct BuilderOptions {
+  pub project: &'static str,
+  pub entry: &'static str,
+}
+
+pub fn basic_compiler_builder(options: BuilderOptions) -> CompilerBuilder {
+  let mut builder = Compiler::builder();
+
+  let dir = PathBuf::from(env!("CARGO_WORKSPACE_DIR"))
+    .join(".bench/rspack-benchcases")
+    .canonicalize()
+    .unwrap()
+    .join(options.project);
+
+  builder
+    .context(dir.to_string_lossy().to_string())
+    .entry("main", options.entry)
+    .module(ModuleOptions::builder().rule(ModuleRule {
+      test: Some(RuleSetCondition::Regexp(
+        RspackRegex::new("\\.(j|t)s(x)?$").unwrap(),
+      )),
+      effect: ModuleRuleEffect {
+        r#use: ModuleRuleUse::Array(vec![ModuleRuleUseLoader {
+        loader: "builtin:swc-loader".to_string(),
+        options: Some(json!({
+            "jsc": {
+                "parser": {
+                    "syntax": "typescript",
+                    "tsx": true,
+                },
+                "transform": {
+                    "react": {
+                        "runtime": "automatic",
+                    },
+                }
+            },
+        }).to_string()),
+      }]),
+        ..Default::default()
+      },
+      ..Default::default()
+    }))
+    .cache(rspack_core::CacheOptions::Disabled)
+    .resolve(Resolve {
+      extensions: Some(vec!["...".to_string(), ".jsx".to_string()]),
+      ..Default::default()
+    })
+    .experiments(Experiments::builder().css(true))
+    .input_filesystem(Arc::new(NativeFileSystem::new(false)))
+    .output_filesystem(Arc::new(MemoryFileSystem::default()))
+    .enable_loader_swc();
+
+  builder
+}
+
+pub fn derive_projects(
+  projects: Vec<(&'static str, CompilerBuilderGenerator)>,
+) -> Vec<(String, CompilerBuilderGenerator)> {
+  projects
+    .into_iter()
+    .flat_map(|(name, builder)| {
+      let mut projects = Vec::new();
+
+      {
+        let builder = builder.clone();
+        projects.push((
+          format!("{name}-development"),
+          Arc::new(move || {
+            let mut builder = builder();
+            builder.mode(Mode::Development);
+            builder
+          }) as CompilerBuilderGenerator,
+        ));
+      }
+
+      {
+        let builder = builder.clone();
+        projects.push((
+          format!("{name}-production"),
+          Arc::new(move || {
+            let mut builder = builder();
+            builder.mode(Mode::Production);
+            builder
+          }),
+        ));
+      }
+
+      {
+        let builder = builder.clone();
+        projects.push((
+          format!("{name}-production-sourcemap"),
+          Arc::new(move || {
+            let mut builder = builder();
+            builder.mode(Mode::Production);
+            builder.devtool(rspack::builder::Devtool::SourceMap);
+            builder
+          }),
+        ));
+      }
+
+      projects
+    })
+    .collect()
+}
