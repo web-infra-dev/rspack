7fad076d53a0df02a2ac2c9573f1a0a59880469d
Cong-Cong Pan <dacongsama@live.com>
2025-06-24 19:04:04 +0800

    feat: css chunking plugin custom maxSize and minSize options (#10773)

--- crates/rspack_binding_api/src/raw_options/raw_builtins/css_chunking.rs
+++ crates/rspack_binding_api/src/raw_options/raw_builtins/css_chunking.rs
@@ -3,6 +3,8 @@ use rspack_regex::RspackRegex;
 #[napi(object, object_to_js = false)]
 pub struct CssChunkingPluginOptions {
   pub strict: Option<bool>,
+  pub min_size: Option<f64>,
+  pub max_size: Option<f64>,
   #[napi(ts_type = "RegExp")]
   pub exclude: Option<RspackRegex>,
 }
@@ -11,6 +13,8 @@ impl From<CssChunkingPluginOptions> for rspack_plugin_css_chunking::CssChunkingP
   fn from(options: CssChunkingPluginOptions) -> Self {
     Self {
       strict: options.strict.unwrap_or(false),
+      min_size: options.min_size,
+      max_size: options.max_size,
       exclude: options.exclude,
     }
   }
