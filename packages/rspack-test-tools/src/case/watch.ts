15a5b67cf0ce231528fc1bc36e08fe977343b69a
GiveMe-A-Name <a455300764b@hotmail.com>
2025-06-27 13:35:08 +0800

    Remove unused import

--- packages/rspack-test-tools/src/case/watch.ts
+++ packages/rspack-test-tools/src/case/watch.ts
@@ -1,7 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
 
-import process from "node:process";
 import { WatchProcessor, WatchStepProcessor } from "../processor/watch";
 import { WatchRunnerFactory } from "../runner";
 import { BasicCaseCreator } from "../test/creator";
