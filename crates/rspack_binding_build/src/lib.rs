124aacbf634ec6710158cee89a8b34a291fae324
CPunisher <1343316114@qq.com>
2025-06-30 10:17:57 +0800

    Add comment

--- crates/rspack_binding_build/src/lib.rs
+++ crates/rspack_binding_build/src/lib.rs
@@ -1,5 +1,7 @@
 pub fn setup() {
   napi_build::setup();
+
+  // Remove this after releasing https://github.com/napi-rs/napi-rs/pull/2748
   #[cfg(target_family = "wasm")]
   println!("cargo:rustc-link-arg=-zstack-size=64000000");
 }
