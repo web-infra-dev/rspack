b1512ed791ff3eb20ff5ba4b541a278d981de9a9
CPunisher <1343316114@qq.com>
2025-06-30 18:13:57 +0800

    Shim node builtins

--- crates/node_binding/rspack.wasi-browser.js
+++ crates/node_binding/rspack.wasi-browser.js
@@ -5,7 +5,6 @@ import {
   WASI as __WASI,
 } from '@napi-rs/wasm-runtime'
 import { memfs } from '@napi-rs/wasm-runtime/fs'
-import __wasmUrl from './rspack.wasm32-wasi.wasm?url'
 
 export const { fs: __fs, vol: __volume } = memfs()
 
@@ -17,6 +16,7 @@ const __wasi = new __WASI({
   },
 })
 
+const __wasmUrl = new URL('./rspack.wasm32-wasi.wasm', import.meta.url).href
 const __emnapiContext = __emnapiGetDefaultContext()
 
 const __sharedMemory = new WebAssembly.Memory({
