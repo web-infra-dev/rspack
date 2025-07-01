e561eb7111691bb01d1ac6a900b4374d7b053242
Cong-Cong <dacongsama@live.com>
2025-07-01 20:22:16 +0800

    remove in_make_stage in JsCleanupPlugin

--- crates/rspack_binding_api/src/plugins/js_cleanup_plugin.rs
+++ crates/rspack_binding_api/src/plugins/js_cleanup_plugin.rs
@@ -1,14 +1,11 @@
-use std::sync::atomic::{AtomicBool, Ordering};
-
 use async_trait::async_trait;
 use derive_more::Debug;
 use napi::{bindgen_prelude::External, threadsafe_function::ThreadsafeFunction, Status};
 use rspack_collections::IdentifierSet;
 use rspack_core::{
-  ApplyContext, Compilation, CompilationRevokedModules, CompilerFinishMake, CompilerMake,
-  CompilerOptions, ModuleIdentifier, PluginContext,
+  ApplyContext, CompilationRevokedModules, CompilerOptions, ModuleIdentifier, PluginContext,
 };
-use rspack_error::{Result, ToStringResultToRspackResultExt};
+use rspack_error::ToStringResultToRspackResultExt;
 use rspack_hook::{plugin, plugin_hook};
 
 pub type CleanupRevokedModulesTsFn = ThreadsafeFunction<
@@ -26,12 +23,11 @@ pub type CleanupRevokedModulesTsFn = ThreadsafeFunction<
 pub struct JsCleanupPlugin {
   #[debug(skip)]
   cleanup_revoked_modules: CleanupRevokedModulesTsFn,
-  in_make_stage: AtomicBool,
 }
 
 impl JsCleanupPlugin {
   pub fn new(cleanup_revoked_modules: CleanupRevokedModulesTsFn) -> Self {
-    Self::new_inner(cleanup_revoked_modules, AtomicBool::new(false))
+    Self::new_inner(cleanup_revoked_modules)
   }
 }
 
@@ -52,29 +48,12 @@ impl rspack_core::Plugin for JsCleanupPlugin {
       .revoked_modules
       .tap(revoked_modules::new(self));
 
-    ctx.context.compiler_hooks.make.tap(make::new(self));
-
-    ctx
-      .context
-      .compiler_hooks
-      .finish_make
-      .tap(finish_make::new(self));
-
     Ok(())
   }
 }
 
-#[plugin_hook(CompilerMake for JsCleanupPlugin)]
-async fn make(&self, _compilation: &mut Compilation) -> Result<()> {
-  self.in_make_stage.store(true, Ordering::Relaxed);
-  Ok(())
-}
-
 #[plugin_hook(CompilationRevokedModules for JsCleanupPlugin)]
 async fn revoked_modules(&self, revoked_modules: &IdentifierSet) -> rspack_error::Result<()> {
-  if !self.in_make_stage.load(Ordering::Relaxed) {
-    return Ok(());
-  }
   self
     .cleanup_revoked_modules
     .call_async(External::new(
@@ -82,11 +61,5 @@ async fn revoked_modules(&self, revoked_modules: &IdentifierSet) -> rspack_error
     ))
     .await
     .to_rspack_result()?;
-  Ok(())
-}
-
-#[plugin_hook(CompilerFinishMake for JsCleanupPlugin)]
-async fn finish_make(&self, _compilation: &mut Compilation) -> Result<()> {
-  self.in_make_stage.store(false, Ordering::Relaxed);
   Ok(())
 }
