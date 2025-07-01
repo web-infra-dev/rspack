ebc01aa58a468a8684ece2aefc3ce23e3803ddaa
CPunisher <1343316114@qq.com>
2025-07-01 15:42:23 +0800

    Export fs and volume

--- packages/rspack/src/browser/index.ts
+++ packages/rspack/src/browser/index.ts
@@ -3,3 +3,9 @@ import { rspack } from "../rspack";
 export * from "../exports";
 export default rspack;
 export { rspack };
+
+// @ts-ignore: shimmed with rspack.wasi-browser.js
+import { __fs, __volume } from "@rspack/binding";
+import type { IFs, Volume } from "memfs";
+export const fs: IFs = __fs;
+export const volume: Volume = __volume;
