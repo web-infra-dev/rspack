ab8603dc8f9e11b75362a80a8336d16ec8b211d0
CPunisher <1343316114@qq.com>
2025-07-01 16:33:10 +0800

    Shim memfs

--- packages/rspack/src/browser/index.ts
+++ packages/rspack/src/browser/index.ts
@@ -1,11 +1,6 @@
 import { rspack } from "../rspack";
 
 export * from "../exports";
-export default rspack;
 export { rspack };
-
-// @ts-ignore: shimmed with rspack.wasi-browser.js
-import { __fs, __volume } from "@rspack/binding";
-import type { IFs, Volume } from "memfs";
-export const fs: IFs = __fs;
-export const volume: Volume = __volume;
+export { fs, volume } from "./fs";
+export default rspack;
