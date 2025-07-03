7a7548df12890677b206070f82cea1bdd6ede8ad
hardfist <yangjianzju@gmail.com>
2025-07-03 16:58:37 +0800

    chore: add comment

--- crates/rspack_util/src/debug_tool.rs
+++ crates/rspack_util/src/debug_tool.rs
@@ -1,3 +1,5 @@
+// wait for user input to continue, useful for debugging and profiling purposes
+// inspired by https://github.com/rust-lang/rust-analyzer/blob/661e7d2ac245f4fca099d982544b3c5408322867/crates/rust-analyzer/src/bin/main.rs#L97
 #[cfg(feature = "debug_tool")]
 pub fn wait_for(phase: &str) {
   println!(
