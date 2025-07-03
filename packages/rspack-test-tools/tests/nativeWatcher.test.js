4f761e5933361950ae18c908bdb9837f67de70b9
GiveMe-A-Name <a455300764b@hotmail.com>
2025-07-03 17:29:15 +0800

    fix: add conditional check for WASM platform in nativeWatcher tests

--- packages/rspack-test-tools/tests/nativeWatcher.test.js
+++ packages/rspack-test-tools/tests/nativeWatcher.test.js
@@ -2,8 +2,15 @@ const path = require("path");
 const { describeByWalk, createNativeWatcher } = require("@rspack/test-tools");
 const tempDir = path.resolve(__dirname, `./js/temp`);
 
-describeByWalk(__filename, (name, src, dist) => {
-	createNativeWatcher(name, src, dist, path.join(tempDir, name));
-}, {
-	source: path.join(__dirname, `./watchCases`),
-});
+const isWasm = Boolean(process.env.WASM)
+
+if (!isWasm) {
+	describeByWalk(__filename, (name, src, dist) => {
+		createNativeWatcher(name, src, dist, path.join(tempDir, name));
+	}, {
+		source: path.join(__dirname, `./watchCases`),
+	});
+} else {
+	it.skip("Should skip in wasm platform, because notify's dependency filetime is not support time", async () => { })
+}
+
