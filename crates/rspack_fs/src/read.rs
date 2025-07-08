fd6aa09fbb90f7f36f3c76a81798b6c23329a8f3
CPunisher <1343316114@qq.com>
2025-07-08 10:58:22 +0800

    Add read_to_string for InputFileSystem

--- crates/rspack_fs/src/read.rs
+++ crates/rspack_fs/src/read.rs
@@ -2,13 +2,34 @@ use std::fmt::Debug;
 
 use rspack_paths::{Utf8Path, Utf8PathBuf};
 
-use crate::{FileMetadata, Result};
+use crate::{Error, FileMetadata, Result};
 
 #[async_trait::async_trait]
 pub trait ReadableFileSystem: Debug + Send + Sync {
   /// See [std::fs::read]
   async fn read(&self, path: &Utf8Path) -> Result<Vec<u8>>;
   fn read_sync(&self, path: &Utf8Path) -> Result<Vec<u8>>;
+
+  // See [std::fs::read_to_string]
+  async fn read_to_string(&self, path: &Utf8Path) -> Result<String> {
+    let data = self.read(path).await?;
+    String::from_utf8(data).map_err(|_| {
+      Error::Io(std::io::Error::new(
+        std::io::ErrorKind::InvalidData,
+        "stream did not contain valid UTF-8",
+      ))
+    })
+  }
+
+  fn read_to_string_sync(&self, path: &Utf8Path) -> Result<String> {
+    let data = self.read_sync(path)?;
+    String::from_utf8(data).map_err(|_| {
+      Error::Io(std::io::Error::new(
+        std::io::ErrorKind::InvalidData,
+        "stream did not contain valid UTF-8",
+      ))
+    })
+  }
 
   /// See [std::fs::metadata]
   async fn metadata(&self, path: &Utf8Path) -> Result<FileMetadata>;
