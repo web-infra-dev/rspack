77d9ccafa775efccdca4256a01a47f27b04c90a3
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2025-07-09 13:51:49 +0800

    fix: multiRspackOptions should apply `extends` configuration (#10959)
    * fix:  mulityRspackOptions should apply extends configuration
    
    * Update core.api.md
    
    * chore: adjuet code location and add a test

--- packages/rspack-cli/tests/build/config/extends/multiple/rspack.config.js
+++ packages/rspack-cli/tests/build/config/extends/multiple/rspack.config.js
@@ -3,13 +3,17 @@ const path = require("path");
 /**
  * @type {import('@rspack/core').RspackOptions}
  */
-module.exports = {
-	extends: [
-		path.resolve(__dirname, "base.rspack.config.js"),
-		path.resolve(__dirname, "dev.rspack.config.js")
-	],
-	entry: "./src/index.js",
-	output: {
-		path: path.resolve(__dirname, "dist")
+// Should apply correctly even if the config is an array
+// See https://github.com/web-infra-dev/rspack/issues/10745
+module.exports = [
+	{
+		extends: [
+			path.resolve(__dirname, "base.rspack.config.js"),
+			path.resolve(__dirname, "dev.rspack.config.js")
+		],
+		entry: "./src/index.js",
+		output: {
+			path: path.resolve(__dirname, "dist")
+		}
 	}
-};
+];
