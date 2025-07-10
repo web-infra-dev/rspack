6b05874535250cd7046059d3ee64ae800e4a87d4
CPunisher <1343316114@qq.com>
2025-07-10 15:49:57 +0800

    SriPlugin

--- crates/rspack_plugin_sri/src/lib.rs
+++ crates/rspack_plugin_sri/src/lib.rs
@@ -5,7 +5,7 @@ mod integrity;
 mod runtime;
 mod util;
 
-use std::sync::LazyLock;
+use std::sync::{Arc, LazyLock};
 
 use asset::{detect_unresolved_integrity, handle_assets, update_hash};
 use config::SRICompilationContext;
@@ -27,11 +27,13 @@ use rspack_plugin_runtime::RuntimePlugin;
 use rspack_util::fx_hash::FxDashMap;
 use runtime::{create_script, handle_runtime, link_preload};
 use rustc_hash::FxHashMap as HashMap;
+use tokio::sync::RwLock;
 
-static COMPILATION_INTEGRITY_MAP: LazyLock<FxDashMap<CompilationId, HashMap<String, String>>> =
-  LazyLock::new(Default::default);
+static COMPILATION_INTEGRITY_MAP: LazyLock<
+  FxDashMap<CompilationId, Arc<RwLock<HashMap<String, String>>>>,
+> = LazyLock::new(Default::default);
 
-static COMPILATION_CONTEXT_MAP: LazyLock<FxDashMap<CompilationId, SRICompilationContext>> =
+static COMPILATION_CONTEXT_MAP: LazyLock<FxDashMap<CompilationId, Arc<SRICompilationContext>>> =
   LazyLock::new(Default::default);
 
 #[plugin]
@@ -45,33 +47,31 @@ impl SubresourceIntegrityPlugin {
     Self::new_inner(options)
   }
 
-  pub fn get_compilation_sri_context(
-    id: CompilationId,
-  ) -> dashmap::mapref::one::Ref<'static, CompilationId, SRICompilationContext> {
+  pub fn get_compilation_sri_context(id: CompilationId) -> Arc<SRICompilationContext> {
     COMPILATION_CONTEXT_MAP
       .get(&id)
       .expect("should have sri context")
+      .clone()
   }
 
   pub fn set_compilation_sri_context(id: CompilationId, ctx: SRICompilationContext) {
-    COMPILATION_CONTEXT_MAP.insert(id, ctx);
+    COMPILATION_CONTEXT_MAP.insert(id, Arc::new(ctx));
   }
 
-  pub fn get_compilation_integrities(
-    id: CompilationId,
-  ) -> dashmap::mapref::one::Ref<'static, CompilationId, HashMap<String, String>> {
+  pub fn get_compilation_integrities(id: CompilationId) -> Arc<RwLock<HashMap<String, String>>> {
     if !COMPILATION_INTEGRITY_MAP.contains_key(&id) {
       COMPILATION_INTEGRITY_MAP.insert(id, Default::default());
     }
     COMPILATION_INTEGRITY_MAP
       .get(&id)
       .expect("should have compilation integrities")
+      .clone()
   }
 
   pub fn get_compilation_integrities_mut(
     id: CompilationId,
-  ) -> dashmap::mapref::one::RefMut<'static, CompilationId, HashMap<String, String>> {
-    COMPILATION_INTEGRITY_MAP.entry(id).or_default()
+  ) -> Arc<RwLock<HashMap<String, String>>> {
+    COMPILATION_INTEGRITY_MAP.entry(id).or_default().clone()
   }
 }
 
