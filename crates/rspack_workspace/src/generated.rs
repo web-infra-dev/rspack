03ec8624769a61d561b5eab818bb02e960c4a3b9
jserfeng <1114550440@qq.com>
2025-07-15 12:02:25 +0800

    chore: release 1.4.7

--- crates/rspack_workspace/src/generated.rs
+++ crates/rspack_workspace/src/generated.rs
@@ -6,7 +6,7 @@ pub const fn rspack_swc_core_version() -> &'static str {
 
 /// The version of the JavaScript `@rspack/core` package.
 pub const fn rspack_pkg_version() -> &'static str {
-  "1.4.7-alpha.1"
+  "1.4.7"
 }
 
 /// The version of the Rust workspace in the root `Cargo.toml` of the repository.
