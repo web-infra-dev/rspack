089828ca4ae50d6ebf163bf389f6a73bb9f6e171
LingyuCoder <lingyucoder@gmail.com>
2025-07-15 15:05:49 +0800

    fix: should not generate SRI for empty mf chunks

---
+++ packages/rspack-test-tools/tests/configCases/sri/mf/rspack.config.js
@@ -0,0 +1,31 @@
+const { experiments, container } = require("@rspack/core");
+
+module.exports = {
+	target: "web",
+	optimization: {
+		moduleIds: "named"
+	},
+	plugins: [
+		new experiments.SubresourceIntegrityPlugin(),
+		new container.ModuleFederationPlugin({
+			name: "app",
+			filename: "remoteEntry.js",
+			exposes: {
+				"./render": "./render.js"
+			},
+			shared: {
+				react: {
+					singleton: true,
+					requiredVersion: "^19.0.0"
+				},
+				"react-dom": {
+					singleton: true,
+					requiredVersion: "^19.0.0"
+				}
+			}
+		})
+	],
+	output: {
+		crossOriginLoading: "anonymous"
+	}
+};
