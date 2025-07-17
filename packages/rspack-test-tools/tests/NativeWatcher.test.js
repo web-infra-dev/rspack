3ec9a1852150d0e2a09590a2f92d6427e71de056
GiveMe-A-Name <a455300764b@hotmail.com>
2025-07-17 14:09:59 +0800

    for test

--- packages/rspack-test-tools/tests/NativeWatcher.test.js
+++ packages/rspack-test-tools/tests/NativeWatcher.test.js
@@ -2,9 +2,17 @@ const path = require("path");
 const { describeByWalk, createNativeWatcher } = require("@rspack/test-tools");
 const tempDir = path.resolve(__dirname, `./js/temp`);
 
+const ignores = ["dynamic-entries"];
+
 describeByWalk(
 	__filename,
 	(name, src, dist) => {
+		if (ignores.some(ignore => name.includes(ignore))) {
+			// Skip ignored cases
+			console.log(`Skipping test case: ${name}`);
+			return;
+		}
+
 		createNativeWatcher(name, src, dist, path.join(tempDir, name));
 	},
 	{
