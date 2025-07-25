f44416e4eb279e3a9af46c51f84517f8dc149778
jerrykingxyz <jerrykingxyz@gmail.com>
2025-07-25 18:03:09 +0800

    add test

---
+++ packages/rspack-test-tools/tests/cacheCases/make/keep_module_error/index.js
@@ -0,0 +1,11 @@
+import value from "./loader.js!./file";
+
+it("should module error exist", async () => {
+	if (COMPILER_INDEX == 0) {
+		expect(value).toBe(1);
+		await NEXT_START();
+	}
+	if (COMPILER_INDEX == 1) {
+		expect(value).toBe(1);
+	}
+});
---
+++ packages/rspack-test-tools/tests/cacheCases/make/keep_module_error/rspack.config.js
@@ -0,0 +1,34 @@
+const rspack = require("@rspack/core");
+
+let index = 0;
+
+/** @type {import("@rspack/core").Configuration} */
+module.exports = {
+	context: __dirname,
+	optimization: {
+		minimize: false
+	},
+	experiments: {
+		cache: {
+			type: "persistent"
+		}
+	},
+	plugins: [
+		{
+			apply(compiler) {
+				compiler.hooks.done.tapPromise("PLUGIN", async stats => {
+					const { errors } = stats.toJson({ errors: true });
+					if (index == 0) {
+						expect(errors.length).toBe(1);
+						expect(errors[0].message).toMatch("LoaderError");
+					} else {
+						// TODO should be same as index 0, change it after
+						// write error to module.diagnostic
+						expect(errors.length).toBe(0);
+					}
+					index++;
+				});
+			}
+		}
+	]
+};
