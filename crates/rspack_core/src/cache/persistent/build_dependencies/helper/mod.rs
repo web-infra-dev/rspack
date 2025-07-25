18a106dcb7175ba973328d5c3494503aec5bc984
jerrykingxyz <jerrykingxyz@gmail.com>
2025-07-25 21:09:33 +0800

    docs: add test and docs for build_dependencies helper

--- crates/rspack_core/src/cache/persistent/build_dependencies/helper/mod.rs
+++ crates/rspack_core/src/cache/persistent/build_dependencies/helper/mod.rs
@@ -16,6 +16,11 @@ use swc_core::{
 use self::visitor::DependencyVisitor;
 use crate::{Resolve as ResolveOption, ResolveResult, Resolver};
 
+/// A toolkit to recursively calculate files used by build dependencies.
+///
+/// The toolkit will use ast to analyze the build dependency files and resolve the requests in them,
+/// treat the files associated with the requests as build dependency files,
+/// and continue processing them until all dependency files are calculated.
 pub struct Helper {
   resolver: Resolver,
   warnings: Vec<String>,
@@ -36,6 +41,9 @@ impl Helper {
     }
   }
 
+  /// Resolve a directory.
+  ///
+  /// Return the sub dir and files.
   async fn resolve_directory(&mut self, dir: &Utf8Path) -> Option<HashSet<Utf8PathBuf>> {
     let entries = self.resolver.inner_fs().read_dir(dir).await;
     match entries {
@@ -52,6 +60,10 @@ impl Helper {
     }
   }
 
+  /// Resolve a file.
+  ///
+  /// This function will try analyze the ast and resolve the requests in them.
+  /// Return the file path that the input file depends on.
   async fn resolve_file(&mut self, file: &Utf8Path) -> Option<HashSet<Utf8PathBuf>> {
     let Some(ext) = file.extension() else {
       // no extension
@@ -124,6 +136,9 @@ impl Helper {
     Some(result)
   }
 
+  /// Resolve a path.
+  ///
+  /// Use the corresponding resolve method according to the type of input file.
   pub async fn resolve(&mut self, path: &Utf8Path) -> Option<HashSet<Utf8PathBuf>> {
     let metadata = match self.resolver.inner_fs().metadata(path).await {
       Ok(metadata) => metadata,
@@ -144,7 +159,93 @@ impl Helper {
     }
   }
 
+  /// Get all warnings.
   pub fn into_warnings(self) -> Vec<String> {
     self.warnings
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use std::sync::Arc;
+
+  use rspack_fs::{MemoryFileSystem, WritableFileSystem};
+
+  use super::Helper;
+
+  #[tokio::test]
+  async fn helper_file_test() {
+    let fs = Arc::new(MemoryFileSystem::default());
+    fs.create_dir_all("/".into()).await.unwrap();
+    fs.write("/a.js".into(), r#"require("./a1")"#.as_bytes())
+      .await
+      .unwrap();
+    fs.write("/a1.js".into(), r#"console.log('a')"#.as_bytes())
+      .await
+      .unwrap();
+    fs.write("/b.js".into(), r#"console.log('b')"#.as_bytes())
+      .await
+      .unwrap();
+    fs.write("/c.txt".into(), r#"123"#.as_bytes())
+      .await
+      .unwrap();
+    fs.write(
+      "/index.js".into(),
+      r#"
+import "./a";
+import "./b";
+
+require("./c.txt");
+require("./d.md");
+"#
+      .as_bytes(),
+    )
+    .await
+    .unwrap();
+
+    let mut helper = Helper::new(fs);
+    let deps = helper
+      .resolve("/index.js".into())
+      .await
+      .expect("should have deps");
+    assert_eq!(deps.len(), 3);
+    let warnings = helper.into_warnings();
+    assert_eq!(warnings.len(), 1);
+  }
+
+  #[tokio::test]
+  async fn helper_dir_test() {
+    let fs = Arc::new(MemoryFileSystem::default());
+    fs.create_dir_all("/configs/test".into()).await.unwrap();
+    fs.write("/configs/a.js".into(), r#"console.log('a')"#.as_bytes())
+      .await
+      .unwrap();
+    fs.write(
+      "/configs/test/b.js".into(),
+      r#"console.log('b')"#.as_bytes(),
+    )
+    .await
+    .unwrap();
+    fs.write(
+      "/configs/test/b1.js".into(),
+      r#"console.log('b1')"#.as_bytes(),
+    )
+    .await
+    .unwrap();
+    fs.write("/configs/c.txt".into(), r#"123"#.as_bytes())
+      .await
+      .unwrap();
+    fs.write("/index.js".into(), r#"console.log('index')"#.as_bytes())
+      .await
+      .unwrap();
+
+    let mut helper = Helper::new(fs);
+    let deps = helper
+      .resolve("/configs/".into())
+      .await
+      .expect("should have deps");
+    assert_eq!(deps.len(), 3);
+    let warnings = helper.into_warnings();
+    assert_eq!(warnings.len(), 0);
   }
 }
