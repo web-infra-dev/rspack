ff8415abb1bba77e1e650961ff371bbafbd83783
jserfeng <1114550440@qq.com>
2025-07-29 10:31:54 +0800

    fix: sri plugin should process async chunk in async entrypoints

---
+++ packages/rspack-test-tools/tests/configCases/sri/async-entrypoints/index.js
@@ -0,0 +1,16 @@
+const worker = new Worker(new URL('./my-worker.worker.js', import.meta.url));
+
+let rx;
+const promise = new Promise(resolve => {
+	rx = resolve;
+})
+
+it('should compile success', async () => {
+	const res = await promise;
+
+	expect(res).toBe('ok')
+})
+
+worker.onmessage = (e) => {
+  rx(e.data)
+}
