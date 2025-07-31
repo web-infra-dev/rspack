08d2927473aa1ffd8b9ebace45e68c67a490caf1
Cong-Cong <dacongsama@live.com>
2025-07-31 16:52:53 +0800

    feat: cache for factory get

--- packages/rspack/src/ResolverFactory.ts
+++ packages/rspack/src/ResolverFactory.ts
@@ -7,8 +7,16 @@ type ResolveOptionsWithDependencyType = Resolve & {
 	resolveToContext?: boolean;
 };
 
+type ResolverCache = {
+	direct: WeakMap<ResolveOptionsWithDependencyType, Resolver>;
+	stringified: Map<string, Resolver>;
+};
+
+const EMPTY_RESOLVE_OPTIONS: ResolveOptionsWithDependencyType = {};
+
 export class ResolverFactory {
 	#binding: binding.JsResolverFactory;
+	#cache: Map<string, ResolverCache> = new Map();
 
 	static __to_binding(
 		resolver_factory: ResolverFactory
@@ -28,12 +36,12 @@ export class ResolverFactory {
 		);
 	}
 
-	get(
+	#create(
 		type: string,
-		resolveOptions?: ResolveOptionsWithDependencyType
+		resolveOptionsWithDepType: ResolveOptionsWithDependencyType
 	): Resolver {
 		const { dependencyCategory, resolveToContext, ...resolve } =
-			resolveOptions || {};
+			resolveOptionsWithDepType;
 
 		const binding = this.#binding.get(type, {
 			...getRawResolve(resolve),
@@ -41,5 +49,33 @@ export class ResolverFactory {
 			resolveToContext
 		});
 		return new Resolver(binding);
+	}
+
+	get(
+		type: string,
+		resolveOptions: ResolveOptionsWithDependencyType = EMPTY_RESOLVE_OPTIONS
+	): Resolver {
+		let typedCaches = this.#cache.get(type);
+		if (!typedCaches) {
+			typedCaches = {
+				direct: new WeakMap(),
+				stringified: new Map()
+			};
+			this.#cache.set(type, typedCaches);
+		}
+		const cachedResolver = typedCaches.direct.get(resolveOptions);
+		if (cachedResolver) {
+			return cachedResolver;
+		}
+		const ident = JSON.stringify(resolveOptions);
+		const resolver = typedCaches.stringified.get(ident);
+		if (resolver) {
+			typedCaches.direct.set(resolveOptions, resolver);
+			return resolver;
+		}
+		const newResolver = this.#create(type, resolveOptions);
+		typedCaches.direct.set(resolveOptions, newResolver);
+		typedCaches.stringified.set(ident, newResolver);
+		return newResolver;
 	}
 }
