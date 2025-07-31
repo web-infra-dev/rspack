b86049fdda761deae0ebfdd68377b9a4513fd1eb
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2025-07-31 16:20:56 +0800

    chore: release v1.4.11 (#11215)

--- crates/rspack_workspace/src/generated.rs
+++ crates/rspack_workspace/src/generated.rs
@@ -6,10 +6,10 @@ pub const fn rspack_swc_core_version() -> &'static str {
 
 /// The version of the JavaScript `@rspack/core` package.
 pub const fn rspack_pkg_version() -> &'static str {
-  "1.4.10"
+  "1.4.11"
 }
 
 /// The version of the Rust workspace in the root `Cargo.toml` of the repository.
 pub const fn rspack_workspace_version() -> &'static str {
-  "0.4.10"
+  "0.4.11"
 }
