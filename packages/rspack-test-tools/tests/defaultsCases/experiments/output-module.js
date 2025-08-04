2dc46bd7a0d67d570292b114ecc670bf3a7fcaa3
pshu <pishu.spf@antfin.com>
2025-08-04 11:47:03 +0800

    chore: verify zstd  uncompress with old cache

--- packages/rspack-test-tools/tests/defaultsCases/experiments/output-module.js
+++ packages/rspack-test-tools/tests/defaultsCases/experiments/output-module.js
@@ -47,9 +47,7 @@ module.exports = {
 		-     "hotUpdateChunkFilename": "[id].[fullhash].hot-update.js",
 		+     "hotUpdateChunkFilename": "[id].[fullhash].hot-update.mjs",
 		@@ ... @@
-		-     "hotUpdateMainFilename": "[runtime].[fullhash].hot-update.json",
 		-     "iife": true,
-		+     "hotUpdateMainFilename": "[runtime].[fullhash].hot-update.json.mjs",
 		+     "iife": false,
 		@@ ... @@
 		-     "module": false,
