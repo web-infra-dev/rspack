8b7d7dc19a194280b1166a85a8be19bd260ba6c3
GiveMe-A-Name <a455300764b@hotmail.com>
2025-08-05 17:35:23 +0800

    feat: update default value for lazyCompilation to { imports: true, entries: false }

--- packages/rspack/src/config/defaults.ts
+++ packages/rspack/src/config/defaults.ts
@@ -211,11 +211,10 @@ const applyExperimentsDefaults = (
 
 	D(experiments, "futureDefaults", false);
 	// IGNORE(experiments.lazyCompilation): In webpack, lazyCompilation is undefined by default
-	D(experiments, "lazyCompilation", {});
-	if (typeof experiments.lazyCompilation === "object") {
-		D(experiments.lazyCompilation, "entries", true);
-		D(experiments.lazyCompilation, "imports", true);
-	}
+	// Rsbuild configure `{lazyCompilation: { imports: true, entries: false }` works well after tests.
+	// So we decide to set lazyCompilation as `{ imports: true, entries: false }` firstly.
+	// Maybe we can change it to true in the future.
+	D(experiments, "lazyCompilation", { imports: true, entries: false });
 
 	D(experiments, "asyncWebAssembly", experiments.futureDefaults);
 	D(experiments, "css", experiments.futureDefaults ? true : undefined);
