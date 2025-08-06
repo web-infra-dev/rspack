73c88401b90c59c1338cb5c4c6dffe91b72aac7e
jinrui <jerrykingxyz@gmail.com>
2025-08-06 11:34:48 +0800

    feat: rspack cli add config path to build dependencies (#11288)
    * feat: rspack cli add config path to build dependencies
    
    * add test
    
    * fix: ci

--- packages/rspack-cli/tests/build/plugin-apply-error/plugin-apply-error.test.ts
+++ packages/rspack-cli/tests/build/plugin-apply-error/plugin-apply-error.test.ts
@@ -3,7 +3,6 @@ import { run } from "../../utils/test-utils";
 describe("plugin apply throw error", () => {
 	it.concurrent("should report error", async () => {
 		const { stderr } = await run(__dirname);
-		console.log(stderr);
 		expect(stderr).toMatch(/error in plugin/);
 	});
 });
