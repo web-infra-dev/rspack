f3b34467c5b59ee868e47087f5b8f31ab6e5e25f
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-08-06 21:26:47 +0800

    fix: export the missing `PathData` type (#11315)
    * fix: export the missing `PathData` type
    
    * fix: rename `PathDataChunkLike` to `ChunkPathData`

--- packages/rspack/src/exports.ts
+++ packages/rspack/src/exports.ts
@@ -1,6 +1,6 @@
 // this is a hack to be compatible with plugin which detect webpack's version
-const rspackVersion = RSPACK_VERSION as string;
-const version = WEBPACK_VERSION as string;
+const rspackVersion = RSPACK_VERSION;
+const version = WEBPACK_VERSION;
 
 export { rspackVersion, version };
 
@@ -8,8 +8,10 @@ export type {
 	Asset,
 	AssetInfo,
 	Assets,
+	ChunkPathData,
 	CompilationParams,
-	LogEntry
+	LogEntry,
+	PathData
 } from "./Compilation";
 export { Compilation } from "./Compilation";
 export { Compiler } from "./Compiler";
