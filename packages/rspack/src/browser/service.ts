48abb3e71387b6d06a6bc352c9f5e8868c7a897e
CPunisher <1343316114@qq.com>
2025-08-19 15:17:32 +0800

    Fill enums in shim

--- packages/rspack/src/browser/service.ts
+++ packages/rspack/src/browser/service.ts
@@ -1,4 +1,27 @@
-export enum RequestType {}
+export enum RequestType {
+	AddDependency = "AddDependency",
+	AddContextDependency = "AddContextDependency",
+	AddMissingDependency = "AddMissingDependency",
+	AddBuildDependency = "AddBuildDependency",
+	GetDependencies = "GetDependencies",
+	GetContextDependencies = "GetContextDependencies",
+	GetMissingDependencies = "GetMissingDependencies",
+	ClearDependencies = "ClearDependencies",
+	Resolve = "Resolve",
+	GetResolve = "GetResolve",
+	GetLogger = "GetLogger",
+	EmitError = "EmitError",
+	EmitWarning = "EmitWarning",
+	EmitFile = "EmitFile",
+	EmitDiagnostic = "EmitDiagnostic",
+	SetCacheable = "SetCacheable",
+	ImportModule = "ImportModule",
+	UpdateLoaderObjects = "UpdateLoaderObjects",
+	CompilationGetPath = "CompilationGetPath",
+	CompilationGetPathWithInfo = "CompilationGetPathWithInfo",
+	CompilationGetAssetPath = "CompilationGetAssetPath",
+	CompilationGetAssetPathWithInfo = "CompilationGetAssetPathWithInfo"
+}
 
 export async function run() {
 	throw new Error("Not support browser");
