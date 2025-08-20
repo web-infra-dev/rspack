d4f67aa11721370d7d82568301596a96d36d5267
CPunisher <1343316114@qq.com>
2025-08-20 11:23:00 +0800

    Fix http issuer

--- packages/rspack/src/browser/BrowserHttpImportPlugin.ts
+++ packages/rspack/src/browser/BrowserHttpImportPlugin.ts
@@ -13,18 +13,33 @@ export class BrowserHttpImportPlugin {
 		compiler.hooks.normalModuleFactory.tap("BrowserHttpImportPlugin", nmf => {
 			nmf.hooks.resolve.tap("BrowserHttpImportPlugin", resolveData => {
 				const request = resolveData.request;
-				if (!this.isNodeModule(request)) {
+
+				const issuerUrl = toUrl(resolveData.contextInfo.issuer);
+				if (issuerUrl) {
+					// If the issuer is a URL, request must be relative to that URL too
+					resolveData.request = this.resolveWithUrlIssuer(issuerUrl, request);
 					return;
 				}
-				const esmUrl = this.buildEsmUrl(request);
-				resolveData.request = esmUrl;
+
+				if (this.isNodeModule(request)) {
+					resolveData.request = this.resolveNodeModule(request);
+					return;
+				}
 			});
 		});
 	}
 
+	resolveWithUrlIssuer(issuer: URL, request: string) {
+		return new URL(request, issuer).href;
+	}
+
+	resolveNodeModule(request: string) {
+		return this.buildEsmUrl(request);
+	}
+
 	isNodeModule(request: string) {
 		// Skip requess like "http://xxx"
-		if (isUrl(request)) {
+		if (toUrl(request)) {
 			return false;
 		}
 
@@ -56,11 +71,11 @@ function getPackageName(request: string) {
 	return request.split("/")[0];
 }
 
-function isUrl(request: string) {
+function toUrl(request: string): URL | undefined {
 	try {
-		new URL(request);
-		return true;
+		const url = new URL(request);
+		return url;
 	} catch {
-		return false;
+		return undefined;
 	}
 }
