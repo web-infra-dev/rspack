946797c986741b9a2d0fe9ba46edc315dc9d25b7
ahabhgk <ahabhgk@gmail.com>
2025-08-25 15:06:08 +0800

    fix

--- tests/webpack-test/cases/chunks/destructuring-assignment/warnings.js
+++ tests/webpack-test/cases/chunks/destructuring-assignment/warnings.js
@@ -1,3 +1,5 @@
+"use strict";
+
 module.exports = [
-	[/`webpackExports` could not be used with destructuring assignment./]
+	/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/
 ];
