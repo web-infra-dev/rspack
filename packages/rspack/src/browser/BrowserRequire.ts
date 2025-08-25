48fcc8a80aa27440f3d27d23b3d107ddd99455dd
CPunisher <1343316114@qq.com>
2025-08-25 14:10:18 +0800

    Fix

--- packages/rspack/src/browser/BrowserRequire.ts
+++ packages/rspack/src/browser/BrowserRequire.ts
@@ -64,7 +64,7 @@ export class BrowserRequirePlugin {
 			);
 
 			execute(cjs.code, { exports, module, require: browserRequire });
-			return exports.default || module.exports;
+			return exports.default ?? module.exports;
 		};
 	}
 }
