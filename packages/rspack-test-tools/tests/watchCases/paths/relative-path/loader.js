623ea5e22e28cb5c06556e43785508d76e812cdf
GiveMe-A-Name <a455300764b@hotmail.com>
2025-08-28 16:56:29 +0800

    fix: resolve dependency path correctly in loader.js

--- packages/rspack-test-tools/tests/watchCases/paths/relative-path/loader.js
+++ packages/rspack-test-tools/tests/watchCases/paths/relative-path/loader.js
@@ -1,7 +1,7 @@
 const path = require('node:path')
 
 module.exports = function (source) {
-  const depPath = this.resource.replace('index.js', '_module.js');
+  const depPath = path.resolve(this.context, '_module.js');
   const currentWorkDir = path.resolve('./');
   const relativePath = path.relative(currentWorkDir, depPath);
   this.addDependency(relativePath);
