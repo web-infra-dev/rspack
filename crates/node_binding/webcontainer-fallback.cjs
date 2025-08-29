3f0e24bf87be209a230adc3cac11b2d8747d1107
CPunisher <1343316114@qq.com>
2025-08-29 10:54:48 +0800

    update

--- crates/node_binding/webcontainer-fallback.cjs
+++ crates/node_binding/webcontainer-fallback.cjs
@@ -1,21 +1,22 @@
 /**
- * From https://github.com/oxc-project/oxc/blob/main/napi/parser/webcontainer-fallback.js
+ * Based on  https://github.com/oxc-project/oxc/blob/main/napi/parser/webcontainer-fallback.js
  */
 
 const fs = require('node:fs');
+const path = require('node:path');
 const childProcess = require('node:child_process');
 
 const pkg = JSON.parse(
-  fs.readFileSync(require.resolve('@rspack/binding/package.json'), 'utf-8'),
+  fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8'),
 );
 const version = pkg.version;
 const baseDir = `/tmp/rspack-${version}`;
-const bindingEntry = `${baseDir}/node_modules/@rspack/binding-wasm32-wasi/rspack.wasi.cjs`;
+const bindingEntry = `${baseDir}/node_modules/${pkg.name}-wasm32-wasi/rspack.wasi.cjs`;
 
 if (!fs.existsSync(bindingEntry)) {
   fs.rmSync(baseDir, { recursive: true, force: true });
   fs.mkdirSync(baseDir, { recursive: true });
-  const bindingPkg = `@rspack/binding-wasm32-wasi@${version}`;
+  const bindingPkg = `${pkg.name}-wasm32-wasi@${version}`;
   console.log(`[rspack] Downloading ${bindingPkg} on WebContainer...`);
   childProcess.execFileSync('pnpm', ['i', bindingPkg], {
     cwd: baseDir,
