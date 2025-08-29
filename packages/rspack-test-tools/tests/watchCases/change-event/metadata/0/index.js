2c3f87a8c20c40ced5265987226ad44a7e12d8e2
GiveMe-A-Name <a455300764b@hotmail.com>
2025-08-29 16:08:23 +0800

    refactor: clean up unused watching change count logic in ShouldRebuildPlugin

--- packages/rspack-test-tools/tests/watchCases/change-event/metadata/0/index.js
+++ packages/rspack-test-tools/tests/watchCases/change-event/metadata/0/index.js
@@ -1,15 +1,1 @@
 const a = 1;
-
-it('should compile', () => {
-  switch (WATCH_STEP) {
-    case "0":
-      // do nothing
-      break;
-    case "1":
-      expect(GLOBAL_WATCH_CHANGE_COUNT).toBe(1)
-
-      break;
-    default:
-      throw new Error('unexpected update');
-  }
-})
--- packages/rspack-test-tools/tests/watchCases/change-event/metadata/rspack.config.js
+++ packages/rspack-test-tools/tests/watchCases/change-event/metadata/rspack.config.js
@@ -4,58 +4,23 @@ const fs = require("node:fs");
 class ShouldRebuildPlugin {
 	constructor() {
 		this.assetName = "bundle.js";
-		this.watchingChangeCount = 0;
 		this.compileCount = 0;
 	}
 	apply(compiler) {
 		const targetFile = path.resolve(compiler.context, "./index.js");
 
-		compiler.hooks.thisCompilation.tap(
-			ShouldRebuildPlugin.name,
-			compilation => {
-				if (compiler.watching) {
-					compiler.watching.onChange = () => {
-						this.watchingChangeCount++;
-					};
-				} else {
-					console.error("Compiler is not in watch mode");
-				}
-				const { Compilation } = compiler.rspack;
-				compilation.hooks.processAssets.tap(
-					{
-						name: ShouldRebuildPlugin.name,
-						stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS
-					},
-					assets => {
-						const asset = assets[this.assetName];
-						if (!asset) {
-							return;
-						}
-
-						const { RawSource } = compiler.rspack.sources;
-						const oldContent = asset.source();
-						const newContent = oldContent.replace(
-							"GLOBAL_WATCH_CHANGE_COUNT",
-							this.watchingChangeCount
-						);
-						const source = new RawSource(newContent);
-
-						compilation.updateAsset(this.assetName, source);
-					}
-				);
-			}
-		);
-
 		compiler.hooks.done.tap(ShouldRebuildPlugin.name, _ => {
 			// After first compilation, touch the file to trigger a rebuild
 			if (this.compileCount === 0) {
-				fs.utimes(targetFile, Date.now(), Date.now(), err => {
-					if (err) {
-						console.error("Error updating file timestamps:", err);
-						return;
-					}
-					// Touch file to trigger rebuild
-				});
+				setTimeout(() => {
+					fs.utimes(targetFile, Date.now(), Date.now(), err => {
+						if (err) {
+							console.error("Error updating file timestamps:", err);
+							return;
+						}
+						// Touch file to trigger rebuild
+					});
+				}, 1000);
 			}
 			this.compileCount++;
 		});
