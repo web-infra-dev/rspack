384f8941014f6aefd0c7506358408cd8c0aa1fa6
fi3ework <fi3ework@gmail.com>
2025-09-04 20:46:02 +0800

    fix(rslib): treat `module` identifier as normal

--- packages/rspack-test-tools/tests/configCases/rslib/plugin-api/index.js
+++ packages/rspack-test-tools/tests/configCases/rslib/plugin-api/index.js
@@ -4,4 +4,11 @@ console.log(require.config)
 console.log(require.version)
 console.log(require.include)
 console.log(require.onError)
+export function g() {
+  console.log(module);
+  if (module.children) {
+    module.children = module.children.filter((item) => item.filename !== path);
+  }
+}
+
 
