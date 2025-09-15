f93137656b0379e3e995e52e28e87b379ebdf7b2
LingyuCoder <lingyucoder@gmail.com>
2025-09-15 18:27:22 +0800

    test: use rspack test tools to run normal hot cases

--- packages/rspack-test-tools/src/case/normal.ts
+++ packages/rspack-test-tools/src/case/normal.ts
@@ -2,7 +2,7 @@ import path from "node:path";
 import { HotModuleReplacementPlugin } from "@rspack/core";
 import { NormalProcessor } from "../processor/normal";
 import { NormalRunnerFactory } from "../runner";
-import { BasicCaseCreator } from "../test/creator";
+import { BasicCaseCreator, type IBasicCaseCreatorOptions } from "../test/creator";
 import { ECompilerType } from "../type";
 
 const NORMAL_CASES_ROOT = path.resolve(
@@ -10,44 +10,34 @@ const NORMAL_CASES_ROOT = path.resolve(
 	"../../../../tests/rspack-test/normalCases"
 );
 
-const creator = new BasicCaseCreator({
-	clean: true,
-	describe: false,
-	steps: ({ name }) => [
-		new NormalProcessor({
-			name,
-			root: NORMAL_CASES_ROOT,
-			compilerOptions: {},
-			runable: true,
-			compilerType: ECompilerType.Rspack
-		})
-	],
-	runner: NormalRunnerFactory,
-	concurrent: true
-});
+const createCaseOptions = (
+	hot: boolean
+): IBasicCaseCreatorOptions<ECompilerType> => {
+	return {
+		clean: true,
+		describe: false,
+		steps: ({ name }) => [
+			new NormalProcessor({
+				name,
+				root: NORMAL_CASES_ROOT,
+				compilerOptions: {
+					plugins: hot ? [new HotModuleReplacementPlugin()] : []
+				},
+				runable: true,
+				compilerType: ECompilerType.Rspack
+			})
+		],
+		runner: NormalRunnerFactory,
+		concurrent: true
+	};
+};
 
+const creator = new BasicCaseCreator(createCaseOptions(false));
 export function createNormalCase(name: string, src: string, dist: string) {
 	creator.create(name, src, dist);
 }
 
-const hotCreator = new BasicCaseCreator({
-	clean: true,
-	describe: false,
-	steps: ({ name }) => [
-		new NormalProcessor({
-			name,
-			root: NORMAL_CASES_ROOT,
-			compilerOptions: {
-				plugins: [new HotModuleReplacementPlugin()]
-			},
-			runable: true,
-			compilerType: ECompilerType.Rspack
-		})
-	],
-	runner: NormalRunnerFactory,
-	concurrent: true
-});
-
+const hotCreator = new BasicCaseCreator(createCaseOptions(true));
 export function createHotNormalCase(name: string, src: string, dist: string) {
 	hotCreator.create(name, src, dist);
 }
