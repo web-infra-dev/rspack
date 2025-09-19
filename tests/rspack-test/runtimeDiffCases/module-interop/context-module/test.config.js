b0d01e7e38c1918853a4c3e9aaed8bf2ccf60f64
ahabhgk <ahabhgk@gmail.com>
2025-09-19 16:57:42 +0800

    fix

--- tests/rspack-test/runtimeDiffCases/module-interop/context-module/test.config.js
+++ tests/rspack-test/runtimeDiffCases/module-interop/context-module/test.config.js
@@ -4,8 +4,7 @@ module.exports = {
 	runtimeModules: false,
 	// TODO: remove renameModule and replacements once webpack merges https://github.com/webpack/webpack/pull/19903
 	renameModule(moduleName) {
-		console.log(moduleName);
-		const matches = moduleName.match(/context-module\/src\/namespace-object-lazy\/dir-(?:cjs|esm|mixed)\|async-weak\|.*\|referencedExports: /);
+		const matches = moduleName.match(/context-module[\\\/]src[\\\/]namespace-object-lazy[\\\/]dir-(?:cjs|esm|mixed)\|async-weak\|\/\^\\\.\\\/\.\*\$\/\|referencedExports: /);
 		if (matches) {
 			return moduleName.replace("|referencedExports: ", "");
 		}
@@ -13,7 +12,7 @@ module.exports = {
 	},
 	replacements: [
 		{
-			from: /"\.\/src\/namespace-object-lazy\/dir-(?:cjs|esm|mixed) async-weak recursive .* referencedExports: "/g,
+			from: /"\.\/src\/namespace-object-lazy\/dir-(?:cjs|esm|mixed) async-weak recursive \^\\\\\.\\\\\/\.\*\$ referencedExports: "/g,
 			to: (substring) => substring.replace(" referencedExports: ", ""),
 		}
 	]
