c5ac63c895dd1902ca4c5d50a0c32839a01953f8
ahabhgk <ahabhgk@gmail.com>
2025-09-23 11:34:56 +0800

    fix

--- tests/rspack-test/normalCases/chunks/statical-dynamic-import-then-destructuring/index.js
+++ tests/rspack-test/normalCases/chunks/statical-dynamic-import-then-destructuring/index.js
@@ -53,9 +53,9 @@ it("should walk with correct order", async () => {
 });
 
 it("should analyze arguments in call member chain", async () => {
-	import("../statical-dynamic-import/dir4/lib?2").then(({ b }) => {
+	await import("../statical-dynamic-import/dir4/lib?2").then(({ b }) => {
 		b.f((async () => {
-			import("../statical-dynamic-import/dir4/a?2").then(({ a, usedExports }) => {
+			await import("../statical-dynamic-import/dir4/a?2").then(({ a, usedExports }) => {
 				expect(a).toBe(1);
 				expect(usedExports).toEqual(["a", "usedExports"]);
 			});
