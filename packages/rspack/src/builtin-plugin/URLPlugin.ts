f3e57972bd3d06b2527897e1f27cc089c9b649ec
Cong-Cong Pan <dacongsama@live.com>
2025-09-29 14:44:46 +0800

    fix: URLPlugin in child compiler (#11785)

--- packages/rspack/src/builtin-plugin/URLPlugin.ts
+++ packages/rspack/src/builtin-plugin/URLPlugin.ts
@@ -2,4 +2,8 @@ import { BuiltinPluginName } from "@rspack/binding";
 
 import { create } from "./base";
 
-export const URLPlugin = create(BuiltinPluginName.URLPlugin, () => {});
+export const URLPlugin = create(
+	BuiltinPluginName.URLPlugin,
+	() => {},
+	"compilation"
+);
