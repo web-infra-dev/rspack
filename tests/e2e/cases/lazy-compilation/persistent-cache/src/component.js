48c014787f8a285b93b9e1b3ae2feb658914c8cb
jerrykingxyz <jerrykingxyz@gmail.com>
2025-10-09 23:38:33 +0800

    ci: fix lazy-compilation persistent cache e2e case

--- tests/e2e/cases/lazy-compilation/persistent-cache/src/component.js
+++ tests/e2e/cases/lazy-compilation/persistent-cache/src/component.js
@@ -1,3 +1,4 @@
 const button = document.createElement("button");
 button.textContent = "Component";
+button.id = "Component";
 document.body.appendChild(button);
--- tests/e2e/cases/make/remove-dynamic-entry-with-loader/index.test.ts
+++ tests/e2e/cases/make/remove-dynamic-entry-with-loader/index.test.ts
@@ -1,19 +1,35 @@
 import { expect, test } from "@/fixtures";
 
 test("should compile", async ({ page, fileAction, rspack }) => {
+	// rspack.compiler.__sharedObj is injected by plugin in rspack.config.js
 	await expect(page.getByText("index1")).toBeVisible();
 	await expect(page.getByText("index2")).toBeVisible();
 
-	fileAction.updateFile("src/index2.js", content => content.replace("div.innerText = \"index2\";", "div.innerText = \"index2 updated\";"));
-	fileAction.updateFile("src/index1.js", content => content.replace("div.innerText = \"index1\";", "div.innerText = \"index1 updated\";"));
+	rspack.compiler.__sharedObj.useFullEntry = false;
+	fileAction.updateFile("src/index2.js", content =>
+		content.replace(
+			'div.innerText = "index2";',
+			'div.innerText = "index2 updated";'
+		)
+	);
+	fileAction.updateFile("src/index1.js", content =>
+		content.replace(
+			'div.innerText = "index1";',
+			'div.innerText = "index1 updated";'
+		)
+	);
 
 	await expect(async () => {
 		await page.reload();
 		expect(await page.locator("#index1").innerText()).toBe("index1 updated");
 	}).toPass();
 	await expect(page.locator("#index2")).toHaveCount(0);
-	await expect(page.locator("#webpack-dev-server-client-overlay")).toHaveCount(0);
+	await expect(page.locator("#webpack-dev-server-client-overlay")).toHaveCount(
+		0
+	);
 
-	const stats = rspack.compiler._lastCompilation?.getStats().toJson({ all: false, errors: true });
+	const stats = rspack.compiler._lastCompilation
+		?.getStats()
+		.toJson({ all: false, errors: true });
 	expect(stats?.errors?.length).toBe(0);
 });
