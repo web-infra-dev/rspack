841232e7e8d93f2978128c2933bba61c54857243
pshu <pishu.spf@antfin.com>
2025-10-10 00:57:37 +0800

    chore: skip window native watcher flaky test

---
+++ tests/rspack-test/watchCases/chunks/dynamic-entry-with-depend-on/test.filter.js
@@ -0,0 +1,8 @@
+module.exports = function (config) {
+    let nativeWatcherEnabled = config.experiments?.nativeWatcher === true;
+
+    if (process.platform === "win32" && nativeWatcherEnabled) {
+        return false
+    }
+    return true;
+}
