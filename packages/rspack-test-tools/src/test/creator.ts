a6e9e692017b18b5ce0f8d2d3ff0a2532762e295
LingyuCoder <lingyucoder@gmail.com>
2025-10-10 19:19:39 +0800

    test: align more test cases with webpack

--- packages/rspack-test-tools/src/test/creator.ts
+++ packages/rspack-test-tools/src/test/creator.ts
@@ -170,7 +170,7 @@ export class BasicCaseCreator<T extends ECompilerType> {
 						cb(e);
 					});
 				},
-				options.timeout || 3000
+				options.timeout || 300000
 			);
 
 			chain = chain.then(
@@ -260,7 +260,7 @@ export class BasicCaseCreator<T extends ECompilerType> {
 						);
 					}
 				},
-				options.timeout || 6000
+				options.timeout || 60000
 			);
 			const env = this.createEnv(testConfig, options);
 		}
