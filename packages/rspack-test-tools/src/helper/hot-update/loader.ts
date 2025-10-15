d7d1a18552774bf89eb3ba32e07c4502d9e420c4
LingyuCoder <lingyucoder@gmail.com>
2025-10-15 22:32:38 +0800

    test: refactor hot cases to async function

--- packages/rspack-test-tools/src/helper/hot-update/loader.ts
+++ packages/rspack-test-tools/src/helper/hot-update/loader.ts
@@ -1,35 +1,3 @@
 export default function (c: string) {
-	let content = c;
-	if (
-		content.includes("NEXT_HMR") &&
-		!content.includes("module.hot.accept") &&
-		!content.includes("import.meta.webpackHot.accept")
-	) {
-		content = `
-			${content}
-			// let __hmr_children__ = [...module.children];
-      // let __hmr_used_exports__ = __hmr_children__.reduce((res, child) => {
-      //   if (__webpack_require__.c[child]) {
-      //     res[child] = __webpack_require__.c[child].exports;
-      //   }
-			// 	return res;
-			// }, {});
-			// module.hot.accept(__hmr_children__, () => {
-			// 	__hmr_children__.forEach((child) => {
-			// 		const reexports = __webpack_require__(child);
-			// 		if (typeof reexports === "object") {
-			// 			for (let key in reexports) {
-			// 				if (!__hmr_used_exports__[child]) { continue; }
-			// 				Object.defineProperty(__hmr_used_exports__[child], key, {
-			// 					configurable: true,
-			// 					enumerable: true,
-			// 					get: () => reexports[key]
-			// 				});
-			// 			}
-			// 		}
-			// 	});
-			// });
-		`;
-	}
-	return content.replace(/NEXT_HMR/g, "NEXT_HMR.bind(null, module)");
+	return c.replace(/NEXT_HMR/g, "NEXT_HMR.bind(null, module)");
 }
