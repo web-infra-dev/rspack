9c50def5c53d6de0e10f85fcbc9923c5c4c9c308
LingyuCoder <lingyucoder@gmail.com>
2025-10-16 10:44:29 +0800

    test: refactor hot cases to async function

--- packages/rspack-test-tools/src/helper/hot-update/loader.ts
+++ packages/rspack-test-tools/src/helper/hot-update/loader.ts
@@ -1,3 +1,5 @@
-export default function (c: string) {
+const loader = function (c: string) {
 	return c.replace(/NEXT_HMR/g, "NEXT_HMR.bind(null, module)");
-}
+};
+module.exports = loader;
+export default loader;
