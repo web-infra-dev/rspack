82536be6c93434547fab995f936167b45c47af59
neverland <chenjiahan.jait@bytedance.com>
2025-10-17 14:53:21 +0800

    fix

--- tests/rspack-test/configCases/externals/umd-validation-failed/errors.js
+++ tests/rspack-test/configCases/externals/umd-validation-failed/errors.js
@@ -1,1 +1,3 @@
-module.exports = [/Error: Invalid Rspack configuration: External object must have "root", "commonjs", "commonjs2", "amd" properties when "libraryType" or "externalsType" is "umd", get:/];
+module.exports = [
+  /Invalid Rspack configuration: External object must have "root", "commonjs", "commonjs2", "amd" properties when "libraryType" or "externalsType" is "umd", get:/
+];
