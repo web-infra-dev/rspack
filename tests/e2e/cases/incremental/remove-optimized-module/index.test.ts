d7240270fe6ea93dd6e7ea5edaa3fbefab4f25ad
jerrykingxyz <jerrykingxyz@gmail.com>
2025-10-20 19:34:11 +0800

    test: add test case for remove-optimized-module

---
+++ tests/e2e/cases/incremental/remove-optimized-module/index.test.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@/fixtures";
+
+test("remove optimized module should not panic", async ({
+	page,
+	fileAction
+}) => {
+	await expect(page.locator("#main")).toHaveText("Button");
+
+	fileAction.deleteFile("comp/Button.js");
+
+	const overlay = page.frameLocator("#webpack-dev-server-client-overlay");
+	await expect(
+		overlay.getByText("ESModulesLinkingWarning: export 'Button'")
+	).toBeVisible();
+	await expect(
+		overlay.getByText("Module not found: Can't resolve './Button'")
+	).toBeVisible();
+
+	fileAction.updateFile(
+		"comp/Button.js",
+		() => "export const Button = 'NewButton';"
+	);
+	await expect(page.locator("#main")).toHaveText("NewButton");
+});
