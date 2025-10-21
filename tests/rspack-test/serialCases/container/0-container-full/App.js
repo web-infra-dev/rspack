23b54dc746f550949144d02c1ab0c6e04ea26af0
hardfist <yangjianzju@gmail.com>
2025-10-21 14:20:23 +0800

    chore: add logging

--- tests/rspack-test/serialCases/container/0-container-full/App.js
+++ tests/rspack-test/serialCases/container/0-container-full/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ComponentA from "containerA/ComponentA";
-
+console.log('react:',React);
 export default () => {
 	return `App rendered with [${React()}] and [${ComponentA()}]`;
 };
