3e23e415802c0dce6ea2ba8dc4931c297c2fe897
neverland <chenjiahan.jait@bytedance.com>
2025-10-23 19:04:22 +0800

    perf(cli): simplify ESM file detection (#11946)
    * perf(rspack-cli): simplify esm file detection
    
    * fix

--- packages/rspack-cli/src/utils/isEsmFile.ts
+++ packages/rspack-cli/src/utils/isEsmFile.ts
@@ -1,18 +1,14 @@
 import path from "node:path";
-
 import readPackageUp from "./readPackageUp";
 
-const isEsmFile = (filePath: string, cwd = process.cwd()) => {
-	const ext = path.extname(filePath);
-	if (/\.(mjs|mts)$/.test(ext)) {
+export const isEsmFile = (filePath: string) => {
+	if (/\.(mjs|mts)$/.test(filePath)) {
 		return true;
 	}
-	if (/\.(cjs|cts)/.test(ext)) {
+	if (/\.(cjs|cts)$/.test(filePath)) {
 		return false;
 	}
 	// package.json should be find from configPath root
 	const packageJson = readPackageUp(path.dirname(filePath));
 	return packageJson?.type === "module";
 };
-
-export default isEsmFile;
