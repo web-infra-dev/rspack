75883ba657105fe4fd7734d917277c940ca1ca76
harpsealjs <lingyucoder@gmail.com>
2025-10-27 15:14:31 +0800

    chore: remove injected test code in diff test cases (#12001)

--- crates/rspack_util/src/diff_mode.rs
+++
@@ -1,8 +0,0 @@
-use std::sync::LazyLock;
-
-static IS_DIFF_MODE: LazyLock<bool> =
-  LazyLock::new(|| std::env::var("RSPACK_DIFF").ok().as_deref() == Some("true"));
-
-pub fn is_diff_mode() -> bool {
-  *IS_DIFF_MODE
-}
