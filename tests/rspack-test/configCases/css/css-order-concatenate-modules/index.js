c8e97e581f33953ed21eee39be1c7aa00db4865a
harpsealjs <lingyucoder@gmail.com>
2025-10-30 16:59:19 +0800

    fix: use match resource first when generating css local ident name (#12040)

--- tests/rspack-test/configCases/css/css-order-concatenate-modules/index.js
+++ tests/rspack-test/configCases/css/css-order-concatenate-modules/index.js
@@ -4,10 +4,11 @@ c()
 b()
 a()
 
-it("keep consistent css order", function() {
+it("keep consistent css order", function () {
 	const fs = __non_webpack_require__("fs");
+	const path = __non_webpack_require__("path");
 	let source = fs.readFileSync(__dirname + "/main.css", "utf-8");
-	expect(removeComments(source)).toMatchSnapshot()
+	expect(removeComments(source)).toMatchFileSnapshot(path.join(__SNAPSHOT__, 'main.css.txt'))
 });
 
 function removeComments(source) {
