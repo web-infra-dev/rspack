d808219fcaa70f5b87c7480222fd00b64108a970
harpsealjs <lingyucoder@gmail.com>
2025-10-31 17:54:31 +0800

    fix: get chunk filename with dependOn (#12053)
    * fix: get chunk filename with dependOn
    
    * fix: get chunk filename with dependOn

--- tests/rspack-test/configCases/node/issue-18409-2/index.js
+++
@@ -1,8 +0,0 @@
-import common from "./common";
-import separate from "./separate";
-
-it("should compile", () => {
-	expect(common).toBe("common");
-	expect(separate).toBe("separate");
-});
-export default "main";
