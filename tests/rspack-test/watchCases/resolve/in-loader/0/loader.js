6737fba6192e1ae02c1f9be64a1a565c4575e0b3
harpsealjs <lingyucoder@gmail.com>
2025-11-06 21:18:40 +0800

    fix: should add file dependencies in `loaderContext.resolve` (#12107)
    fix: should add file dependencies in loaderContext.resolve

--- tests/rspack-test/watchCases/resolve/in-loader/0/loader.js
+++ tests/rspack-test/watchCases/resolve/in-loader/0/loader.js
@@ -4,15 +4,11 @@ module.exports = function () {
 	this.resolve(this.context, "./file", (err, file) => {
 		if (err) return callback(err);
 		if (!file) return callback(new Error("Resolving failed"));
-		// TODO: add dependencies in loaderContext.resolve
-		this.addDependency(file);
-		this.addMissingDependency(file.replace(".js", ""));
 
 		this.fs.readFile(file, (err, result) => {
 			if (err) return callback(err);
 			callback(
 				null,
-				// `export default ${JSON.stringify(result.toString("utf-8").trim())};`
 				`export default ${JSON.stringify(result.toString("utf-8").trim())};`
 			);
 		});
