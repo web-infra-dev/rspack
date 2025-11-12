901914f61b288faac08cb7b29e157d8434d852ad
neverland <chenjiahan.jait@bytedance.com>
2025-11-12 20:48:52 +0800

    fix: snapshot

--- tests/rspack-test/errorCases/error-write.js
+++ tests/rspack-test/errorCases/error-write.js
@@ -40,33 +40,33 @@ module.exports = [
     },
     async check(diagnostics) {
       expect(diagnostics).toMatchInlineSnapshot(`
-			Object {
-			  "errors": Array [
-			    Object {
-			      "code": "Error",
-			      "message": "  × test push",
-			      "moduleTrace": Array [],
-			      "stack": "Error: test push    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
-			    },
-			    Object {
-			      "code": "Error",
-			      "message": "  × test push 2",
-			      "moduleTrace": Array [],
-			      "stack": undefined,
-			    },
-			    Object {
-			      "loc": "1:0-33",
-			      "message": "  × Module not found: Can't resolve './answer' in '<TEST_ROOT>/fixtures/errors/resolve-fail-esm'   ╭──── 1 │ import { answer } from './answer'   · ─────────────────────────────────   ╰────  help: Did you mean './answer.js'?                The request './answer' failed to resolve only because it was resolved as fully specified,        probably because the origin is strict EcmaScript Module,        e. g. a module with javascript mimetype, a '*.mjs' file, or a '*.js' file where the package.json contains '\\"type\\": \\"module\\"'.                The extension in the request is mandatory for it to be fully specified.        Add the extension to the request.",
-			      "moduleId": "./resolve-fail-esm/index.js",
-			      "moduleIdentifier": "javascript/esm|<TEST_ROOT>/fixtures/errors/resolve-fail-esm/index.js",
-			      "moduleName": "./resolve-fail-esm/index.js",
-			      "moduleTrace": Array [],
-			      "stack": undefined,
-			    },
-			  ],
-			  "warnings": Array [],
-			}
-		`);
+        Object {
+          "errors": Array [
+            Object {
+              "code": "Error",
+              "message": "  × test push",
+              "moduleTrace": Array [],
+              "stack": "Error: test push    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
+            },
+            Object {
+              "code": "Error",
+              "message": "  × test push 2",
+              "moduleTrace": Array [],
+              "stack": undefined,
+            },
+            Object {
+              "loc": "1:0-33",
+              "message": "  × Module not found: Can't resolve './answer' in '<TEST_ROOT>/fixtures/errors/resolve-fail-esm'   ╭──── 1 │ import { answer } from './answer'   · ─────────────────────────────────   ╰────  help: Did you mean './answer.js'?                The request './answer' failed to resolve only because it was resolved as fully specified,        probably because the origin is strict EcmaScript Module,        e. g. a module with javascript mimetype, a '*.mjs' file, or a '*.js' file where the package.json contains '\\"type\\": \\"module\\"'.                The extension in the request is mandatory for it to be fully specified.        Add the extension to the request.",
+              "moduleId": "./resolve-fail-esm/index.js",
+              "moduleIdentifier": "javascript/esm|<TEST_ROOT>/fixtures/errors/resolve-fail-esm/index.js",
+              "moduleName": "./resolve-fail-esm/index.js",
+              "moduleTrace": Array [],
+              "stack": undefined,
+            },
+          ],
+          "warnings": Array [],
+        }
+      `);
     }
   },
   {
@@ -87,18 +87,18 @@ module.exports = [
     },
     async check(diagnostics) {
       expect(diagnostics).toMatchInlineSnapshot(`
-			Object {
-			  "errors": Array [
-			    Object {
-			      "code": "Error",
-			      "message": "  × error 1",
-			      "moduleTrace": Array [],
-			      "stack": "Error: error 1    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
-			    },
-			  ],
-			  "warnings": Array [],
-			}
-		`);
+        Object {
+          "errors": Array [
+            Object {
+              "code": "Error",
+              "message": "  × error 1",
+              "moduleTrace": Array [],
+              "stack": "Error: error 1    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
+            },
+          ],
+          "warnings": Array [],
+        }
+      `);
     }
   },
   {
@@ -117,24 +117,24 @@ module.exports = [
     },
     async check(diagnostics) {
       expect(diagnostics).toMatchInlineSnapshot(`
-			Object {
-			  "errors": Array [
-			    Object {
-			      "code": "Error",
-			      "message": "  × error 1",
-			      "moduleTrace": Array [],
-			      "stack": "Error: error 1    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
-			    },
-			    Object {
-			      "code": "Error",
-			      "message": "  × error 2",
-			      "moduleTrace": Array [],
-			      "stack": "Error: error 2    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
-			    },
-			  ],
-			  "warnings": Array [],
-			}
-		`);
+        Object {
+          "errors": Array [
+            Object {
+              "code": "Error",
+              "message": "  × error 1",
+              "moduleTrace": Array [],
+              "stack": "Error: error 1    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
+            },
+            Object {
+              "code": "Error",
+              "message": "  × error 2",
+              "moduleTrace": Array [],
+              "stack": "Error: error 2    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
+            },
+          ],
+          "warnings": Array [],
+        }
+      `);
     }
   },
   {
@@ -157,18 +157,18 @@ module.exports = [
     },
     async check(diagnostics) {
       expect(diagnostics).toMatchInlineSnapshot(`
-			Object {
-			  "errors": Array [
-			    Object {
-			      "code": "Error",
-			      "message": "  × test unshift",
-			      "moduleTrace": Array [],
-			      "stack": "Error: test unshift    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
-			    },
-			  ],
-			  "warnings": Array [],
-			}
-		`);
+        Object {
+          "errors": Array [
+            Object {
+              "code": "Error",
+              "message": "  × test unshift",
+              "moduleTrace": Array [],
+              "stack": "Error: test unshift    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
+            },
+          ],
+          "warnings": Array [],
+        }
+      `);
     }
   },
   {
@@ -187,18 +187,18 @@ module.exports = [
     },
     async check(diagnostics) {
       expect(diagnostics).toMatchInlineSnapshot(`
-			Object {
-			  "errors": Array [
-			    Object {
-			      "code": "Error",
-			      "message": "  × test splice",
-			      "moduleTrace": Array [],
-			      "stack": "Error: test splice    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
-			    },
-			  ],
-			  "warnings": Array [],
-			}
-		`);
+        Object {
+          "errors": Array [
+            Object {
+              "code": "Error",
+              "message": "  × test splice",
+              "moduleTrace": Array [],
+              "stack": "Error: test splice    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
+            },
+          ],
+          "warnings": Array [],
+        }
+      `);
     }
   },
   {
@@ -217,27 +217,27 @@ module.exports = [
     },
     async check(diagnostics) {
       expect(diagnostics).toMatchInlineSnapshot(`
-			Object {
-			  "errors": Array [
-			    Object {
-			      "code": "Error",
-			      "message": "  × test splice",
-			      "moduleTrace": Array [],
-			      "stack": "Error: test splice    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.js<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
-			    },
-			    Object {
-			      "loc": "1:0-33",
-			      "message": "  × Module not found: Can't resolve './answer' in '<TEST_ROOT>/fixtures/errors/resolve-fail-esm'   ╭──── 1 │ import { answer } from './answer'   · ─────────────────────────────────   ╰────  help: Did you mean './answer.js'?                The request './answer' failed to resolve only because it was resolved as fully specified,        probably because the origin is strict EcmaScript Module,        e. g. a module with javascript mimetype, a '*.mjs' file, or a '*.js' file where the package.json contains '\\"type\\": \\"module\\"'.                The extension in the request is mandatory for it to be fully specified.        Add the extension to the request.",
-			      "moduleId": "./resolve-fail-esm/index.js",
-			      "moduleIdentifier": "javascript/esm|<TEST_ROOT>/fixtures/errors/resolve-fail-esm/index.js",
-			      "moduleName": "./resolve-fail-esm/index.js",
-			      "moduleTrace": Array [],
-			      "stack": undefined,
-			    },
-			  ],
-			  "warnings": Array [],
-			}
-		`);
+        Object {
+          "errors": Array [
+            Object {
+              "code": "Error",
+              "message": "  × test splice",
+              "moduleTrace": Array [],
+              "stack": "Error: test splice    at <TEST_ROOT>/errorCases/error-write.js<LINE_COL>    at Object.fn (<RSPACK_ROOT>/dist/index.js<LINE_COL>)    at next (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsyncStageRange (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at AsyncSeriesHook.callAsync (<ROOT>/node_modules/<PNPM_INNER>/@rspack/lite-tapable/dist/index.cjs<LINE_COL>)    at <RSPACK_ROOT>/dist/index.js<LINE_COL>",
+            },
+            Object {
+              "loc": "1:0-33",
+              "message": "  × Module not found: Can't resolve './answer' in '<TEST_ROOT>/fixtures/errors/resolve-fail-esm'   ╭──── 1 │ import { answer } from './answer'   · ─────────────────────────────────   ╰────  help: Did you mean './answer.js'?                The request './answer' failed to resolve only because it was resolved as fully specified,        probably because the origin is strict EcmaScript Module,        e. g. a module with javascript mimetype, a '*.mjs' file, or a '*.js' file where the package.json contains '\\"type\\": \\"module\\"'.                The extension in the request is mandatory for it to be fully specified.        Add the extension to the request.",
+              "moduleId": "./resolve-fail-esm/index.js",
+              "moduleIdentifier": "javascript/esm|<TEST_ROOT>/fixtures/errors/resolve-fail-esm/index.js",
+              "moduleName": "./resolve-fail-esm/index.js",
+              "moduleTrace": Array [],
+              "stack": undefined,
+            },
+          ],
+          "warnings": Array [],
+        }
+      `);
     }
   }
 ];
