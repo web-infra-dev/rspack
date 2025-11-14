a711cbf0cc6bdf51a96b749d493240e4394069dd
neverland <chenjiahan.jait@bytedance.com>
2025-11-14 10:25:38 +0800

    Revert "test: add case"
    This reverts commit 4885814adf473a6c19519cc26522db3de54d9172.

--- tests/rspack-test/hotCases/chunks/split-chunks-arrow-function/index.js
+++
@@ -1,9 +0,0 @@
-import vendor from "vendor";
-
-it("should hot update a splitted initial chunk with `environment.arrowFunction: true`", async () => {
-	expect(vendor).toBe("1");
-	await NEXT_HMR();
-	expect(vendor).toBe("2");
-});
-
-module.hot.accept(["vendor"]);
