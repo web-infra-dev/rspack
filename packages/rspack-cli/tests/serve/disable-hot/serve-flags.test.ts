5c09e49d95aaf7994740fc6689feab333fc6a47e
neverland <chenjiahan.jait@bytedance.com>
2025-11-21 21:15:43 +0800

    fix(cli): `devServer.hot: false` not work (#12261)

---
+++ packages/rspack-cli/tests/serve/disable-hot/serve-flags.test.ts
@@ -0,0 +1,9 @@
+import { normalizeStdout, runWatch } from "../../utils/test-utils";
+
+test("should allow to disable HMR via `server.hot`", async () => {
+	const { stdout } = await runWatch(__dirname, ["serve"], {
+		killString: /localhost/
+	});
+
+	expect(normalizeStdout(stdout)).toContain('{"hot":false}');
+});
