8841af31c50c274290465218b682265eddc775dd
jinrui <jerrykingxyz@gmail.com>
2025-11-25 12:40:44 +0800

    fix: track package.json files in node_modules as build dependencies (#12274)

--- tests/rspack-test/cacheCases/common/build-dependencies-resolve/other.config.js
+++ tests/rspack-test/cacheCases/common/build-dependencies-resolve/other.config.js
@@ -1,3 +1,6 @@
+require("./dep");
+// lib/a will be unusable because lib does not export it.
 require("lib/a");
 require("lib/b");
+// lib/src/index will be unusable because lib does not export it.
 require("lib/src/index");
--- tests/rspack-test/cacheCases/common/build-dependencies-resolve/rspack.config.js
+++ tests/rspack-test/cacheCases/common/build-dependencies-resolve/rspack.config.js
@@ -5,12 +5,12 @@ let index = 0;
 let content = 0;
 
 const buildDependency = path.join(__dirname, "other.config.js");
-const buildDependencyA = path.join(__dirname, "node_modules/lib/src/a.js");
-const buildDependencyB = path.join(__dirname, "node_modules/lib/src/b.js");
-const buildDependencyIndex = path.join(
-	__dirname,
-	"node_modules/lib/src/index.js"
-);
+const libFileA = path.join(__dirname, "node_modules/lib/src/a.js");
+const libFileB = path.join(__dirname, "node_modules/lib/src/b.js");
+const libFileIndex = path.join(__dirname, "node_modules/lib/src/index.js");
+const libPackageJson = path.join(__dirname, "node_modules/lib/package.json");
+const projectDep = path.join(__dirname, "dep.js");
+const projectPackageJson = path.join(__dirname, "package.json");
 
 /** @type {import("@rspack/core").Configuration} */
 module.exports = {
@@ -29,12 +29,26 @@ module.exports = {
 		{
 			apply(compiler) {
 				compiler.hooks.done.tapPromise("Test Plugin", async function () {
-					if (index === 1) {
-						await fs.writeFile(buildDependencyA, String(content));
+					if (index === 0) {
+						await fs.writeFile(projectDep, String(content));
+					} else if (index === 1) {
+						await fs.writeFile(libFileA, String(content));
 					} else if (index === 2) {
-						await fs.writeFile(buildDependencyB, String(content));
+						await fs.writeFile(libFileB, String(content));
 					} else if (index === 3) {
-						await fs.writeFile(buildDependencyIndex, String(content));
+						await fs.writeFile(libFileIndex, String(content));
+					} else if (index === 4) {
+						const content = await fs.readFile(libPackageJson);
+						await fs.writeFile(
+							libPackageJson,
+							content.toString().replace("0.0.1", "0.0.2")
+						);
+					} else if (index === 5) {
+						const content = await fs.readFile(projectPackageJson);
+						await fs.writeFile(
+							projectPackageJson,
+							content.toString().replace("0.0.1", "0.0.2")
+						);
 					}
 					index++;
 					content++;
