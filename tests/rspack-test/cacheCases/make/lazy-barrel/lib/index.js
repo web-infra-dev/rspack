63d238941ae6897cd71a083748ed47323ef7e750
jinrui <jerrykingxyz@gmail.com>
2025-12-08 16:05:23 +0800

    fix: persistent cache save lazy barrel affected modules (#12384)
    * fix: persistent cache save lazy barrel affected modules
    
    * test: update test case

--- tests/rspack-test/cacheCases/make/lazy-barrel/lib/index.js
+++ tests/rspack-test/cacheCases/make/lazy-barrel/lib/index.js
@@ -1,11 +1,3 @@
 export { a } from "./a";
----
-export { a } from "./a";
-export { b } from "./b";
----
-export { a } from "./a";
-export { c } from "./c";
----
-export { a } from "./a";
 export { b } from "./b";
 export { c } from "./c";
