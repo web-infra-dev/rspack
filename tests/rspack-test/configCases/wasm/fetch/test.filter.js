ea105c2385f0f057bfeb92978bf48d674e5e0696
LingyuCoder <lingyucoder@gmail.com>
2025-12-09 17:54:12 +0800

    fix: lint

--- tests/rspack-test/configCases/wasm/fetch/test.filter.js
+++
@@ -1,4 +0,0 @@
-const { FilteredStatus } = require("@rspack/test-tools/helper/util/filterUtil");
-module.exports = function (config) {
-  return [FilteredStatus.PARTIAL_PASS, "TODO: support sync wasm"];
-};
