5d2e13e4c0210c4c88d1165c812bbce7f8ec0c6f
jserfeng <1114550440@qq.com>
2025-12-11 14:55:51 +0800

    chore: remove logic of parallel code splitting

--- tests/rspack-test/defaultsCases/default/base.js
+++ tests/rspack-test/defaultsCases/default/base.js
@@ -48,7 +48,6 @@ module.exports = {
 			    inlineEnum: false,
 			    lazyBarrel: true,
 			    lazyCompilation: false,
-			    parallelCodeSplitting: false,
 			    parallelLoader: false,
 			    rspackFuture: Object {
 			      bundlerInfo: Object {
