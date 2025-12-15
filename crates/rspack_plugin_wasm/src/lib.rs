02b3377e3d7a111f992b2e794a96e8345444016a
jinrui <jerrykingxyz@gmail.com>
2025-12-15 19:10:19 +0800

    fix: wasm plugin panic when use persistent cache (#12455)
    * fix: wasm plugin panic when use persistent cache
    
    * fix: ci

--- crates/rspack_plugin_wasm/src/lib.rs
+++ crates/rspack_plugin_wasm/src/lib.rs
@@ -5,14 +5,4 @@ mod runtime;
 mod wasm_plugin;
 
 pub use loading_plugin::{FetchCompileAsyncWasmPlugin, enable_wasm_loading_plugin};
-use rspack_core::AssetInfo;
 pub use wasm_plugin::AsyncWasmPlugin;
-
-// TODO(ahabhgk): remove this
-type ModuleIdToFileName = std::sync::Arc<
-  dashmap::DashMap<
-    rspack_core::ModuleIdentifier,
-    (String, AssetInfo),
-    std::hash::BuildHasherDefault<rspack_collections::IdentifierHasher>,
-  >,
->;
