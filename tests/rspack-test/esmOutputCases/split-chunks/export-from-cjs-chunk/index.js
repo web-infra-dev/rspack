72d7db14b47f8faf7d5e80e11df89c931b456d45
jserfeng <1114550440@qq.com>
2025-12-17 02:07:31 +0800

    refactor: use exports info to calculate entry exports
    Manually resolve exports that are star exported.
    Improve NodeTargetPlugin support for module output.

--- tests/rspack-test/esmOutputCases/split-chunks/export-from-cjs-chunk/index.js
+++ tests/rspack-test/esmOutputCases/split-chunks/export-from-cjs-chunk/index.js
@@ -1,3 +1,10 @@
-import value from './cjs'
+import obj from './cjs'
 
-export {value}
+export { obj }
+
+it('should have correct export', async () => {
+  const { obj } = await import(/*webpackIgnore: true*/ './main.mjs')
+
+  expect(obj).toHaveProperty('value')
+  expect(obj.value).toBe(42)
+})
