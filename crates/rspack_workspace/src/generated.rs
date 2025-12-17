e5867b777e7cbf915f852b51882b1b0bb3807639
CPunisher <1343316114@qq.com>
2025-12-17 15:30:06 +0800

    chore: bump version

--- crates/rspack_workspace/src/generated.rs
+++ crates/rspack_workspace/src/generated.rs
@@ -6,10 +6,10 @@ pub const fn rspack_swc_core_version() -> &'static str {
 
 /// The version of the JavaScript `@rspack/core` package.
 pub const fn rspack_pkg_version() -> &'static str {
-  "1.6.7"
+  "1.7.0-beta.0"
 }
 
 /// The version of the Rust workspace in the root `Cargo.toml` of the repository.
 pub const fn rspack_workspace_version() -> &'static str {
-  "0.6.7"
+  "0.7.0-beta.0"
 }
