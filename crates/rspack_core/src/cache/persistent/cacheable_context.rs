d27fb3aba4ff03015f055c99481eb34812334093
jerrykingxyz <jerrykingxyz@gmail.com>
2025-12-26 18:49:12 +0800

    feat: rspack .cache version calc use portable options

--- crates/rspack_core/src/cache/persistent/cacheable_context.rs
+++ crates/rspack_core/src/cache/persistent/cacheable_context.rs
@@ -1,8 +1,12 @@
+use rspack_paths::Utf8PathBuf;
+
 #[derive(Debug)]
-pub struct CacheableContext;
+pub struct CacheableContext {
+  pub project_path: Utf8PathBuf,
+}
 
 impl rspack_cacheable::CacheableContext for CacheableContext {
   fn project_root(&self) -> Option<&std::path::Path> {
-    None
+    Some(self.project_path.as_std_path())
   }
 }
