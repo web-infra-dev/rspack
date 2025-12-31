fc0b6db3705ce6af573e387f00f5ce049cdd2ab8
pshu <stormslowly@gmail.com>
2025-12-31 17:40:04 +0800

    refactor: use readonly compilation reference for after seal hook (#12602)
    refactor: use readonly compilation reference

--- crates/rspack_core/src/compilation/mod.rs
+++ crates/rspack_core/src/compilation/mod.rs
@@ -102,7 +102,7 @@ define_hook!(CompilationRenderManifest: Series(compilation: &Compilation, chunk_
 define_hook!(CompilationChunkAsset: Series(compilation: &Compilation, chunk_ukey: &ChunkUkey, filename: &str));
 define_hook!(CompilationProcessAssets: Series(compilation: &mut Compilation));
 define_hook!(CompilationAfterProcessAssets: Series(compilation: &mut Compilation));
-define_hook!(CompilationAfterSeal: Series(compilation: &mut Compilation),tracing=true);
+define_hook!(CompilationAfterSeal: Series(compilation: &Compilation),tracing=true);
 
 #[derive(Debug, Default)]
 pub struct CompilationHooks {
