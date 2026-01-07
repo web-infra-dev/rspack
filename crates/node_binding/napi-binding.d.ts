eb6fde9aa919011063a578922b854a5f886978d2
shaoyu <shaoyumeng@Shaoyus-MacBook-Pro.local>
2026-01-07 12:38:00 +0800

    update auto generated napi-binding.d.ts

--- crates/node_binding/napi-binding.d.ts
+++ crates/node_binding/napi-binding.d.ts
@@ -1739,8 +1739,8 @@ export interface NapiResolveOptions {
    * Default `false`
    */
   enablePnp?: boolean
-  /** 
-   * Path to yarn Plug'n'Play manifest file.
+  /**
+   * Path to PnP manifest file
    *
    * Default `None`
    */
@@ -3007,8 +3007,7 @@ export interface RealDependencyLocation {
   end?: SourcePosition
 }
 
-/**
- * this is a process level tracing, which means it would be shared by all compilers in the same process
+/** * this is a process level tracing, which means it would be shared by all compilers in the same process
  * only the first call would take effect, the following calls would be ignored
  * Some code is modified based on
  * https://github.com/swc-project/swc/blob/d1d0607158ab40463d1b123fed52cc526eba8385/bindings/binding_core_node/src/util.rs#L29-L58
