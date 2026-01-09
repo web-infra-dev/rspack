72f8fdfefcd89659c3859619d2d2e77d84e29523
jinrui <jerrykingxyz@gmail.com>
2026-01-09 13:37:08 +0800

    feat: rspack_cacheable context expose project root (#12685)
    * feat: rspack_cacheable context expose project root
    
    * fix: comment

--- crates/rspack_cacheable_test/tests/macro/manual_cacheable_dyn_with_generics.rs
+++ crates/rspack_cacheable_test/tests/macro/manual_cacheable_dyn_with_generics.rs
@@ -1,9 +1,16 @@
-use rspack_cacheable::{r#dyn::VTablePtr, enable_cacheable as cacheable, from_bytes, to_bytes};
+use rspack_cacheable::{
+  CacheableContext, r#dyn::VTablePtr, enable_cacheable as cacheable, from_bytes, to_bytes,
+};
 
 #[test]
 #[cfg_attr(miri, ignore)]
 fn test_manual_cacheable_dyn_macro_with_generics() {
   struct Context;
+  impl CacheableContext for Context {
+    fn project_root(&self) -> Option<&std::path::Path> {
+      None
+    }
+  }
 
   trait Animal<T = ()>: rspack_cacheable::r#dyn::SerializeDyn {
     fn color(&self) -> &str;
