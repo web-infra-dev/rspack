1eeb185a581ffbde62db2eee8fefec7d75a11307
jinrui <jerrykingxyz@gmail.com>
2026-01-09 13:37:08 +0800

    feat: rspack_cacheable context expose project root (#12685)
    * feat: rspack_cacheable context expose project root
    
    * fix: comment

--- crates/rspack_cacheable/src/context.rs
+++ crates/rspack_cacheable/src/context.rs
@@ -1,4 +1,4 @@
-use std::{any::Any, ptr::NonNull};
+use std::{any::Any, ops::Deref, path::Path, ptr::NonNull};
 
 use rkyv::{
   de::{ErasedPtr, Pooling, PoolingState},
@@ -10,13 +10,31 @@ use crate::error::{Error, Result};
 const CONTEXT_ADDR: usize = 0;
 unsafe fn default_drop(_: ErasedPtr) {}
 
+pub trait CacheableContext: Any {
+  fn project_root(&self) -> Option<&Path>;
+}
+
+// Implement for unit type for convenience in tests and simple cases
+impl CacheableContext for () {
+  fn project_root(&self) -> Option<&Path> {
+    None
+  }
+}
+
 /// A context wrapper that provides shared context methods
 pub struct ContextGuard<'a> {
-  context: &'a dyn Any,
+  context: &'a dyn CacheableContext,
+}
+
+impl<'a> Deref for ContextGuard<'a> {
+  type Target = dyn CacheableContext;
+  fn deref(&self) -> &Self::Target {
+    self.context
+  }
 }
 
 impl<'a> ContextGuard<'a> {
-  pub fn new(context: &'a dyn Any) -> Self {
+  pub fn new(context: &'a dyn CacheableContext) -> Self {
     Self { context }
   }
 
@@ -25,11 +43,11 @@ impl<'a> ContextGuard<'a> {
     sharing.finish_sharing(CONTEXT_ADDR, self as *const _ as usize)
   }
 
-  pub fn sharing_context<S: Sharing<Error> + ?Sized>(sharing: &'a mut S) -> Result<&'a dyn Any> {
+  pub fn sharing_guard<S: Sharing<Error> + ?Sized>(sharing: &'a mut S) -> Result<&'a Self> {
     match sharing.start_sharing(CONTEXT_ADDR) {
       SharingState::Finished(addr) => {
         let guard: &Self = unsafe { &*(addr as *const Self) };
-        Ok(guard.context)
+        Ok(guard)
       }
       _ => Err(Error::NoContext),
     }
@@ -43,13 +61,19 @@ impl<'a> ContextGuard<'a> {
     }
   }
 
-  pub fn pooling_context<P: Pooling<Error> + ?Sized>(pooling: &'a mut P) -> Result<&'a dyn Any> {
+  pub fn pooling_guard<P: Pooling<Error> + ?Sized>(pooling: &'a mut P) -> Result<&'a Self> {
     match pooling.start_pooling(CONTEXT_ADDR) {
       PoolingState::Finished(ptr) => {
         let guard: &Self = unsafe { &*(ptr.data_address() as *const Self) };
-        Ok(guard.context)
+        Ok(guard)
       }
       _ => Err(Error::NoContext),
     }
+  }
+
+  pub fn downcast_context<T: 'static>(&'a self) -> Result<&'a T> {
+    (self.context as &dyn Any)
+      .downcast_ref()
+      .ok_or(Error::NoContext)
   }
 }
