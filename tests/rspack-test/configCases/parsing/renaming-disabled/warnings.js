b21d2ec24029d6d75dbb361988308845b07142c3
harpsealjs <lingyucoder@gmail.com>
2026-01-12 17:38:29 +0800

    revert: "feat: add warning when requireAlias is disabled" (#12702)
    Revert "feat: add warning when requireAlias is disabled (#12700)"
    
    This reverts commit eb5635c6f248853df96245c9d0a710340e41d6ae.

--- tests/rspack-test/configCases/parsing/renaming-disabled/warnings.js
+++
@@ -1,4 +0,0 @@
-module.exports = [
-  [/Critical dependency: please enable 'module.parser.javascript.requireAlias' to analyze require alias/],
-  [/Critical dependency: please enable 'module.parser.javascript.requireAlias' to analyze require alias/]
-]
