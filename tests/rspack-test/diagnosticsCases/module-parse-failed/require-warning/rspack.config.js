fc8eb39eac88897a7aa3654a5dbe00e792299b4f
LingyuCoder <lingyucoder@gmail.com>
2026-01-12 11:04:34 +0800

    feat: disable parsing require alias by defualt

--- tests/rspack-test/diagnosticsCases/module-parse-failed/require-warning/rspack.config.js
+++ tests/rspack-test/diagnosticsCases/module-parse-failed/require-warning/rspack.config.js
@@ -1,11 +1,10 @@
 /** @type {import("@rspack/core").Configuration} */
 module.exports = {
-	amd: false,
 	module: {
 		parser: {
 			javascript: {
-				unknownContextCritical: true,
 				requireAlias: true,
+				requireAsExpression: true,
 			}
 		}
 	}
