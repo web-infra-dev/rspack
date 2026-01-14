bfd543e5d19495989b1fa0dc80fd00db0d87b2d7
pshu <pishu.spf@antfin.com>
2026-01-14 12:16:19 +0800

    test: fix case name

--- tests/e2e/cases/lazy-compilation/active-lots-modules/index.test.ts
+++ tests/e2e/cases/lazy-compilation/active-lots-modules/index.test.ts
@@ -1,6 +1,8 @@
 import { expect, test } from '@/fixtures';
 
-test('should update style', async ({ page }) => {
+test('should activate and compile lots of modules with long names', async ({
+  page,
+}) => {
   const body = await page.locator('body');
   await expect(body).toContainText('All Modules Loaded');
 });
