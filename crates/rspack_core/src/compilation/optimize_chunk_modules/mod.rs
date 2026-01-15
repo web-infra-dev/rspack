d9915de43e528447ef50c85fa8bff4f84a0fb274
hardfist <yangjianzju@gmail.com>
2026-01-15 09:32:14 +0800

    refactor: move pass into separate folder and driven by run_passes (#12735)
    * chore: add rspack_passes.md
    
    * refactor: move pass into separate folder and drive by run_passes
    
    * chore: use function
    
    * refactor: move more into run_passes
    
    * chore: fix stats logging
    
    * chore: fix stats.logging

---
+++ crates/rspack_core/src/compilation/optimize_chunk_modules/mod.rs
@@ -0,0 +1,14 @@
+use super::*;
+
+pub async fn optimize_chunk_modules_pass(
+  compilation: &mut Compilation,
+  plugin_driver: SharedPluginDriver,
+) -> Result<()> {
+  plugin_driver
+    .compilation_hooks
+    .optimize_chunk_modules
+    .call(compilation)
+    .await
+    .map(|_| ())
+    .map_err(|e| e.wrap_err("caused by plugins in Compilation.hooks.optimizeChunkModules"))
+}
