c46a57bfda3a1ec59220d408cdbb86327fd868e5
hardfist <yangjianzju@gmail.com>
2026-01-15 14:11:23 +0800

    refactor: move logger into pass and reorg some passes

--- crates/rspack_core/src/compilation/optimize_chunks/mod.rs
+++ crates/rspack_core/src/compilation/optimize_chunks/mod.rs
@@ -1,9 +1,13 @@
 use super::*;
+use crate::logger::Logger;
 
 pub async fn optimize_chunks_pass(
   compilation: &mut Compilation,
   plugin_driver: SharedPluginDriver,
 ) -> Result<()> {
+  let logger = compilation.get_logger("rspack.Compilation");
+  let start = logger.time("optimize chunks");
+
   while matches!(
     plugin_driver
       .compilation_hooks
@@ -13,5 +17,7 @@ pub async fn optimize_chunks_pass(
       .map_err(|e| e.wrap_err("caused by plugins in Compilation.hooks.optimizeChunks"))?,
     Some(true)
   ) {}
+
+  logger.time_end(start);
   Ok(())
 }
