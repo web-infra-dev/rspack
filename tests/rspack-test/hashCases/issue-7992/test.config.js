48887edd1b8cb8733b1329547c4aceaeb52a23e8
Cong-Cong <dacongsama@live.com>
2026-01-15 16:49:41 +0800

    fix: hash test case

--- tests/rspack-test/hashCases/issue-7992/test.config.js
+++ tests/rspack-test/hashCases/issue-7992/test.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@rspack/test-tools').THashCaseConfig} */
 module.exports = {
 	validate(stats) {
-		const assets = stats.stats[0].toJson().assets.map(i => i.name);
+		const assets = stats.stats[0].toJson({ assets: true }).assets.map(i => i.name);
 		assets.sort();
 		expect(assets).toEqual([
 			"file1.c30068f3cc748ce3.svg",
--- tests/rspack-test/hashCases/real-content-hash-sha256/test.config.js
+++ tests/rspack-test/hashCases/real-content-hash-sha256/test.config.js
@@ -3,7 +3,7 @@
 /** @type {import("@rspack/test-tools").THashCaseConfig} */
 module.exports = {
 	validate(stats) {
-		const assets = stats.stats[0].toJson().assets.map(i => i.name);
+		const assets = stats.stats[0].toJson({ assets: true }).assets.map(i => i.name);
 		assets.sort();
 
 		// `04c785ed39b3beedfad58de4438c4e905d1da406dc6a1b9ed043ebc574819baa` = sha256 of src/file.svg
