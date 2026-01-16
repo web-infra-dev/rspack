67aff3c422f983068970a169c9ae645f92900e52
ahabhgk <ahabhgk@gmail.com>
2026-01-16 17:36:41 +0800

    test

--- tests/rspack-test/configCases/plugins/minify-default-target-by-rspack-target/rspack.config.js
+++ tests/rspack-test/configCases/plugins/minify-default-target-by-rspack-target/rspack.config.js
@@ -1,5 +1,3 @@
-const { rspack } = require("@rspack/core");
-
 /** @type {import("@rspack/core").Configuration} */
 module.exports = {
 	target: ["web", "browserslist:chrome > 95"],
---
+++ tests/rspack-test/configCases/plugins/minify-default-target-override/index.js
@@ -0,0 +1,15 @@
+import "./index.css";
+
+it("should use high-level css syntax", function () {
+	const css = /** @type {string} */ (__non_webpack_require__("fs").readFileSync(__non_webpack_require__("path").resolve(__dirname, "bundle0.css"), "utf-8"));
+	expect(css.includes("-webkit-")).toBe(false);
+	expect(css.includes("-moz-")).toBe(false);
+});
+
+it("should use high-level js syntax", async function () {
+	const { deopt } = await import("./deopt");
+	const js = /** @type {string} */ (__non_webpack_require__("fs").readFileSync(__filename, "utf-8"));
+	const foo = deopt("foo");
+	expect(foo).toBe("foo");
+	expect(js.includes(["let", "{deopt:e}", "=", "await"].join())).toBe(true);
+});
