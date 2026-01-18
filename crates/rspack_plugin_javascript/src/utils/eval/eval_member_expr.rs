e9c3f39cd48b86213b3f1a6a07542a529cd29eb9
intellild <intellild@outlook.com>
2026-01-18 20:29:04 +0800

    revert incorrect changes

--- crates/rspack_plugin_javascript/src/utils/eval/eval_member_expr.rs
+++ crates/rspack_plugin_javascript/src/utils/eval/eval_member_expr.rs
@@ -4,9 +4,7 @@ use swc_core::ecma::ast::{Expr, MemberExpr};
 use super::BasicEvaluatedExpression;
 use crate::{
   parser_plugin::JavascriptParserPlugin,
-  visitors::{
-    AllowedMemberTypes, ExportedVariableInfo, ExprRef, JavascriptParser, MemberExpressionInfo,
-  },
+  visitors::{AllowedMemberTypes, ExprRef, JavascriptParser, MemberExpressionInfo},
 };
 
 pub fn eval_member_expression<'a>(
@@ -17,30 +15,29 @@ pub fn eval_member_expression<'a>(
   let ret = if let Some(MemberExpressionInfo::Expression(info)) =
     parser.get_member_expression_info(ExprRef::Member(member), AllowedMemberTypes::Expression)
   {
-    if let ExportedVariableInfo::VariableInfo(_) = info.root_info {
-      None
-    } else {
-      parser.plugin_drive.clone().evaluate_identifier(
+    parser
+      .plugin_drive
+      .clone()
+      .evaluate_identifier(
         parser,
         &info.name,
         member.span.real_lo(),
         member.span.real_hi(),
       )
-    }
-    .or_else(|| parser.plugin_drive.clone().evaluate(parser, expr))
-    .or_else(|| {
-      // TODO: fallback with `evaluateDefinedIdentifier`
-      let mut eval =
-        BasicEvaluatedExpression::with_range(member.span.real_lo(), member.span.real_hi());
-      eval.set_identifier(
-        info.name.into(),
-        info.root_info,
-        Some(info.members),
-        Some(info.members_optionals),
-        Some(info.member_ranges),
-      );
-      Some(eval)
-    })
+      .or_else(|| parser.plugin_drive.clone().evaluate(parser, expr))
+      .or_else(|| {
+        // TODO: fallback with `evaluateDefinedIdentifier`
+        let mut eval =
+          BasicEvaluatedExpression::with_range(member.span.real_lo(), member.span.real_hi());
+        eval.set_identifier(
+          info.name.into(),
+          info.root_info,
+          Some(info.members),
+          Some(info.members_optionals),
+          Some(info.member_ranges),
+        );
+        Some(eval)
+      })
   } else {
     None
   };
