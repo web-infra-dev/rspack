c3eb7bca716f85b54464e0599239def769f5f54a
intellild <intellild@outlook.com>
2026-01-20 20:45:42 +0800

    fix test

--- tests/rspack-test/normalCases/require/issue-10479/index.js
+++ tests/rspack-test/normalCases/require/issue-10479/index.js
@@ -1,9 +1,9 @@
-import { require } from './require';
+import { require } from './require'
 
 function run() {
   return require.resolve('module')
 }
 
 it('should not evaluate `require.resolve()` as `require()`', () => {
-  expect(run.toString()).toContain(`.resolve('module')`);
+  expect(run.toString()).toContain(`.resolve('module')`)
 })
