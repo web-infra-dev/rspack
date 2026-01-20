75a761d8ab58f3f31354ce5fb77c525c3647b7ed
jerrykingxyz <jerrykingxyz@gmail.com>
2026-01-20 14:34:38 +0800

    test: add test case

--- tests/rspack-test/cacheCases/snapshot/context-dependencies/index.js
+++ tests/rspack-test/cacheCases/snapshot/context-dependencies/index.js
@@ -1,17 +1,21 @@
-import value from "./loader!./file";
+import value from './loader!./file';
 
-it("should snapshot context dependencies work", async () => {
-	if (COMPILER_INDEX === 0) {
-		expect(value).toBe(1);
-		await NEXT_START();
-	}
-	if (COMPILER_INDEX === 1) {
-		expect(value).toBe(2);
-		await NEXT_START();
-	}
-	if (COMPILER_INDEX === 2) {
-		expect(value).toBe(2);
-	}
+it('should snapshot context dependencies work', async () => {
+  if (COMPILER_INDEX === 0) {
+    expect(value).toBe(1);
+    await NEXT_START();
+  }
+  if (COMPILER_INDEX === 1) {
+    expect(value).toBe(2);
+    await NEXT_START();
+  }
+  if (COMPILER_INDEX === 2) {
+    expect(value).toBe(2);
+    await NEXT_START();
+  }
+  if (COMPILER_INDEX === 3) {
+    expect(value).toBe(2);
+  }
 });
 
-module.hot.accept("./file");
+module.hot.accept('./file');
--- tests/rspack-test/cacheCases/snapshot/context-dependencies/rspack.config.js
+++ tests/rspack-test/cacheCases/snapshot/context-dependencies/rspack.config.js
@@ -1,30 +1,34 @@
-const path = require("path");
-const fs = require("fs/promises");
+const path = require('path');
+const fs = require('fs/promises');
 
-const libAIndex = path.resolve(__dirname, "./lib/a/index");
+const libAIndex = path.resolve(__dirname, './lib/a/index');
+const libTestIndex = path.resolve(__dirname, './lib/node_modules/test/index');
 let index = 0;
 
 /** @type {import("@rspack/core").Configuration} */
 module.exports = {
-	context: __dirname,
-	experiments: {
-		cache: {
-			type: "persistent",
-			snapshot: {
-				immutablePaths: [path.resolve(__dirname, "./file.js")]
-			}
-		}
-	},
-	plugins: [
-		{
-			apply(compiler) {
-				compiler.hooks.done.tapPromise("TestPlugin", async function () {
-					index++;
-					if (index === 1) {
-						await fs.writeFile(libAIndex, String(index));
-					}
-				});
-			}
-		}
-	]
+  context: __dirname,
+  experiments: {
+    cache: {
+      type: 'persistent',
+      snapshot: {
+        immutablePaths: [path.resolve(__dirname, './file.js')],
+      },
+    },
+  },
+  plugins: [
+    {
+      apply(compiler) {
+        compiler.hooks.done.tapPromise('TestPlugin', async function () {
+          index++;
+          if (index === 1) {
+            await fs.writeFile(libAIndex, String(index));
+          }
+          if (index === 2) {
+            await fs.writeFile(libTestIndex, String(index));
+          }
+        });
+      },
+    },
+  ],
 };
