0f986e93618b09236b4e842102bac01356658253
pshu <pishu.spf@antfin.com>
2026-01-21 17:48:42 +0800

    chore: more logs

--- tests/rspack-test/watchCases/build-chunk-graph/skip-building-chunk-graph-without-side-effects/test.config.js
+++ tests/rspack-test/watchCases/build-chunk-graph/skip-building-chunk-graph-without-side-effects/test.config.js
@@ -1,7 +1,8 @@
-function assert(condition) {
-	if (!condition) {
-		throw new Error("Assertion failed for");
-	}
+function assert(condition, message) {
+  if (!condition) {
+    console.log("failed with message:", message);
+    throw new Error("Assertion failed for");
+  }
 }
 
 function should_rebuild(stats) {
@@ -12,10 +13,10 @@ module.exports = {
 	checkStats(stepName, _, stats) {
 		switch (stepName) {
 			case "0":
-				assert(should_rebuild(stats));
+				assert(should_rebuild(stats),stats);
 				break;
 			case "1":
-				assert(!should_rebuild(stats));
+				assert(!should_rebuild(stats),stats);
 				break;
 			default:
 				throw "not have more step";
