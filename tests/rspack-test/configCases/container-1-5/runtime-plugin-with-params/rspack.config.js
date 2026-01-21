2ad2fcb3e6b250d6e55fc6fea6b8bfcb0d0b8ef5
ahabhgk <ahabhgk@gmail.com>
2026-01-21 18:11:08 +0800

    fix(mf): defer runtime plugin invocation to handle tree shaking
    When usedExports optimization is enabled, runtime plugin imports may be
    tree-shaken to undefined. This change defers plugin invocation by first
    collecting plugin references and params as objects, then filtering out
    undefined plugins before invoking them.

--- tests/rspack-test/configCases/container-1-5/runtime-plugin-with-params/rspack.config.js
+++ tests/rspack-test/configCases/container-1-5/runtime-plugin-with-params/rspack.config.js
@@ -2,16 +2,12 @@ const { ModuleFederationPlugin } = require("@rspack/core").container;
 
 /** @type {import("@rspack/core").Configuration} */
 module.exports = {
-  output: {
-    filename: "[name].js",
-    uniqueName: "runtime-plugin-with-params"
-  },
   plugins: [
     new ModuleFederationPlugin({
       name: "container-runtime-plugin-with-params",
       filename: "container.js",
       library: { type: "commonjs-module" },
-				shared: {
+      shared: {
 				react: {
 					version: false,
 					requiredVersion: false,
