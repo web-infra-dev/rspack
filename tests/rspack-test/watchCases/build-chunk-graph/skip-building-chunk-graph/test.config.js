38cd7ff026de13adbe72ebf6d8e42c655ba1c2a0
9aoy <9aoyuao@gmail.com>
2026-01-22 11:49:32 +0800

    test: add more message for chunk-graph assert error

--- tests/rspack-test/watchCases/build-chunk-graph/skip-building-chunk-graph/test.config.js
+++ tests/rspack-test/watchCases/build-chunk-graph/skip-building-chunk-graph/test.config.js
@@ -1,6 +1,6 @@
-function assert(condition) {
+function assert(condition, message) {
 	if (!condition) {
-		throw new Error("Assertion failed for");
+		throw new Error(`Assertion failed for ${message}`);
 	}
 }
 
@@ -12,19 +12,19 @@ module.exports = {
 	checkStats(stepName, _, stats) {
 		switch (stepName) {
 			case "0":
-				assert(should_rebuild(stats));
+				assert(should_rebuild(stats), "should rebuild chunk graph");
 				break;
 			case "1":
-				assert(!should_rebuild(stats));
+				assert(!should_rebuild(stats), "should not rebuild chunk graph");
 				break;
 			case "2":
-				assert(should_rebuild(stats));
+				assert(should_rebuild(stats), "should rebuild chunk graph");
 				break;
 			case "3":
-				assert(!should_rebuild(stats));
+				assert(!should_rebuild(stats), "should not rebuild chunk graph");
 				break;
 			case "4":
-				assert(should_rebuild(stats));
+				assert(should_rebuild(stats), "should rebuild chunk graph");
 				break;
 			default:
 				throw "not have more step";
