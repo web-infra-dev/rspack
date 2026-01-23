f41b0531dce461807025efef47a759c525fbc472
ahabhgk <ahabhgk@gmail.com>
2026-01-23 17:08:37 +0800

    typo

--- tests/rspack-test/configCases/source-map/extract-source-map-path-normalization/rspack.config.js
+++ tests/rspack-test/configCases/source-map/extract-source-map-path-normalization/rspack.config.js
@@ -16,11 +16,11 @@ module.exports = {
 			apply(compiler) {
 				compiler.hooks.afterCompile.tap("Test", compilation => {
 					for (const file of compilation.fileDependencies) {
-						// Should already normalized (do not contains '../' like '/extract-source-map-path-normalization/lib/../lib/src/foo.js')
+						// Should already be normalized (do not contains '../' like '/extract-source-map-path-normalization/lib/../lib/src/foo.js')
 						// In the initial start Watchpack will add the fileDependencies to watch list, and then
 						// do a scan (DirectoryWatcher.doScan) to find the actual files that exists in the fs,
 						// and use the file path (**which is already normalized by nodejs path.join()**), if the
-						// fileDependencies' path is not normalized, then file path miss match, watchpack will
+						// fileDependencies' path is not normalized, then file path mismatch, watchpack will
 						// emit a "initial-missing" event to trigger a rebuild.
 						expect(path.normalize(file)).toBe(file);
 					}
