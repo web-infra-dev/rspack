bed37d3ac61b654ec5731a3b279e067a82bbf939
pshu <pishu.spf@antfin.com>
2026-01-24 10:26:22 +0800

    chore: show stats

--- tests/rspack-test/watchCases/build-chunk-graph/skip-building-chunk-graph/test.config.js
+++ tests/rspack-test/watchCases/build-chunk-graph/skip-building-chunk-graph/test.config.js
@@ -12,19 +12,19 @@ module.exports = {
 	checkStats(stepName, _, stats) {
 		switch (stepName) {
 			case "0":
-				assert(should_rebuild(stats), "should rebuild chunk graph");
+				assert(should_rebuild(stats), stats);
 				break;
 			case "1":
-				assert(!should_rebuild(stats), "should not rebuild chunk graph");
+				assert(!should_rebuild(stats), stats);
 				break;
 			case "2":
-				assert(should_rebuild(stats), "should rebuild chunk graph");
+				assert(should_rebuild(stats), stats);
 				break;
 			case "3":
-				assert(!should_rebuild(stats), "should not rebuild chunk graph");
+				assert(!should_rebuild(stats), stats);
 				break;
 			case "4":
-				assert(should_rebuild(stats), "should rebuild chunk graph");
+				assert(should_rebuild(stats), stats);
 				break;
 			default:
 				throw "not have more step";
