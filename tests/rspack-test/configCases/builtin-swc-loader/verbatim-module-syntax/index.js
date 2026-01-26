36b6f6ffba8aa706dcb3c3a264fd56008214ca3e
hardfist <yangjianzju@gmail.com>
2026-01-26 18:54:23 +0800

    feat!:  revert enable verbatimModuleSyntax (#12846)
    Revert "feat!: enable `verbatimModuleSyntax` of `builtin:swc-loader` by default (#12668)"
    
    This reverts commit b14769ef3cb07d65d32634ace6b5af33c4d42bc5.

--- tests/rspack-test/configCases/builtin-swc-loader/verbatim-module-syntax/index.js
+++
@@ -1,8 +0,0 @@
-it("should handle type imports with verbatimModuleSyntax enabled by default", () => {
-	const { getDefaultUser, defaultRole } = require("./lib");
-	const user = getDefaultUser();
-	expect(user.name).toBe("test");
-	expect(user.age).toBe(18);
-	expect(defaultRole).toBe("user");
-});
-
