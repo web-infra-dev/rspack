33bda58cf2c65d3b5123a63e22d679d5dffd290f
jserfeng <1114550440@qq.com>
2026-01-26 17:48:19 +0800

    refactor: incremental.buildChunkGraph works only for skip building chunk graph

--- crates/rspack_core/src/compilation/build_chunk_graph/mod.rs
+++ crates/rspack_core/src/compilation/build_chunk_graph/mod.rs
@@ -10,9 +10,12 @@ pub(crate) mod pass;
 
 #[instrument("Compilation:build_chunk_graph", skip_all)]
 pub fn build_chunk_graph(compilation: &mut Compilation) -> rspack_error::Result<()> {
-  let enable_incremental = compilation
-    .incremental
-    .mutations_readable(IncrementalPasses::BUILD_CHUNK_GRAPH);
+  // TODO: heuristic incremental update is temporarily disabled
+  // Original code:
+  // let enable_incremental = compilation
+  //   .incremental
+  //   .mutations_readable(IncrementalPasses::BUILD_CHUNK_GRAPH);
+  let enable_incremental = false;
   let mut splitter = if enable_incremental {
     std::mem::take(
       &mut compilation
