41ddbee3a7fde39a1e2e5e6dd93de3d075e7d9a0
LingyuCoder <lingyucoder@gmail.com>
2026-02-03 16:55:03 +0800

    chore: release 2.0.0-alpha.2

--- crates/rspack_workspace/src/generated.rs
+++ crates/rspack_workspace/src/generated.rs
@@ -6,10 +6,10 @@ pub const fn rspack_swc_core_version() -> &'static str {
 
 /// The version of the JavaScript `@rspack/core` package.
 pub const fn rspack_pkg_version() -> &'static str {
-  "2.0.0-alpha.1"
+  "2.0.0-alpha.2"
 }
 
 /// The version of the Rust workspace in the root `Cargo.toml` of the repository.
 pub const fn rspack_workspace_version() -> &'static str {
-  "0.100.0-alpha.1"
+  "0.100.0-alpha.2"
 }
