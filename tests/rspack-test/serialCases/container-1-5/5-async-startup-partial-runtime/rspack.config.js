e7261b7e5cdea885c9301507a708441142441839
neverland <chenjiahan.jait@bytedance.com>
2026-02-05 16:44:00 +0800

    fix

--- tests/rspack-test/serialCases/container-1-5/5-async-startup-partial-runtime/rspack.config.js
+++ tests/rspack-test/serialCases/container-1-5/5-async-startup-partial-runtime/rspack.config.js
@@ -9,7 +9,7 @@ const remoteContext = path.resolve(__dirname, '../0-container-full');
 // Reuse the real remote container config so the case exercises emitted remotes.
 const remoteConfigs = require('../0-container-full/rspack.config.js').map(
 	config => {
-		const isModule = config.experiments;
+		const isModule = config.output.module;
 		return {
 			...config,
 			context: remoteContext,
