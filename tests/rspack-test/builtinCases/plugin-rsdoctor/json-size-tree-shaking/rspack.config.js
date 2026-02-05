7f5108ffcfd3a13bad9b596f93c9611b08ac1c4c
easy1090 <congzhe@bytedance.com>
2026-02-05 16:41:44 +0800

    feat: add the map of json data size to rspack_plugin_rsdoctor for use in rsdoctor

--- tests/rspack-test/builtinCases/plugin-rsdoctor/json-size-tree-shaking/rspack.config.js
+++ tests/rspack-test/builtinCases/plugin-rsdoctor/json-size-tree-shaking/rspack.config.js
@@ -1,23 +1,25 @@
-const rspack = require("@rspack/core");
+const {
+  experiments: {
+    RsdoctorPlugin
+  }
+} = require("@rspack/core");
 
 /** @type {import('@rspack/core').Configuration} */
 module.exports = {
-	builtins: {
-		define: {
-			"process.env.NODE_ENV": "'production'",
-		}
-	},
-	optimization: {
-		providedExports: true,
-		usedExports: true,
-		sideEffects: false,
-	},
-	plugins: [
-		new rspack.BuiltinPlugin({
-			name: "rspack.RsdoctorPlugin",
-			options: {
-				moduleGraphFeatures: ["sources"], // Enable module sources feature to collect JSON sizes
-			}
-		})
-	]
+  builtins: {
+    define: {
+      "process.env.NODE_ENV": "'production'",
+    }
+  },
+  optimization: {
+    providedExports: true,
+    usedExports: true,
+    sideEffects: false,
+  },
+  plugins: [
+    new RsdoctorPlugin({
+      moduleGraphFeatures: ["sources"], // Enable module sources feature to collect JSON sizes
+      // chunkGraphFeatures: false
+    })
+  ]
 };
