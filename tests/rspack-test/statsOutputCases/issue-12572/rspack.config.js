cfe03e692ef4376f47bd2a1e74ce6929a465dd42
Cong-Cong <dacongsama@live.com>
2026-02-11 13:49:49 +0800

    chore: add test case

---
+++ tests/rspack-test/statsOutputCases/issue-12572/rspack.config.js
@@ -0,0 +1,19 @@
+var webpack = require("@rspack/core");
+/** @type {import("@rspack/core").Configuration[]} */
+module.exports = {
+    mode: "production",
+    entry: "./index",
+    plugins: [
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1
+        })
+    ],
+    stats: {
+        assets: true,
+        chunkModules: true,
+        dependentModules: true,
+        chunkRelations: true,
+        modules: false,
+        chunks: true
+    }
+};
