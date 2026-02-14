d7a10556c560726da7f2ea066119fe4ca8ca3f73
CPunisher <1343316114@qq.com>
2026-02-14 15:00:33 +0800

    refactor: estree.rs

--- crates/rspack_util/src/atom.rs
+++ crates/rspack_util/src/atom.rs
@@ -1,19 +1,1 @@
-use swc_core::ecma::ast::ModuleExportName;
-
 pub type Atom = swc_core::atoms::Atom;
-
-pub trait ModuleExportNameExt {
-  fn atom_ref(&self) -> &Atom;
-}
-
-impl ModuleExportNameExt for ModuleExportName {
-  fn atom_ref(&self) -> &Atom {
-    match self {
-      ModuleExportName::Ident(ident) => &ident.sym,
-      ModuleExportName::Str(s) => s
-        .value
-        .as_atom()
-        .expect("ModuleExportName should be a valid utf8"),
-    }
-  }
-}
