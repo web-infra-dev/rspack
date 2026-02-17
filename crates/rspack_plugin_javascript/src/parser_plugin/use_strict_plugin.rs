758cd95ff98e0ab7a643a569d65d31379549b0fe
CPunisher <1343316114@qq.com>
2026-02-17 11:03:14 +0800

    refactor: use_strict_plugin.rs

--- crates/rspack_plugin_javascript/src/parser_plugin/use_strict_plugin.rs
+++ crates/rspack_plugin_javascript/src/parser_plugin/use_strict_plugin.rs
@@ -1,6 +1,6 @@
 use rspack_core::ConstDependency;
 use swc_core::atoms::wtf8::Wtf8;
-use swc_experimental_ecma_ast::{Lit, Program};
+use swc_experimental_ecma_ast::{Lit, Program, Spanned};
 
 use super::JavascriptParserPlugin;
 use crate::visitors::JavascriptParser;
@@ -10,19 +10,25 @@ pub struct UseStrictPlugin;
 impl JavascriptParserPlugin for UseStrictPlugin {
   fn program(&self, parser: &mut JavascriptParser, ast: Program) -> Option<bool> {
     let first = match ast {
-      Program::Module(ast) => ast.body(&parser.ast).first().and_then(|i| i.as_stmt()),
-      Program::Script(ast) => ast.body(&parser.ast).first(),
+      Program::Module(ast) => ast
+        .body(&parser.ast)
+        .first()
+        .and_then(|i| parser.ast.get_node_in_sub_range(i).as_stmt()),
+      Program::Script(ast) => ast
+        .body(&parser.ast)
+        .first()
+        .map(|i| parser.ast.get_node_in_sub_range(i)),
     }
     .and_then(|i| i.as_expr());
     if let Some(first) = first
       && first.expr(&parser.ast).as_lit().and_then(|i| match i {
-        Lit::Str(s) => Some(s.value(&parser.ast).as_wtf8()),
+        Lit::Str(s) => Some(parser.ast.get_wtf8(s.value(&parser.ast))),
         _ => None,
       }) == Some(Wtf8::from_str("use strict"))
     {
       // Remove "use strict" expression. It will be added later by the renderer again.
       // This is necessary in order to not break the strict mode when webpack prepends code.
-      let dep = ConstDependency::new(first.span.into(), "".into());
+      let dep = ConstDependency::new(first.span(&parser.ast).into(), "".into());
       parser.add_presentational_dependency(Box::new(dep));
       parser.build_info.strict = true;
     }
