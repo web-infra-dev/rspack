230a95995133ddfcc7582a076cdfa41f05ff4d33
Andrei Ancuta <ancuta@adobe.com>
2026-03-09 10:56:48 +0200

    slightly more thorough tests

--- tests/rspack-test/configCases/optimization/hashed-module-ids-options/index.js
+++ tests/rspack-test/configCases/optimization/hashed-module-ids-options/index.js
@@ -1,10 +1,25 @@
 it("should have unique hashed module ids with custom options", function () {
-	var ids = [];
+	var ids = new Set();
 	for (var i = 1; i <= 5; i++) {
 		var id = require("./files/file" + i + ".js");
 		expect(typeof id).toBe("string");
 		expect(id.length).toBeGreaterThanOrEqual(1);
-		expect(ids.indexOf(id)).toBe(-1);
-		ids.push(id);
+		expect(ids.has(id)).toBe(false);
+		expect(Number.isNaN(Number(id))).toBe(true);
+		ids.add(id);
 	}
+	expect(ids.size).toBe(5);
+});
+
+it("should assign a hashed id to the entry module", function () {
+	var entryId = module.id;
+	expect(typeof entryId).toBe("string");
+	expect(entryId.length).toBeGreaterThanOrEqual(1);
+	expect(Number.isNaN(Number(entryId))).toBe(true);
+});
+
+it("should produce deterministic ids", function () {
+	var id1 = require("./files/file1.js");
+	var id2 = require("./files/file1.js");
+	expect(id1).toBe(id2);
 });
--- tests/rspack-test/configCases/optimization/hashed-module-ids/index.js
+++ tests/rspack-test/configCases/optimization/hashed-module-ids/index.js
@@ -1,10 +1,25 @@
 it("should have unique hashed module ids", function () {
-	var ids = [];
+	var ids = new Set();
 	for (var i = 1; i <= 5; i++) {
 		var id = require("./files/file" + i + ".js");
 		expect(typeof id).toBe("string");
 		expect(id.length).toBeGreaterThanOrEqual(4);
-		expect(ids.indexOf(id)).toBe(-1);
-		ids.push(id);
+		expect(ids.has(id)).toBe(false);
+		expect(Number.isNaN(Number(id))).toBe(true);
+		expect(id).toMatch(/^[A-Za-z0-9+/]+=*$/);
+		ids.add(id);
 	}
+	expect(ids.size).toBe(5);
+});
+
+it("should produce the same id for the same module", function () {
+	var id1 = require("./files/file1.js");
+	var id2 = require("./files/file1.js");
+	expect(id1).toBe(id2);
+});
+
+it("should produce different ids for different modules", function () {
+	var id1 = require("./files/file1.js");
+	var id2 = require("./files/file2.js");
+	expect(id1).not.toBe(id2);
 });
