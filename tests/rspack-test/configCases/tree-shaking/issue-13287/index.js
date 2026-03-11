09f3d56ca6684df135c63c2f8f84183b8af9d696
ahabhgk <ahabhgk@gmail.com>
2026-03-11 15:51:42 +0800

    test

--- tests/rspack-test/configCases/tree-shaking/issue-13287/index.js
+++ tests/rspack-test/configCases/tree-shaking/issue-13287/index.js
@@ -1,4 +1,5 @@
 import { pkg, bin } from "./shared";
+import { name, version, rest } from "./shared2";
 
 it("should preserve json properties used via an exported require result", () => {
 	const { name, version } = pkg;
@@ -6,4 +7,10 @@ it("should preserve json properties used via an exported require result", () => 
 	expect(bin).toBe("cli");
 	expect(name).toBe("rspack-cjs-treeshaking");
 	expect(version).toBe("1.0.0");
+});
+
+it("should preserve destructured exports from require", () => {
+	expect(name).toBe("rspack-cjs-treeshaking");
+	expect(version).toBe("1.0.0");
+	expect(Object.keys(rest.bin)[0]).toBe("cli");
 });
--- tests/rspack-test/configCases/tree-shaking/issue-13287/shared.js
+++ tests/rspack-test/configCases/tree-shaking/issue-13287/shared.js
@@ -1,2 +1,2 @@
-export const pkg = require("./data.json");
+export const pkg = require("./data.json?1");
 export const bin = Object.keys(pkg.bin)[0];
