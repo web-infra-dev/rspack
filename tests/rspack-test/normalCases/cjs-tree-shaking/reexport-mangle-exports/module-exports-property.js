0f42927a75d8e02d340a32a60ccadfd4695d4206
ahabhgk <ahabhgk@gmail.com>
2026-03-19 17:02:58 +0800

    feat: enable mangle exports for cjs reexport

---
+++ tests/rspack-test/normalCases/cjs-tree-shaking/reexport-mangle-exports/module-exports-property.js
@@ -0,0 +1,4 @@
+module.exports.aaa = require("./source?property").aaa;
+module.exports.aaaCanMangle = require("./source?property").aaaCanMangle;
+module.exports.usedExports = require("./source?property").usedExports;
+module.exports.aaaReexportedCanMangle = __webpack_exports_info__.aaa.canMangle;
