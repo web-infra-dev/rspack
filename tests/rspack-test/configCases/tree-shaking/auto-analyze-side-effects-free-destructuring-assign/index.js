529adfe6858d577478e45dee1142533cc6fe5a03
jserfeng <1114550440@qq.com>
2026-03-26 14:17:29 +0800

    test(tree-shaking): fix destructuring mutation expectation

--- tests/rspack-test/configCases/tree-shaking/auto-analyze-side-effects-free-destructuring-assign/index.js
+++ tests/rspack-test/configCases/tree-shaking/auto-analyze-side-effects-free-destructuring-assign/index.js
@@ -3,5 +3,5 @@ import "./consumer";
 
 it("should keep destructuring-reassigned functions impure", () => {
   expect(globalThis.sideEffectCount).toBe(2);
-  expect(Reflect.ownKeys(__webpack_modules__).length).toBe(4);
+  expect(Reflect.ownKeys(__webpack_modules__).length).toBe(5);
 });
