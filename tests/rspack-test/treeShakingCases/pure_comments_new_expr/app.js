b1666499f7582baf194125b2477d8c1376d6a4eb
Fy <1114550440@qq.com>
2026-03-27 15:56:11 +0800

    fix: tighten pure comment matching (#13498)
    fix(javascript): tighten pure comment matching

---
+++ tests/rspack-test/treeShakingCases/pure_comments_new_expr/app.js
@@ -0,0 +1,10 @@
+function createSet() {
+	return new Set();
+}
+
+export const marker = 1;
+
+let directNew = /*#__PURE__*/ new Set();
+let extraTextNew = /* #__PURE__ additional text */ new Set();
+let atPureNew = /* @__PURE__ additional text */ new Set();
+let notPureComment = /* note: keep webpack-compatible #__PURE__ syntax here */ createSet();
