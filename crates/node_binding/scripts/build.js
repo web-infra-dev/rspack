ceba3e6e368ddedad5df34eb02ddb007c82f8b36
CPunisher <1343316114@qq.com>
2026-03-30 22:45:14 +0800

    chore: adds `-Zfmt-debug=none` for non-browser wasm target (#13527)
    * Revert renaming rspack browser
    
    * chore: update uploaded binding names
    
    * chore: download browser bindings
    
    * Revert "chore: download browser bindings"
    
    This reverts commit 4cd6ea0b56e5d5767f085d738fdc09fb03f1fbe2.
    
    * Revert "chore: update uploaded binding names"
    
    This reverts commit 136614458c49f70db62e734d8a14304879b262bb.
    
    * Revert "Revert renaming rspack browser"
    
    This reverts commit b2269146aebe414a963c546f1a10a14aea0678a1.
    
    * Revert "chore: split wasm browser ci (#13504)"
    
    This reverts commit 5f17956633b415c37330457bb853078bf31d9132.
    
    * chore: strip fmt debug for wasm

--- crates/node_binding/scripts/build.js
+++ crates/node_binding/scripts/build.js
@@ -1,5 +1,5 @@
 const path = require("node:path");
-const { readFileSync, writeFileSync } = require("node:fs");
+const { readFileSync, writeFileSync, renameSync } = require("node:fs");
 const { values, positionals } = require("node:util").parseArgs({
 	args: process.argv.slice(2),
 	options: {
@@ -63,6 +63,10 @@ async function build() {
 		}
 		if (process.env.RUST_TARGET) {
 			args.push("--target", process.env.RUST_TARGET);
+			if (process.env.RUST_TARGET.includes("wasm32")) {
+				// Strip debug format to reduce wasm size of wasm artifacts
+				rustflags.push("-Zfmt-debug=none");
+			}
 		}
 		if (!process.env.DISABLE_PLUGIN) {
 			args.push("--no-default-features");
@@ -70,8 +74,6 @@ async function build() {
 		}
 		if (process.env.RSPACK_TARGET_BROWSER) {
 			features.push("browser")
-			// Strip debug format to reduce wasm size of @rspack/browser
-			rustflags.push("-Zfmt-debug=none");
 		}
 		args.push("--no-dts-cache");
 		if (process.env.SFTRACE) {
@@ -144,6 +146,12 @@ async function build() {
 						// This allows users to extend NormalModule with static methods through type augmentation.
 						.replaceAll(/export\s+declare\s+class\s+NormalModule\s*\{([\s\S]*?)\}\s*(?=\n\s*(?:export|declare|class|$))/g, "")
 				);
+
+				// For browser wasm, we rename the artifacts to distinguish them from node wasm
+				if (process.env.RSPACK_TARGET_BROWSER) {
+					renameSync("rspack.wasm32-wasi.debug.wasm", "rspack.browser.debug.wasm")
+					renameSync("rspack.wasm32-wasi.wasm", "rspack.browser.wasm")
+				}
 
 				if (process.env.TRACY) {
 					// split debug symbols for tracy
