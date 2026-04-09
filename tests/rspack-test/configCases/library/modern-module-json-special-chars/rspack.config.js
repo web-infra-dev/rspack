426bcbc5035ec3b5d5c2af3764b6c82b19e103c4
CPunisher <1343316114@qq.com>
2026-04-09 15:14:53 +0800

    chore: fmt

--- tests/rspack-test/configCases/library/modern-module-json-special-chars/rspack.config.js
+++ tests/rspack-test/configCases/library/modern-module-json-special-chars/rspack.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-entry: {
-main: {import: './index.js', filename: 'bundle.mjs'},
-json: {import: './data.json', filename: 'json.mjs'},
-},
-output: {
-module: true,
-library: {
-type: 'modern-module',
-},
-},
-module: {
-parser: {
-javascript: {
-importMeta: false
-}
-}
-},
-}
+  entry: {
+    main: { import: './index.js', filename: 'bundle.mjs' },
+    json: { import: './data.json', filename: 'json.mjs' },
+  },
+  output: {
+    module: true,
+    library: {
+      type: 'modern-module',
+    },
+  },
+  module: {
+    parser: {
+      javascript: {
+        importMeta: false,
+      },
+    },
+  },
+};
