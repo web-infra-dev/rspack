e518506d5f549bf587ac87e45634b2c3cbc55e59
pshu <pishu.spf@antfin.com>
2026-04-09 10:52:01 +0800

    refactor: disable NODE_PATH fallback by default, revert docs

--- crates/rspack_core/src/resolver/resolver_impl.rs
+++ crates/rspack_core/src/resolver/resolver_impl.rs
@@ -322,7 +322,7 @@ fn to_rspack_resolver_options(
     builtin_modules: options.builtin_modules,
     imports_fields,
     enable_pnp: options.pnp.unwrap_or(false),
-    node_path: true,
+    node_path: false,
   }
 }
 
