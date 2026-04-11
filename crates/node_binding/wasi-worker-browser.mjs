b60b40a659c59735695271d15e52f5739cc9fac2
hardfist <yangjianzju@gmail.com>
2026-04-11 19:08:45 +0800

    chore: update

--- crates/node_binding/wasi-worker-browser.mjs
+++ crates/node_binding/wasi-worker-browser.mjs
@@ -16,10 +16,10 @@ const handler = new MessageHandler({
         // eslint-disable-next-line no-console
         console.log.apply(console, arguments)
       },
-      printErr: function() {
+      printErr: function () {
         // eslint-disable-next-line no-console
         console.error.apply(console, arguments)
-        
+
         errorOutputs.push([...arguments])
       },
     })
@@ -32,6 +32,10 @@ const handler = new MessageHandler({
           ...importObject.napi,
           ...importObject.emnapi,
           memory: wasmMemory,
+          // Override emnapi's napi_adjust_external_memory to a no-op.
+          // emnapi implements this by calling memory.grow, but we've disabled memory.grow
+          // (initial == maximum).
+          napi_adjust_external_memory() { return 0 },
         }
       },
     })
