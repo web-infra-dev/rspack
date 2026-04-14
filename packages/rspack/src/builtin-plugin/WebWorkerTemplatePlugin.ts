439f83f4a7b5d0095be0fee00f5ebbf65ccb5fe9
ahabhgk <ahabhgk@gmail.com>
2026-04-14 14:35:55 +0800

    fix(core): implement jsonp and webworker template plugins in js

--- packages/rspack/src/builtin-plugin/WebWorkerTemplatePlugin.ts
+++ packages/rspack/src/builtin-plugin/WebWorkerTemplatePlugin.ts
@@ -1,13 +1,1 @@
-import { type BuiltinPlugin, BuiltinPluginName } from '@rspack/binding';
-
-import type { Compiler } from '../Compiler';
-import { createBuiltinPlugin, RspackBuiltinPlugin } from './base';
-
-export class WebWorkerTemplatePlugin extends RspackBuiltinPlugin {
-  name = BuiltinPluginName.WebWorkerTemplatePlugin;
-
-  raw(compiler: Compiler): BuiltinPlugin | undefined {
-    compiler.options.output.chunkLoading = 'import-scripts';
-    return createBuiltinPlugin(this.name, undefined);
-  }
-}
+export { default as WebWorkerTemplatePlugin } from '../webworker/WebWorkerTemplatePlugin';
