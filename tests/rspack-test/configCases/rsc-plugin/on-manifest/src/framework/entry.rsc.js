86fc7b84c0c40161bc45509d193020ff347a83a8
Cong-Cong <dacongsama@live.com>
2026-04-21 13:53:24 +0800

    fix: test case

--- tests/rspack-test/configCases/rsc-plugin/on-manifest/src/framework/entry.rsc.js
+++ tests/rspack-test/configCases/rsc-plugin/on-manifest/src/framework/entry.rsc.js
@@ -4,21 +4,3 @@ import { App } from '../App';
 export const renderRscStream = () => {
 	return renderToReadableStream(<App />);
 };
-
-it('should resolve css resources for client boundaries with resource queries', async () => {
-	const cssFiles = __rspack_rsc_manifest__.clientManifest[CLIENT_PATH].cssFiles;
-	expect(cssFiles.length).toBe(1);
-
-	let streamOutput = '';
-	const stream = await renderRscStream();
-	const reader = stream.getReader();
-	const decoder = new TextDecoder();
-	while (true) {
-		const { done, value } = await reader.read();
-		if (done) break;
-		if (value) streamOutput += decoder.decode(value, { stream: true });
-	}
-	streamOutput += decoder.decode();
-
-	expect(streamOutput).toContain(cssFiles[0]);
-});
