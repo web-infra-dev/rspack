3c0eafe85c992199a815b936193a47cfcebf73e2
hardfist <yangjianzju@gmail.com>
2026-04-28 10:46:02 +0800

    fix: enable module static cache in development (#13856)

--- crates/rspack_core/src/compilation/run_passes.rs
+++ crates/rspack_core/src/compilation/run_passes.rs
@@ -43,17 +43,13 @@ impl Compilation {
       Box::new(AfterProcessAssetsPass),
       Box::new(AfterSealPass),
     ];
-    if !self.options.mode.is_development() {
-      self.module_static_cache.enable_new_cache();
-    }
+    self.module_static_cache.enable_new_cache();
 
     for pass in &passes {
       pass.run(self, cache).await?;
     }
 
-    if !self.options.mode.is_development() {
-      self.module_static_cache.disable_cache();
-    }
+    self.module_static_cache.disable_cache();
 
     Ok(())
   }
