7c3f9664f9fc7d7771dc95f9727b00cbf3732279
Cong-Cong Pan <dacongsama@live.com>
2026-05-08 16:35:52 +0800

    fix: JsRscCssLinkOptions

--- crates/rspack_binding_api/src/plugins/rsc.rs
+++ crates/rspack_binding_api/src/plugins/rsc.rs
@@ -5,7 +5,7 @@ use napi::{
   Env, Status,
   bindgen_prelude::{
     ClassInstance, Either, Either3, External, ExternalRef, FromNapiValue, Function, JsObjectValue,
-    Null, Object, Promise, Reference, Undefined, WeakReference,
+    Null, Object, Promise, Reference, Undefined, ValidateNapiValue, WeakReference, sys,
   },
   threadsafe_function::ThreadsafeFunction,
 };
@@ -15,6 +15,7 @@ use rspack_error::ToStringResultToRspackResultExt;
 use rspack_plugin_rsc::{
   Coordinator, OnManifest, RscClientPluginOptions, RscCssLinkProps, RscServerPluginOptions,
 };
+use rspack_util::fx_hash::FxIndexMap;
 
 use crate::JsCompiler;
 
@@ -77,31 +78,50 @@ impl From<&JsRscClientPluginOptions<'_>> for RscClientPluginOptions {
   }
 }
 
-#[napi(object, object_to_js = false)]
-pub struct JsRscCssLinkOptions<'a> {
-  #[napi(ts_type = "string | false")]
+#[napi(object, object_from_js = false, object_to_js = false)]
+pub struct JsRscCssLinkOptions {
   pub precedence: Option<Either<String, bool>>,
   #[napi(ts_type = "Record<string, string>")]
-  pub props: Option<Object<'a>>,
+  pub props: Option<FxIndexMap<String, String>>,
+}
+
+impl FromNapiValue for JsRscCssLinkOptions {
+  unsafe fn from_napi_value(env: sys::napi_env, napi_val: sys::napi_value) -> napi::Result<Self> {
+    let object = unsafe { Object::from_napi_value(env, napi_val)? };
+    Ok(Self {
+      precedence: object.get::<Either<String, bool>>("precedence")?,
+      props: object
+        .get::<Object>("props")?
+        .map(object_to_css_link_props)
+        .transpose()?,
+    })
+  }
+}
+
+impl ValidateNapiValue for JsRscCssLinkOptions {
+  unsafe fn validate(
+    env: sys::napi_env,
+    napi_val: sys::napi_value,
+  ) -> napi::Result<sys::napi_value> {
+    unsafe { Object::validate(env, napi_val) }
+  }
 }
 
 #[napi(object, object_to_js = false)]
 pub struct JsRscServerPluginOptions<'a> {
   pub coordinator: ClassInstance<'a, JsCoordinator>,
-  pub css_link: Option<Either3<JsRscCssLinkOptions<'a>, Undefined, Null>>,
+  #[napi(ts_type = "JsRscCssLinkOptions | undefined | null")]
+  pub css_link: Option<Either3<JsRscCssLinkOptions, Undefined, Null>>,
   #[napi(ts_type = "(() => void | Promise<void>) | undefined | null")]
   pub on_server_component_changes:
     Option<Either3<Function<'static, (), OnServerComponentChangesReturn>, Undefined, Null>>,
   pub on_manifest: Option<Either3<Function<'static, String, Promise<()>>, Undefined, Null>>,
 }
 
-fn object_to_css_link_props(object: Object<'_>) -> napi::Result<RscCssLinkProps> {
-  let mut props = RscCssLinkProps::default();
+fn object_to_css_link_props(object: Object<'_>) -> napi::Result<FxIndexMap<String, String>> {
+  let mut props = FxIndexMap::default();
   let keys = Object::keys(&object)?;
   for key in keys {
-    if key == "precedence" {
-      continue;
-    }
     if let Some(value) = object.get::<String>(&key)? {
       props.insert(key, value);
     }
@@ -110,21 +130,16 @@ fn object_to_css_link_props(object: Object<'_>) -> napi::Result<RscCssLinkProps>
 }
 
 fn normalize_css_link_props(
-  css_link_props: Option<Either3<JsRscCssLinkOptions<'_>, Undefined, Null>>,
-) -> napi::Result<RscCssLinkProps> {
+  css_link_props: Option<Either3<JsRscCssLinkOptions, Undefined, Null>>,
+) -> RscCssLinkProps {
   match css_link_props {
     None | Some(Either3::B(_) | Either3::C(_)) => {
       let mut props = RscCssLinkProps::default();
       props.insert("precedence".to_string(), "default".to_string());
-      Ok(props)
+      props
     }
     Some(Either3::A(css_link_options)) => {
-      let mut props = css_link_options
-        .props
-        .map(object_to_css_link_props)
-        .transpose()?
-        .unwrap_or_default();
-
+      let mut props = css_link_options.props.unwrap_or_default();
       match css_link_options.precedence {
         Some(Either::A(precedence)) => {
           props.insert("precedence".to_string(), precedence);
@@ -136,8 +151,7 @@ fn normalize_css_link_props(
           props.insert("precedence".to_string(), "default".to_string());
         }
       }
-
-      Ok(props)
+      props
     }
   }
 }
@@ -202,7 +216,7 @@ impl TryFrom<JsRscServerPluginOptions<'_>> for RscServerPluginOptions {
       coordinator: value.coordinator.i.clone(),
       on_server_component_changes,
       on_manifest,
-      css_link_props: normalize_css_link_props(value.css_link)?,
+      css_link_props: normalize_css_link_props(value.css_link),
     })
   }
 }
