00ffc6b5a4a4c25ec4415c91f83e0feefa9b8d5d
hardfist <yangjianzju@gmail.com>
2026-05-08 18:10:25 +0800

    test: configure rayon for codspeed benchmarks

--- xtask/benchmark/benches/benches.rs
+++ xtask/benchmark/benches/benches.rs
@@ -1,15 +1,23 @@
 #![allow(clippy::unwrap_used)]
 
-use criterion::criterion_main;
+use criterion::{Criterion, criterion_group, criterion_main};
 use groups::{
   build_chunk_graph::chunk_graph, bundle::bundle, compilation_stages::compilation_stages,
   module_graph_api::module_graph_api, persistent_cache::persistent_cache,
   scan_dependencies::scan_dependencies,
 };
+use rspack_core::configure_rayon_current_thread_for_codspeed;
 
 mod groups;
 
+fn configure_rayon_for_codspeed(_: &mut Criterion) {
+  configure_rayon_current_thread_for_codspeed();
+}
+
+criterion_group!(codspeed_setup, configure_rayon_for_codspeed);
+
 criterion_main!(
+  codspeed_setup,
   chunk_graph,
   module_graph_api,
   scan_dependencies,
