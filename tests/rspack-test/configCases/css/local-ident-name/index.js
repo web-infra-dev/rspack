24d4e11b53ab352f16e8c13e63e0ed00b1c32819
intellild <intellild@outlook.com>
2026-05-13 13:42:39 +0800

    test(css): add css module local ident option cases

--- tests/rspack-test/configCases/css/local-ident-name/index.js
+++ tests/rspack-test/configCases/css/local-ident-name/index.js
@@ -1,5 +1,26 @@
-import { foo } from './index.module.css'
+const path = __non_webpack_require__("path");
 
-it("should generate correct css", () => {
-	expect(foo).toBe("foo-XzZlYz-_6ec1c834ac8cc99e")
-})
+it("should have correct local ident for css export locals", async () => {
+  const [idLocal, hash, hashLocal, pathNameLocal, fileLocal, queryFragment, uniqueNameIdContenthash, less] = await Promise.all([
+    import("./style.module.css"),
+    import("./style.module.css?hash"),
+    import("./style.module.css?hash-local"),
+    import("./style.module.css?path-name-local"),
+    import("./style.module.css?file-local"),
+    import("./style.module.css?q#f"),
+    import("./style.module.css?uniqueName-id-contenthash"),
+    import("./style.module.css?hash-local-custom"),
+    import("./style.module.less"),
+  ]);
+
+  expect(idLocal).toMatchFileSnapshotSync(path.join(__SNAPSHOT__, "id-local.txt"));
+  expect(hash).toMatchFileSnapshotSync(path.join(__SNAPSHOT__, "hash.txt"));
+  expect(hashLocal).toMatchFileSnapshotSync(path.join(__SNAPSHOT__, "hash-local.txt"));
+  expect(pathNameLocal).toMatchFileSnapshotSync(path.join(__SNAPSHOT__, "path-name-local.txt"));
+  expect(fileLocal).toMatchFileSnapshotSync(path.join(__SNAPSHOT__, "file-local.txt"));
+  expect(queryFragment).toMatchFileSnapshotSync(path.join(__SNAPSHOT__, "query-fragment.txt"));
+  expect(uniqueNameIdContenthash).toMatchFileSnapshotSync(
+    path.join(__SNAPSHOT__, "unique-name-id-contenthash.txt")
+  );
+  expect(less).toMatchFileSnapshotSync(path.join(__SNAPSHOT__, "less.txt"));
+});
--- tests/rspack-test/configCases/css/local-ident-name/rspack.config.js
+++ tests/rspack-test/configCases/css/local-ident-name/rspack.config.js
@@ -1,16 +1,83 @@
-/** @type {import("@rspack/core").Configuration} */
-module.exports = {
+const common = {
+  mode: 'development',
+  devtool: false,
   module: {
-    generator: {
-      'css/auto': {
-        localIdentName: '[local]-[hash:base64:6]-[hash]',
-      },
-    },
     rules: [
+      {
+        test: /\.less$/,
+        type: 'css/auto',
+        use: ['less-loader'],
+        generator: {
+          localIdentName: '[path][name][ext]__[local]',
+        },
+      },
       {
         test: /\.css$/,
         type: 'css/auto',
+        oneOf: [
+          {
+            resourceQuery: /\?hash$/,
+            generator: {
+              localIdentName: '[hash]',
+            },
+          },
+          {
+            resourceQuery: /\?hash-local$/,
+            generator: {
+              localIdentName: '[hash]-[local]',
+            },
+          },
+          {
+            resourceQuery: /\?path-name-local$/,
+            generator: {
+              localIdentName: '[path][name]__[local]',
+            },
+          },
+          {
+            resourceQuery: /\?file-local$/,
+            generator: {
+              localIdentName: '[file]__[local]',
+            },
+          },
+          {
+            resourceQuery: /\?q$/,
+            resourceFragment: /#f$/,
+            generator: {
+              localIdentName: '[file][query][fragment]__[local]',
+            },
+          },
+          {
+            resourceQuery: /\?uniqueName-id-contenthash$/,
+            generator: {
+              localIdentName: '[uniqueName]-[id]-[contenthash]',
+            },
+          },
+          {
+            resourceQuery: /\?hash-local-custom$/,
+            generator: {
+              localIdentHashSalt: 'salt',
+              localIdentHashDigest: 'base26',
+              localIdentHashDigestLength: 16,
+              localIdentName: '[hash]-[local]',
+            },
+          },
+        ],
       },
     ],
   },
+  experiments: {
+    css: true,
+  },
 };
+
+/** @type {import("@rspack/core").Configuration} */
+module.exports = [
+  {
+    ...common,
+    target: 'web',
+  },
+  {
+    ...common,
+    target: 'node',
+  },
+];
