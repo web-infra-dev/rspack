fb8810fef9b786a6c4d5737dcca74f5849dc92ad
jserfeng <1114550440@qq.com>
2026-06-03 10:08:27 +0800

    fix: keep parsing asset urls with extra args

--- crates/rspack_plugin_javascript/src/parser_plugin/url_plugin.rs
+++ crates/rspack_plugin_javascript/src/parser_plugin/url_plugin.rs
@@ -52,9 +52,6 @@ pub fn get_url_request(
   expr: &NewExpr,
 ) -> Option<(String, u32, u32)> {
   let args = expr.args.as_ref()?;
-  if args.len() > 2 {
-    return None;
-  }
   let ExprOrSpread {
     spread: None,
     expr: arg1,
@@ -122,9 +119,6 @@ impl JavascriptParserPlugin for URLPlugin {
     }
 
     let args = expr.args.as_ref()?;
-    if args.len() > 2 {
-      return None;
-    }
 
     let arg = args.first()?;
     let magic_comment_options = try_extract_magic_comment(parser, expr.span, arg.span());
