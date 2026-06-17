74fce3fbe34073941db39e8daad58ae9b388b458
LingyuCoder <lingyucoder@gmail.com>
2026-06-17 10:57:03 +0800

    test: align runtime mode cases after main merge

---
+++ tests/rspack-test/configCases/node/filename-and-dirname-universal/warnings.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = [
+  /"import\.meta\.dirname" is used and has been mocked/,
+  /"import\.meta\.filename" is used and has been mocked/,
+  /"import\.meta\.dirname" is used and has been mocked/,
+  /"import\.meta\.filename" is used and has been mocked/,
+  /"import\.meta\.dirname" is used and has been mocked/,
+  /"import\.meta\.filename" is used and has been mocked/,
+  /"import\.meta\.dirname" is used and has been mocked/,
+  /"import\.meta\.filename" is used and has been mocked/,
+];
