99709aee70c551b9110c03dc7ba3c83642a5e51b
zhanghang.heal <zhanghang.heal@bytedance.com>
2026-06-23 14:13:05 +0800

    test: fix MF SSR clear cache CI failures

--- tests/rspack-test/configCases/container-1-0/environment-es5/index.js
+++ tests/rspack-test/configCases/container-1-0/environment-es5/index.js
@@ -5,6 +5,6 @@ it("should have single runtime chunk", async () => {
 	await import("./bootstrap");
 	const code = await fs.promises.readFile(path.resolve(__dirname, "container-a.js"), "utf-8");
 	expect(code).not.toContain("=>");
-	expect(code).not.toContain("const");
-	expect(code).not.toContain("let");
+	expect(code).not.toMatch(/\bconst\b/);
+	expect(code).not.toMatch(/\blet\b/);
 });
