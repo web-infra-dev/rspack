015cd4065582beba29fef463f73cd2ff7a6ffd45
hardfist <1562502418@qq.com>
2026-06-30 16:02:02 +0800

    test: move misc benchmark to walltime suite

--- xtask/benchmark/benches/walltime_groups/bundle.rs
+++ xtask/benchmark/benches/walltime_groups/bundle.rs
@@ -1,3 +1,5 @@
+#[path = "../groups/bundle/misc.rs"]
+pub mod misc;
 #[path = "../groups/bundle/threejs_10x.rs"]
 pub mod threejs_10x;
 #[path = "../groups/bundle/util.rs"]
--- xtask/benchmark/cases/bundle_misc_production_sourcemap.rs
+++
@@ -1,3 +0,0 @@
-case_entry!(|c| {
-  crate::groups::bundle::bundle_benchmark_case(c, "misc-production-sourcemap");
-});
