d3776ac5cb31fd6b86678469e2900a3efd84a07c
intellild <intellild@outlook.com>
2026-07-10 15:04:48 +0800

    test: improve worker relative URL coverage

--- tests/rspack-test/configCases/library/modern-module-worker-new-url-relative/rspack.config.js
+++ tests/rspack-test/configCases/library/modern-module-worker-new-url-relative/rspack.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   mode: 'none',
   output: {
-    module: true,
     filename: 'main.js',
     chunkFilename: '[name].bundle.js',
-    publicPath: '/public/',
-    workerPublicPath: '/workers/',
+    library: {
+      type: 'modern-module',
+    },
   },
   module: {
     parser: {
