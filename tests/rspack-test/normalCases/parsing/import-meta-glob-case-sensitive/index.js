3edc0d9cb66eba0684f2a61fa576c09d06cb04ee
intellild <intellild@outlook.com>
2026-07-16 08:35:48 +0000

    test: align import.meta.glob case sensitivity coverage

--- tests/rspack-test/normalCases/parsing/import-meta-glob-case-sensitive/index.js
+++ tests/rspack-test/normalCases/parsing/import-meta-glob-case-sensitive/index.js
@@ -2,7 +2,17 @@ const modules = import.meta.glob('./*.JS', {
   eager: true,
   caseSensitive: 'yes',
 })
+const staticallyEvaluatedModules = import.meta.glob('./case-test/*.JS', {
+  eager: true,
+  caseSensitive: !true,
+})
 
 it('should warn and default to case-sensitive matching for an invalid value', () => {
   expect(Object.keys(modules)).toEqual([])
+})
+
+it('should evaluate a static boolean caseSensitive expression', () => {
+  expect(Object.keys(staticallyEvaluatedModules)).toEqual([
+    './case-test/alpha.js',
+  ])
 })
