f6879c97d31d1630c7f312025cedc96c0b36880c
Fy <1114550440@qq.com>
2026-07-17 16:06:27 +0800

    test: make createRequire define case cross-platform

--- tests/rspack-test/esmOutputCases/create-require/import-meta-url-define/rspack.config.js
+++ tests/rspack-test/esmOutputCases/create-require/import-meta-url-define/rspack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [
     new DefinePlugin({
-      'import.meta.url': JSON.stringify('file:///virtual/index.js'),
+      'import.meta.url': JSON.stringify('file:///C:/virtual/index.js'),
     }),
   ],
 };
