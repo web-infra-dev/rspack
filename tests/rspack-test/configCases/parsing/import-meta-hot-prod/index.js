3cb1411981b595863926dc8dcbea94f8b7c671c0
intellild <intellild@outlook.com>
2026-07-23 03:29:52 +0000

    chore: preserve main changes after rebase

--- tests/rspack-test/configCases/parsing/import-meta-hot-prod/index.js
+++ tests/rspack-test/configCases/parsing/import-meta-hot-prod/index.js
@@ -2,11 +2,11 @@ it('should transform import.meta.webpackHot to false', () => {
 	let hot = false;
 	if (import.meta.webpackHot) {
 		hot = true;
-		import.meta.webpackHot.accept();
-	}
+    import.meta.webpackHot.accept();
+  }
 
 	expect(hot).toBe(false);
-});
+})
 
 it("should short-circuit optional calls on import.meta.webpackHot", () => {
 	let callbackArgumentEvaluated = false;
