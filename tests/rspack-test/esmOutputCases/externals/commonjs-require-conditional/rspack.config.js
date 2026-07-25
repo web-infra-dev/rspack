eda59b0561d5355b3f7f3d861c8002f3a38a6303
Fy <1114550440@qq.com>
2026-07-26 02:05:25 +0800

    Optimize direct CommonJS external requires in ESM output

---
+++ tests/rspack-test/esmOutputCases/externals/commonjs-require-conditional/rspack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  externalsType: 'modern-module',
+  externals: {
+    'external-os': 'os',
+  },
+};
--- tests/rspack-test/esmOutputCases/externals/modern-module-explicit-node/index.js
+++ tests/rspack-test/esmOutputCases/externals/modern-module-explicit-node/index.js
@@ -3,6 +3,7 @@ import { resolve } from "path";
 const fs = require("fs");
 
 export const readFile = fs.readFile;
+export const stat = require("fs").stat;
 export const pathResolve = resolve;
 
 export async function loadPlatform() {
