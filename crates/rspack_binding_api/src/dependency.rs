6b13cb04211f8b53628ceb8a1036b3d8ccf11ace
Cong-Cong Pan <dacongsama@live.com>
2026-07-30 15:47:32 +0800

    refactor(binding): replace compilation pointers with compilation ids (#14988)

--- crates/rspack_binding_api/src/dependency.rs
+++ crates/rspack_binding_api/src/dependency.rs
@@ -13,56 +13,40 @@ use rspack_plugin_javascript::dependency::{
 };
 use rustc_hash::FxHashMap as HashMap;
 
-use crate::module::ModuleObject;
+use crate::{module::ModuleObject, with_compilation};
 
 // allows JS-side access to a Dependency instance that has already
 // been processed and stored in the Compilation.
 #[napi]
 pub struct Dependency {
-  pub(crate) compilation: Option<NonNull<Compilation>>,
+  pub(crate) compilation_id: Option<CompilationId>,
   pub(crate) dependency_id: DependencyId,
   pub(crate) dependency: NonNull<dyn rspack_core::Dependency>,
 }
 
 impl Dependency {
-  fn as_ref<'a>(
-    &'a mut self,
-  ) -> napi::Result<(
-    &'a (dyn rspack_core::Dependency + 'a),
-    Option<&'a Compilation>,
-  )> {
-    if let Some(compilation) = self.compilation {
-      let compilation = unsafe { compilation.as_ref() };
-      let module_graph = compilation.get_module_graph();
-      if let Some(dependency) = internal::try_dependency_by_id(module_graph, &self.dependency_id) {
-        // SAFETY:
-        // We extend `'a` to `'static` to satisfy the `NonNull<dyn Dependency>` field type,
-        // which has an implied `'static` bound on the trait object. All actual accesses are
-        // still mediated by the JS wrapper lifecycle plus runtime dependency lookups, so the
-        // references derived from this pointer must not outlive the original allocation.
-        let dependency_ptr = unsafe {
-          std::mem::transmute::<
-            *const (dyn rspack_core::Dependency + 'a),
-            *mut (dyn rspack_core::Dependency + 'static),
-          >(dependency.as_ref())
-        };
-
-        // SAFETY:
-        // `dependency` is a valid reference, so the erased raw pointer is non-null.
-        self.dependency = unsafe { NonNull::new_unchecked(dependency_ptr) };
-
-        Ok((unsafe { self.dependency.as_ref() }, Some(compilation)))
-      } else {
-        Err(napi::Error::from_reason(format!(
-          "Unable to access dependency with id = {:?} now. The dependency have been removed on the Rust side.",
-          self.dependency_id
-        )))
-      }
+  fn with_ref<R>(
+    &mut self,
+    f: impl FnOnce(&dyn rspack_core::Dependency, Option<&Compilation>) -> napi::Result<R>,
+  ) -> napi::Result<R> {
+    if let Some(compilation_id) = self.compilation_id {
+      with_compilation(compilation_id, |compilation| {
+        let module_graph = compilation.get_module_graph();
+        if let Some(dependency) = internal::try_dependency_by_id(module_graph, &self.dependency_id)
+        {
+          f(dependency.as_ref(), Some(compilation))
+        } else {
+          Err(napi::Error::from_reason(format!(
+            "Unable to access dependency with id = {:?} now. The dependency have been removed on the Rust side.",
+            self.dependency_id
+          )))
+        }
+      })
     } else {
       // SAFETY:
       // We need to make users aware in the documentation that values obtained within the JS hook callback should not be used outside the scope of the callback.
       // We do not guarantee that the memory pointed to by the pointer remains valid when used outside the scope.
-      Ok((unsafe { self.dependency.as_ref() }, None))
+      f(unsafe { self.dependency.as_ref() }, None)
     }
   }
 
@@ -82,65 +66,62 @@ impl Dependency {
     ts_return_type = "Module | undefined"
   )]
   pub fn parent_module(&mut self) -> napi::Result<Option<ModuleObject>> {
-    let (dependency, compilation) = self.as_ref()?;
-    let Some(compilation) = compilation else {
-      return Ok(None);
-    };
-    let module_graph = compilation.get_module_graph();
-    let parent_module = module_graph
-      .get_parent_module(dependency.id())
-      .and_then(|m| compilation.module_by_identifier(m))
-      .map(|m| ModuleObject::with_ref(m.as_ref(), compilation.compiler_id()));
-    Ok(parent_module)
+    self.with_ref(|dependency, compilation| {
+      let Some(compilation) = compilation else {
+        return Ok(None);
+      };
+      let module_graph = compilation.get_module_graph();
+      let parent_module = module_graph
+        .get_parent_module(dependency.id())
+        .and_then(|m| compilation.module_by_identifier(m))
+        .map(|m| ModuleObject::with_ref(m.as_ref(), compilation.compiler_id()));
+      Ok(parent_module)
+    })
   }
 
   #[napi(getter)]
-  pub fn get_type(&mut self) -> napi::Result<&str> {
-    let (dependency, _) = self.as_ref()?;
-
-    Ok(dependency.dependency_type().as_str())
+  pub fn get_type(&mut self) -> napi::Result<String> {
+    self.with_ref(|dependency, _| Ok(dependency.dependency_type().as_str().to_string()))
   }
 
   #[napi(getter)]
-  pub fn category(&mut self) -> napi::Result<&str> {
-    let (dependency, _) = self.as_ref()?;
-
-    Ok(dependency.category().as_str())
+  pub fn category(&mut self) -> napi::Result<String> {
+    self.with_ref(|dependency, _| Ok(dependency.category().as_str().to_string()))
   }
 
   #[napi(getter)]
-  pub fn request(&mut self) -> napi::Result<napi::Either<&str, ()>> {
-    let (dependency, _) = self.as_ref()?;
-
-    Ok(match dependency.as_module_dependency() {
-      Some(dep) => napi::Either::A(dep.request()),
-      None => napi::Either::B(()),
+  pub fn request(&mut self) -> napi::Result<napi::Either<String, ()>> {
+    self.with_ref(|dependency, _| {
+      Ok(match dependency.as_module_dependency() {
+        Some(dep) => napi::Either::A(dep.request().to_string()),
+        None => napi::Either::B(()),
+      })
     })
   }
 
   #[napi(getter, ts_return_type = "Record<string, string> | undefined")]
   pub fn attributes<'a>(&mut self, env: &'a Env) -> napi::Result<Either<Object<'a>, ()>> {
-    let (dependency, _) = self.as_ref()?;
-
-    Ok(match dependency.get_attributes() {
-      Some(attributes) => {
-        let mut object = Object::new(env)?;
-        for (key, value) in attributes.iter() {
-          object.set(key, value)?;
+    self.with_ref(|dependency, _| {
+      Ok(match dependency.get_attributes() {
+        Some(attributes) => {
+          let mut object = Object::new(env)?;
+          for (key, value) in attributes.iter() {
+            object.set(key, value)?;
+          }
+          Either::A(object)
         }
-        Either::A(object)
-      }
-      None => Either::B(()),
+        None => Either::B(()),
+      })
     })
   }
 
   #[napi(getter)]
   pub fn critical(&mut self) -> napi::Result<bool> {
-    let (dependency, _) = self.as_ref()?;
-
-    Ok(match dependency.as_context_dependency() {
-      Some(dep) => dep.critical().is_some(),
-      None => false,
+    self.with_ref(|dependency, _| {
+      Ok(match dependency.as_context_dependency() {
+        Some(dep) => dep.critical().is_some(),
+        None => false,
+      })
     })
   }
 
@@ -159,45 +140,46 @@ impl Dependency {
 
   #[napi(getter, ts_return_type = "Array<string> | undefined")]
   pub fn ids<'a>(&mut self, env: &'a Env) -> napi::Result<Either<Array<'a>, ()>> {
-    let (dependency, compilation) = self.as_ref()?;
-
-    Ok(match compilation {
-      Some(compilation) => {
-        let module_graph = compilation.get_module_graph();
-        if let Some(dependency) = dependency.downcast_ref::<CommonJsExportRequireDependency>() {
-          let ids = dependency.get_ids(module_graph);
-          let mut arr = env.create_array(ids.len() as u32)?;
-          for (i, v) in ids.iter().enumerate() {
-            arr.set(i as u32, v.as_str())?;
+    self.with_ref(|dependency, compilation| {
+      Ok(match compilation {
+        Some(compilation) => {
+          let module_graph = compilation.get_module_graph();
+          if let Some(dependency) = dependency.downcast_ref::<CommonJsExportRequireDependency>() {
+            let ids = dependency.get_ids(module_graph);
+            let mut arr = env.create_array(ids.len() as u32)?;
+            for (i, v) in ids.iter().enumerate() {
+              arr.set(i as u32, v.as_str())?;
+            }
+            Either::A(arr)
+          } else if let Some(dependency) =
+            dependency.downcast_ref::<ESMExportImportedSpecifierDependency>()
+          {
+            let ids = dependency.get_ids(module_graph);
+            let mut arr = env.create_array(ids.len() as u32)?;
+            for (i, v) in ids.iter().enumerate() {
+              arr.set(i as u32, v.as_str())?;
+            }
+            Either::A(arr)
+          } else if let Some(dependency) = dependency.downcast_ref::<ESMImportSpecifierDependency>()
+          {
+            let ids = dependency.get_ids(module_graph);
+            let mut arr = env.create_array(ids.len() as u32)?;
+            for (i, v) in ids.iter().enumerate() {
+              arr.set(i as u32, v.as_str())?;
+            }
+            Either::A(arr)
+          } else {
+            Either::B(())
           }
-          Either::A(arr)
-        } else if let Some(dependency) =
-          dependency.downcast_ref::<ESMExportImportedSpecifierDependency>()
-        {
-          let ids = dependency.get_ids(module_graph);
-          let mut arr = env.create_array(ids.len() as u32)?;
-          for (i, v) in ids.iter().enumerate() {
-            arr.set(i as u32, v.as_str())?;
-          }
-          Either::A(arr)
-        } else if let Some(dependency) = dependency.downcast_ref::<ESMImportSpecifierDependency>() {
-          let ids = dependency.get_ids(module_graph);
-          let mut arr = env.create_array(ids.len() as u32)?;
-          for (i, v) in ids.iter().enumerate() {
-            arr.set(i as u32, v.as_str())?;
-          }
-          Either::A(arr)
-        } else {
-          Either::B(())
         }
-      }
-      None => Either::B(()),
+        None => Either::B(()),
+      })
     })
   }
+
   #[napi(getter)]
   pub fn loc(&mut self) -> napi::Result<Option<crate::location::DependencyLocation>> {
-    let (dependency, _) = self.as_ref()?;
-    Ok(dependency.loc().map(|loc| loc.into()))
+    self.with_ref(|dependency, _| Ok(dependency.loc().map(|loc| loc.into())))
   }
 }
 
@@ -214,7 +196,7 @@ pub struct DependencyWrapper {
   dependency_id: DependencyId,
   dependency: NonNull<dyn rspack_core::Dependency>,
   compilation_id: CompilationId,
-  compilation: Option<NonNull<Compilation>>,
+  registered_compilation_id: Option<CompilationId>,
 }
 
 impl DependencyWrapper {
@@ -241,13 +223,11 @@ impl DependencyWrapper {
     // `dependency` is a valid reference, so the erased raw pointer is non-null.
     let dependency = unsafe { NonNull::new_unchecked(dependency_ptr) };
 
-    #[allow(clippy::unwrap_used)]
     Self {
       dependency_id,
       dependency,
       compilation_id,
-      compilation: compilation
-        .map(|c| NonNull::new(c as *const Compilation as *mut Compilation).unwrap()),
+      registered_compilation_id: compilation.map(Compilation::id),
     }
   }
 
@@ -280,14 +260,14 @@ impl ToNapiValue for DependencyWrapper {
           std::collections::hash_map::Entry::Occupied(mut occupied_entry) => {
             let r = occupied_entry.get_mut();
             let instance = &mut **r;
-            instance.compilation = val.compilation;
+            instance.compilation_id = val.registered_compilation_id;
             instance.dependency = val.dependency;
 
             ToNapiValue::to_napi_value(env, r)
           }
           std::collections::hash_map::Entry::Vacant(vacant_entry) => {
             let js_dependency = Dependency {
-              compilation: val.compilation,
+              compilation_id: val.registered_compilation_id,
               dependency_id: val.dependency_id,
               dependency: val.dependency,
             };
