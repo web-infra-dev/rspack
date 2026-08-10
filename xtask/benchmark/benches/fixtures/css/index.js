4e4113e1f20a5ee6283721c39e4cf717422308a8
intellild <intellild@outlook.com>
2026-08-10 06:40:57 +0000

    test(bench): use CSS entry for CSS benchmark

--- xtask/benchmark/benches/fixtures/css/index.js
+++
@@ -1,2 +0,0 @@
-import './bootstrap.css';
-import './tailwind.css';
--- xtask/benchmark/benches/groups/bundle/css.rs
+++ xtask/benchmark/benches/groups/bundle/css.rs
@@ -5,7 +5,7 @@ use crate::groups::bundle::util::{BuilderOptions, basic_compiler_builder};
 pub fn compiler() -> CompilerBuilder {
   basic_compiler_builder(BuilderOptions {
     project: "css",
-    entry: "./index.js",
+    entry: "./index.css",
     swc_loader: false,
     native_output_filesystem: false,
   })
