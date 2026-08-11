b477a36460608f78cb236926f54d44359f09cdd4
intellild <intellild@outlook.com>
2026-08-11 19:04:30 +0800

    perf: parallelize chunk condition checks

--- crates/rspack_plugin_ensure_chunk_conditions/src/lib.rs
+++ crates/rspack_plugin_ensure_chunk_conditions/src/lib.rs
@@ -1,7 +1,7 @@
 use rspack_core::{Compilation, CompilationOptimizeChunks, Logger, Plugin};
-use rspack_error::Result;
+use rspack_error::{Result, ToStringResultToRspackResultExt};
 use rspack_hook::{plugin, plugin_hook};
-use rustc_hash::{FxHashMap as HashMap, FxHashSet as HashSet};
+use rspack_util::fx_hash::{FxDashMap, FxHashSet as HashSet};
 use tracing::info;
 
 #[plugin]
@@ -12,97 +12,143 @@ pub struct EnsureChunkConditionsPlugin;
 async fn optimize_chunks(&self, compilation: &mut Compilation) -> Result<Option<bool>> {
   let logger = compilation.get_logger(self.name());
   let start = logger.time("ensure chunk conditions");
-  let mut source_module_chunks = HashMap::default();
-  for (module_id, module) in compilation.get_module_graph().modules() {
-    let external_module = module.as_external_module();
-    let module_chunks = compilation
-      .build_chunk_graph_artifact
-      .chunk_graph
-      .get_module_chunks(module.identifier());
-    let mut source_chunks = Vec::new();
-    for chunk in module_chunks {
-      let condition = if let Some(external_module) = external_module {
-        external_module
-          .chunk_condition_with_hooks(chunk, compilation)
-          .await?
-      } else {
-        module.chunk_condition(chunk, compilation)
-      };
-      if matches!(condition, Some(false)) {
-        source_chunks.push(*chunk);
-      }
+  let source_module_chunks = FxDashMap::default();
+  let compilation_ref = &*compilation;
+  let source_module_chunk_results = rspack_parallel::scope::<_, Result<_>>(|token| {
+    for (module_id, module) in compilation_ref.get_module_graph().modules() {
+      let module_id = *module_id;
+      let s = unsafe { token.used((module.as_ref(), compilation_ref, &source_module_chunks)) };
+      s.spawn(
+        move |(module, compilation, source_module_chunks)| async move {
+          let external_module = module.as_external_module();
+          let module_chunks = compilation
+            .build_chunk_graph_artifact
+            .chunk_graph
+            .get_module_chunks(module.identifier());
+          let mut source_chunks = HashSet::default();
+          for chunk in module_chunks {
+            let condition = if let Some(external_module) = external_module {
+              external_module
+                .chunk_condition_with_hooks(chunk, compilation)
+                .await?
+            } else {
+              module.chunk_condition(chunk, compilation)
+            };
+            if matches!(condition, Some(false)) {
+              source_chunks.insert(*chunk);
+            }
+          }
+          if !source_chunks.is_empty() {
+            source_module_chunks.insert(module_id, source_chunks);
+          }
+          Ok(())
+        },
+      );
     }
-    if !source_chunks.is_empty() {
-      source_module_chunks.insert(*module_id, source_chunks);
-    }
+  })
+  .await;
+  for result in source_module_chunk_results {
+    result.to_rspack_result()??;
   }
 
-  let mut target_module_chunks = HashMap::default();
-  let mut visited_chunk_group_keys = HashSet::default();
+  let source_module_chunks = source_module_chunks.into_iter().collect::<Vec<_>>();
+
+  let target_module_chunks = FxDashMap::default();
 
   // The following algorithm has high risk of performance problem, cause it's complexity is N(adjust_chunk_number) * N(adjust_module_number) * N(chunk_group_number) * N(chunk_in_chunk_group_number)
   // this is used to calculate the complexity of the adjust_chunk operation
-  let mut adjust_chunk_size: u64 = 0;
-  let mut adjust_module_size: u64 = 0;
-  let mut adjust_chunk_group_size: u64 = 0;
-  let mut adjust_chunk_in_chunk_group_size: u64 = 0;
-  for (module_id, chunk_keys) in &source_module_chunks {
-    adjust_module_size += 1;
-    let mut target_chunks = HashSet::default();
-    for chunk_key in chunk_keys {
-      adjust_chunk_size += 1;
-      if let Some(chunk) = compilation
-        .build_chunk_graph_artifact
-        .chunk_by_ukey
-        .get(chunk_key)
-      {
-        let mut chunk_group_keys = chunk.groups().iter().collect::<Vec<_>>();
-        visited_chunk_group_keys.clear();
-        'out: while let Some(chunk_group_key) = chunk_group_keys.pop() {
-          if visited_chunk_group_keys.contains(chunk_group_key) {
-            continue;
-          }
-          visited_chunk_group_keys.insert(chunk_group_key);
-          if let Some(chunk_group) = compilation
-            .build_chunk_graph_artifact
-            .chunk_group_by_ukey
-            .get(chunk_group_key)
-          {
-            adjust_chunk_group_size += 1;
-            let module = compilation
-              .get_module_graph()
-              .module_by_identifier(module_id);
-            let external_module = module.and_then(|module| module.as_external_module());
-            for chunk in &chunk_group.chunks {
-              adjust_chunk_in_chunk_group_size += 1;
-              if let Some(module) = module {
-                let condition = if let Some(external_module) = external_module {
-                  external_module
-                    .chunk_condition_with_hooks(chunk, compilation)
-                    .await?
-                } else {
-                  module.chunk_condition(chunk, compilation)
-                };
-                if matches!(condition, Some(true)) {
-                  target_chunks.insert(*chunk);
-                  continue 'out;
+  let target_module_chunk_results = rspack_parallel::scope::<_, Result<_>>(|token| {
+    for (module_id, chunk_keys) in &source_module_chunks {
+      let module_id = *module_id;
+      let s = unsafe { token.used((chunk_keys, compilation_ref, &target_module_chunks)) };
+      s.spawn(
+        move |(chunk_keys, compilation, target_module_chunks)| async move {
+          let module = compilation
+            .get_module_graph()
+            .module_by_identifier(&module_id);
+          let external_module = module.and_then(|module| module.as_external_module());
+          let mut target_chunks = HashSet::default();
+          let mut visited_chunk_group_keys = HashSet::default();
+          let mut adjust_chunk_size: u64 = 0;
+          let mut adjust_chunk_group_size: u64 = 0;
+          let mut adjust_chunk_in_chunk_group_size: u64 = 0;
+
+          for chunk_key in chunk_keys {
+            adjust_chunk_size += 1;
+            if let Some(chunk) = compilation
+              .build_chunk_graph_artifact
+              .chunk_by_ukey
+              .get(chunk_key)
+            {
+              let mut chunk_group_keys = chunk.groups().iter().copied().collect::<Vec<_>>();
+              visited_chunk_group_keys.clear();
+              'out: while let Some(chunk_group_key) = chunk_group_keys.pop() {
+                if !visited_chunk_group_keys.insert(chunk_group_key) {
+                  continue;
+                }
+                if let Some(chunk_group) = compilation
+                  .build_chunk_graph_artifact
+                  .chunk_group_by_ukey
+                  .get(&chunk_group_key)
+                {
+                  adjust_chunk_group_size += 1;
+
+                  for chunk in &chunk_group.chunks {
+                    if chunk_keys.contains(chunk) {
+                      continue;
+                    }
+
+                    adjust_chunk_in_chunk_group_size += 1;
+                    if let Some(module) = module {
+                      let condition = if let Some(external_module) = external_module {
+                        external_module
+                          .chunk_condition_with_hooks(chunk, compilation)
+                          .await?
+                      } else {
+                        module.chunk_condition(chunk, compilation)
+                      };
+                      if matches!(condition, Some(true)) {
+                        target_chunks.insert(*chunk);
+                        continue 'out;
+                      }
+                    }
+                  }
+                  if chunk_group.is_initial() {
+                    return Err(rspack_error::error!(
+                      "Cannot fulfil chunk condition of {}",
+                      module_id
+                    ));
+                  }
+                  chunk_group_keys.extend(chunk_group.parents_iterable().copied());
                 }
               }
             }
-            if chunk_group.is_initial() {
-              return Err(rspack_error::error!(
-                "Cannot fulfil chunk condition of {}",
-                module_id
-              ));
-            }
-            let parent_chunks = chunk_group.parents_iterable();
-
-            chunk_group_keys.extend(parent_chunks);
           }
-        }
-      }
+          target_module_chunks.insert(module_id, target_chunks);
+          Ok((
+            adjust_chunk_size,
+            adjust_chunk_group_size,
+            adjust_chunk_in_chunk_group_size,
+          ))
+        },
+      );
     }
-    target_module_chunks.insert(*module_id, target_chunks);
+  })
+  .await;
+
+  let mut adjust_chunk_size: u64 = 0;
+  let adjust_module_size = target_module_chunk_results.len() as u64;
+  let mut adjust_chunk_group_size: u64 = 0;
+  let mut adjust_chunk_in_chunk_group_size: u64 = 0;
+  for result in target_module_chunk_results {
+    let (
+      module_adjust_chunk_size,
+      module_adjust_chunk_group_size,
+      module_adjust_chunk_in_chunk_group_size,
+    ) = result.to_rspack_result()??;
+    adjust_chunk_size += module_adjust_chunk_size;
+    adjust_chunk_group_size += module_adjust_chunk_group_size;
+    adjust_chunk_in_chunk_group_size += module_adjust_chunk_in_chunk_group_size;
   }
   info!(
     name:"ensure_chunk_conditions.complexity",
