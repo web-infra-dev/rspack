a18931602ad8e2abf2af406126a30d1aa6b33b17
Hashem Khalifa <hashem.khalifa@gmail.com>
2026-08-21 09:08:58 +0200

    fix: await async modules in ProvidePlugin (#15143)

--- crates/rspack_plugin_javascript/src/dependency/esm/esm_compatibility_dependency.rs
+++ crates/rspack_plugin_javascript/src/dependency/esm/esm_compatibility_dependency.rs
@@ -1,10 +1,55 @@
 use rspack_cacheable::{cacheable, cacheable_dyn};
 use rspack_core::{
-  ChunkGraph, DependencyCodeGeneration, DependencyTemplate, DependencyTemplateType,
-  InitFragmentKey, InitFragmentStage, ModuleGraph, NormalInitFragment, RuntimeGlobals,
-  TemplateContext, TemplateReplaceSource, UsageState,
+  ChunkGraph, Compilation, DependencyCodeGeneration, DependencyTemplate, DependencyTemplateType,
+  InitFragmentKey, InitFragmentStage, ModuleCodeTemplate, ModuleGraph, ModuleInitFragments,
+  NormalInitFragment, RuntimeGlobals, TemplateContext, TemplateReplaceSource, UsageState,
 };
 use swc_atoms::Atom;
+
+pub(super) fn add_async_module_boundary(
+  init_fragments: &mut ModuleInitFragments,
+  compilation: &Compilation,
+  module: &dyn rspack_core::Module,
+  runtime_template: &mut ModuleCodeTemplate,
+  use_module_exports: bool,
+) {
+  let module_id = ChunkGraph::get_module_id(&compilation.module_ids_artifact, module.identifier())
+    .map(ToString::to_string)
+    .unwrap_or_default();
+  let module_argument = runtime_template.render_module_argument(module.get_module_argument());
+  let async_module_parameter = if use_module_exports {
+    ", __rspack_async_module"
+  } else {
+    ""
+  };
+  let module_argument_assignment = if use_module_exports {
+    format!("{module_argument} = __rspack_async_module;\n")
+  } else {
+    String::new()
+  };
+  let async_module_arguments = match (
+    module.build_meta().has_top_level_await(),
+    use_module_exports,
+  ) {
+    (false, false) => "",
+    (true, false) => ", 1",
+    (false, true) => ", 0, 1",
+    (true, true) => ", 1, 1",
+  };
+  init_fragments.push(Box::new(NormalInitFragment::new(
+    format!(
+      "{}({}, async function (__rspack_load_async_deps, __rspack_async_done{async_module_parameter}) {{ try {{\n{module_argument_assignment}",
+      runtime_template.render_runtime_globals(&RuntimeGlobals::ASYNC_MODULE),
+      module_argument,
+    ),
+    InitFragmentStage::StageAsyncBoundary,
+    0,
+    InitFragmentKey::AsyncBoundary(module_id),
+    Some(format!(
+      "\n__rspack_async_done();\n}} catch(e) {{ __rspack_async_done(e); }} }}{async_module_arguments});",
+    )),
+  )));
+}
 
 // Mark module `__esModule`.
 // Add `__rspack_require.r(__rspack_exports);`.
@@ -73,33 +118,13 @@ impl DependencyTemplate for ESMCompatibilityDependencyTemplate {
     }
 
     if ModuleGraph::is_async(&compilation.async_modules_artifact, &module.identifier()) {
-      let module_id =
-        ChunkGraph::get_module_id(&compilation.module_ids_artifact, module.identifier())
-          .map(ToString::to_string)
-          .unwrap_or_default();
-      init_fragments.push(Box::new(NormalInitFragment::new(
-        format!(
-          "{}({}, async function (__rspack_load_async_deps, __rspack_async_done) {{ try {{\n",
-          runtime_template.render_runtime_globals(&RuntimeGlobals::ASYNC_MODULE),
-          runtime_template.render_module_argument(
-            module_graph
-              .module_by_identifier(&module.identifier())
-              .expect("should have mgm")
-              .get_module_argument()
-          ),
-        ),
-        InitFragmentStage::StageAsyncBoundary,
-        0,
-        InitFragmentKey::AsyncBoundary(module_id),
-        Some(format!(
-          "\n__rspack_async_done();\n}} catch(e) {{ __rspack_async_done(e); }} }}{});",
-          if module.build_meta().has_top_level_await() {
-            ", 1"
-          } else {
-            ""
-          }
-        )),
-      )));
+      add_async_module_boundary(
+        init_fragments,
+        compilation,
+        module.as_ref(),
+        runtime_template,
+        false,
+      );
     }
   }
 }
