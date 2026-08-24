8d8873a0379c22a2831822e961df7ba4ce409b29
hardfist <1562502418@qq.com>
2026-08-24 15:57:35 +0800

    style(cache): format new cache test

--- tests/rspack-test/cacheCases/common/build-dependencies-new-cache/rspack.config.js
+++ tests/rspack-test/cacheCases/common/build-dependencies-new-cache/rspack.config.js
@@ -1,48 +1,48 @@
-const path = require("path");
+const path = require('path');
 
-const buildDependency = path.join(__dirname, "./configs/index.js");
+const buildDependency = path.join(__dirname, './configs/index.js');
 let compilerIndex = 0;
 
 /** @type {import("@rspack/core").Configuration} */
 module.exports = {
-	context: __dirname,
-	experiments: {
-		newCache: true
-	},
-	cache: {
-		type: "persistent",
-		buildDependencies: [buildDependency]
-	},
-	plugins: [
-		{
-			apply(compiler) {
-				compiler.hooks.done.tap("Test Plugin", stats => {
-					const logging = stats.toJson({
-						all: false,
-						logging: "verbose"
-					}).logging;
-					const entries = logging["rspack.Compilation"]?.entries ?? [];
-					const cacheEntry = entries.find(
-						entry =>
-							entry.type === "cache" &&
-							entry.message?.startsWith("module code generation cache:")
-					);
-					expect(cacheEntry).toBeTruthy();
+  context: __dirname,
+  experiments: {
+    newCache: true,
+  },
+  cache: {
+    type: 'persistent',
+    buildDependencies: [buildDependency],
+  },
+  plugins: [
+    {
+      apply(compiler) {
+        compiler.hooks.done.tap('Test Plugin', (stats) => {
+          const logging = stats.toJson({
+            all: false,
+            logging: 'verbose',
+          }).logging;
+          const entries = logging['rspack.Compilation']?.entries ?? [];
+          const cacheEntry = entries.find(
+            (entry) =>
+              entry.type === 'cache' &&
+              entry.message?.startsWith('module code generation cache:'),
+          );
+          expect(cacheEntry).toBeTruthy();
 
-					const match = cacheEntry.message.match(/\((\d+)\/(\d+)\)/);
-					expect(match).toBeTruthy();
-					const hits = Number(match[1]);
-					const total = Number(match[2]);
-					expect(total).toBeGreaterThan(0);
+          const match = cacheEntry.message.match(/\((\d+)\/(\d+)\)/);
+          expect(match).toBeTruthy();
+          const hits = Number(match[1]);
+          const total = Number(match[2]);
+          expect(total).toBeGreaterThan(0);
 
-					if (compilerIndex === 1 || compilerIndex === 4) {
-						expect(hits).toBe(total);
-					} else {
-						expect(hits).toBe(0);
-					}
-					compilerIndex++;
-				});
-			}
-		}
-	]
+          if (compilerIndex === 1 || compilerIndex === 4) {
+            expect(hits).toBe(total);
+          } else {
+            expect(hits).toBe(0);
+          }
+          compilerIndex++;
+        });
+      },
+    },
+  ],
 };
