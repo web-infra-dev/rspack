3755ec890fe0a68d02c03bf1ce63ba915fb46935
Matthew Costabile <mattcosta7@github.com>
2026-08-29 01:07:21 +0000

    test(css): update local ident hash expectations

--- tests/rspack-test/configCases/css/rspack-issue-6435/index.js
+++ tests/rspack-test/configCases/css/rspack-issue-6435/index.js
@@ -3,10 +3,10 @@ import legacyClasses from "./legacy/index.css";
 
 it("should have consistent hash", () => {
   if (globalThis.__RSPACK_TEST_RUNTIME_MODE_RSPACK) {
-    expect(classes["container-main"]).toBe("_87df500bb85ed2e1b1f0-container-main")
+    expect(classes["container-main"]).toBe("c4e49a91299807229fc9-container-main")
     expect(legacyClasses["legacy-main"]).toBe("_8f645d48bbc8d62b15e8-legacy-main")
   } else {
-    expect(classes["container-main"]).toBe("_467c4885db406636e4bf-container-main")
+    expect(classes["container-main"]).toBe("e49dcf8a2397e06e4127-container-main")
     expect(legacyClasses["legacy-main"]).toBe("_472dae718ba45ef203c9-legacy-main")
   }
 });
