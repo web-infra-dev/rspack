4367dc935b74b1f4f43ed899014138eccaf6b116
hardfist <1562502418@qq.com>
2026-08-31 16:58:53 +0800

    test(cache): cover module build errors

--- tests/rspack-test/cacheCases/make/keep-module-error/rspack.config.js
+++ tests/rspack-test/cacheCases/make/keep-module-error/rspack.config.js
@@ -1,10 +1,22 @@
 const rspack = require('@rspack/core');
+const path = require('path');
 
 let index = 0;
+let builtErrorModules = [];
+let validErrorModules = [];
 
 /** @type {import("@rspack/core").Configuration} */
 module.exports = {
   context: __dirname,
+  experiments: {
+    newCache: {
+      codeGeneration: false,
+      devtool: false,
+      loader: false,
+      minimize: false,
+      module: true,
+    },
+  },
   optimization: {
     minimize: false,
   },
@@ -14,16 +26,41 @@ module.exports = {
   plugins: [
     {
       apply(compiler) {
+        compiler.hooks.compilation.tap(
+          'ModuleCacheErrorTest',
+          (compilation) => {
+            compilation.hooks.buildModule.tap(
+              'ModuleCacheErrorTest',
+              (module) => {
+                if (
+                  module.resource &&
+                  path.basename(module.resource) === 'file.js'
+                ) {
+                  builtErrorModules.push(path.basename(module.resource));
+                }
+              },
+            );
+            compilation.hooks.stillValidModule.tap(
+              'ModuleCacheErrorTest',
+              (module) => {
+                if (
+                  module.resource &&
+                  path.basename(module.resource) === 'file.js'
+                ) {
+                  validErrorModules.push(path.basename(module.resource));
+                }
+              },
+            );
+          },
+        );
         compiler.hooks.done.tapPromise('PLUGIN', async (stats) => {
           const { errors } = stats.toJson({ errors: true });
-          if (index === 0) {
-            expect(errors.length).toBe(1);
-            expect(errors[0].message).toMatch('LoaderError');
-          } else {
-            // TODO should be same as index 0, change it after
-            // write error to module.diagnostic
-            expect(errors.length).toBe(0);
-          }
+          expect(builtErrorModules).toEqual(['file.js']);
+          expect(validErrorModules).toEqual([]);
+          expect(errors).toHaveLength(1);
+          expect(errors[0].message).toMatch('LoaderError');
+          builtErrorModules = [];
+          validErrorModules = [];
           index++;
         });
       },
