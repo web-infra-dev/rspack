8404a95d215f0a090d8a7d48bf9bb6e14e0649f8
hardfist <1562502418@qq.com>
2026-08-31 16:58:53 +0800

    test(cache): cover module build errors

--- tests/rspack-test/cacheCases/make/keep-module-error/index.js
+++ tests/rspack-test/cacheCases/make/keep-module-error/index.js
@@ -1,11 +1,15 @@
-import value from "./loader.js!./file";
+import value from "./value";
+
+const loadErrorModule = () => import("./loader.js!./file");
 
 it("should module error exist", async () => {
 	if (COMPILER_INDEX === 0) {
 		expect(value).toBe(1);
+		expect(loadErrorModule).toBeInstanceOf(Function);
 		await NEXT_START();
 	}
 	if (COMPILER_INDEX === 1) {
 		expect(value).toBe(1);
+		expect(loadErrorModule).toBeInstanceOf(Function);
 	}
 });
