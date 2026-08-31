7d607b7cdeb94047637c02a2bbc7dd2cad155a19
intellild <intellild@outlook.com>
2026-08-31 07:06:20 +0000

    test(css): update legacy local ident hash expectations

--- tests/rspack-test/configCases/css/rspack-issue-6435/index.js
+++ tests/rspack-test/configCases/css/rspack-issue-6435/index.js
@@ -4,9 +4,9 @@ import legacyClasses from "./legacy/index.css";
 it("should have consistent hash", () => {
   if (globalThis.__RSPACK_TEST_RUNTIME_MODE_RSPACK) {
     expect(classes["container-main"]).toBe("c4e49a91299807229fc9-container-main")
-    expect(legacyClasses["legacy-main"]).toBe("_8f645d48bbc8d62b15e8-legacy-main")
+    expect(legacyClasses["legacy-main"]).toBe("fc6b8ed3d0ff87063b2f-legacy-main")
   } else {
     expect(classes["container-main"]).toBe("e49dcf8a2397e06e4127-container-main")
-    expect(legacyClasses["legacy-main"]).toBe("_472dae718ba45ef203c9-legacy-main")
+    expect(legacyClasses["legacy-main"]).toBe("_70bba980f1e761daa7f8-legacy-main")
   }
 });
