196a2af0450f08e5fc4953aa3e0da8814ef523d9
AsyncIter <intellild@outlook.com>
2026-09-14 14:01:17 +0800

    Revert "feat(core): expose NormalModule beforeLoaders hook to JS (#15433)"
    This reverts commit 9558edec3bd02766198217748c3df856cd663d66.

--- crates/rspack_core/src/normal_module_factory.rs
+++ crates/rspack_core/src/normal_module_factory.rs
@@ -1368,9 +1368,9 @@ async fn resolve_each(
     .ok_or_else(|| error!("Unable to resolve loader {}", l.loader))
 }
 
-pub(crate) struct ResolvedLoader {
-  pub(crate) loader: BoxLoader,
-  pub(crate) options: LoaderRunnerOptions,
+struct ResolvedLoader {
+  loader: BoxLoader,
+  options: LoaderRunnerOptions,
 }
 
 impl ResolvedLoader {
@@ -1382,7 +1382,7 @@ impl ResolvedLoader {
   }
 }
 
-pub(crate) async fn resolve_each_with_options(
+async fn resolve_each_with_options(
   plugin_driver: &SharedPluginDriver,
   options: &CompilerOptions,
   loader_resolver: &Resolver,
