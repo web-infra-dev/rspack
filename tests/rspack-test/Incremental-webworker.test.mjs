b06e1a187a62c000839fc1aad911597c1c43addd
neverland <chenjiahan.jait@bytedance.com>
2026-09-15 22:33:13 +0800

    test: migrate cache and incremental test entries to ESM

---
+++ tests/rspack-test/Incremental-webworker.test.mjs
@@ -0,0 +1,23 @@
+import path from "node:path";
+import {
+	describeByWalk,
+	createHotIncrementalCase
+} from "@rspack/test-tools";
+const tempDir = path.resolve(import.meta.dirname, `./js/temp/incremental-webworker`);
+
+function v(name) {
+	return path.join(import.meta.dirname, `incremental ${name}`);
+}
+
+// Run tests rspack-test/tests/hotCases in target webworker
+describeByWalk(
+	v("hot webworker"),
+	(name, src, dist) => {
+		createHotIncrementalCase(name, src, dist, path.join(tempDir, name), "webworker", false);
+	},
+	{
+		source: path.resolve(import.meta.dirname, "./hotCases"),
+		dist: path.resolve(import.meta.dirname, `./js/incremental/hot-worker`),
+		exclude: [/^css$/]
+	}
+);
