2b1dd37161ad269f6eb8bf988b1fd32717639c77
AsyncIter <intellild@outlook.com>
2026-09-16 20:01:55 +0800

    fix(loader): revert native loader hook invocation (#15719)

--- packages/rspack/src/taps/index.ts
+++ packages/rspack/src/taps/index.ts
@@ -5,4 +5,3 @@ export { createContextModuleFactoryHooksRegisters } from './contextModuleFactory
 export { createExternalModuleHooksRegisters } from './externalModule';
 export { createJavaScriptModulesHooksRegisters } from './javascriptModules';
 export { createNormalModuleFactoryHooksRegisters } from './normalModuleFactory';
-export { createNormalModuleHooksRegisters } from './normalModule';
