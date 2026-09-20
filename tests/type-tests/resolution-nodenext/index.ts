840248e68395fef02dd4a5c1a436e7c3a2f3916d
neverland <chenjiahan.jait@bytedance.com>
2026-09-20 21:47:15 +0800

    test: rely on migrated configs for RslibPlugin type coverage

--- tests/type-tests/resolution-nodenext/index.ts
+++ tests/type-tests/resolution-nodenext/index.ts
@@ -14,10 +14,6 @@ const config: RspackOptions = {
     new rspack.DefinePlugin({
       __TYPE_TEST__: JSON.stringify(true),
     }),
-    new rspack.experiments.RslibPlugin(),
-    new rspack.experiments.RslibPlugin(undefined),
-    new rspack.experiments.RslibPlugin({}),
-    new rspack.experiments.RslibPlugin({ forceNodeShims: true }),
   ],
   devServer: {
     proxy: [
