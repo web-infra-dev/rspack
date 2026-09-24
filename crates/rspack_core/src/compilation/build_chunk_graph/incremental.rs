7562f5af2aeb06324de039239b8ad7a279892c0d
Fy <1114550440@qq.com>
2026-09-24 14:17:36 +0800

    perf: skip unused code splitting cache copies (#15867)

--- crates/rspack_core/src/compilation/build_chunk_graph/incremental.rs
+++ crates/rspack_core/src/compilation/build_chunk_graph/incremental.rs
@@ -493,12 +493,10 @@ impl CodeSplitter {
 
   #[instrument(skip_all)]
   pub fn update_with_compilation(&mut self, compilation: &mut Compilation) -> Result<()> {
-    // TODO: heuristic incremental update is temporarily disabled
-    // Original code:
-    // let enable_incremental = compilation
-    //   .incremental
-    //   .mutations_readable(IncrementalPasses::BUILD_CHUNK_GRAPH);
-    let enable_incremental = false;
+    let enable_incremental = super::ENABLE_HEURISTIC_INCREMENTAL
+      && compilation
+        .incremental
+        .mutations_readable(IncrementalPasses::BUILD_CHUNK_GRAPH);
 
     // This optimization is from  https://github.com/webpack/webpack/pull/18090 by https://github.com/dmichon-msft
     // Thanks!
